09ff2f15d006ceac5f25e2a40f9091db2b427179
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-05-26 14:23:30 +0800

    ACX-6974: move dataApiURL closer to services

--- libs/analytics/services/src/index.ts
+++ libs/analytics/services/src/index.ts
@@ -1,7 +1,7 @@
 import { createApi }               from '@reduxjs/toolkit/query/react'
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
 
-import { dataApiURL } from '@acx-ui/analytics/utils'
+export const dataApiURL = '/api/a4rc/api/rsa-data-api/graphql/analytics'
 
 export const dataApi = createApi({
   baseQuery: graphqlRequestBaseQuery({
--- libs/analytics/utils/src/constants.ts
+++ libs/analytics/utils/src/constants.ts
@@ -1,4 +1,2 @@
 
 export interface NetworkPath extends Array<{ type: string; name: string }> {}
-
-export const dataApiURL = '/api/a4rc/api/rsa-data-api/graphql/analytics'
