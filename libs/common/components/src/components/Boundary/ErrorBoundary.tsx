aa06b8d793c321d4ee0f2d1f77383ceb13850c5b
jason.cheow <jason.cheow@ruckuswireless.com>
2022-05-27 14:52:19 +0800

    ACX-8081: show details when Error

--- libs/common/components/src/components/Boundary/ErrorBoundary.tsx
+++ libs/common/components/src/components/Boundary/ErrorBoundary.tsx
@@ -5,23 +5,34 @@ export interface ErrorBoundaryProps {
 }
 
 export class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
-  override state = { hasError: false, error: null }
+  override state: { hasError: boolean, error: unknown }
+    = { hasError: false, error: null }
 
-  static getDerivedStateFromError (error: Error) {
+  static getDerivedStateFromError (error: unknown) {
     return { hasError: Boolean(error), error }
   }
 
   defaultFallback () {
+    // TODO:
+    // hide detailed error/show summary in prod
+    let error
+    if (this.state.error instanceof Error) {
+      const { name, message, stack } = this.state.error
+      error = <>
+        {name}: {message}<br />
+        {(stack || '').split('\n').map(line => <>{line}<br /></>)}
+      </>
+    } else {
+      error = JSON.stringify(this.state.error)
+    }
     return <>
       <p>Something went wrong.</p>
-      <p>{JSON.stringify(this.state.error)}</p>
+      <p>{error}</p>
     </>
   }
 
   override render () {
     if (this.state.error) {
-      // TODO:
-      // hide detailed error/show summary in prod
       return this.props.fallback || this.defaultFallback()
     }
     return this.props.children
