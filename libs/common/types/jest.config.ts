3b6379db28a43aef1e441a13a43afe265ee4ff29
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-05-28 23:53:03 +0800

    ACX-8374: add @acx-ui/types
    - Add types for RTK Query to help add types to helpers create around it

---
+++ libs/common/types/jest.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'common-types',
+  preset: '../../../jest.preset.ts',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/common/types'
+}
