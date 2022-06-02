46581c248e37c37d31a1e5b8edb07ccc10e8923b
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-06-02 18:41:19 +0800

    ACx-8374: fix forwardRef warning when render Badge under Popover

--- apps/main/src/App/Alarms/HeaderButton/styledComponents.tsx
+++ apps/main/src/App/Alarms/HeaderButton/styledComponents.tsx
@@ -1,7 +1,16 @@
-import { Badge } from 'antd'
-import styled    from 'styled-components/macro'
+import React from 'react'
 
-export const NotificationCounter = styled(Badge)`
+import { Badge, BadgeProps } from 'antd'
+import styled                from 'styled-components/macro'
+
+const RefBadge = React.forwardRef<
+  unknown,
+  React.PropsWithChildren<BadgeProps>
+// There doesn't seems to have ref for Badge...
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+>((props, ref) => <Badge {...props} />)
+
+export const NotificationCounter = styled(RefBadge)`
   margin: 8px 6px 0px 0px;
   sup {
     padding: 0 3px;
