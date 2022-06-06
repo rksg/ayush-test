953761fb0479a33350ef47ba9ffcf103eb537733
jason.cheow <jason.cheow@ruckuswireless.com>
2022-06-06 19:26:28 +0800

    ACX-6974: use window.location.origin instead of hard-coded URLs

--- libs/analytics/services/src/index.ts
+++ libs/analytics/services/src/index.ts
@@ -1,11 +1,7 @@
 import { createApi }               from '@reduxjs/toolkit/query/react'
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
 
-// for TypeError: Only absolute URLs are supported
-export const dataApiURL = (process.env['NODE_ENV'] === 'production'
-  ?'https://devalto.ruckuswireless.com'
-  :'http://localhost:3000')
-  +'/api/a4rc/api/rsa-data-api/graphql/analytics'
+export const dataApiURL = `${window.location.origin}/api/a4rc/api/rsa-data-api/graphql/analytics`
 
 // GraphQL queries are place in the context of their respective route/widget,
 // please refer to them in source folder under /apps/analytics/src
