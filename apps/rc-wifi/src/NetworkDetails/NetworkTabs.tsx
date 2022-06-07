a38a10f76a8614effae6b6b5b2820f752006d969
Guo, Yizhu <yguo1@ruckuswireless.com>
2022-06-07 11:13:22 +0800

    ACX-8372: adjust style according to wireframe

--- apps/rc-wifi/src/NetworkDetails/NetworkTabs.tsx
+++ apps/rc-wifi/src/NetworkDetails/NetworkTabs.tsx
@@ -1,8 +1,14 @@
 
 import { Tabs } from 'antd'
+import styled   from 'styled-components/macro'
 
 import { useNavigate, useParams, useTenantLink } from '@acx-ui/react-router-dom'
 
+const Wrapper = styled.div`
+ .ant-tabs-tab + .ant-tabs-tab {
+  margin-left: 30px;
+ }
+`
 function NetworkTabs () {
   const { networkId, activeTab } = useParams()
   const basePath = useTenantLink(`/networks/${networkId}/network-details/`)
@@ -13,14 +19,16 @@ function NetworkTabs () {
       pathname: `${basePath.pathname}/${tab}`
     })
   return (
-    <Tabs onChange={onTabChange} activeKey={activeTab}>
-      <Tabs.TabPane tab='Overview' key='overview' />
-      <Tabs.TabPane tab='APs(0)' key='aps' />
-      <Tabs.TabPane tab='Venues(0)' key='venues' />
-      <Tabs.TabPane tab='Services(0)' key='services' />
-      <Tabs.TabPane tab='Events' key='events' />
-      <Tabs.TabPane tab='Incidents' key='incidents' />
-    </Tabs>
+    <Wrapper>
+      <Tabs onChange={onTabChange} activeKey={activeTab}>
+        <Tabs.TabPane tab='Overview' key='overview' />
+        <Tabs.TabPane tab='APs (0)' key='aps' />
+        <Tabs.TabPane tab='Venues (0)' key='venues' />
+        <Tabs.TabPane tab='Services (0)' key='services' />
+        <Tabs.TabPane tab='Events' key='events' />
+        <Tabs.TabPane tab='Incidents' key='incidents' />
+      </Tabs>
+    </Wrapper>
   )
 }
 
