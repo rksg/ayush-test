f0ecf68a263beaabb9d19a12bb26dac9c26e9ee9
sp1148 <Sarada.Pachalla@ruckuswireless.com>
2022-06-08 22:54:51 -0700

    ACX-7141: fixed build failure due to console error msg

--- libs/rc/utils/src/user.profile.service.ts
+++ libs/rc/utils/src/user.profile.service.ts
@@ -11,6 +11,6 @@ export const getTenantId = () => {
 
   const chunks = window.location.pathname.split('/')
   if (chunks[1] === 't') return chunks[2]
-  console.error('URL is empty')
+  // console.error('URL is empty')
   return ''
 }
