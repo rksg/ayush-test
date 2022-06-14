61d49b39652fc3c67087bd90900f3c59b92c3d3c
tingan.chen <tingan.chen@ruckuswireless.com>
2022-06-14 12:54:17 +0800

    ACX-9182: feat: unit test

--- libs/common/components/src/components/Layout/index.spec.tsx
+++ libs/common/components/src/components/Layout/index.spec.tsx
@@ -1,9 +1,6 @@
 import '@testing-library/jest-dom'
-import { render }                  from '@testing-library/react'
-import { Space }                   from 'antd'
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import { Card } from '../Card'
+import { render }        from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 
 import menuConfig from './stories/menuConfig'
 
@@ -12,27 +9,14 @@ import { Layout } from '.'
 describe('NetworkDetails', () => {
   it('should render correctly', async () => {
     const { asFragment } = render(
-      <Router>
+      <BrowserRouter>
         <Layout
           menuConfig={menuConfig}
-          rightHeaderContent={
-            <div style={{ color: 'var(--acx-primary-white)' }}>
-                Right header.
-            </div>
-          }
-          leftHeaderContent={
-            <div style={{ color: 'var(--acx-primary-white)' }}>
-                Left header.
-            </div>
-          }
-          content={
-            <Space direction='vertical' style={{ width: '100%' }}>
-              <Card>Full width content.</Card>
-              <div>content</div>
-            </Space>
-          }
+          rightHeaderContent={<div>Right header</div>}
+          leftHeaderContent={<div>Left header</div>}
+          content={<div>content</div>}
         />
-      </Router>
+      </BrowserRouter>
     )
     expect(asFragment()).toMatchSnapshot()
   })
