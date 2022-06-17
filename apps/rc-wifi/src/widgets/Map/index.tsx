606750ca51e1342bde84946e5b8cdd649cad30ab
jitendra.dangi <jitendra.dangi@ruckuswireless.com>
2022-06-17 12:13:41 +0530

    ACX:6982: chore: add widget loader

--- apps/rc-wifi/src/widgets/Map/index.tsx
+++ apps/rc-wifi/src/widgets/Map/index.tsx
@@ -1,3 +1,4 @@
+import { Loader }                    from '@acx-ui/components'
 import { useDashboardOverviewQuery } from '@acx-ui/rc/services'
 import { useParams }                 from '@acx-ui/react-router-dom'
 
@@ -5,10 +6,14 @@ import { GoogleMap }         from '../../GoogleMap'
 import { massageVenuesData } from '../../GoogleMap/helper'
 
 export function Map () {
-  const { data } = useDashboardOverviewQuery({
+  const queryResults = useDashboardOverviewQuery({
     params: useParams()
   })
   
-  const massagedVenues = massageVenuesData(data)
-  return (<GoogleMap cluster={true} data={massagedVenues} />)
+  const massagedVenues = massageVenuesData(queryResults.data)
+  return (
+    <Loader states={[queryResults]}>
+      <GoogleMap cluster={true} data={massagedVenues} />
+    </Loader>
+  )
 }
