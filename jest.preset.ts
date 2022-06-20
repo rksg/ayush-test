ae5e023663c81732b08eb9551fdbf146d8e949b5
tingan.chen <tingan.chen@ruckuswireless.com>
2022-06-20 11:01:25 +0800

    ACX-9182: chore: remove 'text-summaryi' which would handle by ACX-7695

--- jest.preset.ts
+++ jest.preset.ts
@@ -8,7 +8,7 @@ module.exports = {
   transform: {
     '\\.svg$': `${__dirname}/tools/tests/svgrTransformer.js`
   },
-  coverageReporters: [ 'lcov', 'text-summary'],
+  coverageReporters: [ 'lcov' ],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/**/stories.tsx',
