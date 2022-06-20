ee9e18cd9507115ff6fe64de8ba4db502bd2a5b2
Guo, Yizhu <yguo1@ruckuswireless.com>
2022-06-20 17:27:26 +0800

    ACX-8793: Add activate network test

--- apps/rc-wifi/src/NetworkDetails/NetworkVenuesTab/index.spec.tsx
+++ apps/rc-wifi/src/NetworkDetails/NetworkVenuesTab/index.spec.tsx
@@ -2,13 +2,13 @@
 import '@testing-library/jest-dom'
 import { rest } from 'msw'
 
-import { CommonUrlsInfo }                                        from '@acx-ui/rc/utils'
-import { Provider }                                              from '@acx-ui/store'
-import { mockServer, render, screen, waitForElementToBeRemoved } from '@acx-ui/test-utils'
+import { CommonUrlsInfo }                                                   from '@acx-ui/rc/utils'
+import { Provider }                                                         from '@acx-ui/store'
+import { fireEvent, mockServer, render, screen, waitForElementToBeRemoved } from '@acx-ui/test-utils'
 
 import { NetworkVenuesTab } from './index'
 
-const network = {
+let network = {
   type: 'aaa',
   tenantId: 'ecc2d7cf9d2342fdb31ae0e24958fcac',
   venues: [
@@ -29,10 +29,10 @@ const network = {
   id: '373377b0cb6e46ea8982b1c80aabe1fa'
 }
 
-const user = { WIFI: '{"network-venues":{"columns":{"name":true,"description":true,"city":true,"country":true,"networks":true,"aggregatedApStatus":true,"activated":true,"vlan":true,"aps":true,"radios":true,"scheduling":true}}}',COMMON: '{"tab-networking-devices":"wifi","activity":{"showUnreadMark":true,"isFirstTime":false},"tab-events-logs":"activities","tab-venue-networks":"switch","tab-users":"switch","report-time-active-product":"SWITCH","tab-venue-clients":"wifi"}',SWITCH: '{"switches-list":{"columns-customization":{"activeSerial":"171px"}}}' }
+const user = { COMMON: '{"supportTriRadio":true,"tab-venue-clients":"wifi"}' }
 
-const list = {
-  totalCount: 3,
+let list = {
+  totalCount: 2,
   page: 1,
   data: [
     {
@@ -46,7 +46,8 @@ const list = {
       networks: { count: 1, names: ['03'], vlans: [1] },
       aggregatedApStatus: { '1_01_NeverContactedCloud': 1 },
       status: '1_InSetupPhase',
-      mesh: { enabled: false }
+      mesh: { enabled: false },
+      allApDisabled: false
     },
     {
       id: '02e2ddbc88e1428987666d31edbc3d9a',
@@ -59,47 +60,8 @@ const list = {
       switchClients: 2,
       switches: 1,
       status: '1_InSetupPhase',
-      mesh: { enabled: false }
-    },
-    {
-      id: 'd3424e21e80143c89d84f2531306b4b6',
-      name: 'pingtung',
-      description: '',
-      city: 'Pingtung',
-      country: 'Taiwan',
-      latitude: '22.6747543',
-      longitude: '120.4918824',
-      status: '1_InSetupPhase'
-    }
-  ],
-  subsequentQueries: [
-    {
-      fields: ['scheduling', 'vlan', 'aps', 'radios'],
-      url: '/api/tenant/ecc2d7cf9d2342fdb31ae0e24958fcac/wifi/network/get/deep',
-      httpMethod: 'POST',
-      payload: ['373377b0cb6e46ea8982b1c80aabe1fa']
-    },
-    {
-      fields: ['activated'],
-      url: '/api/tenant/ecc2d7cf9d2342fdb31ae0e24958fcac/wifi/venue/network-ap-group',
-      httpMethod: 'POST',
-      payload: [
-        {
-          venueId: 'd7b1a9a350634115a92ee7b0f11c7e75',
-          networkId: '373377b0cb6e46ea8982b1c80aabe1fa',
-          ssids: ['03']
-        },
-        {
-          venueId: '02e2ddbc88e1428987666d31edbc3d9a',
-          networkId: '373377b0cb6e46ea8982b1c80aabe1fa',
-          ssids: ['03']
-        },
-        {
-          venueId: 'd3424e21e80143c89d84f2531306b4b6',
-          networkId: '373377b0cb6e46ea8982b1c80aabe1fa',
-          ssids: ['03']
-        }
-      ]
+      mesh: { enabled: false },
+      wlan: { wlanSecurity: 'WPA3' }
     }
   ]
 }
@@ -149,5 +111,85 @@ describe('NetworkVenuesTab', () => {
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
     await screen.findByText('network-venue-1')
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('activate Network', async () => {
+    mockServer.use(
+      rest.post(
+        CommonUrlsInfo.getNetworksVenuesList.url,
+        (req, res, ctx) => res(ctx.json(list))
+      ),
+      rest.get(
+        CommonUrlsInfo.getNetwork.url,
+        (req, res, ctx) => res(ctx.json(network))
+      ),
+      rest.get(
+        CommonUrlsInfo.getAllUserSettings.url,
+        (req, res, ctx) => res(ctx.json(user))
+      )
+    )
+    const params = {
+      tenantId: 'ecc2d7cf9d2342fdb31ae0e24958fcac',
+      networkId: '373377b0cb6e46ea8982b1c80aabe1fa'
+    }
+
+    const { asFragment } = render(<Provider><NetworkVenuesTab /></Provider>, {
+      route: { params, path: '/:tenantId/:networkId' }
+    })
+
+    expect(screen.getByRole('img', { name: 'loader' })).toBeVisible()
+
+    await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
+    await screen.findByText('network-venue-1')
+    expect(asFragment()).toMatchSnapshot()
+    const toogleButton = screen.getByRole('switch', { checked: false })
+    fireEvent.click(toogleButton)
+    mockServer.use(
+      rest.post(
+        CommonUrlsInfo.addNetworkVenue.url,
+        (req, res, ctx) => res(ctx.json({ requestId: '123' }))
+      )
+    )
+    expect(screen.getByRole('img', { name: 'loader' })).toBeVisible()
+  })
+  
+  it('deactivate Network', async () => {
+    mockServer.use(
+      rest.post(
+        CommonUrlsInfo.getNetworksVenuesList.url,
+        (req, res, ctx) => res(ctx.json(list))
+      ),
+      rest.get(
+        CommonUrlsInfo.getNetwork.url,
+        (req, res, ctx) => res(ctx.json(network))
+      ),
+      rest.get(
+        CommonUrlsInfo.getAllUserSettings.url,
+        (req, res, ctx) => res(ctx.json(user))
+      )
+    )
+    const params = {
+      tenantId: 'ecc2d7cf9d2342fdb31ae0e24958fcac',
+      networkId: '373377b0cb6e46ea8982b1c80aabe1fa'
+    }
+
+    const { asFragment } = render(<Provider><NetworkVenuesTab /></Provider>, {
+      route: { params, path: '/:tenantId/:networkId' }
+    })
+
+    expect(screen.getByRole('img', { name: 'loader' })).toBeVisible()
+
+    await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
+    await screen.findByText('network-venue-1')
+    expect(asFragment()).toMatchSnapshot()
+    const toogleButton = screen.getByRole('switch', { checked: true })
+    fireEvent.click(toogleButton)
+    mockServer.use(
+      rest.delete(
+        CommonUrlsInfo.deleteNetworkVenue.url,
+        (req, res, ctx) => res(ctx.json({ requestId: '456' }))
+      )
+    )
+    expect(screen.getByRole('img', { name: 'loader' })).toBeVisible()
   })
 })
