c7bb811b1d0afb776b928728cb10cefef706a009
cbokil <chinmay.bokil@ruckuswireless.com>
2022-07-04 17:35:55 +0800

    Address review comments

--- apps/analytics/src/components/Header/index.spec.tsx
+++ apps/analytics/src/components/Header/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { BrowserRouter } from '@acx-ui/react-router-dom'
 
@@ -8,14 +8,14 @@ import { Header } from './index'
 
 describe('Analytics dumb header', () => {
 
- const props = {
-   title: 'title',
-   subTitle: [{ key : '1', value: ['1']}, { key : '12', value: ['1', '2'] }],
-   breadCrumb: [{ text: 'page', link: 'link' }]
- }
+  const props = {
+    title: 'title',
+    subTitles: [{ key: '1', value: ['1'] }, { key: '12', value: ['1', '2'] }],
+    breadCrumb: [{ text: 'page', link: 'link' }]
+  }
   it('should render correctly', () => {
     const { asFragment } = render(
-    <BrowserRouter><Header {... props}/> </BrowserRouter>)
+      <BrowserRouter><Header {... props}/> </BrowserRouter>)
     const fragment = asFragment()
     expect(fragment).toMatchSnapshot()
   })
--- libs/common/components/src/components/PageHeader/stories/stories.tsx
+++ libs/common/components/src/components/PageHeader/stories/stories.tsx
@@ -1,15 +1,14 @@
 import { storiesOf } from '@storybook/react'
 
-import { Basic }        from './Basic'
-import { WithButtons }  from './WithButtons'
-import { WithSubTitle } from './WithSubTitleText'
-import { WithSubTitle as SubTitleComp } from './WithSubTitleComp'
-import { WithTabs }     from './WithTabs'
+import { Basic }                              from './Basic'
+import { WithButtons }                        from './WithButtons'
+import { WithSubTitleText, WithSubTitleComp } from './WithSubTitle'
+import { WithTabs }                           from './WithTabs'
 
 storiesOf('PageHeader', module)
   .add('Basic', Basic)
   .add('With Buttons', WithButtons)
   .add('With Tabs', WithTabs)
-  .add('With Sub Title Text', WithSubTitle)
-  .add('With Sub Title Component', SubTitleComp)
+  .add('With Sub Title Text', WithSubTitleText)
+  .add('With Sub Title Component', WithSubTitleComp)
 export {}
