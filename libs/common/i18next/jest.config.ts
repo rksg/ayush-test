3febcd876a72bb1cadc884a863ead93ac8a656ff
sp1148 <Sarada.Pachalla@ruckuswireless.com>
2022-07-06 12:45:24 -0700

    ACX-6693: Fixed build failure issue due to code coverage %, Removed code related to i18next framework as decision was to go with react-intl

--- libs/common/i18next/jest.config.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'common-i18next',
-  preset: '../../../jest.preset.ts',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/common/i18next'
-}
