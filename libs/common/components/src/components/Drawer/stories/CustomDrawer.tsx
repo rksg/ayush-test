2ccdb36d424f3d6d7cb9e492b271705d6bd51174
wilson.loh <wilson.loh@ruckuswireless.com>
2022-07-06 14:26:06 +0800

    ACX-10339: chore: update styling and custom drawer story

--- libs/common/components/src/components/Drawer/stories/CustomDrawer.tsx
+++ libs/common/components/src/components/Drawer/stories/CustomDrawer.tsx
@@ -2,22 +2,58 @@ import { useState } from 'react'
 import { Drawer }   from '..'
 import { Button }   from '../../Button'
 
+import { Col, Form, Input, Row } from 'antd';
+
 export function CustomDrawer () {
   const [visible, setVisible] = useState(false)
+  const [resetField, setResetField] = useState(false)
+
   const onClose = () => {
     setVisible(false)
   }
   const onOpen = () => {
     setVisible(true)
   }
+  const resetFields = () => {
+    setResetField(true)
+    onClose()
+  }
+
   const content = <>
-    <p>some content</p>
-    <p>some content</p>
-    <p>some content</p>
+    <Form layout="vertical" hideRequiredMark>
+      <Row>
+        <Col span={24}>
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: 'Please enter name' }]}
+          >
+            <Input placeholder="Please enter name" />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <Form.Item
+            name="description"
+            label="Description"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter description',
+              }
+            ]}
+          >
+            <Input.TextArea rows={4} placeholder="Please enter description" />
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
   </>
 
   const footer = <>
-    <Button onClick={onClose}>Cancel</Button>
+    <Button onClick={onClose} type={'primary'}>Save</Button>
+    <Button onClick={resetFields}>Reset</Button>
   </>
   
   return (
@@ -29,6 +65,7 @@ export function CustomDrawer () {
         onClose={onClose}
         content={content}
         footer={footer}
+        destroyOnClose={resetField}
       />
     </>
   )
