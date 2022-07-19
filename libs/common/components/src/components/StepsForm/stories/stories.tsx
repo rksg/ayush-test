84dfb80fa9390b2e97cb9900c5a3e6e419c5872f
jason.cheow <jason.cheow@ruckuswireless.com>
2022-07-19 12:28:15 +0800

    ACX-9420: Update StepsForm stories
    - Fix double validation icon showing
    - Add Row/Col similar to actual usage
    - Remove MultiColumnsStory as design system doesn't have it

--- libs/common/components/src/components/StepsForm/stories/stories.tsx
+++ libs/common/components/src/components/StepsForm/stories/stories.tsx
@@ -3,12 +3,10 @@ import { storiesOf } from '@storybook/react'
 import { AsyncValidationStory }     from './AsyncValidationStory'
 import { BasicMultiStepsFormStory } from './BasicMultiStepsFormStory'
 import { EditModeStory }            from './EditModeStory'
-import { MultiColumnsStory }        from './MultiColumnsStory'
 
 storiesOf('StepsForm', module)
-  .add('Basic Multiple Steps Form', BasicMultiStepsFormStory)
+  .add('Basic', BasicMultiStepsFormStory)
   .add('With Async Validation', AsyncValidationStory)
   .add('Edit Mode', EditModeStory)
-  .add('Multi Columns Form', MultiColumnsStory)
 
 export {}
