852124638a76b971148d97566a8074ad58807633
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-07-19 14:26:55 +0800

    ACX-9420: temporary silent missing translation error until we implements extract & compile

--- libs/common/components/src/components/ConfigProvider/index.tsx
+++ libs/common/components/src/components/ConfigProvider/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { OnErrorFn, IntlErrorCode, MissingTranslationError } from '@formatjs/intl'
 import {
   ConfigProvider as AntConfigProvider
 }    from 'antd'
@@ -17,12 +18,19 @@ export type ConfigProviderProps = Omit<AntConfigProviderProps, 'locale'> & {
   lang?: LocaleProviderProps['lang']
 }
 
+export const onError: OnErrorFn = (error) => {
+  if (process.env['NODE_ENV'] === 'production') return
+  if (error.code === IntlErrorCode.MISSING_TRANSLATION) return
+  // eslint-disable-next-line no-console
+  console.error(error)
+}
+
 export function ConfigProvider (props: ConfigProviderProps) {
   return (
     <LocaleProvider lang={props.lang}>
       <LocaleContext.Consumer>
         {context => <Loader states={[{ isLoading: !Boolean(context.messages) }]}>
-          <IntlProvider locale={context.lang} messages={context.messages}>
+          <IntlProvider locale={context.lang} messages={context.messages} onError={onError}>
             <AntConfigProvider locale={context.messages}>
               <AntProConfigProvider {...props} locale={context.messages} />
             </AntConfigProvider>
