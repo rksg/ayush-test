0632bfb449b3893f9e8b53b4d92a566b5613bdf8
Asraf Ali <asraf.najmudeen@ruckuswireless.com>
2022-07-25 19:00:30 +0530

    ACX-10485 | chore: added topN application based on upload and download

--- apps/analytics/src/components/TrafficByApplication/index.tsx
+++ apps/analytics/src/components/TrafficByApplication/index.tsx
@@ -1,8 +1,9 @@
 
-import { useGlobalFilter }                                        from '@acx-ui/analytics/utils'
-import { Card, Loader, Table, ContentToggle, ContentToggleProps } from '@acx-ui/components'
+import { useGlobalFilter }                                                        from '@acx-ui/analytics/utils'
+import { Card, Loader, Table, SparklineChart, ContentToggle, ContentToggleProps } from '@acx-ui/components'
+import { formatter }                                                              from '@acx-ui/utils'
 
-import { useTrafficByApplicationQuery } from './services'
+import { useTrafficByApplicationQuery, TrafficByApplicationData } from './services'
 
 export function TrafficByApplicationWidget () {
   const filters = useGlobalFilter()
@@ -14,9 +15,7 @@ export function TrafficByApplicationWidget () {
       })
     }
   )
-  // eslint-disable-next-line no-console
-  console.log('queryResults.data:',queryResults.data)
-
+  
   const columns=(direction:string)=>([
     {
       title: 'Application',
@@ -35,24 +34,55 @@ export function TrafficByApplicationWidget () {
     },
     {
       title: 'Traffic History',
-      dataIndex: 'trafficHistory',
+      dataIndex: direction === 'download' ? 'downloadTrafficHistory' : 'uploadTrafficHistory',
       key: 'trafficHistory'
     }
   ])
 
+  const getDataSource= (data: TrafficByApplicationData[] | undefined) => {
+    if(!data)
+      return []
+    return data.map((item,index) => {
+      const uploadSparkLineData = item.timeseries.map(tsDatapoints => tsDatapoints.rxBytes)
+      const downloadSparkLineData = item.timeseries.map(tsDatapoints => tsDatapoints.txBytes)
+      const sparklineChartStyle = { height: 16, width: '100%', display: 'inline' }
+      return {
+        ...item,
+        upload: formatter('bytesFormat')(item.rxBytes),
+        uploadTrafficHistory: <SparklineChart 
+          key={index}
+          data={uploadSparkLineData}
+          style={sparklineChartStyle}/>,
+        download: formatter('bytesFormat')(item.txBytes),
+        downloadTrafficHistory: <SparklineChart
+          key={index}
+          data={downloadSparkLineData}
+          style={sparklineChartStyle}/>
+      }
+    })
+  }
+
+  // // eslint-disable-next-line no-console
+  // console.log('topNAppByUpload:', 
+  //   getDataSource(queryResults.data?.topNAppByUpload))
+  // // eslint-disable-next-line no-console
+  // console.log('topNAppByDownload:', 
+  //   getDataSource(queryResults.data?.topNAppByDownload))
+
   const uploadTable = <Table
     columns={columns('upload')}
-    dataSource={queryResults.data}
+    dataSource={getDataSource(queryResults.data?.topNAppByUpload)}
+    type={'compact'}
     pagination={false}
-
   />
 
   const downloadTable = <Table
     columns={columns('download')}
-    dataSource={queryResults.data}
+    dataSource={getDataSource(queryResults.data?.topNAppByDownload)}
+    type={'compact'}
     pagination={false}
   />
-
+  
   const tabDetails:ContentToggleProps['tabDetails']=[
     { label: 'Upload', content: uploadTable, value: 'upload' },
     { label: 'Download', content: downloadTable, value: 'download' }
--- apps/analytics/src/components/TrafficByApplication/services.ts
+++ apps/analytics/src/components/TrafficByApplication/services.ts
@@ -3,6 +3,12 @@ import { gql } from 'graphql-request'
 import { dataApi }      from '@acx-ui/analytics/services'
 import { GlobalFilter } from '@acx-ui/analytics/utils'
 
+export type HierarchyNodeData = {
+  totalTraffic: number
+  topNAppByUpload: TrafficByApplicationData[]
+  topNAppByDownload: TrafficByApplicationData[]
+}
+
 export type TrafficTimeseriesData = {
     timestamp: Date
     traffic: number
@@ -14,50 +20,55 @@ export type TrafficTimeseriesData = {
 export type TrafficByApplicationData = {
     appName: string
     totalTraffic: number
-    download: number
-    upload: number
+    txBytes: number
+    rxBytes: number
     clientMacCount: number
     timeseries: TrafficTimeseriesData[]
 }
 
 interface Response <T> {
   network: {
-    hierarchyNode: {
-        topNApplicationByTraffic: T[]
-    }
+    hierarchyNode: T
   }
 }
 
 export const api = dataApi.injectEndpoints({
   endpoints: (build) => ({
     trafficByApplication: build.query<
-      TrafficByApplicationData[],
+      HierarchyNodeData,
       GlobalFilter
     >({
       query: (payload) => ({
         document: gql`
-        query HierarchyNode($path: [HierarchyNodeInput], $end: DateTime, $start: DateTime,
-             $n: Int!, $granularity: String) {
-            network(end: $end, start: $start) {
-              hierarchyNode(path: $path) {
-                topNApplicationByTraffic(n: $n) {
-                  appName
-                  totalTraffic:traffic
-                  download:txBytes
-                  upload:rxBytes
-                  clientMacCount
-                  timeseries(granularity: $granularity) {
-                    timestamp
-                    traffic
-                    txBytes
-                    rxBytes
-                    clientMacCount
-                  }
-                }
+        query HierarchyNode($path: [HierarchyNodeInput], $start: DateTime, $end: DateTime,
+          $n: Int!, $granularity: String!) {
+          network(end: $end, start: $start) {
+            hierarchyNode(path: $path) {
+              totalTraffic
+              topNAppByUpload:topNApplicationByTraffic(n: $n, direction: "rx") {
+                ...applicationTrafficData
+              }
+              topNAppByDownload:topNApplicationByTraffic(n: $n, direction: "tx") {
+                ...applicationTrafficData
               }
             }
           }
-          
+        }
+        
+        fragment applicationTrafficData on ApplicationTraffic{
+          appName
+          traffic
+          rxBytes
+          txBytes
+          clientMacCount
+          timeseries(granularity: $granularity) {
+            timestamp
+            rxBytes
+            txBytes
+            traffic
+            clientMacCount
+          }
+        }
         `,
         variables: {
           path: payload.path,
@@ -67,8 +78,8 @@ export const api = dataApi.injectEndpoints({
           n: 5
         }
       }),
-      transformResponse: (response: Response<TrafficByApplicationData>) =>
-        response.network.hierarchyNode.topNApplicationByTraffic
+      transformResponse: (response: Response<HierarchyNodeData>) =>
+        response.network.hierarchyNode
     })
   })
 })
