9aef38dcd586bfb4ceda771d35f72db27508bad0
sp1179 <Sammanita.Pattnayak@ruckuswireless.com>
2022-07-25 10:58:28 +0530

    ACX-10150: Implemented connected clients over time widget

---
+++ apps/analytics/src/components/ConnectedClientsOverTime/index.spec.tsx
@@ -0,0 +1,59 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+
+import { dataApiURL }                      from '@acx-ui/analytics/services'
+import { Provider, store }                 from '@acx-ui/store'
+import { mockGraphqlQuery, mockAutoSizer } from '@acx-ui/test-utils'
+
+import { api } from './services'
+
+import ConnectedClientsOverTimeWidget from '.'
+
+const sample = {
+  time: [
+    '2022-04-07T09:15:00.000Z',
+    '2022-04-07T09:30:00.000Z',
+    '2022-04-07T09:45:00.000Z',
+    '2022-04-07T10:00:00.000Z',
+    '2022-04-07T10:15:00.000Z'
+  ],
+  totalTraffic_all: [1, 2, 3, 4, 5],
+  totalTraffic_6: [6, 7, 8, 9, 10],
+  totalTraffic_5: [11, 12, 13, 14, 15],
+  totalTraffic_24: [16, 17, 18, 19, 20]
+}
+
+describe('ConnectedClientsOverTimeWidget', () => {
+  mockAutoSizer()
+
+  beforeEach(() =>
+    store.dispatch(api.util.resetApiState())
+  )
+
+  it('should render loader', () => {
+    mockGraphqlQuery(dataApiURL, 'ConnectedClientsOverTimeWidget', {
+      data: { network: { hierarchyNode: { timeSeries: sample } } }
+    })
+    render( <Provider> <ConnectedClientsOverTimeWidget/></Provider>)
+    expect(screen.getByRole('img', { name: 'loader' })).toBeVisible()
+  })
+  it('should render chart', async () => {
+    mockGraphqlQuery(dataApiURL, 'ConnectedClientsOverTimeWidget', {
+      data: { network: { hierarchyNode: { timeSeries: sample } } }
+    })
+    const { asFragment } =render( <Provider> <ConnectedClientsOverTimeWidget/></Provider>)
+    await screen.findByText('Traffic by Volume')
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(asFragment().querySelector('div[_echarts_instance_^="ec_"]')).not.toBeNull()
+  })
+  it('should render error', async () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+    mockGraphqlQuery(dataApiURL, 'ConnectedClientsOverTimeWidget', {
+      error: new Error('something went wrong!')
+    })
+    render( <Provider> <ConnectedClientsOverTimeWidget/> </Provider>)
+    await screen.findByText('Something went wrong.')
+    jest.resetAllMocks()
+  })
+})
---
+++ apps/analytics/src/components/ConnectedClientsOverTime/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import AutoSizer from 'react-virtualized-auto-sizer'
+
+import { useGlobalFilter }          from '@acx-ui/analytics/utils'
+import { getSeriesData }            from '@acx-ui/analytics/utils'
+import { Card }                     from '@acx-ui/components'
+import { Loader }                   from '@acx-ui/components'
+import { MultiLineTimeSeriesChart } from '@acx-ui/components'
+import { cssStr }                   from '@acx-ui/components'
+import { formatter }                from '@acx-ui/utils'
+
+import { ConnectedClientsOverTimeData }     from './services'
+import { useConnectedClientsOverTimeQuery } from './services'
+
+export const seriesMapping = [
+  { key: 'uniqueUsers_all', name: 'All Radios' },
+  { key: 'uniqueUsers_24', name: formatter('radioFormat')('2.4') },
+  { key: 'uniqueUsers_5', name: formatter('radioFormat')('5') },
+  { key: 'uniqueUsers_6', name: formatter('radioFormat')('6') }
+] as Array<{ key: keyof Omit<ConnectedClientsOverTimeData, 'time'>, name: string }>
+
+const lineColors = [
+  cssStr('--acx-accents-blue-30'),
+  cssStr('--acx-accents-blue-50'),
+  cssStr('--acx-accents-orange-50'),
+  cssStr('--acx-semantics-yellow-40')
+]
+
+function ConnectedClientsOverTimeWidget () {
+  const filters = useGlobalFilter()
+  const queryResults = useConnectedClientsOverTimeQuery(filters,
+    {
+      selectFromResult: ({ data, ...rest }) => ({
+        data: getSeriesData(data!, seriesMapping),
+        ...rest
+      })
+    })
+
+  return (
+    <Loader states={[queryResults]}>
+      <Card title='Connected Clients Over Time' >
+        <AutoSizer>
+          {({ height, width }) => (
+            <MultiLineTimeSeriesChart
+              style={{ width, height }}
+              data={queryResults.data}
+              lineColors={lineColors}
+            />
+          )}
+        </AutoSizer>
+      </Card>
+    </Loader>
+  )
+}
+
+export default ConnectedClientsOverTimeWidget
