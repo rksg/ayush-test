25e48bafe2fa594ba79ce6d192e6a79d6318e2c0
jitendra.dangi <jitendra.dangi@ruckuswireless.com>
2022-07-26 17:44:18 +0530

    ACX-10527: chore: format label brackets

--- libs/common/components/src/components/BarChart/stories.tsx
+++ libs/common/components/src/components/BarChart/stories.tsx
@@ -61,8 +61,8 @@ function switchUsageLabelFormatter (params: CallbackDataParams): string {
   const usage = Array.isArray(params.data) ? params.data[1] : params.data
   const utilisation_per = Array.isArray(params.data) ? params.data[2] : params.data
   return '{poe_usage|' +
-    formatter('milliWattsFormat')(usage) + '} ({utilisationPer|' +
-    utilisation_per + '%})'
+    formatter('milliWattsFormat')(usage) + '} {utilisationPer|(' +
+    utilisation_per + '%)}'
 }
 
 function switchTrafficLabelFormatter (params: CallbackDataParams): string {
