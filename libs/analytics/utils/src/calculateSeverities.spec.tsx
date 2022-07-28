172dfdd433ea9e9378116a531257b6e9c8569eaf
wilson.loh <wilson.loh@ruckuswireless.com>
2022-07-28 15:06:05 +0800

    ACX-10741: chore: rename file and description

--- libs/analytics/utils/src/calculateSeverities.spec.tsx
+++ libs/analytics/utils/src/calculateSeverities.spec.tsx
@@ -1,7 +1,7 @@
 import { calculateSeverity } from './calculateSeverities'
 
 describe('calculateSeverity', () => {
-  it('should', () => {
+  it('should return correct value', () => {
     const output = [0.1, 0.65, 0.76, 0.92].map((severity) => calculateSeverity(severity))
     expect(output).toEqual(['P4', 'P3', 'P2', 'P1'])
   })
