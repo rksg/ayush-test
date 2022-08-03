94233f68752c11f5c52705b7aa698e2c74b51189
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-08-03 12:03:59 +0800

    ACX-11468: fix StepsForm unable to dynamically add/remove step

--- libs/common/components/src/components/StepsForm/stories/stories.tsx
+++ libs/common/components/src/components/StepsForm/stories/stories.tsx
@@ -2,11 +2,13 @@ import { storiesOf } from '@storybook/react'
 
 import { AsyncValidationStory }     from './AsyncValidationStory'
 import { BasicMultiStepsFormStory } from './BasicMultiStepsFormStory'
+import { DynamicStepsStory }        from './DynamicStepsStory'
 import { EditModeStory }            from './EditModeStory'
 
 storiesOf('StepsForm', module)
   .add('Basic', BasicMultiStepsFormStory)
   .add('With Async Validation', AsyncValidationStory)
   .add('Edit Mode', EditModeStory)
+  .add('Dynamic Steps', DynamicStepsStory)
 
 export {}
