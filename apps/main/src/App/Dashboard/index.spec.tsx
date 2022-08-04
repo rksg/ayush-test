86b136f512e77f2e6130d5c1dd503a50923f65f5
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-08-04 15:09:00 +0800

    ACX-10721: add coverage for dashboard tabs

--- apps/main/src/App/Dashboard/index.spec.tsx
+++ apps/main/src/App/Dashboard/index.spec.tsx
@@ -1,16 +1,16 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@acx-ui/test-utils'
+import { fireEvent, render, screen } from '@acx-ui/test-utils'
 
 import Dashboard from '.'
 
 jest.mock(
   'analytics/Widgets',
-  () => ({ name }: { name: string }) => <div data-testid={`analytics-${name}`} />,
+  () => ({ name }: { name: string }) => <div data-testid={`analytics-${name}`} title={name} />,
   { virtual: true })
 
 jest.mock(
   'rc/Widgets',
-  () => ({ name }: { name: string }) =><div data-testid={`networks-${name}`} />,
+  () => ({ name }: { name: string }) => <div data-testid={`networks-${name}`} title={name} />,
   { virtual: true })
 
 describe('Dashboard', () => {
@@ -19,5 +19,28 @@ describe('Dashboard', () => {
 
     expect(await screen.findAllByTestId(/^analytics/)).toHaveLength(6)
     expect(await screen.findAllByTestId(/^networks/)).toHaveLength(5)
+  })
+
+  it('switches between tabs', async () => {
+    render(<Dashboard />)
+
+    const wifiWidgets = [
+      'trafficByVolume',
+      'networkHistory',
+      'connectedClientsOverTime',
+      'topApplicationsByTraffic'
+    ]
+    wifiWidgets.forEach(widget => expect(screen.getByTitle(widget)).toBeVisible())
+
+    fireEvent.click(screen.getByRole('radio', { name: 'Switch' }))
+
+    const switchWidgets = [
+      'switchTrafficByVolume',
+      'topSwitchesByPoeUsage',
+      'topSwitchesByTraffic',
+      'topSwitchesByErrors',
+      'topSwitchesByModels'
+    ]
+    switchWidgets.forEach(widget => expect(screen.getByTitle(widget)).toBeVisible())
   })
 })
