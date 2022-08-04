b6a66ec8d516617ea55a7bd99aa6ef376f10abb0
jason.cheow <jason.cheow@ruckuswireless.com>
2022-08-04 19:06:46 +0800

    ACX-10741: Add test for StackedBarChart

--- libs/common/components/src/components/StackedBarChart/index.spec.tsx
+++ libs/common/components/src/components/StackedBarChart/index.spec.tsx
@@ -1,5 +1,7 @@
 import { render } from '@acx-ui/test-utils'
 
+import { cssStr } from '../../theme/helper'
+
 import { data } from './stories'
 
 import { StackedBarChart } from '.'
@@ -19,6 +21,12 @@ describe('StackedBarChart', () => {
         barColors={barColors}/>)
     expect(asFragment().querySelector('div[_echarts_instance_^="ec_"]')).not.toBeNull()
     expect(asFragment().querySelector('svg')).toBeDefined()
+  })
+  it('should have default barColors', async () => {
+    const { asFragment } = render(
+      <StackedBarChart data={data} />)
+    expect(asFragment().querySelector(`path[fill="${cssStr('--acx-semantics-red-70')}"]`))
+      .not.toBeNull()
   })
   it('should not render labels and total count', async () => {
     const { asFragment } = render(
