128b6272daf63010c5bac4be0756b7717172f4b4
jason.cheow <jason.cheow@ruckuswireless.com>
2022-08-04 15:18:16 +0800

    ACX-11468: Exclude using collectCoverageFrom instead of istanbul ignore

--- libs/common/components/jest.config.ts
+++ libs/common/components/jest.config.ts
@@ -9,7 +9,7 @@ module.exports = {
   coverageThreshold: {
     global: {
       statements: 94.37,
-      branches: 97.37,
+      branches: 97.08,
       functions: 95.07,
       lines: 94.57
     }
