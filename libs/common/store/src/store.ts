04f2f1d3dd717d80c3fe113f836e7502f74ffb75
Jacky Chiu <jacky.chiu@ruckuswireless.com>
2022-08-09 11:22:55 +0800

    ACX-11826: ACX-11837: Fix lint error

--- libs/common/store/src/store.ts
+++ libs/common/store/src/store.ts
@@ -1,7 +1,7 @@
 import { configureStore }                                 from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
-import { dataApi }          from '@acx-ui/analytics/services'
+import { dataApi }         from '@acx-ui/analytics/services'
 import {
   baseNetworkApi as networkApi,
   baseCloudpathApi as cloudpathApi,
