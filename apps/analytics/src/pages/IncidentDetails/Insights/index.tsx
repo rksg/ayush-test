87fdcc7bb9718351ee9c18340cbdea09a73b4198
wilson.loh <wilson.loh@ruckuswireless.com>
2022-08-17 18:19:06 +0800

    ACX-10745: chore: added i18 for text

--- apps/analytics/src/pages/IncidentDetails/Insights/index.tsx
+++ apps/analytics/src/pages/IncidentDetails/Insights/index.tsx
@@ -1,5 +1,6 @@
 import { Row, Typography }  from 'antd'
 import { FormattedMessage } from 'react-intl'
+import { useIntl }          from 'react-intl'
 
 import { getRootCauseAndRecommendations } from '@acx-ui/analytics/utils'
 import { Incident }                       from '@acx-ui/analytics/utils'
@@ -9,6 +10,7 @@ import { BulbOutlined }                   from '@acx-ui/icons'
 import * as UI from './styledComponents'
 
 export const Insights = (props: Incident) => {
+  const { $t } = useIntl()
   const [{ rootCauses, recommendations }] = getRootCauseAndRecommendations(
     props.code, props.metadata.rootCauseChecks)
   const values = {
@@ -22,19 +24,19 @@ export const Insights = (props: Incident) => {
       <UI.InsightHeader>
         <BulbOutlined />
         <UI.InsightTitle>
-          <Typography.Title level={3}>Insights</Typography.Title>
+          <Typography.Title level={3}>{$t({ defaultMessage: 'Insights' })}</Typography.Title>
         </UI.InsightTitle>
       </UI.InsightHeader>
       <Row>
         <UI.LeftInsightDetails span={12}>
-          <Subtitle level={4}>Root Cause Analysis</Subtitle>
+          <Subtitle level={4}>{$t({ defaultMessage: 'Root Cause Analysis' })}</Subtitle>
           <FormattedMessage 
             {...rootCauses}
             values={values}
           />
         </UI.LeftInsightDetails>
         <UI.RightInsightDetails span={12}>
-          <Subtitle level={4}>Recommended Action</Subtitle>
+          <Subtitle level={4}>{$t({ defaultMessage: 'Recommended Action' })}</Subtitle>
           <FormattedMessage
             {...recommendations}
             values={values}
