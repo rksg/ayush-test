95020e003bb136b7cd08abd250fbe1035bb143bb
TLIN4 <Tsaihuei.Lin@ruckuswireless.com>
2022-08-18 18:59:40 +0800

    ACX-11616: update venue route

--- apps/main/src/AllRoutes.tsx
+++ apps/main/src/AllRoutes.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 import { Route, TenantNavigate, rootRoutes } from '@acx-ui/react-router-dom'
 
-import App             from './App'
-import AnalyticsBase   from './App/Analytics'
-import Dashboard       from './App/Dashboard'
-import NetworksBase    from './App/Networks'
-import { VenuesTable } from './App/Venues/VenuesTable'
+import App              from './App'
+import AnalyticsBase    from './App/Analytics'
+import Dashboard        from './App/Dashboard'
+import NetworksBase     from './App/Networks'
+import { VenueDetails } from './App/Venues/VenueDetails'
+import { VenuesTable }  from './App/Venues/VenuesTable'
 
 const WifiRoutes = React.lazy(() => import('rc/Routes'))
 const AnalyticsRoutes = React.lazy(() => import('analytics/Routes'))
@@ -22,7 +23,10 @@ function AllRoutes () {
       <Route path='networks/*' element={<NetworksBase />}>
         <Route path='*' element={<WifiRoutes />} />
       </Route>
-      <Route path='venues' element={<VenuesTable />} />
+      <Route path='venues/*'>
+        <Route path='*' element={<VenuesTable />} />
+        <Route path=':venueId/venue-details/:activeTab' element={<VenueDetails />} />
+      </Route>
     </Route>
   )
 }
