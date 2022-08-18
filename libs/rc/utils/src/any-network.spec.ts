42b2ba9edd46ae951cec638f5c1258702d4d3873
yw1046 <Yc.Wong@ruckuswireless.com>
2022-08-18 18:55:50 +0800

    ACX-8650: Add inspection

--- libs/rc/utils/src/any-network.spec.ts
+++ libs/rc/utils/src/any-network.spec.ts
@@ -8,21 +8,26 @@ describe('Test Any Network model type', () => {
   it('should initialize Open Network model correctly', async () => {
     const openNetwork = new OpenNetwork()
     openNetwork.name = 'test'
+    expect(openNetwork.name).toEqual('test')
   })
   it('should initialize Network Venue model correctly', async () => {
     const networkVenue = new NetworkVenue()
     networkVenue.id = 'test-id'
+    expect(networkVenue.id).toEqual('test-id')
   })
   it('should initialize AAAWlanAdvancedCustomization model correctly', async () => {
     const aaaWlan = new AAAWlanAdvancedCustomization()
     aaaWlan.hideSsid = false
+    expect(aaaWlan.hideSsid).toEqual(false)
   })
   it('should initialize DpskWlanAdvancedCustomization model correctly', async () => {
-    const apskWlan = new DpskWlanAdvancedCustomization()
-    apskWlan.hideSsid = false
+    const dpskWlan = new DpskWlanAdvancedCustomization()
+    dpskWlan.hideSsid = false
+    expect(dpskWlan.hideSsid).toEqual(false)
   })
   it('should initialize PskWlanAdvancedCustomization model correctly', async () => {
     const pskWlan = new PskWlanAdvancedCustomization()
     pskWlan.hideSsid = false
+    expect(pskWlan.hideSsid).toEqual(false)
   })
 })
