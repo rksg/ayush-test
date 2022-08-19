3be2af0e0572642aeed7936b70fc9c2f4d88775f
sp1148 <Sarada.Pachalla@ruckuswireless.com>
2022-08-18 22:13:07 -0700

    ACX-10726: updated docker scripts and moved localeGenerator.sh under tools folder

--- tools/docker/localesGenerator.sh
+++ tools/docker/localesGenerator.sh
@@ -4,14 +4,13 @@ echo "Locale files generation started..."
 if [ -d  apps/main/src/locales/compiled ]; then
   echo "path exists"
   rm -rf 'apps/main/src/locales/compiled'
-  echo "folder temp deleted"
+  echo "folder compiled deleted"
 else
-  echo "No temp dir"
+  echo "No compiled dir"
 fi
 
 for file in $(find apps/main/src/locales/* -name '*.json')
 do
-  echo "sara testing " $file
   npx formatjs compile $file --out-file 'apps/main/temp/'$file
   echo $file
 done
