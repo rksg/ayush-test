2b32ede5288875e474936c056f671cf385e901c3
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-08-23 18:11:53 +0800

    ACX-10751: update client events code based on ETL constants

--- libs/analytics/utils/src/mapping/clientEventsMap.ts
+++ libs/analytics/utils/src/mapping/clientEventsMap.ts
@@ -3,35 +3,35 @@ import { defineMessage } from 'react-intl'
 
 import  { ccd80211ReasonCodes } from './ccd80211ReasonCodeMap'
 // reference for available codes:
-// https://github.com/rksg/rsa-mlisa-etl/blob/develop/src/main/scala/com/ruckuswireless/mlisa/MLISAConstants.scala
+// https://github.com/rksg/rsa-mlisa-etl/blob/develop/etl/src/main/scala/com/ruckuswireless/mlisa/constants/MLISAConstants.scala
 // but text is specific to UI
 export const clientEvents = [
   {
-    id: 0,
+    id: 201,
     code: 'EVENT_CLIENT_JOIN',
     text: defineMessage({ defaultMessage: 'Client join' })
   },
   {
-    id: 1,
+    id: 236,
     code: 'EVENT_CLIENT_INFO_UPDATED',
     text: defineMessage({ defaultMessage: 'Client associated (802.11)' })
   },
   {
-    id: 2,
+    id: 204,
     code: 'EVENT_CLIENT_DISCONNECT',
     text: defineMessage({ defaultMessage: 'Client disconnected' })
   },
   {
-    id: 3,
+    id: 209,
     code: 'EVENT_CLIENT_ROAMING',
     text: defineMessage({ defaultMessage: 'Client roamed' })
   },
   {
     ...ccd80211ReasonCodes.find(reason => reason.code === 'SG_DHCP_CCD_REASON_DISASSOC_STA_HAS_LEFT'),
-    id: 4
+    id: -1
   },
   {
     ...ccd80211ReasonCodes.find(reason => reason.code === 'SG_DHCP_CCD_REASON_DEAUTH_LEAVING'),
-    id: 5
+    id: -2
   }
 ]
