6eaaa067eccd1c716da8428af4786f1a59bceee0
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-08-25 12:51:29 +0800

    ACX-10726: add more info to log

--- tools/docker/localesGenerator.sh
+++ tools/docker/localesGenerator.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-for file in $(find apps/main/src/locales/* -name '*.json' -not -path 'apps/main/src/locales/compiled/*')
+COMPILED_PATH="apps/main/src/locales/compiled"
+
+echo "Locale files generation started..."
+
+for file in $(find apps/main/src/locales/* -name '*.json' -not -path "$COMPILED_PATH/*")
 do
-  npx formatjs compile $file --out-file "apps/main/src/locales/compiled/$(basename $file)"
-  echo "compiled $file"
+  npx formatjs compile $file --out-file "$COMPILED_PATH/$(basename $file)"
+  echo "compiled $file > $COMPILED_PATH/$(basename $file)"
 done
 
 echo "Locale files generation completed!!"
