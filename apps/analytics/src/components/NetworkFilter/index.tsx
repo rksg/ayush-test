06274b27e7cadcceda45b8b3f140d2e5706f4424
ny1012 <Nishanth.Yerramsetty@ruckuswireless.com>
2022-08-31 10:38:03 +0800

    ACX-12974: Add incident severity to network filter

--- apps/analytics/src/components/NetworkFilter/index.tsx
+++ apps/analytics/src/components/NetworkFilter/index.tsx
@@ -3,49 +3,97 @@ import { omit, uniqBy }      from 'lodash'
 import { SingleValueType }   from 'rc-cascader/lib/Cascader'
 import { useIntl }           from 'react-intl'
 
-import { useAnalyticsFilter, defaultNetworkPath } from '@acx-ui/analytics/utils'
-import { NetworkFilter, Option, Loader }          from '@acx-ui/components'
+import { useAnalyticsFilter, defaultNetworkPath, calculateSeverity } from '@acx-ui/analytics/utils'
+import { NetworkFilter, Option, Loader }                             from '@acx-ui/components'
 
 import { Child, useNetworkFilterQuery, ApOrSwitch } from './services'
 import * as UI                                      from './styledComponents'
-
 
 const getFilterData = (data: Child[], $t: CallableFunction): Option [] => {
   const venues = data.filter(node => node.type === 'zone')
   const uniqSwGrps = uniqBy(data, 'name').filter(node => node.type === 'switchGroup')
   const switchGroups = data
-    .filter(node => node.type === 'switchGroup')
+    .filter((node) => node.type === 'switchGroup')
     .reduce((obj, node) => obj.set(node.name, node), new Map())
   return [...venues, ...uniqSwGrps].map((node, index) => {
+    const severityCircles = getSeverityCircles([...(node.aps || []), ...(node.switches || [])])
     const venue = {
-      label: node.name,
+      label: (
+        <UI.LabelContainer>
+          <UI.Label>{node.name}</UI.Label>
+          <UI.SeverityContainer>
+            { severityCircles.map((severityCircle)=>
+              <UI.SeveritySpan severity={severityCircle} />)
+            }
+          </UI.SeverityContainer>
+        </UI.LabelContainer>
+      ), // function call
       value: JSON.stringify(node.path),
-      children: [] as Option[]
+      children: [] as Option[],
+      displayLabel: node.name
     }
-    if(node.aps?.length) {
+    if (node.aps?.length) {
+      const severityCircles = getSeverityCircles(node.aps)
       venue.children.push({
-        label: <UI.NonSelectableItem key={index}>
-          {$t({ defaultMessage: 'APs' })}
-        </UI.NonSelectableItem>,
+        label: (
+          <UI.NonSelectableItem key={index}>
+            <UI.LabelContainer>
+              <UI.Label>{$t({ defaultMessage: 'APs' })}</UI.Label>
+              <UI.SeverityContainer>
+                { severityCircles.map((severityCircle)=>
+                  <UI.SeveritySpan severity={severityCircle} />)
+                }
+              </UI.SeverityContainer>
+            </UI.LabelContainer>
+          </UI.NonSelectableItem>
+        ), // function call
         displayLabel: $t({ defaultMessage: 'APs' }),
         ignoreSelection: true,
         value: `aps${index}`,
         children: node.aps.map((ap: ApOrSwitch) => ({
-          label: ap.name,
+          label: (
+            <UI.LabelContainer>
+              <UI.Label>{ap.name}</UI.Label>
+              <UI.SeverityContainer>
+                <UI.SeveritySpan severity={calculateSeverity(ap.incidentSeverity) as string} />
+              </UI.SeverityContainer>
+            </UI.LabelContainer>
+          ), //easy straight forward
+          displayLabel: ap.name,
           value: JSON.stringify([...node.path, { type: 'AP', name: ap.mac }])
         }))
       })
     }
-    if(switchGroups.get(node.name)?.switches.length) {
+    if (switchGroups.get(node.name)?.switches.length) {
+      const severityCircles = getSeverityCircles(switchGroups.get(node.name).switches)
       venue.children.push({
-        label: <UI.NonSelectableItem key={index}>
-          {$t({ defaultMessage: 'Switches' })}
-        </UI.NonSelectableItem>,
+        label: (
+          <UI.NonSelectableItem key={index}>
+            <UI.LabelContainer>
+              <UI.Label>{$t({ defaultMessage: 'Switches' })}</UI.Label>
+              <UI.SeverityContainer>
+                { severityCircles.map((severityCircle)=>
+                  <UI.SeveritySpan severity={severityCircle} />)
+                }
+              </UI.SeverityContainer>
+            </UI.LabelContainer>
+          </UI.NonSelectableItem>
+        ), // function call
         displayLabel: $t({ defaultMessage: 'Switches' }),
         ignoreSelection: true,
         value: `switches${index}`,
         children: switchGroups.get(node.name).switches.map((switchNode: ApOrSwitch) => ({
-          label: switchNode.name,
+          label: (
+            <UI.LabelContainer>
+              <UI.Label>{switchNode.name}</UI.Label>
+              <UI.SeverityContainer>
+                <UI.SeveritySpan
+                  severity={calculateSeverity(switchNode.incidentSeverity) as string}
+                />
+              </UI.SeverityContainer>
+            </UI.LabelContainer>
+          ), //easy straight forward
+          displayLabel: switchNode.name,
           value: JSON.stringify([
             ...switchGroups.get(node.name).path,
             { type: 'switch', name: switchNode.mac }
@@ -56,13 +104,30 @@ const getFilterData = (data: Child[], $t: CallableFunction): Option [] => {
     return venue
   })
 }
+
+const getSeverityCircles = (
+  data: ApOrSwitch[]
+) => {
+  const severityArray = data.reduce(
+    (acc: string[], val: ApOrSwitch) => {
+      const severity = calculateSeverity(val.incidentSeverity)
+      if(severity && !acc.includes(severity))
+        acc.push(severity)
+      return acc
+    },
+    []
+  )
+  return severityArray
+}
+
+
 const search = (input: string, path: DefaultOptionType[]) : boolean => {
   const item = path.slice(-1)[0]
   return item.ignoreSelection // non-selection implies non-searchable
     ? false
-    : (item?.label as string)?.toLowerCase().includes(input.toLowerCase())
+    : (item?.displayLabel as string)?.toLowerCase().includes(input.toLowerCase())
 }
-export const displayRender = ({}, selectedOptions: DefaultOptionType[] | undefined) => 
+export const displayRender = (label: string[], selectedOptions: DefaultOptionType[] | undefined) => 
   selectedOptions
     ?.map(option => option?.displayLabel || option?.label).join(' / ')
 export const onApply = (
--- apps/analytics/src/components/NetworkFilter/styledComponents.ts
+++ apps/analytics/src/components/NetworkFilter/styledComponents.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components/macro'
 
+import {
+  IncidentSeverities,
+  incidentSeverities
+} from '@acx-ui/analytics/utils'
+import { cssStr } from '@acx-ui/components'
+export type SeveritySpanProps = {
+  severity: string
+}
+
 export const NonSelectableItem = styled.div.attrs(
   { onClick: e => e.stopPropagation() }
 )`
@@ -8,4 +17,37 @@ export const NonSelectableItem = styled.div.attrs(
 export const Container = styled.div`
   min-width: 150px;
   max-width: 180px;
+`
+export const SeverityContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  line-height: 8px;
+  justify-content: right
+
+`
+export const LabelContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: 15px;
+  align-items: center;
+
+`
+export const Label = styled.div`
+  width : 100px;
+  max-width: 120px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  justify-content: left
+`
+export const SeveritySpan = styled.span.attrs((props: SeveritySpanProps) => props)`
+  background-color: ${(props) => {
+    const color =
+      incidentSeverities[props.severity as IncidentSeverities]?.color ?? ''
+    return cssStr(color)
+  }};
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin-left: -2px;
 `
