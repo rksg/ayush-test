c985b1b36af5ee3bf2a2d5cf23b7c904656c7a97
sean.huang <sean.huang@ruckuswireless.com>
2022-08-31 16:48:35 +0800

    ACX-13021: add unit test for DHCPDiagram

--- apps/rc/src/pages/services/DHCPForm/DHCPDiagram/DHCPDiagram.spec.tsx
+++ apps/rc/src/pages/services/DHCPForm/DHCPDiagram/DHCPDiagram.spec.tsx
@@ -1,8 +1,36 @@
 import '@testing-library/jest-dom'
 
+import { DHCPConfigTypeEnum } from '@acx-ui/rc/utils'
+import { render, screen }     from '@acx-ui/test-utils'
 
+import { DHCPDiagram } from './DHCPDiagram'
 
 describe('DHCPDiagram', () => {
+  it('should render default diagram successfully', async () => {
+    const { asFragment } = render(<DHCPDiagram type={undefined} />)
+    const diagram = screen.getByRole('img') as HTMLImageElement
+    expect(diagram.src).toContain('dpsk.png')
+    expect(asFragment()).toMatchSnapshot()
+  })
 
+  describe('DHCPDiagram - MULTIPLE', () => {
+    const type = DHCPConfigTypeEnum.MULTIPLE
+    it('should render MULTIPLE diagram successfully', async () => {
+      const { asFragment } = render(<DHCPDiagram type={type} />)
+      const diagram = screen.getByRole('img') as HTMLImageElement
+      expect(diagram.src).toContain('open.png')
+      expect(asFragment()).toMatchSnapshot()
+    })
+  })
+
+  describe('DHCPDiagram - HIERARCHICAL', () => {
+    const type = DHCPConfigTypeEnum.HIERARCHICAL
+    it('should render HIERARCHICAL diagram successfully', async () => {
+      const { asFragment } = render(<DHCPDiagram type={type} />)
+      const diagram = screen.getByRole('img') as HTMLImageElement
+      expect(diagram.src).toContain('open-cloudpath-on-prem-deployment.png')
+      expect(asFragment()).toMatchSnapshot()
+    })
+  })
 
 })
