d015f22f1eb31e4310b7ac9f268fbb759f1f327a
TLIN4 <Tsaihuei.Lin@ruckuswireless.com>
2022-09-07 19:59:28 +0800

    ACX-12542: Implement Anchor Layout component

---
+++ libs/common/components/src/components/Anchor/index.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+
+import { Wrapper, Anchor, Container } from './styledComponents'
+
+export * from './styledComponents'
+
+const { Link } = Anchor
+
+export interface AnchorPageItem {
+  title: string,
+  content: ReactNode
+}
+
+export const AnchorLayout = (props: {
+  items: AnchorPageItem[]
+}) => {
+  return <Wrapper>
+    <Anchor>{
+      props?.items.map(item => 
+        <Link href={`#${item.title.split(' ').join('-')}`} title={item.title} />
+      )
+    }
+    </Anchor>
+    <Container>{
+      props.items.map(item => 
+        <div className='section' id={item.title.split(' ').join('-')}>
+          {item.content}
+        </div>
+      )
+    }</Container>
+  </Wrapper>
+}
