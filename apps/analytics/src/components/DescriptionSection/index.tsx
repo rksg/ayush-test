ed3503e210e79e1bc113ba839a483b01b2170eda
jason.cheow <jason.cheow@ruckuswireless.com>
2022-09-15 17:41:15 +0800

    ACX-13654: Add tooltip for scope to incident attribute
    Also fix line-breaks in tooltip.

--- apps/analytics/src/components/DescriptionSection/index.tsx
+++ apps/analytics/src/components/DescriptionSection/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
+import { Tooltip } from 'antd'
+
 import { Descriptions, TextContent } from './styledComponents'
 
 export interface DescriptionRowProps {
   label: string
-  title?: string
+  tooltip?: string
   children: React.ReactNode
   onClick?: () => void
 }
 
 export const DescriptionRow: React.FC<DescriptionRowProps> = (props) => {
-  return <TextContent
-    title={props.title || ((typeof props.children === 'string') ? props.children : undefined)}
+  let textContent = <TextContent
+    title={!props.tooltip && typeof props.children === 'string' ? props.children : undefined}
     onClick={props.onClick}
   >
     {props.children}
   </TextContent>
+  if (props.tooltip) { textContent = <Tooltip title={props.tooltip}>{textContent}</Tooltip> }
+  return textContent
 }
 
 export const DescriptionSection: React.FC<{
