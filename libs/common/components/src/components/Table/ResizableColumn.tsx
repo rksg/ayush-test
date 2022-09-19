2560e38110106e1f7369084c42466bfd4695d288
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2022-09-19 17:49:20 +0800

    ACX-13654: remove listener when resize end

--- libs/common/components/src/components/Table/ResizableColumn.tsx
+++ libs/common/components/src/components/Table/ResizableColumn.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useCallback } from 'react'
 
 import _             from 'lodash'
 import { Resizable } from 'react-resizable'
@@ -10,11 +10,15 @@ interface ResizableColumnProps extends React.PropsWithChildren {
   onResize: (width: number) => void
 }
 
+
 export const ResizableColumn: React.FC<ResizableColumnProps> = (props) => {
   const { onResize, width: columnWidth, ...rest } = props
   const [width, setWidth] = useState(columnWidth)
   const [currentHeaderCell, setCurrentHeaderCell] = useState<HTMLTableHeaderCellElement>()
   const headerCellRef = useRef<HTMLTableHeaderCellElement>(null)
+
+  const handleResizeStop = useCallback(() => setCurrentHeaderCell(undefined), [])
+  const handleStopPropagation = useCallback((e: Event) => e.stopPropagation(), [])
 
   useEffect(() => {
     if (headerCellRef && headerCellRef.current !== currentHeaderCell) {
@@ -30,10 +34,14 @@ export const ResizableColumn: React.FC<ResizableColumnProps> = (props) => {
     width={width}
     height={0}
     handle={<ResizableHandle />}
+    onResizeStop={() => {
+      document.removeEventListener('mouseup', handleResizeStop)
+      headerCellRef.current!.addEventListener('click', handleStopPropagation)
+    }}
     onResizeStart={() => {
       setCurrentHeaderCell(headerCellRef.current!)
-      document.addEventListener('mouseup', () => setCurrentHeaderCell(undefined), { once: true })
-      headerCellRef.current!.addEventListener('click', e => e.stopPropagation(), { once: true })
+      document.addEventListener('mouseup', handleResizeStop, { once: true })
+      headerCellRef.current!.addEventListener('click', handleStopPropagation, { once: true })
     }}
     onResize={(_: React.SyntheticEvent<Element>, { size }) => {
       onResize(size.width)
