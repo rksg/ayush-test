77fb85e31790e1ff4b34a534494e8c0e2e6f872e
yw1046 <Yc.Wong@ruckuswireless.com>
2022-09-19 12:22:09 +0800

    ACX-13774: Add tabs

--- apps/main/src/pages/Venues/VenueDetails/VenueDevicesTab/index.tsx
+++ apps/main/src/pages/Venues/VenueDetails/VenueDevicesTab/index.tsx
@@ -1,7 +1,22 @@
+import { Tabs }    from 'antd'
+import { useIntl } from 'react-intl'
+
 import { VenueMeshApsTable } from './VenueMeshAps'
 
 export function VenueDevicesTab () {
+  const { $t } = useIntl()
+
   return (
-    <VenueMeshApsTable />
+    <Tabs
+      defaultActiveKey='wifi'
+      type='card'
+    >
+      <Tabs.TabPane tab={$t({ defaultMessage: 'Wi-Fi' })} key='wifi'>
+        <VenueMeshApsTable />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab={$t({ defaultMessage: 'Switch' })} key='switch'>
+        {$t({ defaultMessage: 'Switch' })} 
+      </Tabs.TabPane>
+    </Tabs>
   )
 }
