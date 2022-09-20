04995e6f516ab4ce41d857f1d59a4e2b434e6497
sp1148 <Sarada.Pachalla@ruckuswireless.com>
2022-09-20 16:32:10 -0700

    ACX-13736: addressed PR comments

--- libs/common/utils/src/locales.spec.tsx
+++ libs/common/utils/src/locales.spec.tsx
@@ -13,7 +13,8 @@ import {
 import {
   loadLocale,
   LocaleProvider,
-  LocaleContext
+  LocaleContext,
+  localeLoaders
 } from './locales'
 
 const messages = {
@@ -74,7 +75,6 @@ describe('loadLocale', () => {
     }))
   })
 
-
   it('loads from cache when available', async () => {
     mockServer.close()
     mockServer.listen({ onUnhandledRequest: 'error' })
@@ -88,6 +88,14 @@ describe('loadLocale', () => {
     await loadLocale('en-US')
     // should throw if cache not being use
     await loadLocale('en-US')
+  })
+
+  it('locale provided is in allowed list of languages', async () => {
+    const allowedalang = Object.keys(localeLoaders)
+    const locale = 'ja-JP'
+    expect(allowedalang).toHaveLength(3)
+    expect(allowedalang).toContain(locale)
+    await loadLocale(locale)
   })
 })
 
--- libs/common/utils/src/locales.tsx
+++ libs/common/utils/src/locales.tsx
@@ -28,8 +28,6 @@ function localePath (locale: string) {
   return `locales/compiled/${locale}.json`
 }
 
-const allowedLang = ['en-US', 'de-DE', 'ja-JP']
-
 async function loadEnUS (): Promise<Messages> {
   const [base, proBase, translation] = await Promise.all([
     import('antd/lib/locale/en_US').then(result => result.default),
@@ -63,12 +61,13 @@ async function loadJp (): Promise<Messages> {
   return Object.assign({}, combine, flattenMessages(combine as unknown as NestedMessages))
 }
 
-const localeLoaders = {
+export const localeLoaders = {
   'en-US': loadEnUS,
   'de-DE': loadDe,
   'ja-JP': loadJp
 }
 
+const allowedLang = Object.keys(localeLoaders)
 type Key = keyof typeof localeLoaders
 const cache: Partial<Record<Key, Messages>> = {}
 export async function loadLocale (locale: Key) {
