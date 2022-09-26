79712a09c73da9468b3d2ed7fb55e669b617f2eb
cbokil <chinmay.bokil@ruckuswireless.com>
2022-09-26 13:47:37 +0800

    ACX-14728: added util method to calculate granularity

--- apps/analytics/src/utils.ts
+++ apps/analytics/src/utils.ts
@@ -7,3 +7,34 @@ export const getSparklineGranularity = (start: string, end: string): string => {
   if (duration >= 1) return 'PT15M'
   return 'PT180S'
 }
+export const calculateGranularity = (
+  start: string, end: string, minGranularity?: string
+): string => {
+  const interval = moment.duration(moment(end).diff(moment(start))).asHours()
+  let gran
+  switch (true) {
+    case interval > 24 * 7: // 7 days
+      gran = 'PT1H'
+      break
+    case interval > 24 * 3: // 3 days
+      gran = 'PT15M'
+      break
+    default:
+      gran = 'PT180S'
+  }
+  return getMaxGranularity(gran, minGranularity || '')
+}
+function getMaxGranularity (...durations: string[]): string {
+  let max = ''
+  for (let i = 0; i < durations.length; i++) {
+    const duration = durations[i]
+    if (!max) {
+      max = duration
+      continue
+    }
+    if (moment.duration(duration).asSeconds() > moment.duration(max).asSeconds()) {
+      max = durations[i]
+    }
+  }
+  return max
+}
