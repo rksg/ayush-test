54968792da4d05e97484eefd31aedd7ed399ecf7
wilson.loh <wilson.loh@ruckuswireless.com>
2022-09-26 14:39:42 +0800

    ACX-14546: test: update test snapshots

--- apps/analytics/src/pages/IncidentDetails/Details/ApWanthroughputLow.tsx
+++ apps/analytics/src/pages/IncidentDetails/Details/ApWanthroughputLow.tsx
@@ -1,2 +1,60 @@
-import { ApInfraTemplate } from './ApInfraTemplate'
-export default ApInfraTemplate
+import { useIntl } from 'react-intl'
+
+import {
+  calculateSeverity,
+  Incident,
+  useShortDescription
+} from '@acx-ui/analytics/utils'
+import { PageHeader, SeverityPill, GridRow, GridCol } from '@acx-ui/components'
+
+import { IncidentAttributes, Attributes } from '../IncidentAttributes'
+import { Insights }                       from '../Insights'
+
+import * as UI from './styledComponents'
+
+export const ApWanthroughputLow = (incident: Incident) => {
+  const { $t } = useIntl()
+  const attributeList = [
+    Attributes.ApImpactCount,
+    Attributes.IncidentCategory,
+    Attributes.IncidentSubCategory,
+    Attributes.Type,
+    Attributes.Scope,
+    Attributes.Duration,
+    Attributes.EventStartTime,
+    Attributes.EventEndTime
+  ]
+
+  return (
+    <>
+      <PageHeader
+        title={$t({ defaultMessage: 'Incident Details' })}
+        titleExtra={<SeverityPill severity={calculateSeverity(incident.severity)!} />}
+        breadcrumb={[
+          { text: $t({ defaultMessage: 'Incidents' }), link: '/analytics/incidents' }
+        ]}
+        subTitle={useShortDescription(incident)}
+      />
+      <GridRow>
+        <GridCol col={{ span: 4 }}>
+          <UI.FixedAutoSizer>
+            {({ width }) => (<div style={{ width }}>
+              <IncidentAttributes incident={incident} visibleFields={attributeList} />
+            </div>)}
+          </UI.FixedAutoSizer>
+        </GridCol>
+        <GridCol col={{ span: 20 }}>
+          <Insights incident={incident} />
+        </GridCol>
+        <GridCol col={{ offset: 4, span: 20 }}>
+          <div>Network Impact</div>
+        </GridCol>
+        <GridCol col={{ offset: 4, span: 20 }}>
+          <div>PoE Impact</div>
+        </GridCol>
+      </GridRow>
+    </>
+  )
+}
+
+export default ApWanthroughputLow
