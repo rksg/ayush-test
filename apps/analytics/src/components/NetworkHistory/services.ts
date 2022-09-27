2adc67e932ac29cc5b7655234a1ecb839a099a95
ki1003 <khairul.iman@ruckuswireless.com>
2022-09-27 13:09:28 +0800

    ACX-13983: revert granularity calc in network health

--- apps/analytics/src/components/NetworkHistory/services.ts
+++ apps/analytics/src/components/NetworkHistory/services.ts
@@ -1,9 +1,9 @@
 import { gql } from 'graphql-request'
+import moment  from 'moment'
 
 import { dataApi }                        from '@acx-ui/analytics/services'
 import {  incidentCodes, IncidentFilter } from '@acx-ui/analytics/utils'
 
-import { calculateGranularity } from '../../utils'
 
 export type NetworkHistoryData = {
   connectedClientCount: number[]
@@ -17,6 +17,12 @@ interface Response <TimeSeriesData> {
       timeSeries: TimeSeriesData
     }
   }
+}
+export const calcGranularity = (start: string, end: string): string => {
+  const duration = moment.duration(moment(end).diff(moment(start))).asHours()
+  if (duration > 24 * 7) return 'PT1H' // 1 hour if duration > 7 days
+  if (duration > 1) return 'PT30M'
+  return 'PT180S'
 }
 
 export const api = dataApi.injectEndpoints({
@@ -54,7 +60,7 @@ export const api = dataApi.injectEndpoints({
           path: payload.path,
           start: payload.startDate,
           end: payload.endDate,
-          granularity: calculateGranularity(payload.startDate, payload.endDate),
+          granularity: calcGranularity(payload.startDate, payload.endDate),
           severity: [{ gt: 0, lte: 1 }], // all severities
           code: payload.code ?? incidentCodes
         }
