37dd424dd26af9edd939a19102a06a32c7888947
jason.cheow <jason.cheow@ruckuswireless.com>
2022-10-02 23:15:31 +0800

    ACX-10793: Add Dropdown test for branch coverage

--- libs/common/components/src/components/Dropdown/index.spec.tsx
+++ libs/common/components/src/components/Dropdown/index.spec.tsx
@@ -1,13 +1,14 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent          from '@testing-library/user-event'
+import { Menu }           from 'antd'
 
 import { regionMenu } from './stories'
 
 import { Dropdown } from '.'
 
 describe('Dropdown', () => {
-  it('should render dropdown and handle events', async () => {
+  it('renders dropdown and handle events', async () => {
     const mockLog = jest.spyOn(console, 'log').mockImplementation(() => {})
     render(
       <Dropdown overlay={regionMenu}>{(selectedKeys) =>
@@ -20,5 +21,15 @@ describe('Dropdown', () => {
     await userEvent.click(screen.getByRole('menuitem', { name: 'Asia' }))
     expect(trigger).toHaveTextContent('Asia')
     expect(mockLog).toHaveBeenCalledWith(expect.objectContaining({ domEvent: expect.anything() }))
+  })
+
+  it('renders null if there are no selected items', () => {
+    const menu = <Menu items={[ { key: 'US', label: 'US' } ]} />
+    render(
+      <Dropdown overlay={menu}>{(selectedKeys) =>
+        <span data-testid='trigger'>{selectedKeys}</span>
+      }</Dropdown>
+    )
+    expect(screen.getByTestId('trigger')).toHaveTextContent('')
   })
 })
