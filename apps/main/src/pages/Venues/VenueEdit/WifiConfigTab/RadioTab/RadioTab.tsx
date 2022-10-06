9d86e41afa4e22488081082bd057e0956044100a
Guo, Yizhu <yguo1@ruckuswireless.com>
2022-10-06 18:11:09 +0800

    ACX-13772: Add StepsForm in Edit Venue RadioTab

--- apps/main/src/pages/Venues/VenueEdit/WifiConfigTab/RadioTab/RadioTab.tsx
+++ apps/main/src/pages/Venues/VenueEdit/WifiConfigTab/RadioTab/RadioTab.tsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react'
+
 import { useIntl } from 'react-intl'
 
-import { AnchorLayout, StepsForm } from '@acx-ui/components'
+import { AnchorLayout, showToast, StepsForm }    from '@acx-ui/components'
+import { useNavigate, useParams, useTenantLink } from '@acx-ui/react-router-dom'
+
+import { VenueEditContext } from '../..'
 
 import { ExternalAntenna } from './ExternalAntenna'
 import { RadioSettings }   from './RadioSettings'
 
 export function RadioTab () {
   const { $t } = useIntl()
+  const { venueId } = useParams()
+  const navigate = useNavigate()
+  const { editContextData, setEditContextData } = useContext(VenueEditContext)
+  const basePath = useTenantLink('/venues/')
   const wifiSettingTitle = $t({ defaultMessage: 'Wi-Fi Radio Settings' })
   const externalTitle = $t({ defaultMessage: 'External Antenna' })
   const anchorItems = [{
@@ -30,7 +39,41 @@ export function RadioTab () {
       </>
     )
   }]
+
+  const handleUpdateSetting = async (redirect?: boolean) => {
+    try {
+      setEditContextData({
+        ...editContextData,
+        oldData: editContextData?.newData,
+        isDirty: false
+      })
+      //  TODO: Call APIs
+      if (redirect) {
+        navigate({
+          ...basePath,
+          pathname: `${basePath.pathname}/${venueId}/venue-details/overview`
+        })
+      }
+    } catch {
+      showToast({
+        type: 'error',
+        content: $t({ defaultMessage: 'An error occurred' })
+      })
+    }
+  }
+
   return (
-    <AnchorLayout items={anchorItems} offsetTop={275} />
+    <StepsForm
+      onFinish={() => handleUpdateSetting(true)}
+      onCancel={() => navigate({
+        ...basePath,
+        pathname: `${basePath.pathname}/${venueId}/venue-details/overview`
+      })}
+      buttonLabel={{ submit: $t({ defaultMessage: 'Save' }) }}
+    >
+      <StepsForm.StepForm>
+        <AnchorLayout items={anchorItems} offsetTop={275} />
+      </StepsForm.StepForm>
+    </StepsForm>
   )
 }
