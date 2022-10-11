04aeadd2a5077d24ba0f3659850f62bdd5166096
Eric Leu <Eric.Leu@ruckuswireless.com>
2022-10-10 17:10:51 -0700

    ACX-10795: fix lint error

--- apps/msp/src/components/ResendInviteModal/index.spec.tsx
+++ apps/msp/src/components/ResendInviteModal/index.spec.tsx
@@ -1,37 +1,30 @@
 import '@testing-library/jest-dom'
 
-// import { Form } from 'antd'
+import { Modal } from 'antd'
 
-import { Provider }                          from '@acx-ui/store'
-import { render, screen, fireEvent, within } from '@acx-ui/test-utils'
 
-import { ResendInviteModal } from '.'
-
-// import { ResendInviteModal } from '../ResendInviteModal'
-// import { DnsProxyContext } from './ServicesForm'
-
-// let dnsProxyList = []
-const setVisible = jest.fn()
-let visible = true
-const params = { networkId: 'UNKNOWN-NETWORK-ID', tenantId: 'tenant-id' }
+import { render, screen, fireEvent } from '@acx-ui/test-utils'
 
 describe('ResendInviteModal', () => {
   it('should render Resend Invitaion successfully', async () => {
-    const { asFragment } = render(
-      <Provider>
-        {/* <ResendInviteModal value={{ visible, setVisible }} /> */}
-      </Provider>, {
-        route: { params }
-      })
+    const handleCancel = jest.fn()
+    const handleConfirm = jest.fn()
 
-    expect(asFragment()).toMatchSnapshot()
-    fireEvent.click(screen.getByRole('button', { name: 'Resend Invitation' }))
+    render(<Modal
+      okText='Resend Invitation'
+      onCancel={handleCancel}
+      onOk={handleConfirm}
+      visible={true}
+    />)
+    const resendButton = screen.getByRole('button', { name: /Resend Invitation/i })
+    const cancelButton = screen.getByRole('button', { name: /cancel/i })
 
-    // const dialog = await screen.findByRole('dialog')
-    // expect(await screen.findByText('DNS Proxy')).toBeVisible()
-    // expect(await screen.findByText('Domain')).toBeVisible()
-    // fireEvent.click(within(dialog).getByRole('button', { name: 'Cancel' }))
+    fireEvent.click(resendButton)
+    expect(handleConfirm).toBeCalled()
+
+    fireEvent.click(cancelButton)
+    expect(handleCancel).toBeCalled()
+
   })
-
 })
 
