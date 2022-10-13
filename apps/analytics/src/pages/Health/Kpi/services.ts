4ecd50de03a1a8d0f0d8f14d39b1d6bf7893047e
ki1003 <khairul.iman@ruckuswireless.com>
2022-10-13 11:10:38 +0800

    ACX-14969: add kpi config fetch query

--- apps/analytics/src/pages/Health/Kpi/services.ts
+++ apps/analytics/src/pages/Health/Kpi/services.ts
@@ -2,6 +2,7 @@ import { gql } from 'graphql-request'
 
 import { dataApi }                    from '@acx-ui/analytics/services'
 import { AnalyticsFilter, kpiConfig } from '@acx-ui/analytics/utils'
+import { NetworkPath }                from '@acx-ui/utils'
 
 import { calculateGranularity } from '../../../utils'
 
@@ -20,6 +21,29 @@ export type KPIHistogramResponse = {
 
 interface HistogramResponse <HistogramData> {
   histogram: HistogramData
+}
+
+type ThresholdResponse = {
+  threshold: {
+    value: number,
+    updatedBy: string,
+    updatedAt: string
+  }
+}
+
+type ThresholdPayload = {
+  path: NetworkPath,
+  name: keyof typeof kpiConfig
+}
+
+type ThresholdPermissionResponse = {
+  data: {
+    ThresholdMutationAllowed: boolean
+  }
+}
+
+type ThresholdPermissionPayload = {
+  path: NetworkPath
 }
 
 type KpiPayload = AnalyticsFilter & { kpi: string, threshold?: string }
@@ -104,5 +128,45 @@ export const histogramApi = dataApi.injectEndpoints({
   })
 })
 
+export const configApi = dataApi.injectEndpoints({
+  endpoints: (build) => ({
+    fetchThreshold: build.query<
+    ThresholdResponse,
+    ThresholdPayload
+    >({
+      query: (payload) => ({
+        document: gql`
+          query KPI($path: [HierarchyNodeInput], $name: String) {
+          threshold: KPIThreshold(name: $name, networkPath: $path) {
+            value
+            updatedBy
+            updatedAt
+          }
+        }`,
+        variables: {
+          path: payload.path,
+          name: payload.name
+        }
+      })
+    }),
+    fetchThresholdWithPermission: build.query<
+    ThresholdPermissionResponse,
+    ThresholdPermissionPayload
+    >({
+      query: (payload) => ({
+        document: gql`
+        query KPI($path: [HierarchyNodeInput]) {
+          ThresholdMutationAllowed(networkPath: $path)
+        }
+        `,
+        variables: {
+          path: payload.path
+        }
+      })
+    })
+  })
+})
+
 export const { useKpiTimeseriesQuery } = timeseriesApi
 export const { useKpiHistogramQuery } = histogramApi
+export const { useFetchThresholdQuery, useFetchThresholdWithPermissionQuery } = configApi
