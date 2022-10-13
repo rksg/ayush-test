ac4ffcc616c4ebc123c80e14ddc96e2d43662d90
wilson.loh <wilson.loh@ruckuswireless.com>
2022-10-13 12:10:29 +0800

    ACX-15598: chore: added dynamic barColors

--- apps/analytics/src/pages/IncidentDetails/TimeSeries/charts/RssDistributionChart.tsx
+++ apps/analytics/src/pages/IncidentDetails/TimeSeries/charts/RssDistributionChart.tsx
@@ -3,13 +3,21 @@ import { renderToString } from 'react-dom/server'
 import { useIntl }        from 'react-intl'
 import AutoSizer          from 'react-virtualized-auto-sizer'
 
-import { BarChartData }                            from '@acx-ui/analytics/utils'
-import { Card, DistributionChart, TooltipWrapper } from '@acx-ui/components'
+import { BarChartData, calculateSeverity, Incident } from '@acx-ui/analytics/utils'
+import { Card, DistributionChart, TooltipWrapper }   from '@acx-ui/components'
 
-import type { TimeSeriesChartProps }                    from '../types'
-import type { TooltipComponentFormatterCallbackParams } from 'echarts'
+import type { TimeSeriesChartProps, TimeSeriesChartResponse } from '../types'
+import type { TooltipComponentFormatterCallbackParams }       from 'echarts'
 
-export const rssDistributionScale = 5
+const rssDistributionScale = 5
+const rssThreshold = -80
+const incidentsColors = {
+  Grey: 'rgba(237,240,244,1)',
+  P1: 'rgb(207,43,32)',
+  P2: 'rgb(236,93,51)',
+  P3: 'rgb(244,180,68)',
+  P4: 'rgb(245,235,80)'
+}
 
 const rssDistributionChartQuery = () => gql`
   rssDistribution(scale: ${rssDistributionScale}) {
@@ -33,9 +41,19 @@ export const tooltipFormatter = (params: TooltipComponentFormatterCallbackParams
   )
 }
 
-export const RssDistributionChart = ({ data }: TimeSeriesChartProps) => {
+const barColors = (incident: Incident, data: TimeSeriesChartResponse) => {
+  const normalColor = 'rgb(156, 172, 185)'
+  const severityColor = incidentsColors[calculateSeverity(incident.severity)]
+
+  return data.rssDistribution?.map(data => (
+    data.rss <= rssThreshold ? severityColor : normalColor
+  )).reverse()
+}
+
+export const RssDistributionChart = ({ data, incident }: TimeSeriesChartProps) => {
   const { rssDistribution } = data
   const { $t } = useIntl()
+  const colors = barColors(incident, data)
 
   const xValue = $t({ defaultMessage: 'RSS Distribution' })
   const yValue = $t({ defaultMessage: 'Samples' })
@@ -53,8 +71,9 @@ export const RssDistributionChart = ({ data }: TimeSeriesChartProps) => {
           data={expectedResult}
           style={{ height, width }}
           grid={{ bottom: '10%', top: '5%' }}
-          title={$t({ defaultMessage: 'RSS (in dBm)' })}
+          xAxisName={$t({ defaultMessage: 'RSS (in dBm)' })}
           tooltipFormatter={tooltipFormatter}
+          barColors={colors}
         />
       )}
     </AutoSizer>
