fee275fc1d1e9b69254333b90ca4c900592e1a04
Asraf Ali <asraf.najmudeen@ruckuswireless.com>
2022-10-20 12:18:37 +0530

    ACX-14007 | chore: height unset for health widget

--- apps/analytics/src/components/VenueHealthWidget/styledComponents.ts
+++ apps/analytics/src/components/VenueHealthWidget/styledComponents.ts
@@ -1,6 +1,12 @@
 import { Space } from 'antd'
 import styled    from 'styled-components/macro'
 
+import { CardTitle } from '@acx-ui/components'
+
+export const Title = styled(CardTitle)`
+  height: unset;
+`
+
 export const Wrapper = styled(Space)`
   justify-content: left;
   width: 100%;
