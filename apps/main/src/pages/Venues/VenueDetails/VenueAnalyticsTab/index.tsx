a2a21dcfc4a6a0fa05f9a1ffb354b827130bd9f5
ki1003 <khairul.iman@ruckuswireless.com>
2022-11-01 17:41:51 +0800

    ACX-15376: wip: add skeleton to venue analytics tab

--- apps/main/src/pages/Venues/VenueDetails/VenueAnalyticsTab/index.tsx
+++ apps/main/src/pages/Venues/VenueDetails/VenueAnalyticsTab/index.tsx
@@ -1,8 +1,38 @@
+
+import { Tooltip } from 'antd'
 import { useIntl } from 'react-intl'
+
+import { Tabs }            from '@acx-ui/components'
+import { notAvailableMsg } from '@acx-ui/utils'
 
 export function VenueAnalyticsTab () {
   const { $t } = useIntl()
+
   return (
-    <>{$t({ defaultMessage: 'AI Analytics' })}</>
+    <Tabs
+      defaultActiveKey='incidents'
+      type='card'
+    >
+      <Tabs.TabPane tab={$t({ defaultMessage: 'Incidents' })} key='incidents'>
+        <>{$t({ defaultMessage: 'Incident Venue Table' })}</>
+      </Tabs.TabPane>
+      <Tabs.TabPane tab={$t({ defaultMessage: 'Health' })} key='health'>
+        <>{$t({ defaultMessage: 'Health Venue Table' })}</>
+      </Tabs.TabPane>
+      <Tabs.TabPane tab={<Tooltip title={$t(notAvailableMsg)}>
+        {$t({ defaultMessage: 'Configuration' })}
+      </Tooltip>}
+      key='configuration'
+      disabled>
+        <>{$t({ defaultMessage: 'Configuration Venue Table' })}</>
+      </Tabs.TabPane>
+      <Tabs.TabPane tab={<Tooltip title={$t(notAvailableMsg)}>
+        {$t({ defaultMessage: 'Reccomendations' })}
+      </Tooltip>}
+      key='reccomendations'
+      disabled>
+        <>{$t({ defaultMessage: 'Recommendations Venue Table' })}</>
+      </Tabs.TabPane>
+    </Tabs>
   )
 }
