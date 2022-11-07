c896891a234e687790fdd6bffcc20b0b785cd610
Jacky Chiu <jacky.chiu@ruckuswireless.com>
2022-11-07 12:25:14 +0800

    ACX-15525: Fix import error

--- apps/main/src/pages/Policies/index.tsx
+++ apps/main/src/pages/Policies/index.tsx
@@ -1,12 +1,12 @@
 import { useIntl } from 'react-intl'
 
-import { Loader }                      from '@acx-ui/components'
-import { Features, useSplitTreatment } from '@acx-ui/feature-toggle'
-import { Outlet }                      from '@acx-ui/react-router-dom'
+import { Loader }                 from '@acx-ui/components'
+import { Features, useIsSplitOn } from '@acx-ui/feature-toggle'
+import { Outlet }                 from '@acx-ui/react-router-dom'
 
 
 function Policies () {
-  const isPoliciesEnabled = useSplitTreatment(Features.POLICIES)
+  const isPoliciesEnabled = useIsSplitOn(Features.POLICIES)
   const { $t } = useIntl()
 
   if (!isPoliciesEnabled) {
