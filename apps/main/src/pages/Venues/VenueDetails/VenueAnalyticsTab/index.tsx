5de1b922c05148b126c479f5bad25db8b4ff9334
mickael.dehez <mickael.dehez@ruckuswireless.com>
2022-11-08 14:00:12 +0800

    ACX-15376: remove recommendation and config change tabs

--- apps/main/src/pages/Venues/VenueDetails/VenueAnalyticsTab/index.tsx
+++ apps/main/src/pages/Venues/VenueDetails/VenueAnalyticsTab/index.tsx
@@ -1,11 +1,10 @@
-import { Tooltip } from 'antd'
 import { useIntl } from 'react-intl'
 
 import { useAnalyticsFilter, AnalyticsFilter } from '@acx-ui/analytics/utils'
 import { Tabs }                                from '@acx-ui/components'
 import { useVenueDetailsHeaderQuery }          from '@acx-ui/rc/services'
 import { useLocation, useNavigate, useParams } from '@acx-ui/react-router-dom'
-import { defaultNetworkPath, notAvailableMsg } from '@acx-ui/utils'
+import { defaultNetworkPath }                  from '@acx-ui/utils'
 
 export function VenueAnalyticsTab () {
   const { $t } = useIntl()
@@ -15,13 +14,11 @@ export function VenueAnalyticsTab () {
   const { data } = useVenueDetailsHeaderQuery({ params })
   const { filters } = useAnalyticsFilter()
   const venueName = data?.venue.name
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const healthFilter = {
     ...filters,
     path: [{ type: 'zone', name: venueName }]
   } as AnalyticsFilter
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const incidentFilter = {
     ...filters,
@@ -47,33 +44,17 @@ export function VenueAnalyticsTab () {
     }
   }
 
-  return (
-    <Tabs
-      onChange={onTabChange}
-      activeKey={params.activeSubTab}
-      defaultActiveKey='incidents'
-      type='card'
-    >
-      <Tabs.TabPane tab={$t({ defaultMessage: 'Incidents' })} key='incidents'>
-        <>{$t({ defaultMessage: 'Incident Venue Table' })}</>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab={$t({ defaultMessage: 'Health' })} key='health'>
-        <>{$t({ defaultMessage: 'Health Venue Table' })}</>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab={<Tooltip title={$t(notAvailableMsg)}>
-        {$t({ defaultMessage: 'Configuration' })}
-      </Tooltip>}
-      key='configuration'
-      disabled>
-        <>{$t({ defaultMessage: 'Configuration Venue Table' })}</>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab={<Tooltip title={$t(notAvailableMsg)}>
-        {$t({ defaultMessage: 'Reccomendations' })}
-      </Tooltip>}
-      key='reccomendations'
-      disabled>
-        <>{$t({ defaultMessage: 'Recommendations Venue Table' })}</>
-      </Tabs.TabPane>
-    </Tabs>
-  )
+  return <Tabs
+    onChange={onTabChange}
+    activeKey={params.activeSubTab}
+    defaultActiveKey='incidents'
+    type='card'
+  >
+    <Tabs.TabPane tab={$t({ defaultMessage: 'Incidents' })} key='incidents'>
+      <>{$t({ defaultMessage: 'Incident Venue Table' })}</>
+    </Tabs.TabPane>
+    <Tabs.TabPane tab={$t({ defaultMessage: 'Health' })} key='health'>
+      <>{$t({ defaultMessage: 'Health Venue Table' })}</>
+    </Tabs.TabPane>
+  </Tabs>
 }
