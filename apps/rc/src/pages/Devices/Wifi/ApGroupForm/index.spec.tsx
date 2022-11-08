8ea66d48305d4473e3b6472f66a3ff10214cbb6c
Karen Yeh <karen.yeh@ruckuswireless.com>
2022-11-08 19:12:38 +0800

    ACX-16314: Implement the test case

--- apps/rc/src/pages/Devices/Wifi/ApGroupForm/index.spec.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApGroupForm/index.spec.tsx
@@ -3,7 +3,6 @@ import userEvent      from '@testing-library/user-event'
 import { Modal }      from 'antd'
 import { rest }       from 'msw'
 
-import { useSplitTreatment }            from '@acx-ui/feature-toggle'
 import { apApi, venueApi }              from '@acx-ui/rc/services'
 import { CommonUrlsInfo, WifiUrlsInfo } from '@acx-ui/rc/utils'
 import { Provider, store }              from '@acx-ui/store'
@@ -28,55 +27,12 @@ import {
 
 import { ApGroupForm } from '.'
 
-const validCoordinates = ['40.769141, -101.629519', '51.508506, -0.124915']
-const invalidCoordinates = '51.508506, -0.12xxxx'
 const mockedUsedNavigate = jest.fn()
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate
 }))
 
-async function fillInForm () {
-  fireEvent.change(screen.getByLabelText(/AP Name/), { target: { value: 'apname' } })
-  fireEvent.blur(screen.getByLabelText(/AP Name/))
-  await waitForElementToBeRemoved(await screen.findByRole('img', { name: 'loading' }))
-
-  fireEvent.change(screen.getByLabelText(/Serial Number/), { target: { value: '123456789000' } })
-  fireEvent.blur(screen.getByLabelText(/Serial Number/))
-  await waitForElementToBeRemoved(await screen.findByRole('img', { name: 'loading' }))
-}
-
-async function changeVenue () {
-  await waitForElementToBeRemoved(screen.queryByRole('img', { name: 'loader' }))
-  expect(await screen.findByText('Select venue...')).toBeVisible()
-  expect(screen.getByLabelText('AP Group')).toBeDisabled()
-  expect(await screen.findByText('GPS Coordinates')).toBeInTheDocument()
-
-  fireEvent.mouseDown(screen.getByLabelText(/Venue/))
-  await userEvent.click(await screen.getAllByText('My-Venue')[0])
-  await waitFor(() => screen.findByText('No group (inherit from Venue)'))
-  expect(screen.getByLabelText('AP Group')).not.toBeDisabled()
-}
-
-async function changeCoordinates (data, applyData) {
-  await userEvent.click(await screen.findByRole('button', { name: 'Change' }))
-  const dialog = await screen.findByRole('dialog')
-  await within(dialog).findByText('GPS Coordinates')
-  // eslint-disable-next-line testing-library/no-unnecessary-act
-  await act(() => {
-    fireEvent.change(within(dialog).getByTestId('coordinates-input'), { target: { value: data } })
-  })
-
-  if (!validCoordinates.includes(data)) {
-    expect(await screen.findByText('Please enter the valid GPS coordinates')).toBeVisible()
-    fireEvent.change(
-      within(dialog).getByTestId('coordinates-input'), { target: { value: validCoordinates[1] } }
-    )
-  }
-  if (applyData) {
-    await userEvent.click(await within(dialog).findByRole('button', { name: 'Apply' }))
-  }
-}
 
 describe('AP Form - Add', () => {
   const params = { tenantId: 'tenant-id', action: 'add' }
@@ -84,21 +40,38 @@ describe('AP Form - Add', () => {
     store.dispatch(apApi.util.resetApiState())
     store.dispatch(venueApi.util.resetApiState())
     initialize()
+
     mockServer.use(
       rest.post(CommonUrlsInfo.getVenuesList.url,
-        (_, res, ctx) => res(ctx.json(venuelist))),
-      rest.get(WifiUrlsInfo.getWifiCapabilities.url,
-        (_, res, ctx) => res(ctx.json(venueCaps))),
-      rest.post(CommonUrlsInfo.getApsList.url,
-        (_, res, ctx) => res(ctx.json(aplist))),
-      rest.get(CommonUrlsInfo.getApGroupList.url,
-        (_, res, ctx) => res(ctx.json(apGrouplist))),
-      rest.post(WifiUrlsInfo.addAp.url,
-        (_, res, ctx) => res(ctx.json(successResponse)))
+        (_, res, ctx) => res(ctx.json(venuelist)))
     )
+
+    mockServer.use(
+      rest.post(WifiUrlsInfo.getApGroupsList.url,
+        (_, res, ctx) => res(ctx.json(venuelist)))
+    )
+
+    // getVenueDefaultApGroup
   })
   afterEach(() => {
     Modal.destroyAll()
+  })
+  it('should render correctly', async () => {
+    const { asFragment } = render(<Provider><ApGroupForm /></Provider>, {
+      route: { params, path: '/:tenantId/devices/apgroups/:action' }
+    })
+
+    await waitForElementToBeRemoved(screen.queryByRole('img', { name: 'loader' }))
+    expect(asFragment()).toMatchSnapshot()
+    expect(await screen.findByText('Add AP Group')).toBeVisible()
+    expect(await screen.findByText('Select venue...')).toBeVisible()
+    expect(await screen.findByText('Group Member')).toBeVisible()
+    await userEvent.click(await screen.findByRole('button', { name: 'Cancel' }))
+    expect(mockedUsedNavigate).toHaveBeenCalledWith({
+      pathname: `/t/${params.tenantId}/devices/aps`,
+      hash: '',
+      search: ''
+    })
   })
 
 })
