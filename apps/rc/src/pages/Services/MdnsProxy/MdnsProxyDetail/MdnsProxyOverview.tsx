7267140ea08b6c4d1c8d633ff2acae3b60c50007
Jacky Chiu <jacky.chiu@ruckuswireless.com>
2022-11-14 15:08:44 +0800

    ACX-12778: Refactor code

--- apps/rc/src/pages/Services/MdnsProxy/MdnsProxyDetail/MdnsProxyOverview.tsx
+++ apps/rc/src/pages/Services/MdnsProxy/MdnsProxyDetail/MdnsProxyOverview.tsx
@@ -1,8 +1,8 @@
 import { Typography } from 'antd'
 import { useIntl }    from 'react-intl'
 
-import { Card, GridRow, GridCol } from '@acx-ui/components'
-import { MdnsProxyFormData }      from '@acx-ui/rc/utils'
+import { Card }              from '@acx-ui/components'
+import { MdnsProxyFormData } from '@acx-ui/rc/utils'
 
 export interface MdnsProxyOverviewProps {
   data: MdnsProxyFormData
@@ -11,16 +11,11 @@ export interface MdnsProxyOverviewProps {
 export function MdnsProxyOverview (props: MdnsProxyOverviewProps) {
   const { $t } = useIntl()
   const { data } = props
-  const { Paragraph } = Typography
 
   return (
     <Card>
-      <GridRow>
-        <GridCol col={{ span: 6 }}>
-          <Card.Title>{$t({ defaultMessage: 'Forwarding Rules' })}</Card.Title>
-          <Paragraph>{data.forwardingRules?.length}</Paragraph>
-        </GridCol>
-      </GridRow>
+      <Card.Title>{$t({ defaultMessage: 'Forwarding Rules' })}</Card.Title>
+      <Typography.Paragraph>{data.forwardingRules?.length}</Typography.Paragraph>
     </Card>
   )
 }
