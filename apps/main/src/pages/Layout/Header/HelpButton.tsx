288b5964583004c901124e641d924325602310ea
sean.huang <sean.huang@ruckuswireless.com>
2022-11-15 14:03:03 +0800

    ACX-17484: Help for this page Skeleton implementation

--- apps/main/src/pages/Layout/Header/HelpButton.tsx
+++ apps/main/src/pages/Layout/Header/HelpButton.tsx
@@ -1,40 +1,167 @@
-import React from 'react'
+import { useState } from 'react'
+import { ExportOutlined }             from '@ant-design/icons'
+import { Typography, Input, Divider } from 'antd'
+import { useIntl }                    from 'react-intl'
 
-import { LogoutOutlined } from '@ant-design/icons'
-import { Menu, Dropdown } from 'antd'
-import { useIntl }        from 'react-intl'
+import { LayoutUI, GridRow, GridCol, Button }      from '@acx-ui/components'
+import { QuestionMarkCircleSolid, SearchOutlined } from '@acx-ui/icons'
+import { useParams }                               from '@acx-ui/react-router-dom'
 
+import About from './About'
 import {
-  LayoutUI
-}                        from '@acx-ui/components'
-import {  QuestionMarkCircleSolid } from '@acx-ui/icons'
+  RowWrapper,
+  StyledPopover,
+  Description,
+  MenuLink,
+  MenuRowContainer,
+  LinkItem,
+  SearchRow,
+  LinkCol,
+  MenuIcon } from './styledComponents'
 
-const HelpButton = () => {
+
+
+
+export default function HelpHeaderButton () {
+  const params = useParams()
   const { $t } = useIntl()
 
-  const menuHeaderDropdown = (
-    <Menu selectedKeys={[]} onClick={()=>{}}>
-      <Menu.Item key='center'>
-        {$t({ defaultMessage: 'User Profile' })}
-      </Menu.Item>
+  const [aboutModalState, setIsModalOpen] = useState(false)
 
-      <Menu.Item key='settings'>
-        {$t({ defaultMessage: 'Change Password' })}
-      </Menu.Item>
+  const content = (<div style={{ width: 500, paddingBottom: 20 }}>
+    <GridRow style={{ padding: 25 }}>
+      <GridCol col={{ span: 24 }}>
+        <Typography.Title level={3}>
+          {$t({ defaultMessage: 'Hellp & Support' })}
+        </Typography.Title>
+      </GridCol>
+    </GridRow>
+    <RowWrapper>
+      <SearchRow>
+        <SearchOutlined/>
+        <Input
+          style={{ width: '90%' }}
+          placeholder={$t(({ defaultMessage: 'Search...' }))}
+          bordered={false}
+        />
+      </SearchRow>
+    </RowWrapper>
+    <MenuRowContainer>
+      <GridCol col={{ span: 24 }} style={{ paddingLeft: 0 }}>
+        <Typography.Title level={4} style={{ fontWeight: 600, marginBottom: 0 }}>
+          {$t({ defaultMessage: 'Help For This Page' })}
+        </Typography.Title>
+        <Description>{$t({ defaultMessage: 'Contextual Help specific to this page' })}</Description>
+      </GridCol>
+      <Divider style={{ margin: 0 }}/>
+    </MenuRowContainer>
+    <MenuRowContainer>
+      <GridCol col={{ span: 24 }}>
+        <Typography.Title level={4} style={{ fontWeight: 600, marginBottom: 0 }}>
+          {$t({ defaultMessage: 'Get Started Tour' })}
+        </Typography.Title>
+        <Description>{$t({ defaultMessage: 'Walk through the key functionalities' })}</Description>
+      </GridCol>
+      <Divider style={{ margin: 0 }}/>
+    </MenuRowContainer>
+    <MenuRowContainer>
+      <GridCol col={{ span: 24 }}>
+        <Typography.Title level={4} style={{ fontWeight: 600, marginBottom: 0 }}>
+          {$t({ defaultMessage: 'Contact Support' })}
+        </Typography.Title>
+        <Description>{$t({ defaultMessage: 'We are here to help you' })}</Description>
+      </GridCol>
+      <Divider style={{ margin: 0 }}/>
+    </MenuRowContainer>
 
-      <Menu.Divider />
 
-      <Menu.Item key='logout'>
-        <LogoutOutlined />
-        {$t({ defaultMessage: 'Log out' })}
-      </Menu.Item>
-    </Menu>
+    <LinkItem paddingTop={'25'}>
+      <GridCol col={{ span: 5 }} >
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'How to Videos' })}</MenuLink>
+        </Button>
+      </GridCol>
+    </LinkItem>
+    <LinkItem>
+      <LinkCol>
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'Dictionary' })}</MenuLink>
+        </Button>
+      </LinkCol>
+    </LinkItem>
+    <LinkItem>
+      <LinkCol>
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'Documentation' })}</MenuLink>
+        </Button>
+      </LinkCol>
+    </LinkItem>
+
+    <LinkItem>
+      <LinkCol>
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'Knowledge Articles' })}</MenuLink>
+        </Button>
+      </LinkCol>
+    </LinkItem>
+    <LinkItem>
+      <LinkCol>
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'Forums' })}</MenuLink>
+        </Button>
+      </LinkCol>
+    </LinkItem>
+    <LinkItem>
+      <Divider style={{ margin: 0 }}/>
+      <LinkCol>
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'My Open Case' })}</MenuLink>
+        </Button>
+      </LinkCol>
+      <Divider style={{ margin: 0 }}/>
+    </LinkItem>
+    <LinkItem>
+      <Divider style={{ margin: 0 }}/>
+      <LinkCol>
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'Privacy' })}</MenuLink>
+        </Button>
+      </LinkCol>
+    </LinkItem>
+    <LinkItem>
+      <LinkCol>
+        <Button type='link' onClick={() => { }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'New Features' })}</MenuLink>
+        </Button>
+      </LinkCol>
+    </LinkItem>
+    <LinkItem>
+      <LinkCol>
+        <Button type='link' onClick={() => { setIsModalOpen(true) }}>
+          <MenuIcon/>
+          <MenuLink>{$t({ defaultMessage: 'About RUCKUS One' })}</MenuLink>
+        </Button>
+      </LinkCol>
+    </LinkItem>
+  </div>
   )
-  return (
-    <Dropdown overlay={menuHeaderDropdown} placement='bottomLeft'>
-      <LayoutUI.ButtonSolid icon={<QuestionMarkCircleSolid />} />
-    </Dropdown>
-  )
+
+  const HelpHeaderButton = () => {
+    return <>
+      <StyledPopover placement='topRight' content={content} trigger='click'>
+        <LayoutUI.ButtonSolid icon={<QuestionMarkCircleSolid />} />
+      </StyledPopover>
+      <About modalState={aboutModalState} setIsModalOpen={setIsModalOpen}/>
+    </>
+  }
+
+  return <HelpHeaderButton />
 }
-
-export default HelpButton
--- apps/main/src/pages/Layout/Header/styledComponents.tsx
+++ apps/main/src/pages/Layout/Header/styledComponents.tsx
@@ -1,6 +1,10 @@
-import styled from 'styled-components/macro'
+import { ExportOutlined }                      from '@ant-design/icons'
+import { Popover as AntPopover, PopoverProps, Modal } from 'antd'
+import styled                                  from 'styled-components/macro'
 
-import { LayoutUI } from '@acx-ui/components'
+
+import { LayoutUI, GridRow, GridCol } from '@acx-ui/components'
+
 
 export const UserNameButton = styled(LayoutUI.ButtonSolid)`
     width: 32px;
@@ -9,4 +13,99 @@ export const UserNameButton = styled(LayoutUI.ButtonSolid)`
     justify-content: center;
     font-family: 'Roboto';
     font-size: 14px;
+`
+
+export const Popover = styled(AntPopover)`
+  &.ant-popover
+  > .ant-popover-content
+    > .ant-popover-arrow {
+      display: none;
+    }
+    > .ant-popover-inner-content {
+      padding: 0px;
+    }
+  }
+`
+
+const CustomPopover = ( props:PopoverProps ) => (
+  <Popover overlayClassName={props.className}
+    getPopupContainer={triggerNode => triggerNode.parentNode as HTMLElement}
+    {...props} >
+    {props.children}
+  </Popover>
+)
+
+export const StyledPopover = styled(CustomPopover)`
+  .ant-popover-arrow {
+    display: none;
+  }
+  .ant-popover-inner-content {
+    padding: 0px;
+  }
+`
+
+export const RowWrapper = styled.div`
+  padding: 20px;
+  background: var(--acx-neutrals-20);
+  display: flex;
+  justify-content: center;
+`
+
+export const Description = styled.div`
+  color: var(--acx-neutrals-70);
+  margin-top: 4px;
+  font-size: var(--acx-subtitle-5-font-size);
+`
+export const MenuLink = styled.div`
+  color: var(--acx-neutrals-80);
+  font-size: var(--acx-subtitle-5-font-size);
+`
+
+export const MenuRowContainer = styled(GridRow)`
+  padding: 25px 25px 0px 25px;
+  margin-left: 0px !important;
+  margin-right: 0px !important;
+  :hover {
+    background-color: var(--acx-accents-orange-10);
+  }
+`
+
+export const LinkCol = styled(GridCol).attrs({ col: { span: 5 } })`
+  align-items: flex-start;
+  `
+export const LinkItem = styled(GridRow)<{ paddingTop?: string }>`
+  padding: ${props => `${props.paddingTop||0}px 25px 0px 25px`};
+  display: flex;
+  margin-left: 0px !important;
+  margin-right: 0px !important;
+  align-items: end;
+  :hover {
+    background-color: var(--acx-accents-orange-10);
+  }
+`
+
+export const SearchRow = styled(GridRow)`
+  width: 100%;
+  align-items: center;
+  display: flex;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 6px;
+  background: var(--acx-neutrals-10);
+`
+
+export const MenuIcon = styled(ExportOutlined)`
+  font-size: 17px;
+  color: var(--acx-primary-black)
+`
+
+export const AboutModal = styled(Modal)`
+  .ant-modal-content {
+    background: var(--acx-neutrals-80);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+    border-radius: 8px;
+  }
 `
