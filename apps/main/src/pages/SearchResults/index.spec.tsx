40703f9fcb48850542f947dca6127c8311bf9bfb
ny1012 <Nishanth.Yerramsetty@ruckuswireless.com>
2022-11-15 23:16:14 +0800

    ACX-16662: Add test cases

---
+++ apps/main/src/pages/SearchResults/index.spec.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom'
+
+import { Provider }       from '@acx-ui/store'
+import { render, screen } from '@acx-ui/test-utils'
+
+import SearchResults from '.'
+
+const params = { searchString: 'dGVzdA==' }
+
+describe('Search Results', () => {
+  it('should render correctly', async () => {
+    const { baseElement } = render(
+      <Provider>
+        <SearchResults />
+      </Provider>,
+      { route: { params } }
+    )
+    expect(baseElement).toHaveTextContent('Search Results')
+  })
+
+  it('should decode search string correctly', async () => {
+    render(
+      <Provider>
+        <SearchResults />
+      </Provider>,
+      { route: { params } }
+    )
+    expect(screen.getByText('Search Results for "test" (1)')).toBeVisible()
+  })
+})
