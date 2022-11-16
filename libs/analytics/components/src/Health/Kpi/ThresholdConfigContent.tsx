94f14f91bdab69e604e2874d16439ead6801b57b
ki1003 <khairul.iman@ruckuswireless.com>
2022-11-16 18:11:24 +0800

    ACX-17388: fix: replaced onClick to onMouseDown to remove focus onApply

--- libs/analytics/components/src/Health/Kpi/ThresholdConfigContent.tsx
+++ libs/analytics/components/src/Health/Kpi/ThresholdConfigContent.tsx
@@ -1,3 +1,5 @@
+import { MouseEventHandler } from 'react'
+
 import { useIntl,  defineMessage, MessageDescriptor } from 'react-intl'
 
 import { Button, DisabledButton } from '@acx-ui/components'
@@ -41,7 +43,10 @@ function ThresholdConfig ({
   const isDisabled = !Boolean(canSave)
   const disabledMsg = $t(getDisabledToolTip(isNetwork))
   const resetCallback = () => onReset()
-  const applyCallback = () => onApply()
+  const applyCallback: MouseEventHandler<HTMLElement> = (e) => {
+    e.preventDefault()
+    onApply()
+  }
   return (
     <UI.HistogramConfig>
       <UI.HistogramSpanContent>
@@ -79,7 +84,7 @@ function ThresholdConfig ({
             style={{ width: 70 }}
             size='small'
             type='secondary'
-            onClick={applyCallback}
+            onMouseDown={applyCallback}
           >
             {$t(thresholdDescText.applyBtn)}
           </Button>
