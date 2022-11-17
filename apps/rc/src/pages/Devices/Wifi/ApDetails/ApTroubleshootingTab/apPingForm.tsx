e0aadf77cb861c1f7f9e58a30aabf33d9b79b443
Karen Yeh <karen.yeh@ruckuswireless.com>
2022-11-17 18:44:33 +0800

    ACX-17237: Implement the ping form

--- apps/rc/src/pages/Devices/Wifi/ApDetails/ApTroubleshootingTab/apPingForm.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApDetails/ApTroubleshootingTab/apPingForm.tsx
@@ -1,33 +1,35 @@
-import { useRef } from 'react'
-import React      from 'react'
+import { useRef, useState } from 'react'
+import React                from 'react'
 
-import { Row, Col, Form, Input, FormInstance, Button } from 'antd'
-import TextArea                                        from 'antd/lib/input/TextArea'
-import { useIntl }                                     from 'react-intl'
-import { useParams, useNavigate }                      from 'react-router-dom'
 
-import { Loader, PageHeader, StepsForm, StepsFormInstance, Tabs, Tooltip } from '@acx-ui/components'
-import { QuestionMarkCircleOutlined }                                      from '@acx-ui/icons'
-import { usePingApMutation }                                               from '@acx-ui/rc/services'
-import { WifiNetworkMessages, WifiTroubleshootingMessages }                from '@acx-ui/rc/utils'
-import { useTenantLink }                                                   from '@acx-ui/react-router-dom'
+import { Row, Col, Form, Input, Button } from 'antd'
+import TextArea                          from 'antd/lib/input/TextArea'
+import _                                 from 'lodash'
+import { useIntl }                       from 'react-intl'
+import { useParams }                     from 'react-router-dom'
+
+import { Loader, showToast, Tooltip }  from '@acx-ui/components'
+import { QuestionMarkCircleOutlined }  from '@acx-ui/icons'
+import { usePingApMutation }           from '@acx-ui/rc/services'
+import { WifiTroubleshootingMessages } from '@acx-ui/rc/utils'
 
 export function ApPingForm () {
   const { $t } = useIntl()
-  const params = useParams()
-  const navigate = useNavigate()
   const { tenantId, serialNumber } = useParams()
-  const basePath = useTenantLink(`/devices/aps/${serialNumber}/details/troubleshooting/`)
-
-  const [pingAp] = usePingApMutation()
+  const [form] = Form.useForm()
+  const [isValid, setIsValid] = useState(false)
+  const [pingAp, { isLoading: isPingingAp }] = usePingApMutation()
   const handlePingAp = async () => {
     try {
       const payload = {
-        targetHost: '1.1.1.1'
+        targetHost: form.getFieldValue('name')
       }
 
-      await pingAp({ params: { tenantId, serialNumber }, payload }).unwrap()
-      // navigate(`${basePath.pathname}/aps`, { replace: true })
+      const pingApResult = await pingAp({ params: { tenantId, serialNumber }, payload }).unwrap()
+      if (pingApResult) {
+
+        form.setFieldValue('result', _.get(pingApResult, 'response.response'))
+      }
     } catch {
       showToast({
         type: 'error',
@@ -36,18 +38,24 @@ export function ApPingForm () {
     }
   }
 
+  const onChangeForm = function () {
+    form.validateFields()
+      .then(() => {
+        setIsValid(true)
+      })
+      .catch(() => {
 
+        setIsValid(false)
+      })
+  }
 
-  const [form] = Form.useForm()
   return <Form
     form={form}
-
     layout='vertical'
-
+    onChange={onChangeForm}
   >
     <Row gutter={20}>
       <Col span={8}>
-
         <Form.Item
           name='name'
           label={<>
@@ -60,14 +68,16 @@ export function ApPingForm () {
             </Tooltip>
           </>}
           rules={[
+            { required: true }
           ]}
           validateFirst
-          hasFeedback
+          // hasFeedback
           children={<Input />}
         />
         <Form.Item wrapperCol={{ offset: 0, span: 16 }}>
           <Button type='primary'
             htmlType='submit'
+            disabled={!isValid}
             onClick={handlePingAp}>
             Run
           </Button>
@@ -75,11 +85,12 @@ export function ApPingForm () {
       </Col>
     </Row>
     <Loader states={[{
-      isLoading: false//venuesList.isLoading
+      isLoading: isPingingAp
     }]}>
-      <Form.Item>
+      <Form.Item
+        name='result'>
         <TextArea
-          style={{ resize: 'none' }}
+          style={{ resize: 'none', height: '300px' }}
           autoSize={false}
           readOnly={true}
         />
@@ -89,8 +100,5 @@ export function ApPingForm () {
   </Form>
 
 
-}
-function showToast (arg0: { type: string; content: string }) {
-  throw new Error('Function not implemented.')
 }
 
