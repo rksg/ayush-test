98cf55a96dda5a1667707b1cd17c4746c26eae6a
Li, Roil <Roil.Li@ruckuswireless.com>
2022-11-21 15:43:00 +0800

    ACX-15002: Add i18n into EdgeStatusLight

--- apps/rc/src/pages/Devices/Edge/EdgeStatusLight.tsx
+++ apps/rc/src/pages/Devices/Edge/EdgeStatusLight.tsx
@@ -1,3 +1,5 @@
+import { useIntl } from 'react-intl'
+
 import { StatusLight }    from '@acx-ui/rc/components'
 import { EdgeStatusEnum } from '@acx-ui/rc/utils'
 
@@ -6,30 +8,33 @@ type EdgeStatusLightProps = {
   data: string
 }
 
-const EdgeStatusLightConfig = {
-  [EdgeStatusEnum.REQUIRES_ATTENTION]: {
-    color: '--acx-semantics-red-50',
-    text: EdgeStatusEnum.REQUIRES_ATTENTION
-  },
-  [EdgeStatusEnum.TEMPORARILY_DEGRADED]: {
-    color: '--acx-semantics-yellow-50',
-    text: EdgeStatusEnum.TEMPORARILY_DEGRADED
-  },
-  [EdgeStatusEnum.IN_SETUP_PHASE]: {
-    color: '--acx-neutrals-50',
-    text: EdgeStatusEnum.IN_SETUP_PHASE
-  },
-  [EdgeStatusEnum.OFFLINE]: {
-    color: '--acx-neutrals-50',
-    text: EdgeStatusEnum.OFFLINE
-  },
-  [EdgeStatusEnum.OPERATIONAL]: {
-    color: '--acx-semantics-green-50',
-    text: EdgeStatusEnum.OPERATIONAL
-  }
-}
-
 export const EdgeStatusLight = (props: EdgeStatusLightProps) => {
+
+  const { $t } = useIntl()
+
+  const EdgeStatusLightConfig = {
+    [EdgeStatusEnum.REQUIRES_ATTENTION]: {
+      color: '--acx-semantics-red-50',
+      text: $t({ defaultMessage: 'Requires Attention' })
+    },
+    [EdgeStatusEnum.TEMPORARILY_DEGRADED]: {
+      color: '--acx-semantics-yellow-50',
+      text: $t({ defaultMessage: 'Temporarily degraded' })
+    },
+    [EdgeStatusEnum.IN_SETUP_PHASE]: {
+      color: '--acx-neutrals-50',
+      text: $t({ defaultMessage: 'In Setup Phase' })
+    },
+    [EdgeStatusEnum.OFFLINE]: {
+      color: '--acx-neutrals-50',
+      text: $t({ defaultMessage: 'Offline' })
+    },
+    [EdgeStatusEnum.OPERATIONAL]: {
+      color: '--acx-semantics-green-50',
+      text: $t({ defaultMessage: 'Operational' })
+    }
+  }
+
   return (
     <StatusLight config={EdgeStatusLightConfig} data={props.data} />
   )
