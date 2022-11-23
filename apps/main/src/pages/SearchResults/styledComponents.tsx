7b1cab4424000e8764deb97ca9b0dc83c1706709
ki1003 <khairul.iman@ruckuswireless.com>
2022-11-24 01:19:06 +0800

    ACX-18030: fix: collapse in search alignment

--- apps/main/src/pages/SearchResults/styledComponents.tsx
+++ apps/main/src/pages/SearchResults/styledComponents.tsx
@@ -2,7 +2,6 @@ import { Collapse as AntCollapse } from 'antd'
 import styled                      from 'styled-components'
 
 export const Collapse = styled(AntCollapse)`
-  border-bottom: 0px;
 
   .ant-collapse-header-text {
     padding-left: 10px;
