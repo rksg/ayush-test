9d7e4822c238388a2fb09ee23f7f5edd7e0bc981
ki1003 <khairul.iman@ruckuswireless.com>
2022-11-24 17:17:50 +0800

    ACX-18030: fix: search result styling to match ds

--- apps/main/src/pages/SearchResults/styledComponents.tsx
+++ apps/main/src/pages/SearchResults/styledComponents.tsx
@@ -4,15 +4,23 @@ import styled                      from 'styled-components'
 export const Collapse = styled(AntCollapse)`
 
   .ant-collapse-header-text {
-    padding-left: 10px;
+    padding-left: 6px;
     padding-bottom: 10px;
   }
 
   .ant-collapse-content-box {
     padding: 0px;
     padding-top: 0px;
-    padding-bottom: 5px;
+    padding-bottom: 2px;
   }
 `
 
-export const Panel = styled(AntCollapse.Panel)``
+export const Panel = styled(AntCollapse.Panel)`
+`
+
+export const StyledPageHeader = styled.div`
+  .ant-page-header-heading {
+    margin-top: var(--acx-content-horizontal-space);
+    margin-bottom: var(--acx-content-horizontal-space);
+  }
+`
