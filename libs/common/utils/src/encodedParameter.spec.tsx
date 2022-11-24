50c7261cca50871c9734429dbf61af1969a1b3af
mickael.dehez <mickael.dehez@ruckuswireless.com>
2022-11-24 17:20:22 +0800

    ACX-17727: test encodedParameter handles old params

--- libs/common/utils/src/encodedParameter.spec.tsx
+++ libs/common/utils/src/encodedParameter.spec.tsx
@@ -1,5 +1,5 @@
-import { renderHook }    from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
+import { renderHook }                  from '@testing-library/react'
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
 
 import { fixedEncodeURIComponent, useEncodedParameter } from './encodedParameter'
 
@@ -20,5 +20,19 @@ describe('useEncodedParameter', () => {
     expect(result.current.read()).toBe(null)
     result.current.write({ value: 'value1' })
     expect(result.current.read()).toEqual({ value: 'value1' })
+  })
+  it('does not crash the page having  old parameters', () => {
+    const { result } = renderHook(
+      () => useEncodedParameter<{ value: string }>('name2'),
+      {
+        wrapper: ({ children }) => <MemoryRouter
+          initialEntries={[{
+            pathname: '/incidents',
+            search: '?name2=eyJyYW5nZSI6Ikxhc3QgNyBEYXlzIiwiaW5pdGlhdGVkIjoxNjY5MjgxNDgxODM1fQ'
+          }]}
+        >{children}</MemoryRouter>
+      }
+    )
+    expect(result.current.read()).toBe(null)
   })
 })
