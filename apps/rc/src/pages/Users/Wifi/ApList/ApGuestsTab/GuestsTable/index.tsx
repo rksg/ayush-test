be0a0136f5162f7dd0ced7832b1a12b1e524a98c
Karen Yeh <karen.yeh@ruckuswireless.com>
2022-11-27 17:54:18 +0800

    ACX-18624: Implement the drawer for guest client

--- apps/rc/src/pages/Users/Wifi/ApList/ApGuestsTab/GuestsTable/index.tsx
+++ apps/rc/src/pages/Users/Wifi/ApList/ApGuestsTab/GuestsTable/index.tsx
@@ -1,8 +1,8 @@
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 
 import { useIntl } from 'react-intl'
 
-import { Subtitle, Tooltip }                                              from '@acx-ui/components'
+import { Alert, Subtitle, Tooltip }                                              from '@acx-ui/components'
 import { Button, PageHeader, Table, TableProps, Loader, showActionModal } from '@acx-ui/components'
 import { useGetGuestsListQuery }                                          from '@acx-ui/rc/services'
 import {
@@ -16,129 +16,9 @@ import {
 import { TenantLink, useNavigate, useTenantLink, useParams } from '@acx-ui/react-router-dom'
 import { getIntl }                                           from '@acx-ui/utils'
 import moment        from 'moment-timezone'
+import { Divider, Drawer, Form } from 'antd'
 
 const disabledType = [NetworkTypeEnum.DPSK, NetworkTypeEnum.CAPTIVEPORTAL]
-
-function getCols (intl: ReturnType<typeof useIntl>) {
-  const columns: TableProps<Guest>['columns'] = [
-    {
-      key: 'creationDate',
-      title: intl.$t({ defaultMessage: 'Created' }),
-      dataIndex: 'creationDate',
-      sorter: false,
-      defaultSortOrder: 'ascend',
-      // render: (data, row) =>
-      //   <TenantLink to={`/networks/${row.id}/network-details/aps`}>{data}</TenantLink>
-    },
-    {
-      key: 'name',
-      title: intl.$t({ defaultMessage: 'Name' }),
-      dataIndex: 'name',
-      sorter: true,
-      defaultSortOrder: 'ascend',
-      // render: function (data, row) {
-      //   if(disabledType.indexOf(row.nwSubType as NetworkTypeEnum) > -1){
-      //     return data
-      //   }else{
-      //     return (
-      //       <TenantLink to={`/networks/${row.id}/network-details/aps`}>{data}</TenantLink>
-      //     )
-      //   }
-      // }
-    },
-    {
-      key: 'mobilePhoneNumber',
-      title: intl.$t({ defaultMessage: 'Phone' }),
-      dataIndex: 'mobilePhoneNumber',
-      sorter: true
-    },
-    {
-      key: 'emailAddress',
-      title: intl.$t({ defaultMessage: 'E-mail' }),
-      dataIndex: 'emailAddress',
-      sorter: true,
-    },
-    {
-      key: 'guestType',
-      title: intl.$t({ defaultMessage: 'Type' }),
-      dataIndex: 'guestType',
-      sorter: true,
-      render: function (data) {
-        switch (data) {
-          case GuestTypesEnum.MANAGED:
-            return 'Managed'
-          case GuestTypesEnum.SELF_SIGN_IN:
-            return 'Self Sign In';
-          case GuestTypesEnum.HOST_GUEST:
-            return 'Self Sign In'
-          default:
-            return data
-        }
-      }
-    },
-    {
-      key: 'ssid',
-      title: intl.$t({ defaultMessage: 'Allowed Network' }),
-      dataIndex: 'ssid',
-      sorter: true,
-      render: function (data, row) {
-        const hasGuestManagerRole = false // from userProfile()
-        if (row.networkId && !hasGuestManagerRole) {
-          return (
-            <TenantLink to={`/networks/${row.networkId}/network-details/aps`}>{data}</TenantLink>
-          )
-        } else if (row.networkId && hasGuestManagerRole) {
-          return data
-        } else {
-          return 'None'
-        }
-      }
-    },
-    {
-      key: 'expiryDate',
-      title: intl.$t({ defaultMessage: 'Expires' }),
-      dataIndex: 'expiryDate',
-      sorter: true,
-      render: function (data, row) {
-        if (data && data !== '0') {
-          return moment(row.expiryDate).format('DD/MM/YYYY HH:mm')
-        } else if (!data || data === '0') {
-          let result = ''
-          if (row.passDurationHours) {
-            if (row.passDurationHours > 24) {
-              const days = Math.floor(row.passDurationHours / 24)
-              const hours = row.passDurationHours % 24;
-              const dayText = days > 1 ? ' days' : ' day';
-              const hourText = hours > 1 ? ' hours' : ' hour';
-              result = `${days}${dayText} ${hours}${hourText}`;
-            } else {
-              result = row.passDurationHours + (row.passDurationHours === 1 ? ' hour' : ' hours');
-            }
-          }
-          return `${result} since first login`
-        }
-        return ''
-      }
-    },
-    {
-      key: 'guestStatus',
-      title: intl.$t({ defaultMessage: 'Status' }),
-      dataIndex: 'guestStatus',
-      sorter: false
-    }
-  ]
-  return columns
-}
-
-
-const transformVLAN = (row: Network) => {
-  const { $t } = getIntl()
-  if (row.vlanPool) {
-    const vlanPool = row.vlanPool
-    return $t({ defaultMessage: 'VLAN Pool: {poolName}' }, { poolName: vlanPool?.name ?? '' })
-  }
-  return $t({ defaultMessage: 'VLAN-{id}' }, { id: row.vlan })
-}
 
 const defaultPayload = {
   searchString: '',
@@ -177,23 +57,166 @@ export default function GuestsTable () {
     })
     const { tenantId } = useParams()
 
+    const notificationMessage = $t({
+      defaultMessage: 'Guests cannot be added since there are no guest networks'
+    })
+
+    const [visible, setVisible] = useState(false)
+
     // this.subscriptions.push(this.userProfileService.getUserProfile(false).subscribe(user => {
     //   if (this.userProfileService.userHasRole(user, 'OFFICE_ADMIN')) {
     //     this.hasGuestManagerRole = true;
     //   }
     // }));
 
+    const columns: TableProps<Guest>['columns'] = [
+      {
+        key: 'creationDate',
+        title: $t({ defaultMessage: 'Created' }),
+        dataIndex: 'creationDate',
+        sorter: false,
+        defaultSortOrder: 'ascend',
+        render: (data, row) =>
+        <Button
+        type='link'
+        onClick={() => {
+          setVisible(true)
+        }}
+      >
+        {data}
+      </Button>
+      },
+      {
+        key: 'name',
+        title: $t({ defaultMessage: 'Name' }),
+        dataIndex: 'name',
+        sorter: true,
+        defaultSortOrder: 'ascend',
+        // render: function (data, row) {
+        //   if(disabledType.indexOf(row.nwSubType as NetworkTypeEnum) > -1){
+        //     return data
+        //   }else{
+        //     return (
+        //       <TenantLink to={`/networks/${row.id}/network-details/aps`}>{data}</TenantLink>
+        //     )
+        //   }
+        // }
+      },
+      {
+        key: 'mobilePhoneNumber',
+        title: $t({ defaultMessage: 'Phone' }),
+        dataIndex: 'mobilePhoneNumber',
+        sorter: true
+      },
+      {
+        key: 'emailAddress',
+        title: $t({ defaultMessage: 'E-mail' }),
+        dataIndex: 'emailAddress',
+        sorter: true,
+      },
+      {
+        key: 'guestType',
+        title: $t({ defaultMessage: 'Type' }),
+        dataIndex: 'guestType',
+        sorter: true,
+        render: function (data) {
+          switch (data) {
+            case GuestTypesEnum.MANAGED:
+              return 'Managed'
+            case GuestTypesEnum.SELF_SIGN_IN:
+              return 'Self Sign In';
+            case GuestTypesEnum.HOST_GUEST:
+              return 'Self Sign In'
+            default:
+              return data
+          }
+        }
+      },
+      {
+        key: 'ssid',
+        title: $t({ defaultMessage: 'Allowed Network' }),
+        dataIndex: 'ssid',
+        sorter: true,
+        render: function (data, row) {
+          const hasGuestManagerRole = false // from userProfile()
+          if (row.networkId && !hasGuestManagerRole) {
+            return (
+              <TenantLink to={`/networks/${row.networkId}/network-details/aps`}>{data}</TenantLink>
+            )
+          } else if (row.networkId && hasGuestManagerRole) {
+            return data
+          } else {
+            return 'None'
+          }
+        }
+      },
+      {
+        key: 'expiryDate',
+        title: $t({ defaultMessage: 'Expires' }),
+        dataIndex: 'expiryDate',
+        sorter: true,
+        render: function (data, row) {
+          if (data && data !== '0') {
+            return moment(row.expiryDate).format('DD/MM/YYYY HH:mm')
+          } else if (!data || data === '0') {
+            let result = ''
+            if (row.passDurationHours) {
+              if (row.passDurationHours > 24) {
+                const days = Math.floor(row.passDurationHours / 24)
+                const hours = row.passDurationHours % 24;
+                const dayText = days > 1 ? ' days' : ' day';
+                const hourText = hours > 1 ? ' hours' : ' hour';
+                result = `${days}${dayText} ${hours}${hourText}`;
+              } else {
+                result = row.passDurationHours + (row.passDurationHours === 1 ? ' hour' : ' hours');
+              }
+            }
+            return `${result} since first login`
+          }
+          return ''
+        }
+      },
+      {
+        key: 'guestStatus',
+        title: $t({ defaultMessage: 'Status' }),
+        dataIndex: 'guestStatus',
+        sorter: false
+      }
+    ]
+
+    const onClose = () => {
+      setVisible(false)
+    }
+
     return (
       <Loader states={[
         tableQuery
       ]}>
+         {
+        !tableQuery.data?.data?.length &&
+        <Alert message={notificationMessage} type='info' showIcon closable />
+      }
         <Table
-          columns={getCols(useIntl())}
+          columns={columns}
           dataSource={tableQuery.data?.data}
           pagination={tableQuery.pagination}
           onChange={tableQuery.handleTableChange}
           rowKey='id'
         />
+
+        <Drawer
+          title={$t({ defaultMessage: 'AP Details' })}
+          visible={visible}
+          onClose={onClose}
+          mask={false}
+          children={<>  
+           <Divider />
+            <Form.Item
+              label={$t({ defaultMessage: 'Uptime' })}
+            /></>}
+          width={'400px'}
+        />
+       
       </Loader>
     )
   }
