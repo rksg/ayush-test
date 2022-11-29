57dd3e3e8008832e9a04082dbddd4b7f23163c53
mickael.dehez <mickael.dehez@ruckuswireless.com>
2022-11-29 17:16:03 +0800

    ACX-18031: refactor searches into one place

--- apps/main/src/pages/SearchResults/index.tsx
+++ apps/main/src/pages/SearchResults/index.tsx
@@ -1,9 +1,7 @@
-import { Fragment, useEffect, useState } from 'react'
-
 import { IntlShape, useIntl } from 'react-intl'
 import { useParams }          from 'react-router-dom'
 
-import { GridRow, GridCol, PageHeader }                  from '@acx-ui/components'
+import { PageHeader }                                    from '@acx-ui/components'
 import { CollapseActive, CollapseInactive }              from '@acx-ui/icons'
 import { NetworkTable }                                  from '@acx-ui/rc/components'
 import { useNetworkListQuery, useVenuesListQuery }       from '@acx-ui/rc/services'
@@ -13,95 +11,69 @@ import { defaultVenuePayload, VenueTable } from '../Venues/VenuesTable'
 
 import { Collapse, Panel } from './styledComponents'
 
+const pagination = { pageSize: 5 }
 
-const useGetPanelHeader = ({ count, title, $t }
-  : { count: number, title: string, $t: IntlShape['$t'] }) => {
-  return $t({ defaultMessage: '{title} ({count})' }, { title, count })
-}
+const searches = [
+  (searchString: string, $t: IntlShape['$t']) => {
+    const result = useTableQuery<Venue, RequestPayload<unknown>, unknown>({
+      useQuery: useVenuesListQuery,
+      defaultPayload: {
+        ...defaultVenuePayload,
+        searchString,
+        searchTargetFields: ['name', 'description']
+      },
+      pagination
+    })
+    return {
+      result,
+      title: $t({ defaultMessage: 'Venues' }),
+      component: <VenueTable tableQuery={result} globalSearch={searchString} />
+    }
+  },
+  (searchString: string, $t: IntlShape['$t']) => {
+    const result = useTableQuery<Network, RequestPayload<unknown>, unknown>({
+      useQuery: useNetworkListQuery,
+      defaultPayload: {
+        ...NetworkTable.defaultNetworkPayload,
+        searchString,
+        searchTargetFiled: ['name', 'description']
+      },
+      pagination
+    })
+    return {
+      result,
+      title: $t({ defaultMessage: 'Networks' }),
+      component: <NetworkTable tableQuery={result} globalSearch={searchString} />
+    }
+  }
+]
 
 function SearchResult () {
   const { $t } = useIntl()
   const { searchVal } = useParams()
-  const [count, setCount] = useState(0)
-
-  const searchVenuePayload = {
-    ...defaultVenuePayload,
-    searchString: searchVal,
-    searchTargetFields: ['name', 'description']
-  }
-
-  const venueQuery = useTableQuery<Venue, RequestPayload<unknown>, unknown>({
-    useQuery: useVenuesListQuery,
-    defaultPayload: searchVenuePayload,
-    pagination: {
-      pageSize: 5
-    }
-  })
-
-  const networkQuery = useTableQuery<Network, RequestPayload<unknown>, unknown>({
-    useQuery: useNetworkListQuery,
-    defaultPayload: {
-      ...NetworkTable.defaultNetworkPayload,
-      searchString: searchVal,
-      searchTargetFiled: ['name', 'description']
-    },
-    pagination: {
-      pageSize: 5
-    }
-  })
-
-  const venueCount = venueQuery.data?.totalCount ?? 0
-  const networkCount = networkQuery.data?.totalCount ?? 0
-
-  useEffect(() => {
-    // sum all table queries here
-    setCount(() => venueCount + networkCount)
-  }, [searchVal, networkCount, venueCount])
-
-  return (
-    <Fragment key='search-results'>
-      <PageHeader
-        title={$t(
-          { defaultMessage: 'Search Results for "{searchVal}" ({count})' },
-          { searchVal, count }
-        )}
-      />
-      <GridRow>
-        <GridCol col={{ span: 24 }} style={{ height: '280px' }}>
-          <Collapse
-            defaultActiveKey={['1', '2']}
-            expandIconPosition='end'
-            expandIcon={({ isActive }) => (isActive)
-              ? <CollapseActive />
-              : <CollapseInactive />
-            }
-            bordered={false}
-          >
-            <Panel
-              header={useGetPanelHeader({ title: 'Venue', count: venueCount, $t })}
-              key='1'
-            >
-              <VenueTable
-                key={`venue-search-${searchVal}`}
-                globalSearch={searchVal}
-                tableQuery={venueQuery}
-              />
-            </Panel>
-            <Panel
-              header={useGetPanelHeader({ title: 'Networks', count: networkCount, $t })}
-              key='2'
-            >
-              <NetworkTable
-                key={`network-search-${searchVal}`}
-                tableQuery={networkQuery}
-                globalSearch={searchVal}
-              />
-            </Panel>
-          </Collapse>
-        </GridCol>
-      </GridRow>
-    </Fragment>
-  )
+  const results = searches.map(search => search(searchVal || '', $t))
+  const count = results.reduce((count, { result }) => count + (result.data?.totalCount || 0), 0)
+  return <>
+    <PageHeader title={$t(
+      { defaultMessage: 'Search Results for "{searchVal}" ({count})' },
+      { searchVal, count }
+    )} />
+    <Collapse
+      defaultActiveKey={Object.keys(results)}
+      expandIconPosition='end'
+      expandIcon={({ isActive }) => (isActive)
+        ? <CollapseActive />
+        : <CollapseInactive />
+      }
+      bordered={false}
+    >
+      {results.map(({ component, title, result }, index) =>
+        <Panel key={index} header={`${title} (${result.data?.totalCount})`}>
+          {component}
+        </Panel>
+      )}
+    </Collapse>
+  </>
 }
 
 export default function SearchResults () {
