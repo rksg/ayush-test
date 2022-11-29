fa3dd42154b5fe6b71446bd1c51dd9046ef23e1e
Karen Yeh <karen.yeh@ruckuswireless.com>
2022-11-29 10:26:17 +0800

    ACX-18040: Adjust the jest setting

--- libs/common/components/jest.config.ts
+++ libs/common/components/jest.config.ts
@@ -9,7 +9,7 @@ module.exports = {
   coverageThreshold: {
     global: {
       statements: 98.29,
-      branches: 97.73,
+      branches: 97.74,
       functions: 98.17,
       lines: 98.62
     }
