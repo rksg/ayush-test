34c39cb2041ea26769179891478c14689bfa6d81
cbokil <chinmay.bokil@ruckuswireless.com>
2022-11-30 15:27:18 +0800

    ACX-18218: wip: rc lint fix

--- apps/rc/src/pages/Users/Wifi/ClientDetails/ClientTroubleshootingTab/index.spec.tsx
+++ apps/rc/src/pages/Users/Wifi/ClientDetails/ClientTroubleshootingTab/index.spec.tsx
@@ -1,6 +1,6 @@
 
-import { Provider }         from '@acx-ui/store'
-import { render, screen }   from '@acx-ui/test-utils'
+import { Provider }       from '@acx-ui/store'
+import { render, screen } from '@acx-ui/test-utils'
 
 import { ClientTroubleshootingTab } from './index'
 
--- apps/rc/src/pages/Users/Wifi/ClientDetails/ClientTroubleshootingTab/index.tsx
+++ apps/rc/src/pages/Users/Wifi/ClientDetails/ClientTroubleshootingTab/index.tsx
@@ -1,13 +1,13 @@
 
+import { ClientTroubleshooting } from '@acx-ui/analytics/components'
 import { useParams }             from '@acx-ui/react-router-dom'
 
-import { ClientTroubleshooting } from '@acx-ui/analytics/components'
 
 
 export function ClientTroubleshootingTab () {
-  
-  const { clientId }  = useParams()
-  
+
+  const { clientId } = useParams()
+
   return <ClientTroubleshooting clientMac={(clientId as string ?? '').toUpperCase()} />
-   
+
 }
