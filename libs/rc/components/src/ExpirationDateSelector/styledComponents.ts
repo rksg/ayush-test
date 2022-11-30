97e9b8b45aa926294ae1bc94ddacd031655685e2
Jacky Chiu <jacky.chiu@ruckuswireless.com>
2022-11-30 11:16:20 +0800

    ACX-12983: Fix calendar and dropdown issue in expiration date

--- libs/rc/components/src/ExpirationDateSelector/styledComponents.ts
+++ libs/rc/components/src/ExpirationDateSelector/styledComponents.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
-export const FieldLabel = styled.div<{ columns: string }>`
+export const FieldSpace = styled.div<{ columns: string }>`
   display: grid;
   grid-template-columns: ${props => props.columns};
-  grid-column-gap: 10px;
-  align-items: baseline;
+  grid-column-gap: 20px;
+  height: 55px;
 `
