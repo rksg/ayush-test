20116a86e7a242e69a56f8e18ab09e40cf812155
cbokil <chinmay.bokil@ruckuswireless.com>
2022-12-05 15:43:11 +0800

    ACX-19028: wip: common util method to transform connectionEvents data

--- libs/analytics/components/src/ClientTroubleshooting/config.tsx
+++ libs/analytics/components/src/ClientTroubleshooting/config.tsx
@@ -2,6 +2,91 @@ import { defineMessage } from 'react-intl'
 
 import { categoryOptions } from '@acx-ui/analytics/utils'
 
+import { ConnectionEvent } from './services'
+
+export const INFO_UPDATED = 'info-updated'
+export const JOIN = 'join'
+export const ROAMED = 'roamed'
+export const DISCONNECTED = 'disconnected'
+export const FAILURE = 'failure'
+export const INCIDENT = 'incident'
+export const RADIO2DOT4G = '2.4'
+export const RADIO5G = '5'
+export const RADIO65G = '6(5)'
+export const EAPOLMessageIds = ['21', '22', '23', '24']
+export const filterEventMap = {
+  [JOIN]: 'EVENT_CLIENT_JOIN',
+  [INFO_UPDATED]: 'EVENT_CLIENT_INFO_UPDATED',
+  [ROAMED]: 'EVENT_CLIENT_ROAMING',
+  [DISCONNECTED]: 'EVENT_CLIENT_DISCONNECT',
+  [FAILURE]: 'FAILURE',
+  [RADIO2DOT4G]: '2.4',
+  [RADIO5G]: '5',
+  [RADIO65G]: '6(5)'
+}
+export const EVENT_STATES = {
+  NORMAL: 'normal',
+  JOIN: 'join',
+  REASSOC: 're-associate',
+  SPURIOUS_DISCONNECT: 'spurious-disconnect',
+  SPURIOUS_INFO_UPDATED: 'spurious-info-updated'
+}
+
+export const TYPES = {
+  CONNECTION_EVENTS: 'connectionEvents',
+  ROAMING: 'roaming'
+}
+
+export const eventsToHide = [
+  EVENT_STATES.JOIN,
+  EVENT_STATES.SPURIOUS_DISCONNECT,
+  EVENT_STATES.SPURIOUS_INFO_UPDATED
+]
+
+export const spuriousEvents = [
+  EVENT_STATES.JOIN,
+  EVENT_STATES.SPURIOUS_DISCONNECT,
+  EVENT_STATES.SPURIOUS_INFO_UPDATED
+]
+
+export const categorizeEvent = (name: string, ttc: number) => {
+  const successEvents = [INFO_UPDATED, JOIN, ROAMED].map(
+    key => filterEventMap[key as keyof typeof filterEventMap]
+  )
+  if (name === 'EVENT_CLIENT_DISCONNECT') return 'disconnect'
+  if (!successEvents.includes(name)) return 'failure'
+  if (ttc !== null && ttc >= 4000) return 'slow'
+  return 'success'
+}
+// common utility for history and connection events chart
+export const transformEvents = (events: ConnectionEvent[], selectedEvents: string[]) => {
+  return events.reduce((acc, data, index) => {
+    const { event, state, timestamp, mac, ttc, radio, code, failedMsgId  } = data
+    if (code === 'eap' && EAPOLMessageIds.includes(failedMsgId)) {
+      data.code = 'eapol'
+    }
+
+    const category = categorizeEvent(event, ttc)
+    const eventType = category === 'failure' ? filterEventMap[FAILURE] : event
+    const time = +new Date(timestamp)
+
+    const skip = eventsToHide.includes(state) ||
+      !selectedEvents.includes(eventType) ||
+      !selectedEvents.includes(radio)
+    if (skip) return acc
+
+    acc.push({
+      ...data,
+      type: event === 'EVENT_CLIENT_ROAMING' ? TYPES.ROAMING : TYPES.CONNECTION_EVENTS,
+      key: time + mac + eventType + index,
+      start: time,
+      end: time,
+      category
+    })
+    return acc
+  }, [] as object[])
+}
+
 export const ClientTroubleShootingConfig = {
   selection: [
     {
@@ -16,23 +101,23 @@ export const ClientTroubleShootingConfig = {
       placeHolder: defineMessage({ defaultMessage: 'All Types' }),
       options: [
         {
-          value: 'info-updated',
+          value: INFO_UPDATED,
           label: defineMessage({ defaultMessage: 'Client associated' })
         },
         {
-          value: 'roamed',
+          value: ROAMED,
           label: defineMessage({ defaultMessage: 'Client roamed' })
         },
         {
-          value: 'disconnected',
+          value: DISCONNECTED,
           label: defineMessage({ defaultMessage: 'Client disconnected' })
         },
         {
-          value: 'failure',
+          value: FAILURE,
           label: defineMessage({ defaultMessage: 'Connection failure' })
         },
         {
-          value: 'incident',
+          value: INCIDENT,
           label: defineMessage({ defaultMessage: 'Incident' })
         }
       ]
@@ -43,15 +128,15 @@ export const ClientTroubleShootingConfig = {
       placeHolder: defineMessage({ defaultMessage: 'All Radios' }),
       options: [
         {
-          value: '2.4',
+          value: RADIO2DOT4G,
           label: defineMessage({ defaultMessage: '2.4 GHz' })
         },
         {
-          value: '5',
+          value: RADIO5G,
           label: defineMessage({ defaultMessage: '5 GHz' })
         },
         {
-          value: '6(5)',
+          value: RADIO65G,
           label: defineMessage({ defaultMessage: '6 GHz' })
         }
       ]
