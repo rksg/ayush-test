c9c757b48998fea4fdf84a8bc0e2f8d4e82c487d
ki1003 <khairul.iman@ruckuswireless.com>
2022-12-06 11:10:18 +0800

    ACX-19016: tests: simplify to snapshot test

--- apps/main/src/pages/SearchResults/NoData/index.spec.tsx
+++ apps/main/src/pages/SearchResults/NoData/index.spec.tsx
@@ -1,61 +1,13 @@
-import { ReactNode } from 'react'
-
-import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom'
-
-import { cleanup, render, screen, fireEvent } from '@acx-ui/test-utils'
+import { cleanup, render } from '@acx-ui/test-utils'
 
 import NoData from '.'
 
 describe('SearchResults - NoData', () => {
 
-  const LocationDisplay = () => {
-    const location = useLocation()
-    return <div data-testid='location-display'>{location.pathname}</div>
-  }
-
-  const Wrapper = ({ children }: { children?: ReactNode }) => {
-    return <MemoryRouter initialEntries={['/t/accb1231/dragons']}>
-      <Routes>
-        <Route
-          path='*'
-          element={children}
-        />
-      </Routes>
-    </MemoryRouter>
-
-  }
-
   afterEach(() => cleanup())
 
-  it('should render point header', async () => {
-    render(<NoData />, { wrapper: Wrapper, route: false })
-
-    expect(await screen.findByText(
-      /Check for typos or use a different search term/i
-    )).toBeInTheDocument()
-  })
-
-  it ('should route correctly for Venues', async () => {
-    render(<><NoData /><LocationDisplay /></>, { wrapper: Wrapper, route: false })
-
-    const venuesLink = await screen.findByText('Venues')
-    fireEvent.click(venuesLink)
-    expect(screen.getByTestId('location-display')).toHaveTextContent('/venues')
-  })
-
-  it ('should route correctly for Dashboard', async () => {
-    render(<><NoData /><LocationDisplay /></>, { wrapper: Wrapper, route: false })
-
-    const venuesLink = await screen.findByText('Dashboard')
-    fireEvent.click(venuesLink)
-    expect(screen.getByTestId('location-display')).toHaveTextContent('/dashboard')
-  })
-
-  it ('should route correctly for Networks', async () => {
-    render(<><NoData /><LocationDisplay /></>, { wrapper: Wrapper, route: false })
-
-    const venuesLink = await screen.findByText('Networks')
-    fireEvent.click(venuesLink)
-    expect(screen.getByTestId('location-display')).toHaveTextContent('/networks')
+  it ('should render correctly for snapshot test', async () => {
+    const { asFragment } = render(<NoData />, { route: { params: { tenantId: '1234' } } })
+    expect(asFragment()).toMatchSnapshot()
   })
 })
