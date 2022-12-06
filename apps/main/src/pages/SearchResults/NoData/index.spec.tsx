6ef95f0dc7c1f45cc8983cfa4d1713217e64e7ec
ki1003 <khairul.iman@ruckuswireless.com>
2022-12-06 15:23:18 +0800

    ACX-19016: tests: add snapshots for toggles on & off

--- apps/main/src/pages/SearchResults/NoData/index.spec.tsx
+++ apps/main/src/pages/SearchResults/NoData/index.spec.tsx
@@ -1,3 +1,4 @@
+import { useIsSplitOn }    from '@acx-ui/feature-toggle'
 import { cleanup, render } from '@acx-ui/test-utils'
 
 import NoData from '.'
@@ -6,7 +7,14 @@ describe('SearchResults - NoData', () => {
 
   afterEach(() => cleanup())
 
-  it ('should render correctly for snapshot test', async () => {
+  it('should render correctly for snapshot test with toggles on', async () => {
+    jest.mocked(useIsSplitOn).mockReturnValue(true)
+    const { asFragment } = render(<NoData />, { route: { params: { tenantId: '1234' } } })
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should render correctly for snapshot test with toggles off', async () => {
+    jest.mocked(useIsSplitOn).mockReturnValue(false)
     const { asFragment } = render(<NoData />, { route: { params: { tenantId: '1234' } } })
     expect(asFragment()).toMatchSnapshot()
   })
