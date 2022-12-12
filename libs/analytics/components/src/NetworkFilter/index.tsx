3a07ace97234a422187bf06f9eb98f5681d701cd
mickael.dehez <mickael.dehez@ruckuswireless.com>
2022-12-12 18:14:41 +0800

    ACX-17893: adjust popover position to be able to always select

--- libs/analytics/components/src/NetworkFilter/index.tsx
+++ libs/analytics/components/src/NetworkFilter/index.tsx
@@ -227,13 +227,13 @@ function ConnectedNetworkFilter (
           value={raw}
           options={queryResults.data}
           onApply={(value) => onApply(value, setNetworkPath)}
-          placement='bottomLeft'
           displayRender={displayRender}
           showSearch={{ filter: search }}
           allowClear
           dropdownAlign={{
-            points: ['bl', 'tr'],
+            // https://github.com/react-component/trigger/blob/eac2f8f3b846b14cc5663af8a148201361835587/src/interface.ts#L9
             useCssTransform: true,
+            points: ['tr','bc'],
             overflow: {
               adjustX: true,
               adjustY: true
