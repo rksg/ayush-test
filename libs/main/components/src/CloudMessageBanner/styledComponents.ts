d112208f08de02bd05f7191a5194386113099444
Eric Leu <Eric.Leu@ruckuswireless.com>
2022-12-16 07:27:24 -0800

    ACX-18181: adjust css style based on PR comments

--- libs/main/components/src/CloudMessageBanner/styledComponents.ts
+++ libs/main/components/src/CloudMessageBanner/styledComponents.ts
@@ -5,13 +5,16 @@ import {
 } from '@acx-ui/icons'
 
 export const Container = styled.div`
-  padding-bottom: 5px
+  margin-top: -12px;
+  padding-bottom: 3px;
+  z-index:2000;
 `
 export const Label = styled.div`
   height: 36px;
-  padding: 12px;
+  padding: 10px;
   border: 1px solid var(--acx-accents-orange-25);
   background-color: var(--acx-accents-orange-10);
+  border-radius: 4px;
 `
 export const InformationIcon = styled(InformationSolid)`
 path:nth-child(2) {
