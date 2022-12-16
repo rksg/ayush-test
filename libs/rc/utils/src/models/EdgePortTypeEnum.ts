da8b3836affe4d6a975f87dd2f9ce6662f94e873
Cheng, Amy <Amy.Cheng@ruckuswireless.com>
2022-12-16 12:25:30 +0800

    ACX-19172: delete unused files.

--- libs/rc/utils/src/models/EdgePortTypeEnum.ts
+++
@@ -1,5 +0,0 @@
-export enum EdgePortTypeEnum {
-    UNSPECIFIED = 'PORT_TYPE_UNSPECIFIED',
-    WAN = 'WAN',
-    LAN = 'LAN',
-  }
--- libs/rc/utils/src/models/EdgeResourceUtilizationEnum.ts
+++
@@ -1,5 +0,0 @@
-export enum EdgeResourceUtilizationEnum {
-    CPU = 'CPU',
-    MEMORY = 'MEMORY',
-    STORAGE = 'STORAGE',
-}
