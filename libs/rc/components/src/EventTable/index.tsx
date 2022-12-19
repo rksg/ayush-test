951101fa3cc9629ca51c92f12a4b6a9ddbdd07f3
tingan.chen <tingan.chen@ruckuswireless.com>
2022-12-19 15:28:28 +0800

    ACX-20010: refactor: Move Timeline tables from rc to rc-components

---
+++ libs/rc/components/src/EventTable/index.tsx
@@ -0,0 +1,152 @@
+import { useState } from 'react'
+
+import { defineMessage, useIntl } from 'react-intl'
+
+import { Loader, Table, TableProps, Button } from '@acx-ui/components'
+import { Event, RequestPayload, TableQuery } from '@acx-ui/rc/utils'
+import { formatter }                         from '@acx-ui/utils'
+
+import { TimelineDrawer } from '../TimelineDrawer'
+
+import { severityMapping, eventTypeMapping, productMapping } from './mapping'
+
+interface EventTableProps {
+  tableQuery: TableQuery<Event, RequestPayload<unknown>, unknown>
+}
+
+const EventTable = ({ tableQuery }: EventTableProps) => {
+  const { $t } = useIntl()
+  const [visible, setVisible] = useState(false)
+  const [current, setCurrent] = useState<Event>()
+
+  const getSource = (data: Event) => {
+    const sourceMapping = {
+      AP: 'apName',
+      CLIENT: 'clientName'
+    }
+    const key = sourceMapping[data.entity_type as keyof typeof sourceMapping]
+    return data[key as keyof Event] as string
+  }
+
+  const getDescription = (data: Event) => {
+    let message = data.message && JSON.parse(data.message).message_template
+    message && message.match(new RegExp(/@@\w+/, 'g'))?.forEach((match: string) => {
+      message = message.replace(match, data[match.replace('@@','') as keyof Event])
+    })
+    return message
+  }
+  const columns: TableProps<Event>['columns'] = [
+    {
+      key: 'event_datetime',
+      title: $t({ defaultMessage: 'Date' }),
+      dataIndex: 'event_datetime',
+      defaultSortOrder: 'descend',
+      sorter: true,
+      render: function (_, row) {
+        return <Button
+          type='link'
+          size='small'
+          onClick={()=>{
+            setVisible(true)
+            setCurrent(row)
+          }}
+        >{formatter('dateTimeFormatWithSeconds')(row.event_datetime)}</Button>
+      }
+    },
+    {
+      key: 'severity',
+      title: $t({ defaultMessage: 'Severity' }),
+      dataIndex: 'severity',
+      sorter: true,
+      render: function (_, row) {
+        const msg = severityMapping[row.severity as keyof typeof severityMapping]
+        return $t(msg)
+      }
+    },
+    {
+      key: 'entity_type',
+      title: $t({ defaultMessage: 'Event Type' }),
+      dataIndex: 'entity_type',
+      sorter: true,
+      render: function (_, row) {
+        const msg = eventTypeMapping[
+          row.entity_type as keyof typeof eventTypeMapping]
+        return $t(msg)
+      }
+    },
+    {
+      key: 'product',
+      title: $t({ defaultMessage: 'Product' }),
+      dataIndex: 'product',
+      sorter: true,
+      render: function (_, row) {
+        const msg = productMapping[row.product as keyof typeof productMapping]
+        return $t(msg)
+      }
+    },
+    {
+      key: 'source',
+      title: $t({ defaultMessage: 'Source' }),
+      dataIndex: 'source',
+      sorter: true,
+      render: function (_, row) {
+        return getSource(row)
+      }
+    },
+    {
+      key: 'message',
+      title: $t({ defaultMessage: 'Description' }),
+      dataIndex: 'message',
+      sorter: true,
+      render: function (_, row) {
+        return getDescription(row)
+      }
+    }
+  ]
+  const getDrawerData = (data: Event) => [
+    {
+      title: defineMessage({ defaultMessage: 'Time' }),
+      value: formatter('dateTimeFormatWithSeconds')(data.event_datetime)
+    },
+    {
+      title: defineMessage({ defaultMessage: 'Severity' }),
+      value: (() => {
+        const msg = severityMapping[data.severity as keyof typeof severityMapping]
+        return $t(msg)
+      })()
+    },
+    {
+      title: defineMessage({ defaultMessage: 'Event Type' }),
+      value: (() => {
+        const msg = eventTypeMapping[
+          data.entity_type as keyof typeof eventTypeMapping]
+        return $t(msg)
+      })()
+    },
+    {
+      title: defineMessage({ defaultMessage: 'Source' }),
+      value: getSource(data)
+    },
+    {
+      title: defineMessage({ defaultMessage: 'Description' }),
+      value: getDescription(data)
+    }
+  ]
+
+  return <Loader states={[tableQuery]}>
+    <Table
+      rowKey='id'
+      columns={columns}
+      dataSource={tableQuery.data?.data}
+      pagination={tableQuery.pagination}
+      onChange={tableQuery.handleTableChange}
+    />
+    {visible && <TimelineDrawer
+      title={defineMessage({ defaultMessage: 'Event Details' })}
+      visible={visible}
+      onClose={()=>setVisible(false)}
+      data={getDrawerData(current!)}
+    />}
+  </Loader>
+}
+export { EventTable }
