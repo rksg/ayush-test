cb1db9f8efe487f92b4b14222b97387d1cc7298e
TLIN4 <Tsaihuei.Lin@ruckuswireless.com>
2022-12-19 17:21:40 +0800

    ACX-19862: update test

--- apps/rc/src/pages/Users/Wifi/ClientList/ClientsTab/index.tsx
+++ apps/rc/src/pages/Users/Wifi/ClientList/ClientsTab/index.tsx
@@ -1,9 +1,12 @@
-import { ConnectedClientsTable, HistoricalClientsTable } from '@acx-ui/rc/components'
+import {
+  ConnectedClientsTable
+  // HistoricalClientsTable
+} from '@acx-ui/rc/components'
 
 export function ClientsTab () {
   return <>
     <ConnectedClientsTable />
     {/* TODO: change string from search input */}
-    {false && <HistoricalClientsTable searchString='' />}
+    {/* {false && <HistoricalClientsTable searchString='' />} */}
   </>
 }
