307bebe4fdf718f1eac53f242f0e166f4ea7b458
ki1003 <khairul.iman@ruckuswireless.com>
2022-12-21 14:46:56 +0800

    ACX-19353: wip: thick arrows & missed spacings

--- libs/analytics/components/src/ClientTroubleshooting/sequenceComponents.tsx
+++ libs/analytics/components/src/ClientTroubleshooting/sequenceComponents.tsx
@@ -2,10 +2,8 @@ import styled, { css } from 'styled-components/macro'
 
 export const Wrapper = styled.section.attrs((props: { layers: Array<unknown> }) => props)`
   display: grid;
-  min-height: 100%;
-  grid-gap: 5px 16px;
+  grid-gap: 0px 16px;
   padding-left: 8px;
-  padding-right: ${8 + 16}px;
   grid-template-columns:
     minmax(140px, 260px)
     ${props => Array(props.layers.length - 1).fill('minmax(20px, 60px)').join('\n')}
@@ -15,9 +13,7 @@ export const Wrapper = styled.section.attrs((props: { layers: Array<unknown> }) 
 export const Container = styled.section.attrs((props: { layers: Array<unknown> }) => props)`
   grid-area: 1 / 1 / -1 / -1;
   display: grid;
-  min-height: 100%;
-  grid-gap: 5px 16px;
-  grid-auto-rows: min-content;
+  grid-gap: 0px 16px;
   grid-template-columns:
     minmax(140px, 260px)
     ${props => Array(props.layers.length - 1).fill('minmax(20px, 60px)').join('\n')}
@@ -43,9 +39,9 @@ export const Layer = styled.div`
   }
 `
 
-const stepFlowColorHelper = (state: string) => (state === 'normal')
-  ? 'var(--acx-neutrals-60)'
-  : 'var(--acx-semantics-red-50)'
+const stepFlowColorHelper = (state: string) => (state === 'failed')
+  ? 'var(--acx-semantics-red-50)'
+  : 'var(--acx-neutrals-60)'
 
 const stepFlowMapper = (color: string) => ({
   left: css`
@@ -57,28 +53,30 @@ const stepFlowMapper = (color: string) => ({
 })
 
 export const StepFlow = styled.div.attrs((props: { direction: string, state: string }) => props)`
-  height: 10px;
   align-self: center;
   display: flex;
   align-items: center;
   flex-direction: ${props => props.direction === 'left' ? 'row' : 'row-reverse'};
-  &:before, &:after {
+
+  &:after {
     content: '';
     display: block;
-  }
-  &:after {
     height: 6px;
     background-color: ${props => stepFlowColorHelper(props.state)};
     flex: 1;
+    ${props => (props.direction === 'left') ? 'margin-right: 2px' : 'margin-left: 2px'};
   }
+
   &:before {
+    content: '';
+    display: block;
     border: 0 solid transparent;
     border-width: 7px 0;
     ${props => {
     const color = stepFlowColorHelper(props.state)
     const stepFlowColor = stepFlowMapper(color)[props.direction as 'left' | 'right']
-    return stepFlowColor
-  }};
+    return stepFlowColor}}
+
   }
 ` // before missing directions, flex-directions to flip based on left / right direction
 
