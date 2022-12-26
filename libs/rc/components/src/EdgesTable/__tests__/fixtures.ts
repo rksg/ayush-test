3b05ca86a418c443db1c04f81347b4109b4fa707
Cheng, Amy <Amy.Cheng@ruckuswireless.com>
2022-12-26 14:39:57 +0800

    ACX-20381: make "EdgesTable" as @acx-ui/rc/components

---
+++ libs/rc/components/src/EdgesTable/__tests__/fixtures.ts
@@ -0,0 +1,70 @@
+export const mockEdgeList = {
+  fields: [
+    'name','deviceStatus','type','model','serialNumber','ip',
+    'ports','venueName','venueId','tags'
+  ],
+  totalCount: 5,
+  page: 1,
+  data: [
+    {
+      name: 'Smart Edge 1',
+      deviceStatus: '2_00_Operational',
+      type: 'type 1',
+      model: 'model 1',
+      serialNumber: '0000000001',
+      ip: '0.0.0.0',
+      ports: '80',
+      venueName: 'Venue 1',
+      venueId: '00001',
+      tags: ['Tag1', 'Tag2']
+    },
+    {
+      name: 'Smart Edge 2',
+      deviceStatus: '2_00_Operational',
+      type: 'type 1',
+      model: 'model 1',
+      serialNumber: '0000000002',
+      ip: '0.0.0.0',
+      ports: '80',
+      venueName: 'Venue 1',
+      venueId: '00001',
+      tags: ['Tag1', 'Tag2']
+    },
+    {
+      name: 'Smart Edge 3',
+      deviceStatus: '2_00_Operational',
+      type: 'type 1',
+      model: 'model 1',
+      serialNumber: '0000000003',
+      ip: '0.0.0.0',
+      ports: '80',
+      venueName: 'Venue 1',
+      venueId: '00001',
+      tags: ['Tag1', 'Tag2']
+    },
+    {
+      name: 'Smart Edge 4',
+      deviceStatus: '2_00_Operational',
+      type: 'type 1',
+      model: 'model 1',
+      serialNumber: '0000000004',
+      ip: '0.0.0.0',
+      ports: '80',
+      venueName: 'Venue 1',
+      venueId: '00001',
+      tags: ['Tag1', 'Tag2']
+    },
+    {
+      name: 'Smart Edge 5',
+      deviceStatus: '1_01_NeverContactedCloud',
+      type: 'type 1',
+      model: 'model 1',
+      serialNumber: '0000000005',
+      ip: '0.0.0.0',
+      ports: '80',
+      venueName: 'Venue 1',
+      venueId: '00001',
+      tags: ['Tag1', 'Tag2']
+    }
+  ]
+}
