e1e9d9aadb074015da04a2edfee33f4b6aa358f7
Eric Leu <Eric.Leu@ruckuswireless.com>
2022-12-27 08:31:05 -0800

    ACX-18181: remove unneeded code based on PR comment

--- libs/main/components/src/CloudMessageBanner/index.tsx
+++ libs/main/components/src/CloudMessageBanner/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 
 import {  Alert }                      from '@acx-ui/components'
 import { useGetPlmMessageBannerQuery } from '@acx-ui/rc/services'
@@ -7,16 +6,10 @@ import { useParams  }                  from '@acx-ui/react-router-dom'
 export function CloudMessageBanner () {
 
   const params = useParams()
-  const [showMessage, setMessageFlag] = useState(false)
 
   const { data } = useGetPlmMessageBannerQuery({ params })
-  useEffect(() => {
-    if (data && data.description) {
-      if (data?.description) setMessageFlag(true)
-    }
-  }, [data])
 
   return (
-    showMessage ? (<Alert message={data?.description} type='info' showIcon closable />) : null
+    data?.description ? (<Alert message={data?.description} type='info' showIcon closable />) : null
   )
 }
