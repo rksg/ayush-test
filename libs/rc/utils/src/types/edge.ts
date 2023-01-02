55b40eb267eaaa3ab64816954093b47530782b08
Li, Roil <Roil.Li@ruckuswireless.com>
2023-01-02 16:57:09 +0800

    ACX-19131: Remove duplicated target

--- libs/rc/utils/src/types/edge.ts
+++ libs/rc/utils/src/types/edge.ts
@@ -86,13 +86,4 @@ export interface EdgePort {
   portIndex?: number
 }
 
-export enum EdgeStatusEnum {
-  NEVER_CONTACTED_CLOUD = '1_01_NeverContactedCloud',
-  INITIALIZING = '1_07_Initializing',
-  NEEDS_CONFIG = '1_10_NeedsConfig',
-  OPERATIONAL = '2_00_Operational',
-  APPLYING_CONFIGURATION = '2_02_ApplyingConfiguration',
-  DISCONNECTED_FROM_CLOUD = '3_04_DisconnectedFromCloud'
-}
-
 export type EdgeDNS = string
