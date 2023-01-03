8deb17fa534531eb232b3858f397ad40d16a753c
Jacky Chiu <jacky.chiu@ruckuswireless.com>
2023-01-03 12:03:52 +0800

    ACX-21329: Add Client Isolation url file

--- libs/rc/utils/src/urls/policies/clientIsolationUrls.ts
+++ libs/rc/utils/src/urls/policies/clientIsolationUrls.ts
@@ -1,3 +1,24 @@
 import { ApiInfo } from '../../apiService'
 
-export const ClientIsolationUrls: { [key: string]: ApiInfo } = {}
+export const ClientIsolationUrls: { [key: string]: ApiInfo } = {
+  addClientIsolation: {
+    method: 'post',
+    url: '/api/tenant/:tenantId/wifi/clientIsolationPolicyProfiles'
+  },
+  updateClientIsolation: {
+    method: 'put',
+    url: '/api/tenant/:tenantId/wifi/clientIsolationPolicyProfiles/:policyId'
+  },
+  getClientIsolation: {
+    method: 'get',
+    url: '/api/tenant/:tenantId/wifi/clientIsolationPolicyProfiles/:policyId'
+  },
+  getClientIsolationList: {
+    method: 'get',
+    url: '/api/tenant/:tenantId/wifi/clientIsolationPolicyProfiles'
+  },
+  deleteClientIsolation: {
+    method: 'delete',
+    url: '/api/tenant/:tenantId/wifi/clientIsolationPolicyProfiles/:policyId'
+  }
+}
