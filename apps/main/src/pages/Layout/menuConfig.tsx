62e6fa4ebbbdd532b40081e01f71719e76781ca8
Li, Roil <Roil.Li@ruckuswireless.com>
2023-01-04 18:07:48 +0800

    ACX-19128: Hide edge on menu when FF is off

--- apps/main/src/pages/Layout/menuConfig.tsx
+++ apps/main/src/pages/Layout/menuConfig.tsx
@@ -106,11 +106,10 @@ export function useMenuConfig () {
             name: $t({ defaultMessage: 'Switch' }),
             disabled: !useIsSplitOn(Features.DEVICES)
           },
-          {
+          ...useIsSplitOn(Features.EDGES) ? [{
             path: '/devices/edge/list',
-            name: $t({ defaultMessage: 'Edge' }),
-            disabled: !useIsSplitOn(Features.EDGES)
-          }
+            name: $t({ defaultMessage: 'Edge' })
+          }] : []
         ]
     },
     {
