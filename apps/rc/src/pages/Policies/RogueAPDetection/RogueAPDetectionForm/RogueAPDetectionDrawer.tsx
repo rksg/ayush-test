37fec885ca911ad95d4fd13ab7d4164774c42293
jeffrey.syu <jeffrey.syu@ruckuswireless.com>
2023-01-05 14:57:18 +0800

    ACX-21450:[Rogue Ap] remove the wrapper to make form.item validator work properly

--- apps/rc/src/pages/Policies/RogueAPDetection/RogueAPDetectionForm/RogueAPDetectionDrawer.tsx
+++ apps/rc/src/pages/Policies/RogueAPDetection/RogueAPDetectionForm/RogueAPDetectionDrawer.tsx
@@ -64,97 +64,82 @@ const RogueAPDetectionDrawer = (props: RogueAPDetectionDrawerProps) => {
   }, [isEditMode, queryRuleName])
 
   const selectRule = (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <Select
-        data-testid='selectRogueRule'
-        defaultValue={ruleObj.type}
-        style={{ width: '70%', marginRight: '5px' }}
-        onChange={(options) => {
-          setRuleType(options.toString() as RogueRuleType)
-          form.setFieldValue('type', options)
-        }}>
-        <Option value={RogueRuleType.AD_HOC_RULE}>
-          {$t({ defaultMessage: 'Ad Hoc' })}
-        </Option>
-        <Option value={RogueRuleType.CTS_ABUSE_RULE}>
-          {$t({ defaultMessage: 'CTS Abuse' })}
-        </Option>
-        <Option value={RogueRuleType.DEAUTH_FLOOD_RULE}>
-          {$t({ defaultMessage: 'Deauth Flood' })}
-        </Option>
-        <Option value={RogueRuleType.DISASSOC_FLOOD_RULE}>
-          {$t({ defaultMessage: 'Disassoc Flood' })}
-        </Option>
-        <Option value={RogueRuleType.EXCESSIVE_POWER_RULE}>
-          {$t({ defaultMessage: 'Excessive Power' })}
-        </Option>
-        <Option value={RogueRuleType.LOW_SNR_RULE}>
-          {$t({ defaultMessage: 'Low SNR' })}
-        </Option>
-        <Option value={RogueRuleType.MAC_OUI_RULE}>
-          {$t({ defaultMessage: 'MAC OUI' })}
-        </Option>
-        <Option value={RogueRuleType.MAC_SPOOFING_RULE}>
-          {$t({ defaultMessage: 'MAC Spoofing' })}
-        </Option>
-        <Option value={RogueRuleType.NULL_SSID_RULE}>
-          {$t({ defaultMessage: 'Null SSID' })}
-        </Option>
-        <Option value={RogueRuleType.RTS_ABUSE_RULE}>
-          {$t({ defaultMessage: 'RTS Abuse' })}
-        </Option>
-        <Option value={RogueRuleType.SAME_NETWORK_RULE}>
-          {$t({ defaultMessage: 'Same Network' })}
-        </Option>
-        <Option value={RogueRuleType.SSID_RULE}>
-          {$t({ defaultMessage: 'SSID' })}
-        </Option>
-        <Option value={RogueRuleType.SSID_SPOOFING_RULE}>
-          {$t({ defaultMessage: 'SSID Spoofing' })}
-        </Option>
-      </Select>
-      <Tooltip
-        title={$t({ defaultMessage: 'The type of rule to match for this category policy.' })}
-        placement='bottom'
-      >
-        <QuestionMarkCircleOutlined />
-      </Tooltip>
-    </div>
+    <Select
+      data-testid='selectRogueRule'
+      defaultValue={ruleObj.type}
+      style={{ width: '100%', marginRight: '5px' }}
+      onChange={(options) => {
+        setRuleType(options.toString() as RogueRuleType)
+        form.setFieldValue('type', options)
+      }}>
+      <Option value={RogueRuleType.AD_HOC_RULE}>
+        {$t({ defaultMessage: 'Ad Hoc' })}
+      </Option>
+      <Option value={RogueRuleType.CTS_ABUSE_RULE}>
+        {$t({ defaultMessage: 'CTS Abuse' })}
+      </Option>
+      <Option value={RogueRuleType.DEAUTH_FLOOD_RULE}>
+        {$t({ defaultMessage: 'Deauth Flood' })}
+      </Option>
+      <Option value={RogueRuleType.DISASSOC_FLOOD_RULE}>
+        {$t({ defaultMessage: 'Disassoc Flood' })}
+      </Option>
+      <Option value={RogueRuleType.EXCESSIVE_POWER_RULE}>
+        {$t({ defaultMessage: 'Excessive Power' })}
+      </Option>
+      <Option value={RogueRuleType.LOW_SNR_RULE}>
+        {$t({ defaultMessage: 'Low SNR' })}
+      </Option>
+      <Option value={RogueRuleType.MAC_OUI_RULE}>
+        {$t({ defaultMessage: 'MAC OUI' })}
+      </Option>
+      <Option value={RogueRuleType.MAC_SPOOFING_RULE}>
+        {$t({ defaultMessage: 'MAC Spoofing' })}
+      </Option>
+      <Option value={RogueRuleType.NULL_SSID_RULE}>
+        {$t({ defaultMessage: 'Null SSID' })}
+      </Option>
+      <Option value={RogueRuleType.RTS_ABUSE_RULE}>
+        {$t({ defaultMessage: 'RTS Abuse' })}
+      </Option>
+      <Option value={RogueRuleType.SAME_NETWORK_RULE}>
+        {$t({ defaultMessage: 'Same Network' })}
+      </Option>
+      <Option value={RogueRuleType.SSID_RULE}>
+        {$t({ defaultMessage: 'SSID' })}
+      </Option>
+      <Option value={RogueRuleType.SSID_SPOOFING_RULE}>
+        {$t({ defaultMessage: 'SSID Spoofing' })}
+      </Option>
+    </Select>
   )
 
   const selectCategory = (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <Select
-        data-testid='selectRogueCategory'
-        defaultValue={ruleObj.classification}
-        style={{ width: '70%', marginRight: '5px' }}
-        onChange={(options) => setCategory(options.toString() as RogueCategory)}>
-        <Option value={RogueCategory.IGNORED}>
-          {$t({ defaultMessage: 'Ignored' })}
-        </Option>
-        <Option value={RogueCategory.KNOWN}>
-          {$t({ defaultMessage: 'Known' })}
-        </Option>
-        <Option value={RogueCategory.UNCLASSIFIED}>
-          {$t({ defaultMessage: 'Unclassified' })}
-        </Option>
-        <Option value={RogueCategory.MALICIOUS}>
-          {$t({ defaultMessage: 'Malicious' })}
-        </Option>
-      </Select>
-      <Tooltip
-        title={$t({ defaultMessage: 'Classify rogue APs when the above rule type is matched.' })}
-        placement='bottom'
-      >
-        <QuestionMarkCircleOutlined />
-      </Tooltip>
-    </div>
+    <Select
+      data-testid='selectRogueCategory'
+      defaultValue={ruleObj.classification}
+      style={{ width: '100%', marginRight: '5px' }}
+      onChange={(options) => setCategory(options.toString() as RogueCategory)}>
+      <Option value={RogueCategory.IGNORED}>
+        {$t({ defaultMessage: 'Ignored' })}
+      </Option>
+      <Option value={RogueCategory.KNOWN}>
+        {$t({ defaultMessage: 'Known' })}
+      </Option>
+      <Option value={RogueCategory.UNCLASSIFIED}>
+        {$t({ defaultMessage: 'Unclassified' })}
+      </Option>
+      <Option value={RogueCategory.MALICIOUS}>
+        {$t({ defaultMessage: 'Malicious' })}
+      </Option>
+    </Select>
   )
 
   const content = <Form layout='vertical' form={form}>
     <Form.Item
       name='ruleName'
       label={$t({ defaultMessage: 'Rule Name' })}
+      style={{ width: '90%' }}
       rules={[
         { required: true },
         { validator: (rule, value) => {
@@ -168,37 +153,58 @@ const RogueAPDetectionDrawer = (props: RogueAPDetectionDrawerProps) => {
       ]}
       initialValue={isEditMode ? ruleObj.name : queryRuleName}
       children={<Input
-        style={{ width: '70%' }}
         defaultValue={queryRuleName}
         onChange={(event) => setRuleName(event.target.value)}
         placeholder={$t({ defaultMessage: 'Please enter the Rule name' })} />}
     />
-    <Form.Item
-      name='type'
-      label={$t({ defaultMessage: 'Rule Type' })}
-      rules={[
-        { required: true },
-        { validator: (rule, value) => {
-          if (value && state.rules
-            .filter(e => isEditMode ? (e.type !== ruleObj.type) : true)
-            .findIndex(e => e.type === value) !== -1) {
-            return Promise.reject(
-              $t({ defaultMessage: 'A Rule with that type already exists in this Policy' })
-            )
-          }
-          return Promise.resolve()
-        } }
-      ]}
-      initialValue={ruleObj.type}
-      children={selectRule}
-    />
-    <Form.Item
-      name='classification'
-      label={$t({ defaultMessage: 'Category' })}
-      rules={[{ required: true }]}
-      initialValue={ruleObj.classification}
-      children={selectCategory}
-    />
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Form.Item
+        name='type'
+        label={$t({ defaultMessage: 'Rule Type' })}
+        style={{ width: '90%' }}
+        rules={[
+          { required: true },
+          { validator: (rule, value) => {
+            if (value && state.rules
+              .filter(e => isEditMode ? (e.type !== ruleObj.type) : true)
+              .findIndex(e => e.type === value) !== -1) {
+              return Promise.reject(
+                $t({ defaultMessage: 'A Rule with that type already exists in this Policy' })
+              )
+            }
+            return Promise.resolve()
+          } }
+        ]}
+        validateFirst
+        validateTrigger={['onChange']}
+        initialValue={ruleObj.type}
+        children={selectRule}
+      />
+      <Tooltip
+        title={$t({ defaultMessage: 'The type of rule to match for this category policy.' })}
+        placement='bottom'
+      >
+        <QuestionMarkCircleOutlined />
+      </Tooltip>
+    </div>
+
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Form.Item
+        name='classification'
+        label={$t({ defaultMessage: 'Category' })}
+        style={{ width: '90%' }}
+        rules={[{ required: true }]}
+        initialValue={ruleObj.classification}
+        children={selectCategory}
+      />
+      <Tooltip
+        title={$t({ defaultMessage: 'Classify rogue APs when the above rule type is matched.' })}
+        placement='bottom'
+      >
+        <QuestionMarkCircleOutlined />
+      </Tooltip>
+    </div>
+
   </Form>
 
   return (
