308af0faa9ad3387d66acba6b62f51aed58aea18
ki1003 <khairul.iman@ruckuswireless.com>
2023-01-10 17:14:24 +0800

    ACX-18684: wip: add IncidentTabContent to devices page

--- apps/rc/src/pages/Devices/Switch/SwitchDetails/SwitchIncidentsTab/index.tsx
+++ apps/rc/src/pages/Devices/Switch/SwitchDetails/SwitchIncidentsTab/index.tsx
@@ -1,9 +1,22 @@
-import { useIntl } from 'react-intl'
+import { IncidentTabContent }         from '@acx-ui/analytics/components'
+import { AnalyticsFilter }            from '@acx-ui/analytics/utils'
+import { useSwitchDetailHeaderQuery } from '@acx-ui/rc/services'
+import { useParams  }                 from '@acx-ui/react-router-dom'
+import { useDateFilter }              from '@acx-ui/utils'
+
 
 export function SwitchIncidentsTab () {
-  const { $t } = useIntl()
+  const { dateFilter } = useDateFilter()
+  const params = useParams()
+  const switchDetailQuery = useSwitchDetailHeaderQuery({ params })
+  const filters = {
+    ...dateFilter,
+    path: [
+      { type: 'network', name: 'Network' },
+      { type: 'switchGroup', name: switchDetailQuery.data?.venueName },
+      { type: 'switch', name: switchDetailQuery.data?.switchMac }
+    ]
+  } as AnalyticsFilter
 
-  return (
-    <>{ $t({ defaultMessage: 'Incidents' })}</>
-  )
+  return <IncidentTabContent filters={filters} disableGraphs/>
 }
