0af4aeb0c55b128f49cd3e8c7098c98b2e65715f
ki1003 <khairul.iman@ruckuswireless.com>
2023-01-11 11:04:52 +0800

    Revert "ACX-19505: wip: remove double on pagination"
    This reverts commit 3471a49f46bea19a0ed9d730d0a91c6b94f97d6c.

--- apps/main/src/pages/SearchResults/index.tsx
+++ apps/main/src/pages/SearchResults/index.tsx
@@ -115,9 +115,7 @@ function SearchResult ({ searchVal }: { searchVal: string | undefined }) {
   const { $t } = useIntl()
   const results = searches.map(search => search(searchVal as string, $t))
   const count = results.reduce((count, { result }) => count + (result.data?.totalCount || 0), 0)
-  return <Loader
-    states={results.map(({ result }) => ({ isLoading: result.isLoading, isFetching: false }))}
-  >
+  return <Loader states={results.map(({ result }) => result)}>
     {count
       ? <>
         <PageHeader title={$t(
