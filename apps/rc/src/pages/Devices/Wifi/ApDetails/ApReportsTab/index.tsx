8c61271dd4c870ec56ca9fbb4b64af7b086fecff
jitendra.dangi <jitendra.dangi@ruckuswireless.com>
2023-01-12 11:36:43 +0530

    ACX-22198: chore: used useApContext for the apMac

--- apps/rc/src/pages/Devices/Wifi/ApDetails/ApReportsTab/index.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApDetails/ApReportsTab/index.tsx
@@ -1,27 +1,18 @@
-import { useParams } from 'react-router-dom'
-
-import { useApViewModelQuery }  from '@acx-ui/rc/services'
 import { EmbeddedReport }       from '@acx-ui/reports/components'
 import {
   ReportType,
   reportTypeDataStudioMapping
 } from '@acx-ui/reports/components'
 
+import { useApContext } from '../ApContext'
 
 export function ApReportsTab () {
-  const params = useParams()
-  const apViewModelPayload = {
-    fields: ['apMac'],
-    filters: { serialNumber: [params.apId] }
-  }
-  const { data: currentAP }= useApViewModelQuery({
-    params, payload: apViewModelPayload
-  })
+  const { apMac } = useApContext()
 
   return (
     <EmbeddedReport
       embedDashboardName={reportTypeDataStudioMapping[ReportType.AP_DETAIL]}
-      rlsClause={`"apMac" in ('${currentAP?.apMac}')`}
+      rlsClause={`"apMac" in ('${apMac}')`}
     />
   )
 }
