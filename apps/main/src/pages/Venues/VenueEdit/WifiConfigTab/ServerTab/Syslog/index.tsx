e9aac3c93d0fc3ee18c022b1590d0262c2ddc22e
peter.lin <Peter.Lin@ruckuswireless.com>
2023-01-18 17:03:00 +0800

    ACX-21978: ACX-UI: Syslog instance page

--- apps/main/src/pages/Venues/VenueEdit/WifiConfigTab/ServerTab/Syslog/index.tsx
+++ apps/main/src/pages/Venues/VenueEdit/WifiConfigTab/ServerTab/Syslog/index.tsx
@@ -154,7 +154,7 @@ export function Syslog () {
             })}
             style={{ width: '200px' }}
           />
-          {!unreleased &&
+          {unreleased &&
           <TenantLink
             to={getPolicyRoutePath({
               type: PolicyType.SYSLOG,
