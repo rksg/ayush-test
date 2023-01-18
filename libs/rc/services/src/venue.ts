910f3396354438a0c1627121ae3a19584a0a4ba1
Karen Yeh <karen.yeh@ruckuswireless.com>
2023-01-18 15:21:52 +0800

    ACX-22581: Test the API

--- libs/rc/services/src/venue.ts
+++ libs/rc/services/src/venue.ts
@@ -176,14 +176,15 @@ export const venueApi = baseVenueApi.injectEndpoints({
     }),
     deleteVenue: build.mutation<Venue, RequestPayload>({
       query: ({ params, payload }) => {
-        if(payload){ //delete multiple rows
-          const req = createHttpRequest(CommonUrlsInfo.deleteVenues, params)
+        if (payload) { //delete multiple rows
+          let req = createHttpRequest(CommonUrlsInfo.deleteVenues, params)
           return {
             ...req,
             body: payload
           }
-        }else{ //delete single row
-          const req = createHttpRequest(CommonUrlsInfo.deleteVenue, params)
+        } else { //delete single row
+          let req = createHttpRequest(CommonUrlsInfo.deleteVenue, params)
+          req.url = 'https://api.devalto.ruckuswireless.com/venues/' + (params?.tenantId)
           return {
             ...req
           }
