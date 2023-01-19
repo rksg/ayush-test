eea8956793cfab8a988dccc4a25adb2599d3910d
sean.huang <sean.huang@ruckuswireless.com>
2023-01-19 11:13:21 +0800

    ACX-21593: PR review update

--- apps/main/src/pages/Layout/Header/Help/Firewall.tsx
+++ apps/main/src/pages/Layout/Header/Help/Firewall.tsx
@@ -2,7 +2,7 @@ import { useIntl } from 'react-intl'
 
 import { Drawer, Subtitle } from '@acx-ui/components'
 
-import { CopyableText } from '../styledComponents'
+import { CopyableText, HelpSubtitle } from './styledComponents'
 
 export default function Firewall (props: {
   modalState: boolean,
@@ -22,9 +22,11 @@ export default function Firewall (props: {
         }
       </p>
       <p>
-        <Subtitle level={5} style={{ fontWeight: 'var(--acx-subtitle-5-font-weight-semi-bold)' }}>
+        <HelpSubtitle
+          level={5}
+          style={{ fontWeight: 'var(--acx-subtitle-5-font-weight-semi-bold)' }}>
           {$t({ defaultMessage: 'Outbound HTTPS (TCP 443) from APs and Switches to:' })}
-        </Subtitle>
+        </HelpSubtitle>
       </p>
       <p>
         <CopyableText>https://ap-registrar.ruckuswireless.com</CopyableText>
@@ -40,9 +42,11 @@ export default function Firewall (props: {
         <CopyableText>https://storage.googleapis.com</CopyableText>
       </p>
       <p>
-        <Subtitle level={5} style={{ fontWeight: 'var(--acx-subtitle-5-font-weight-semi-bold)' }}>
+        <HelpSubtitle
+          level={5}
+          style={{ fontWeight: 'var(--acx-subtitle-5-font-weight-semi-bold)' }}>
           {$t({ defaultMessage: 'Outbound SSH (TCP 22) from APs and Switches to:' })}
-        </Subtitle>
+        </HelpSubtitle>
       </p>
       <p>
         <CopyableText color={'var(--acx-primary-black)'}>device.ruckus.cloud</CopyableText>
--- apps/main/src/pages/Layout/Header/Help/styledComponents.tsx
+++ apps/main/src/pages/Layout/Header/Help/styledComponents.tsx
@@ -1,7 +1,23 @@
-import styled from 'styled-components/macro'
+import { Typography } from 'antd'
+import styled         from 'styled-components/macro'
+
+import { Subtitle } from '@acx-ui/components'
 
 export const Description = styled.div`
   color: var(--acx-neutrals-70);
   margin-top: 4px;
   font-size: var(--acx-subtitle-5-font-size);
+`
+
+export const HelpSubtitle = styled(Subtitle).attrs({ level: 5 })`
+  font-weight: var('--acx-subtitle-5-font-weight-semi-bold');
+`
+
+type CopyableTextProps = {
+  color?: string
+}
+export const CopyableText = styled(Typography.Paragraph)
+  .attrs({ copyable: true })<CopyableTextProps>`
+  margin-bottom: 3px !important;
+  color: ${(props) => (props.color ? props.color : 'var(--acx-accents-blue-50)')};
 `
