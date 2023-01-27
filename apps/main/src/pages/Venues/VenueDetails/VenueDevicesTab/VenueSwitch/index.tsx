51f3ae15721d56ec5c70050c3aa9ec2434105d4f
jitendra.dangi <jitendra.dangi@ruckuswireless.com>
2023-01-27 12:47:43 +0530

    ACX-22863: feat: embed switches report

--- apps/main/src/pages/Venues/VenueDetails/VenueDevicesTab/VenueSwitch/index.tsx
+++ apps/main/src/pages/Venues/VenueDetails/VenueDevicesTab/VenueSwitch/index.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 
-import { Radio } from 'antd'
+import { Radio }     from 'antd'
+import { useParams } from 'react-router-dom'
 
 import { LineChartOutline, ListSolid, PortSolid } from '@acx-ui/icons'
 import { SwitchPortTable, SwitchTable }           from '@acx-ui/rc/components'
+import { EmbeddedReport }                         from '@acx-ui/reports/components'
+import {
+  ReportType,
+  reportTypeDataStudioMapping
+} from '@acx-ui/reports/components'
 
 import { IconRadioGroup } from '../VenueWifi/styledComponents'
 
 export function VenueSwitch () {
+  const params = useParams()
   const [ showIdx, setShowInx ] = useState(1)
   return (<>
     <IconRadioGroup value={showIdx}
@@ -18,7 +25,13 @@ export function VenueSwitch () {
       <Radio.Button value={1}><ListSolid /></Radio.Button>
       <Radio.Button value={2}><PortSolid /></Radio.Button>
     </IconRadioGroup>
-    { showIdx === 0 && <div></div>} {/* TODO: Venue Device Switch Report */}
+    { showIdx === 0 && (
+      <div style={{ paddingTop: 20 }}>
+        <EmbeddedReport
+          embedDashboardName={reportTypeDataStudioMapping[ReportType.SWITCH]}
+          rlsClause={`"switchGroupLevelOneName" in ('${params?.venueId}')`}
+        />
+      </div>)} {/* TODO: Venue Device Switch Report */}
     { showIdx === 1 && <SwitchTable />}
     { showIdx === 2 && <SwitchPortTable isVenueLevel={true} />}
   </>)
