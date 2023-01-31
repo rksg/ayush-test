c87f6c17dd39ee827d529e460167574977b84d60
Eric Leu <Eric.Leu@ruckuswireless.com>
2023-01-31 10:30:21 -0800

    ACX-19811: merge from master

--- apps/main/src/pages/Layout/index.tsx
+++ apps/main/src/pages/Layout/index.tsx
@@ -8,18 +8,20 @@ import {
 import { SplitProvider } from '@acx-ui/feature-toggle'
 import { HomeSolid }     from '@acx-ui/icons'
 import {
-  MspEcDropdownList,
-  RegionDropdown
-} from '@acx-ui/msp/components'
-import {
   ActivityButton,
   AlarmsButton,
   HelpButton,
   UserButton
 } from '@acx-ui/main/components'
-import { CloudMessageBanner } from '@acx-ui/rc/components'
-import { getBasePath, Link, Outlet }             from '@acx-ui/react-router-dom'
-import { isDelegationMode, TenantIdFromJwt, notAvailableMsg }    from '@acx-ui/utils'
+import {
+  MspEcDropdownList,
+  RegionDropdown
+} from '@acx-ui/msp/components'
+import { CloudMessageBanner }                from '@acx-ui/rc/components'
+import { isDelegationMode, TenantIdFromJwt } from '@acx-ui/rc/utils'
+import { getBasePath, Link, Outlet }         from '@acx-ui/react-router-dom'
+import { notAvailableMsg }                   from '@acx-ui/utils'
+
 import { useMenuConfig } from './menuConfig'
 import SearchBar         from './SearchBar'
 
@@ -42,12 +44,6 @@ function Layout () {
         </>
       }
 
-      // leftHeaderContent={
-      //   <div style={{ width: '40%', display: 'flex', alignItems: 'center' }}>
-      //     {/* <RegionButton/>
-      //     <LicenseBar/> */}
-      //   </div>
-      // }
       rightHeaderContent={<>
         {isDelegationMode() && <MspEcDropdownList/>}
         <SearchBar />
