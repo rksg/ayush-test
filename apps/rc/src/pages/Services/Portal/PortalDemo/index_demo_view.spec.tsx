a0278826e4cd1675cfba50832c335bc61d68bc17
John Lin <john.lin@ruckuswireless.com>
2023-01-31 11:08:50 +0800

    ACX-10990 update test case

--- apps/rc/src/pages/Services/Portal/PortalDemo/index_demo_view.spec.tsx
+++ apps/rc/src/pages/Services/Portal/PortalDemo/index_demo_view.spec.tsx
@@ -5,8 +5,8 @@ import { rest }  from 'msw'
 
 import { defaultComDisplay, PortalUrlsInfo
 }     from '@acx-ui/rc/utils'
-import { Provider }                              from '@acx-ui/store'
-import { render, fireEvent, screen, mockServer } from '@acx-ui/test-utils'
+import { Provider }                   from '@acx-ui/store'
+import { render, screen, mockServer } from '@acx-ui/test-utils'
 
 import Photo                     from '../../../../assets/images/portal-demo/PortalPhoto.svg'
 import Powered                   from '../../../../assets/images/portal-demo/PoweredLogo.svg'
@@ -69,57 +69,51 @@ describe('PortalDemo', () => {
         </Form>
       </Provider>, { route: { params } }
     )
-    const file = new File(['logo ruckus'],
-      'https://storage.cloud.google.com/ruckus-web-1/acx-ui-static-resources/logo-ruckus.png',
-      { type: 'image/png' })
-    await userEvent.click(await screen.findByTitle('background setting'))
-    await userEvent.click(await screen.findByText('Set background color'))
-    await userEvent.click(await screen.findByTitle('#D0021B'))
-
-    await userEvent.upload(await screen.findByLabelText('Select image'),file)
-    await userEvent.click(await screen.findByText('Select image'))
-
-
-    fireEvent.mouseLeave(await screen.findByPlaceholderText('welcometext'))
-
-    await userEvent.click(await screen.findByPlaceholderText('welcometext'))
-    await userEvent.click(await screen.findByTitle('textplus'))
-    await userEvent.click(await screen.findByTitle('textplus'))
-    await userEvent.click(await screen.findByTitle('textminus'))
-
-    await userEvent.click(await screen.findByTitle('colorpick'))
-    await userEvent.click(await screen.findByTitle('eyehide'))
-    await userEvent.type(await screen.findByPlaceholderText('welcometext'),'welcome text')
-    fireEvent.mouseOver(await screen.findByPlaceholderText('buttonsetting'))
-    fireEvent.mouseLeave(await screen.findByPlaceholderText('buttonsetting'))
-    fireEvent.click(await screen.findByPlaceholderText('buttonsetting'))
-    await userEvent.click((await screen.findAllByTitle('colorpick'))[1])
-    await userEvent.click(await screen.findByTitle('#F5A623'))
-    fireEvent.click(await screen.findByText('Reset'))
-    fireEvent.click(await screen.findByText('Preview'))
-  })
-
-  it('should render portal demo preview successfully', async () => {
-    render(
-      <Provider>
-        <Form>
-          <PortalDemo value={mockDemo} isPreview={true}/>
-        </Form>
-      </Provider>
-    )
     await userEvent.click((await screen.findAllByTitle('Click Through'))[0])
     await userEvent.click((await screen.findAllByTitle('Guest Pass - Connect'))[0])
+    await userEvent.click(await screen.findByPlaceholderText('buttonsetting'))
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[0])
+    await userEvent.click(await screen.findByTitle('#F5A623'))
     await userEvent.click((await screen.findAllByTitle('Guest Pass - Connect'))[0])
+
     await userEvent.click((await screen.findAllByTitle('Guest Pass - Forgot password'))[0])
+    await userEvent.click(await screen.findByPlaceholderText('buttonsetting'))
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[0])
+
+    await userEvent.click(await screen.findByTitle('#F5A623'))
+    await userEvent.click((await screen.findAllByRole('tab'))[1])
+    await userEvent.click((await screen.findAllByPlaceholderText('buttonsetting'))[1])
+
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[1])
+    await userEvent.click(await screen.findByTitle('#F5A623'))
     await userEvent.click((await screen.findAllByTitle('Guest Pass - Forgot password'))[0])
+
     await userEvent.click((await screen.findAllByTitle('Self Sign In - Connect'))[0])
     await userEvent.click((await screen.findAllByTitle('Self Sign In - Connect'))[0])
     await userEvent.click((await screen.findAllByTitle('Self Sign In - Register/Confirm'))[0])
+    await userEvent.click(await screen.findByPlaceholderText('buttonsetting'))
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[0])
+    await userEvent.click(await screen.findByTitle('#F5A623'))
+    await userEvent.click((await screen.findAllByRole('tab'))[1])
+    await userEvent.click((await screen.findAllByPlaceholderText('buttonsetting'))[1])
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[1])
+    await userEvent.click(await screen.findByTitle('#F5A623'))
     await userEvent.click((await screen.findAllByTitle('Self Sign In - Register/Confirm'))[0])
     await userEvent.click((await screen.findAllByTitle('Host Approval - Register/Confirm'))[0])
+    await userEvent.click(await screen.findByPlaceholderText('buttonsetting'))
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[0])
+    await userEvent.click(await screen.findByTitle('#F5A623'))
+    await userEvent.click((await screen.findAllByRole('tab'))[1])
+    await userEvent.click((await screen.findAllByPlaceholderText('buttonsetting'))[1])
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[1])
+    await userEvent.click(await screen.findByTitle('#F5A623'))
     await userEvent.click((await screen.findAllByTitle('Host Approval - Register/Confirm'))[0])
+
     await userEvent.click((await screen.findAllByTitle('Connection confirmed'))[0])
     await userEvent.click((await screen.findAllByTitle('Connection confirmed'))[0])
-    fireEvent.click((await screen.findAllByTitle('Terms & Conditions'))[0])
+    await userEvent.click((await screen.findAllByTitle('Terms & Conditions'))[0])
+    await userEvent.click(await screen.findByPlaceholderText('buttonsetting'))
+    await userEvent.click((await screen.findAllByTitle('colorpick'))[0])
+    await userEvent.click(await screen.findByTitle('#F5A623'))
   })
 })
