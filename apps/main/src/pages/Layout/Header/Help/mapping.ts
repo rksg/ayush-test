398977250c4c2343d4e923d30825f1c22cbc9a09
sean.huang <sean.huang@ruckuswireless.com>
2023-01-31 16:14:13 +0800

    ACX-17482: mapping file adjustment

--- apps/main/src/pages/Layout/Header/Help/mapping.ts
+++ apps/main/src/pages/Layout/Header/Help/mapping.ts
@@ -1,6 +1,7 @@
 //https://docs.cloud.ruckuswireless.com/r1/mapfile/doc-mapper.json
 const mappingTable = {
   '/dashboard': 'dashboard',
-  '/venue-details/overview': 'venue-overview'
+  '/venue-details/overview': 'venue-overview',
+  '/administration/accountSettings': 'administration'
 }
 export default mappingTable
