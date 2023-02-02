3d8140f0aa2b1fa7a1453f54673a36e76b0a3f2b
sean.huang <sean.huang@ruckuswireless.com>
2023-02-02 10:26:07 +0800

    Merge branch 'master' into feature/ACX-21593
    # Conflicts:
    #	apps/rc/src/Routes.tsx
    #	apps/rc/src/pages/Networks/NetworkForm/NetworkForm.tsx
    #	apps/rc/src/pages/Networks/NetworkForm/parser.tsx
    #	apps/rc/src/pages/Policies/PoliciesTable/index.tsx
    #	libs/common/utils/src/validationMessages.ts
    #	libs/rc/services/src/policy.ts
    #	libs/rc/utils/src/types/policies/index.ts

--- apps/rc/src/pages/Policies/PoliciesTable/index.tsx
+++ apps/rc/src/pages/Policies/PoliciesTable/index.tsx
@@ -3,8 +3,6 @@ import { useParams } from 'react-router-dom'
 
 import { Button, Loader, PageHeader, showActionModal, Table, TableProps } from '@acx-ui/components'
 import {
-  useDelRoguePolicyMutation,
-  usePolicyListQuery,
   useDelVLANPoolPolicyMutation,
   useDeleteClientIsolationMutation,
   useDelRoguePolicyMutation,
