cf12739c0eb9c91d631e8747abc0cb3241a5b6e5
sean.huang <sean.huang@ruckuswireless.com>
2023-02-02 11:12:15 +0800

    ACX-21593: fix eslint error

--- apps/rc/src/pages/Policies/PoliciesTable/index.tsx
+++ apps/rc/src/pages/Policies/PoliciesTable/index.tsx
@@ -96,7 +96,6 @@ export default function PoliciesTable () {
   const navigate = useNavigate()
   const tenantBasePath: Path = useTenantLink('')
 
-  const [ delRoguePolicy ] = useDelRoguePolicyMutation()
   const [ delVLANPoolPolicy ] = useDelVLANPoolPolicyMutation()
   // const [ delRoguePolicy ] = useDelRoguePolicyMutation()
 
