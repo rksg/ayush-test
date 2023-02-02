24bc9dc41f3cfd36dfdab7d40dab97b99e302baf
cbokil <chinmay.bokil@ruckuswireless.com>
2023-02-02 16:49:13 +0800

    ACX-22773: wip: added incident tab in networks with ssid filter

--- apps/rc/src/pages/Networks/NetworkDetails/NetworkIncidentsTab/index.tsx
+++ apps/rc/src/pages/Networks/NetworkDetails/NetworkIncidentsTab/index.tsx
@@ -1,8 +1,17 @@
-import { useIntl } from 'react-intl'
+import { IncidentTabContent }         from '@acx-ui/analytics/components'
+import { AnalyticsFilter, defaultNetworkPath }            from '@acx-ui/analytics/utils'
+import { useSearchParams  }                 from '@acx-ui/react-router-dom'
+import { useDateFilter, useEncodedParameter }              from '@acx-ui/utils'
 
 export function NetworkIncidentsTab () {
-  const { $t } = useIntl()
-  return (
-    <>{$t({ defaultMessage: 'Incidents' })}</>
-  )
+  const { dateFilter } = useDateFilter()
+  const [searchParams] = useSearchParams()
+  const { read } = useEncodedParameter('ssid')
+  const ssids = searchParams.get('ssid') ? [read()] : []
+  const filters = {
+    ...dateFilter,
+    path: defaultNetworkPath,
+    filter: { ssids }
+  } as AnalyticsFilter
+  return <IncidentTabContent filters={filters} disableGraphs/>
 }
