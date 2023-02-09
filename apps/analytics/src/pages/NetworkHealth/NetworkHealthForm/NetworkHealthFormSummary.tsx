802a4bd99e6da7547a2e9ba29d230e0b1c9eb9cb
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2023-02-09 12:52:59 +0800

    ACX-21909: update row gutter to match other form

--- apps/analytics/src/pages/NetworkHealth/NetworkHealthForm/NetworkHealthFormSummary.tsx
+++ apps/analytics/src/pages/NetworkHealth/NetworkHealthForm/NetworkHealthFormSummary.tsx
@@ -1,4 +1,5 @@
-import { useIntl } from 'react-intl'
+import { Col, Row } from 'antd'
+import { useIntl }  from 'react-intl'
 
 import {
   StepsFormNew
@@ -13,21 +14,24 @@ export function NetworkHealthFormSummary () {
 
   return <>
     <StepsFormNew.Title children={$t(contents.steps.summary)} />
+    <Row gutter={20}>
+      <Col span={12} xl={10} xxl={8}>
+        <FormItems.TestName.FieldSummary />
+        <FormItems.ClientType.FieldSummary />
+        <FormItems.TestType.FieldSummary />
+        <FormItems.WlanName.FieldSummary />
+        <FormItems.AuthenticationMethod.FieldSummary />
+        <FormItems.Username.FieldSummary />
+        <FormItems.Password.FieldSummary />
+        <FormItems.RadioBand.FieldSummary />
+        <FormItems.DnsServer.FieldSummary />
+        <FormItems.PingAddress.FieldSummary />
+        <FormItems.TracerouteAddress.FieldSummary />
+        <FormItems.SpeedTest.FieldSummary />
 
-    <FormItems.TestName.FieldSummary />
-    <FormItems.ClientType.FieldSummary />
-    <FormItems.TestType.FieldSummary />
-    <FormItems.WlanName.FieldSummary />
-    <FormItems.AuthenticationMethod.FieldSummary />
-    <FormItems.Username.FieldSummary />
-    <FormItems.Password.FieldSummary />
-    <FormItems.RadioBand.FieldSummary />
-    <FormItems.DnsServer.FieldSummary />
-    <FormItems.PingAddress.FieldSummary />
-    <FormItems.TracerouteAddress.FieldSummary />
-    <FormItems.SpeedTest.FieldSummary />
-
-    <FormItems.APsSelection.FieldSummary />
+        <FormItems.APsSelection.FieldSummary />
+      </Col>
+    </Row>
   </>
 }
 
