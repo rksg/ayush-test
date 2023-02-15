036ed1f1e575c5a76db40b5886c10fc8f689df1e
cbokil <chinmay.bokil@ruckuswireless.com>
2023-02-15 20:01:30 +0800

    ACX-22775: fix: added avgTTc api under network wrapper, added tests

--- apps/rc/src/pages/Networks/wireless/NetworkDetails/NetworkOverviewTab/index.tsx
+++ apps/rc/src/pages/Networks/wireless/NetworkDetails/NetworkOverviewTab/index.tsx
@@ -1,3 +1,5 @@
+import { useIntl } from 'react-intl'
+
 import {
   IncidentBySeverityDonutChart,
   KpiWidget,
@@ -18,11 +20,14 @@ import {
   Loader,
   Card
 } from '@acx-ui/components'
-import { useDateFilter } from '@acx-ui/utils'
+import { NetworkTypeEnum } from '@acx-ui/rc/utils'
+import { useDateFilter }   from '@acx-ui/utils'
 
+import { networkTypes }                     from '../../NetworkForm/contentsMap'
 import { extractSSIDFilter, useGetNetwork } from '../services'
 
 export function NetworkOverviewTab () {
+  const { $t } = useIntl()
   const { dateFilter } = useDateFilter()
   const network = useGetNetwork()
   const ssids = extractSSIDFilter(network)
@@ -31,28 +36,32 @@ export function NetworkOverviewTab () {
     path: defaultNetworkPath,
     filter: { ssids }
   } as AnalyticsFilter
+  const title = network.data?.type
+    ? $t(networkTypes[network.data?.type as NetworkTypeEnum])
+    : ''
   return (<Loader states={[network]}>
     <GridRow>
-      <GridCol col={{ span: 24 }} style={{ height: '140px' }}>
-        <Card title={'Network Type'}>
+      <GridCol col={{ span: 24 }} style={{ height: '142px' }}>
+        <Card>
+          <Card.Title>{title}</Card.Title>
           <GridRow style={{ flexGrow: '1' }}>
             <GridCol col={{ span: 2 }} />
             <GridCol col={{ span: 4 }} style={{ margin: 'auto' }}>
-                <IncidentBySeverityDonutChart type='no-card-style' filters={filters}/>
+              <IncidentBySeverityDonutChart type='no-card-style' filters={filters}/>
             </GridCol>
             <GridCol col={{ span: 6 }} style={{ margin: 'auto' }}>
-                <KpiWidget type='no-chart-style' filters={filters} name='connectionSuccess' />
+              <KpiWidget type='no-chart-style' filters={filters} name='connectionSuccess' />
             </GridCol>
             <GridCol col={{ span: 6 }} style={{ margin: 'auto' }}>
-                <TtcTimeWidget filters={filters}/>
+              <TtcTimeWidget filters={filters}/>
             </GridCol>
             <GridCol col={{ span: 6 }} style={{ margin: 'auto' }}>
-                <KpiWidget
-                  type='no-chart-style'
-                  filters={filters}
-                  name='clientThroughput'
-                  threshold={kpiConfig.clientThroughput.histogram.initialThreshold}
-                />
+              <KpiWidget
+                type='no-chart-style'
+                filters={filters}
+                name='clientThroughput'
+                threshold={kpiConfig.clientThroughput.histogram.initialThreshold}
+              />
             </GridCol>
           </GridRow>
         </Card>
