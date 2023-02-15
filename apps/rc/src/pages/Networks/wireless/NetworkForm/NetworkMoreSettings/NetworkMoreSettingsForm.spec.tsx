e3359525d8d2fedc756373ae1a0472cb734c5182
Karen Yeh <karen.yeh@ruckuswireless.com>
2023-02-15 11:33:45 +0800

    ACX-16173: Fix the lint issue

--- apps/rc/src/pages/Networks/wireless/NetworkForm/NetworkMoreSettings/NetworkMoreSettingsForm.spec.tsx
+++ apps/rc/src/pages/Networks/wireless/NetworkForm/NetworkMoreSettings/NetworkMoreSettingsForm.spec.tsx
@@ -5,9 +5,9 @@ import userEvent from '@testing-library/user-event'
 import { Form }  from 'antd'
 import { rest }  from 'msw'
 
-import { CommonUrlsInfo, NetworkSaveData }               from '@acx-ui/rc/utils'
-import { Provider }                                      from '@acx-ui/store'
-import { mockServer, fireEvent, within, render, screen } from '@acx-ui/test-utils'
+import { CommonUrlsInfo, NetworkSaveData }    from '@acx-ui/rc/utils'
+import { Provider }                           from '@acx-ui/store'
+import { mockServer, within, render, screen } from '@acx-ui/test-utils'
 
 import { externalProviders, policyListResponse } from '../__tests__/fixtures'
 
