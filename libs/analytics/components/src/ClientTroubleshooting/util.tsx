3bdac3725dd41119ca1b7ba8990ced4d7fa9c74a
ki1003 <khairul.iman@ruckuswireless.com>
2023-02-16 15:18:19 +0800

    ACX-22276: wip: add name parameter to scatter point

--- libs/analytics/components/src/ClientTroubleshooting/util.tsx
+++ libs/analytics/components/src/ClientTroubleshooting/util.tsx
@@ -93,6 +93,7 @@ export const transformEvents = (
       ...data,
       type: event === 'EVENT_CLIENT_ROAMING' ? TYPES.ROAMING : TYPES.CONNECTION_EVENTS,
       key: time + mac + eventType + index,
+      name: time + mac + eventType + index,
       start: time,
       end: time,
       ssid,
