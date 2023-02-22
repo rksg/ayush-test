906614df5f26870cad1c7a42152a7e42b2df1373
ny1012 <Nishanth.Yerramsetty@ruckuswireless.com>
2023-02-22 17:57:11 +0800

    ACX-23297 : Add column conditions

--- apps/analytics/src/pages/NetworkHealth/NetworkHealthDetails/DetailsTable/index.tsx
+++ apps/analytics/src/pages/NetworkHealth/NetworkHealthDetails/DetailsTable/index.tsx
@@ -1,17 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable comma-dangle */
-import { upperFirst, without } from 'lodash'
-import { MessageDescriptor }   from 'react-intl'
+import { upperFirst, without }        from 'lodash'
+import { MessageDescriptor, useIntl } from 'react-intl'
 
 import { Table, TableProps } from '@acx-ui/components'
+import { cssStr }            from '@acx-ui/components'
+import { Tooltip }           from '@acx-ui/components'
+import { EyeOpenSolid }      from '@acx-ui/icons'
+import { TenantLink }        from '@acx-ui/react-router-dom'
+import {  formatter }        from '@acx-ui/utils'
 
 import { authMethodsByCode }            from '../../authMethods'
 import {  useNetworkHealthTestResults } from '../../services'
 import { stage }                        from '../../stages'
 
-
-// import {specs}
-import { data as testData } from './data'
+import * as UI from './styledComponents'
 
 type RecordType = {
   key: string
@@ -24,7 +27,13 @@ type RecordType = {
   children?: RecordType[]
 }
 
-
+const badgeTextMap = {
+  'success': 'Pass',
+  'error': 'Error',
+  'fail': 'Fail',
+  'n/a': 'N/A',
+  'pending': 'Pending'
+}
 
 export const columns: TableProps<RecordType>['columns'] = [
   {
@@ -82,6 +91,8 @@ export const columns: TableProps<RecordType>['columns'] = [
 
 const Details = () => {
 
+  const { $t } = useIntl()
+
   const queryResults = useNetworkHealthTestResults()
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -93,94 +104,199 @@ const Details = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const wlanAuthSettings = (queryResults?.data as any)?.wlanAuthSettings
 
-  console.log(queryResults)
-  console.log(config)
-  console.log(clientType)
-  console.log(items)
-  console.log(wlanAuthSettings)
   const isWirelessClient = clientType === 'virtual-wireless-client'
   const stagesFromConfig = (config : any) => {
     if (!config?.authenticationMethod) { return [] }
-    const stages = [authMethodsByCode[config.authenticationMethod].stages, stage('dns')]
+    const stages = [...authMethodsByCode[config.authenticationMethod].stages,stage('dns')]
     if (config?.pingAddress) { stages.push(stage('ping')) }
     if (config?.tracerouteAddress) { stages.push(stage('traceroute')) }
     if (config?.speedTestEnabled) { stages.push(stage('speedTest')) }
     return stages
-  }
-  const genStageCol = (key : any) => ({
-    title: (stages?.find((s : any) => s?.key === 'traceroute')?.title),
-    dataIndex: 'name',
-    key: 'name',
-    filterable: true,
-    searchable: true
-  })
-
-  const genSpeedCol = key => {
-    return {
-      title: upperFirst(key),
-      dataIndex: 'name',
-      key: 'name',
-      filterable: true,
-      searchable: true
-    }
   }
 
   const stages = stagesFromConfig(config)?.map(
     ({ key, title }: { key: any; title: MessageDescriptor }) => ({
       key,
       title
-      // title: title?.replace('Authentication', 'Auth'),
     })
   )
+  const genStageCol = (key : any) => ({
+    title: $t(stages?.find((s : any) => s?.key === key)?.title).replace('Authentication', 'Auth'),
+    dataIndex: key,
+    key: key,
+    filterable: false,
+    searchable: true,
+    render: function (text: unknown, row) {
+      return (
+        <UI.Badge type={row[key]}>{badgeTextMap[row[key]]}</UI.Badge>
+      )
+    }
+  })
+  const genSpeedCol = key => {
+    return {
+      title: upperFirst(key),
+      dataIndex: key,
+      key: key,
+      filterable: false,
+      searchable: false,
+      render: function (text: unknown, row) {
+        const { speedTest, speedTestServer } = row
+        text=speedTest === 'success'
+          ? formatter('networkSpeedFormat')(row[key])
+          : badgeTextMap[speedTest]
+
+        return (
+          <UI.Badge type={speedTest}>{text}</UI.Badge>
+        )
+      }
+    }
+  }
   const columns: TableProps<RecordType>['columns'] = [
     {
       title: isWirelessClient ? ('Target AP Name') : ('AP Name'),
       dataIndex: 'apName',
       key: 'apName',
-      filterable: true,
-      searchable: true
+      filterable: false,
+      searchable: true,
+      render: function (text: unknown, row) {
+        const { apMac } = row
+        return (
+          <UI.VenueName>
+            <TenantLink
+              to={`devices/wifi/${apMac}/details/overview`}
+              title={isWirelessClient ? 'Target AP Details' : 'AP Details' as string}>
+              {text as string}
+            </TenantLink>
+          </UI.VenueName>
+        )
+      }
     },
     {
       title: isWirelessClient ? ('Target AP MAC') : ('AP MAC'),
       dataIndex: 'apMac',
       key: 'apMac',
       align: 'center',
-      filterable: true
+      filterable: false,
+      render: function (text: unknown, row) {
+        const { apMac } = row
+        return (
+          <UI.VenueName>
+            <TenantLink
+              to={`devices/wifi/${apMac}/details/overview`}
+              title={isWirelessClient ? 'Target AP Details' : 'AP Details' as string}>
+              {text as string}
+            </TenantLink>
+          </UI.VenueName>
+        )
+      }
     }
   ]
   if (isWirelessClient) {
     columns.push(
       {
         title: 'Station AP Name',
-        dataIndex: 'name',
-        key: 'name',
-        filterable: true,
-        searchable: true
+        dataIndex: 'stationAp',
+        key: 'stationAp',
+        filterable: false,
+        searchable: true,
+        render: function (text: unknown, row) {
+          const { stationAp, apMac } = row
+          return (
+            <UI.VenueName>
+              <TenantLink
+                to={`devices/wifi/${stationAp.mac}/details/overview`}
+                title={'Station AP Details' as string}>
+                {stationAp.name as string}
+              </TenantLink>
+            </UI.VenueName>
+          )
+        }
       },
       {
         title: 'Station AP MAC',
-        dataIndex: 'name',
-        key: 'name',
-        filterable: true,
-        searchable: true
+        dataIndex: 'stationAp',
+        key: 'stationAp',
+        filterable: false,
+        searchable: true,
+        render: function (text: unknown, row) {
+          const { apMac, stationAp } = row
+          return (
+            <UI.VenueName>
+              <TenantLink
+                to={`devices/wifi/${stationAp.mac}/details/overview`}
+                title={'Station AP Details' as string}>
+                {stationAp.mac as string}
+              </TenantLink>
+            </UI.VenueName>
+          )
+        }
       },
       {
         title: 'Station AP SNR',
-        dataIndex: 'name',
-        key: 'name',
-        filterable: true,
-        searchable: true
+        dataIndex: 'stationAp',
+        key: 'stationAp',
+        filterable: false,
+        searchable: true,
+        render: function (text: unknown, row) {
+          const { stationAp } = row
+          return (
+            <span>
+              {stationAp ? formatter('decibelFormat')(stationAp.snr) : '' as string}
+            </span>
+          )
+        }
       }
     )
+  }
+  const stagesKeys = stages.map(s => s.key)
+  columns.push(...without(stagesKeys, 'ping', 'traceroute', 'speedTest').map(s => genStageCol(s)))
+
+  if (stagesKeys.includes('ping')) {
+    columns.push({
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      title: $t(stages.find((s : any) => s.key === 'ping').title),
+      dataIndex: 'ping',
+      key: 'ping',
+      filterable: false,
+      searchable: false,
+      render: function (text: unknown, row) {
+        const { ping, avgPingTime, pingTotal, pingReceive, error } = row
+        text= avgPingTime
+          ? formatter('durationFormat')(avgPingTime)
+          : badgeTextMap[ping]
+
+        return (
+          <UI.Badge type={ping}>{text}</UI.Badge>
+        )
+      }
+    })
   }
   if (config?.tracerouteAddress) {
     columns.push({
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      title: (stages.find((s : any) => s.key === 'traceroute').title),
-      dataIndex: 'name',
-      key: 'name',
-      filterable: true,
-      searchable: true
+      title: $t(stages.find((s : any) => s.key === 'traceroute').title),
+      dataIndex: 'tracerouteLog',
+      key: 'tracerouteLog',
+      filterable: false,
+      searchable: false,
+      render: function (text: unknown, row) {
+        console.log(row)
+        const { traceroute, tracerouteLog, error } = row
+        return (
+          <UI.Badge type={traceroute}>
+            {tracerouteLog ? (
+              <EyeOpenSolid
+                stroke={cssStr('--acx-primary-white')}
+                height={12}
+                width={12}
+                color={cssStr('--acx-primary-white')}
+              />
+            ) : (
+              badgeTextMap[traceroute]
+            )}
+          </UI.Badge>
+        )
+      }
     })
   }
   if (config?.speedTestEnabled) {
@@ -189,11 +305,6 @@ const Details = () => {
       genSpeedCol('download')
     )
   }
-  const stagesKeys = stages.map(s => s.key)
-
-  columns.push(...without(stagesKeys, 'ping', 'traceroute', 'speedTest').map(s => genStageCol(s)))
-  console.log(columns)
-
   return <Table<RecordType>
     columns={columns}
     dataSource={items}
---
+++ apps/analytics/src/pages/NetworkHealth/NetworkHealthDetails/DetailsTable/styledComponents.ts
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components/macro'
+
+export type TrendType = 'success' | 'fail' | 'n/a' | 'error'
+
+
+const badgeColor = ({ type }: { type: TrendType }) => {
+  switch (type) {
+    case 'success': return '--acx-semantics-green-50'
+    case 'fail': return '--acx-semantics-red-50'
+    case 'n/a': return '--acx-neutrals-50'
+    case 'error': return '--acx-accents-orange-50'
+    default: return '--acx-neutrals-50'
+  }
+}
+const textStyle = css`
+  font-family: var(--acx-neutral-brand-font);
+  font-size: var(--acx-subtitle-6-font-size);
+  color: var(--acx-primary-white);
+`
+export const VenueName = styled.div`
+    white-space: nowrap;
+    width: 125px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+export const Badge = styled.span`
+  display: inline-block;
+  border-radius: 10px;
+  padding: 3px 8px;
+  background-color: var(${badgeColor});
+  line-height: var(--acx-subtitle-6-line-height);
+  ${textStyle}
+  font-weight: var(--acx-subtitle-6-font-weight-bold);
+`
