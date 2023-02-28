07c05edc9bcc7222526b904823e424ed68805711
wilson.loh <wilson.loh@ruckuswireless.com>
2023-02-28 12:29:13 +0800

    ACX-22751: test - update unit tests

--- apps/rc/src/pages/Devices/Wifi/ApDetails/ApTimelineTab/index.spec.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApDetails/ApTimelineTab/index.spec.tsx
@@ -1,36 +1,31 @@
-import { rest }    from 'msw'
-import * as router from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import { rest }  from 'msw'
 
-import { CommonUrlsInfo }                                        from '@acx-ui/rc/utils'
-import { Provider }                                              from '@acx-ui/store'
-import { mockServer, render, screen, waitForElementToBeRemoved } from '@acx-ui/test-utils'
+import { CommonUrlsInfo }                                                          from '@acx-ui/rc/utils'
+import { Provider }                                                                from '@acx-ui/store'
+import { mockServer, render, screen, waitForElementToBeRemoved, mockRestApiQuery } from '@acx-ui/test-utils'
 
-import { events, eventsMeta } from './__tests__/fixtures'
+import { activities, events, eventsMeta } from './__tests__/fixtures'
 
 import { ApTimelineTab } from '.'
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: jest.fn(),
-  useParams: jest.fn()
-}))
-
 describe('ApTimelineTab', ()=>{
   it('should render', async () => {
-    jest.spyOn(router, 'useParams').mockImplementation(
-      () => ({ tenantId: 't1', serialNumber: 'serialNumber' })
-    )
+    mockRestApiQuery(CommonUrlsInfo.getActivityList.url, 'post', activities)
     mockServer.use(
       rest.post(CommonUrlsInfo.getEventList.url, (_, res, ctx) => res(ctx.json(events))),
       rest.post(CommonUrlsInfo.getEventListMeta.url, (_, res, ctx) => res(ctx.json(eventsMeta)))
     )
     render(<Provider><ApTimelineTab /></Provider>, {
       route: {
-        params: { tenantId: 't1', serialNumber: 'serialNumber' },
-        path: '/t/:tenantId/devices/wifi/:serialNumber/details/timeline/'
+        params: { tenantId: 't1', serialNumber: 'serialNumber', activeSubTab: 'activities' },
+        path: '/t/:tenantId/devices/wifi/:serialNumber/details/timeline/:activeSubTab'
       }
     })
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
-    expect(await screen.findAllByText('730-11-60')).toHaveLength(2)
+    expect(await screen.findAllByText('123roam')).toHaveLength(1)
+    await userEvent.click(screen.getByRole('tab', { name: /events/i }))
+    await new Promise(resolve => setTimeout(resolve, 300))
+    expect(await screen.findAllByText('730-11-60')).toHaveLength(4)
   })
 })
