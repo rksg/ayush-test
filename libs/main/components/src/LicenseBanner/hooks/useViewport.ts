427e2ebdf1e5585218a7184cffa6b8f39e05cf09
sean.huang <sean.huang@ruckuswireless.com>
2023-02-28 11:28:27 +0800

    ACX-25298: Layout adjustment

---
+++ libs/main/components/src/LicenseBanner/hooks/useViewport.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+
+export default function useViewPort () {
+  const [width, setWidth] = useState(window.innerWidth)
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth)
+    window.addEventListener('resize', handleWindowResize)
+    return () => window.removeEventListener('resize', handleWindowResize)
+  }, [])
+
+  return { width }
+}
