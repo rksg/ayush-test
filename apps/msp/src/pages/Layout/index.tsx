446f13c56b6e13a80ff21b6122487fc85a8e2233
sean.huang <sean.huang@ruckuswireless.com>
2023-03-01 14:20:28 +0800

    ACX-24044: fix eslint error

--- apps/msp/src/pages/Layout/index.tsx
+++ apps/msp/src/pages/Layout/index.tsx
@@ -1,17 +1,9 @@
 import { useEffect, useState } from 'react'
 
-import { Menu }    from 'antd'
-import { useIntl } from 'react-intl'
-
 import {
   Layout as LayoutComponent,
-  LayoutUI,
-  Dropdown
+  LayoutUI
 } from '@acx-ui/components'
-import {
-  WorldSolid,
-  ArrowExpand
-} from '@acx-ui/icons'
 import {
   ActivityButton,
   AlarmsButton,
@@ -29,13 +21,12 @@ import {
 import {
   useGetTenantDetailQuery
 } from '@acx-ui/rc/services'
-import { Outlet, TenantLink, useParams } from '@acx-ui/react-router-dom'
+import { Outlet, useParams } from '@acx-ui/react-router-dom'
 
 import { useMenuConfig }     from './menuConfig'
 import { LeftHeaderWrapper } from './styledComponents'
 
 function Layout () {
-  const { $t } = useIntl()
   const { tenantId } = useParams()
   const [tenantType, setTenantType] = useState('')
   const [supportStatus,setSupportStatus] = useState('')
@@ -55,16 +46,6 @@ function Layout () {
     }
   }, [data, userProfile])
 
-
-  const regionMenu = <Menu
-    selectable
-    defaultSelectedKeys={['US']}
-    items={[
-      { key: 'US', label: <TenantLink to='TODO'>{$t({ defaultMessage: 'US' })}</TenantLink> },
-      { key: 'EU', label: <TenantLink to='TODO'>{$t({ defaultMessage: 'EU' })}</TenantLink> },
-      { key: 'Asia', label: <TenantLink to='TODO'>{$t({ defaultMessage: 'ASIA' })}</TenantLink> }
-    ]}
-  />
   return (
     <LayoutComponent
       menuConfig={useMenuConfig(tenantType)}
