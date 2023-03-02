71c3b2425fe8518624750bed8aecb0afe3c7cb94
Cheng, Amy <Amy.Cheng@ruckuswireless.com>
2023-03-02 11:24:11 +0800

    ACX-25448: boost coverage

--- libs/rc/components/src/UserProfileContext/index.spec.tsx
+++ libs/rc/components/src/UserProfileContext/index.spec.tsx
@@ -1,8 +1,8 @@
 import { rest } from 'msw'
 
-import { CommonUrlsInfo }          from '@acx-ui/rc/utils'
-import { BrowserRouter as Router } from '@acx-ui/react-router-dom'
-import { Provider }                from '@acx-ui/store'
+import { CommonUrlsInfo, RolesEnum } from '@acx-ui/rc/utils'
+import { BrowserRouter as Router }   from '@acx-ui/react-router-dom'
+import { Provider }                  from '@acx-ui/store'
 import {
   mockServer,
   render,
@@ -16,6 +16,12 @@ import {
 
 const tenantId = 'a27e3eb0bd164e01ae731da8d976d3b1'
 
+const mockedUserProfile = {
+  firstName: 'First',
+  lastName: 'Last',
+  roles: [RolesEnum.PRIME_ADMIN]
+}
+
 function TestUserProfile () {
   const { data: userProfile } = useUserProfileContext()
   return <div>
@@ -25,15 +31,6 @@ function TestUserProfile () {
 
 describe('UserProfileContext', () => {
   beforeEach(async () => {
-    mockServer.use(
-      rest.get(
-        CommonUrlsInfo.getUserProfile.url,
-        (req, res, ctx) => res(ctx.json({ firstName: 'First', lastName: 'Last' }))
-      )
-    )
-  })
-
-  it('requests for user profile and stores in context', async () => {
     const location = {
       ...window.location,
       pathname: `/t/${tenantId}`
@@ -44,14 +41,71 @@ describe('UserProfileContext', () => {
       value: location
     })
 
+    mockServer.use(
+      rest.get(
+        CommonUrlsInfo.getUserProfile.url,
+        (req, res, ctx) => res(ctx.json(mockedUserProfile))
+      )
+    )
+  })
+
+  it('requests for user profile and stores in context', async () => {
     render(<Router>
       <Provider>
         <UserProfileProvider>
-          <TestUserProfile/ >
+          <TestUserProfile />
         </UserProfileProvider>
       </Provider>
     </Router>)
 
     expect(await screen.findByText('First Last')).toBeVisible()
+  })
+
+  it('should be able to recognize prime admin', async () => {
+    const TestPrimeAdmin = () => {
+      const { isPrimeAdmin } = useUserProfileContext()
+      return <div>
+        {isPrimeAdmin()+''}
+      </div>
+    }
+
+    render(<Router>
+      <Provider>
+        <UserProfileProvider>
+          <TestPrimeAdmin />
+        </UserProfileProvider>
+      </Provider>
+    </Router>)
+
+    expect(await screen.findByText('true')).toBeVisible()
+  })
+
+  it('user profile hasRole()', async () => {
+    mockServer.use(
+      rest.get(
+        CommonUrlsInfo.getUserProfile.url,
+        (req, res, ctx) => res(ctx.json({
+          ...mockedUserProfile,
+          roles: [RolesEnum.ADMINISTRATOR]
+        }))
+      )
+    )
+
+    const TestUserRole = ({ role }:{ role: RolesEnum }) => {
+      const { hasRole } = useUserProfileContext()
+      return <div>
+        {hasRole(role)+''}
+      </div>
+    }
+
+    render(<Router>
+      <Provider>
+        <UserProfileProvider>
+          <TestUserRole role={RolesEnum.GUEST_MANAGER} />
+        </UserProfileProvider>
+      </Provider>
+    </Router>)
+
+    expect(await screen.findByText('false')).toBeVisible()
   })
 })
