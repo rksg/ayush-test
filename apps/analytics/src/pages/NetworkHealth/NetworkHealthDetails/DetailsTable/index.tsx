a04a12f049b055db56db9aee080cb1bccccb1a46
ny1012 <Nishanth.Yerramsetty@ruckuswireless.com>
2023-03-02 14:45:10 +0800

    ACX-23297 : fix lint

--- apps/analytics/src/pages/NetworkHealth/NetworkHealthDetails/DetailsTable/index.tsx
+++ apps/analytics/src/pages/NetworkHealth/NetworkHealthDetails/DetailsTable/index.tsx
@@ -2,32 +2,34 @@ import { MessageDescriptor } from 'react-intl'
 
 import { Table, Loader } from '@acx-ui/components'
 
-import { authMethodsByCode }            from '../../authMethods'
-import {  useNetworkHealthTestResults } from '../../services'
-import { stage }                        from '../../stages'
-import {
-  ClientType,
-  NetworkHealthConfig,
-  TestResultByAP,
-  TestStage
-} from '../../types'
+import { authMethodsByCode }                                          from '../../authMethods'
+import { useNetworkHealthTestResults }                                from '../../services'
+import { stage }                                                      from '../../stages'
+import { ClientType, NetworkHealthConfig, TestResultByAP, TestStage } from '../../types'
 
-import {  getTableColumns } from './helper'
+import { getTableColumns } from './helper'
 
 const Details = () => {
-  const { tableQuery, onPageChange, pagination } = useNetworkHealthTestResults()
-  const queryResults = tableQuery
-  const config = (queryResults?.data)?.config as NetworkHealthConfig
-  const items = (queryResults?.data)?.aps?.items
-  const clientType = (queryResults?.data)?.spec?.clientType
-  const wlanAuthSettings = (queryResults?.data)?.wlanAuthSettings
+  const { tableQuery: queryResults, onPageChange, pagination } = useNetworkHealthTestResults()
+  const config = queryResults?.data?.config as NetworkHealthConfig
+  const items = queryResults?.data?.aps?.items
+  const clientType = queryResults?.data?.spec?.clientType
+  const wlanAuthSettings = queryResults?.data?.wlanAuthSettings
   const isWirelessClient = clientType === ClientType.VirtualWirelessClient
-  const stagesFromConfig = (config : NetworkHealthConfig) => {
-    if (!config?.authenticationMethod) { return [] }
-    const stages = [...authMethodsByCode[config.authenticationMethod].stages,stage('dns')]
-    if (config?.pingAddress) { stages.push(stage('ping')) }
-    if (config?.tracerouteAddress) { stages.push(stage('traceroute')) }
-    if (config?.speedTestEnabled) { stages.push(stage('speedTest')) }
+  const stagesFromConfig = (config: NetworkHealthConfig) => {
+    if (!config?.authenticationMethod) {
+      return []
+    }
+    const stages = [...authMethodsByCode[config.authenticationMethod].stages, stage('dns')]
+    if (config?.pingAddress) {
+      stages.push(stage('ping'))
+    }
+    if (config?.tracerouteAddress) {
+      stages.push(stage('traceroute'))
+    }
+    if (config?.speedTestEnabled) {
+      stages.push(stage('speedTest'))
+    }
     return stages
   }
   const stages = stagesFromConfig(config)?.map(
@@ -36,7 +38,7 @@ const Details = () => {
       title
     })
   )
-  const stagesKeys = stages.map(s => s.key) as TestStage[]
+  const stagesKeys = stages.map((s) => s.key) as TestStage[]
   return (
     <Loader states={[queryResults]}>
       <Table<TestResultByAP>
@@ -57,6 +59,3 @@ const Details = () => {
 }
 
 export { Details }
-
-
-
