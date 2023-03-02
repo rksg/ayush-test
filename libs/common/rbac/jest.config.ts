723b087cd13e1a6ea3513faa91b895e9548dd41d
shiawuen.tan <shiawuen.tan@ruckuswireless.com>
2023-03-02 15:34:40 +0800

    ACX-23867: add coverage and reduce to 0

--- libs/common/rbac/jest.config.ts
+++ libs/common/rbac/jest.config.ts
@@ -5,5 +5,13 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'babel-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/common/rbac'
+  coverageDirectory: '../../../coverage/libs/common/rbac',
+  coverageThreshold: {
+    global: {
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0
+    }
+  }
 }
