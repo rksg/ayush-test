a52e6786c719ce323260bc15854fc1d15c4e058c
ki1003 <khairul.iman@ruckuswireless.com>
2023-03-03 18:18:24 +0800

    ACX-12585: wip: cleaned linting & workaround for module federation boundaries

--- libs/common/components/src/components/Table/stories/GroupTable.tsx
+++ libs/common/components/src/components/Table/stories/GroupTable.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 
-import { APExtended } from '../../../../../../rc/utils/src/index'
-
-import { Table, TableProps } from '..'
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { APExtended }     from 'libs/rc/utils/src/types/ap'
 import { omit, uniqueId } from 'lodash'
 
-type RecordType = {
-  key: string
-  name: string
-  givenName: string
-  surname: string
-  age: number
-  description: string
-  address: string,
-  children?: RecordType[]
-}
+import { Table, TableProps } from '..'
+
 
 type APExtendedGroupedResponse = {
   totalCount: number
@@ -35,15 +26,22 @@ type APExtendedGroupedResponse = {
   })[]
 }
 
-function cleanResponse(response: APExtendedGroupedResponse) {
+function cleanResponse (response: APExtendedGroupedResponse) {
   return response.data.map(apGroup => {
     const parent = omit(apGroup, ['deviceGroupName', 'clients', 'aps'])
     const { deviceGroupName, members, incidents, clients, networks, aps } = apGroup
     return {
       ...parent,
       id: uniqueId(), // hacky trick, set the parent's device group as serialNumber since the table's id focuses on aps serial number
-      name: `${deviceGroupName !== ' ' ? deviceGroupName : 'Uncategorized'} Members: ${members} Incidents: ${incidents} Connected Clients: ${clients} Wireless Networks: ${networks.count}`,
-      children: aps.map(ap => ({ ...ap, deviceGroupName: (ap.deviceGroupName !== '') ? ap.deviceGroupName : 'Uncategorized', id: uniqueId() }))
+      name: `${deviceGroupName !== ' ' ? deviceGroupName : 'Uncategorized'} 
+      Members: ${members} 
+      Incidents: ${incidents} 
+      Connected Clients: ${clients} 
+      Wireless Networks: ${networks.count}`,
+      children: aps.map(ap => ({ ...ap, deviceGroupName: (ap.deviceGroupName !== '')
+        ? ap.deviceGroupName
+        : 'Uncategorized',
+      id: uniqueId() }))
     }
   })
 }
@@ -206,269 +204,269 @@ const apGroupResponse: APExtendedGroupedResponse = {
 }
 
 const deviceStatusResponse: APExtendedGroupedResponse = {
-  "totalCount": 4,
-  "page": 1,
-  "data": [ 
-//List ALL deviceStatus including empty deviceStatus
+  totalCount: 4,
+  page: 1,
+  data: [
+    //List ALL deviceStatus including empty deviceStatus
     {
-      "deviceGroupId": "", 
-      "deviceGroupName": "",
-      "deviceStatus": "2_00_Operational",
-      "model": "",
-      "networks": {
-        "count": 0,
-        "names": []
-      }, 
-      "members": 2,
-      "incidents": 220,
-      "clients": 4,
-      "aps": [ // List the ap list under this deviceStatus
+      deviceGroupId: '',
+      deviceGroupName: '',
+      deviceStatus: '2_00_Operational',
+      model: '',
+      networks: {
+        count: 0,
+        names: []
+      },
+      members: 2,
+      incidents: 220,
+      clients: 4,
+      aps: [ // List the ap list under this deviceStatus
         {
-          "serialNumber": "302002015736",
-          "name": "302002015736-0802",
-          "model": "R550",
-          "fwVersion": "6.2.0.103.500",
-          "venueId": "4c778ed630394b76b17bce7fe230cf9f",
-          "venueName": "My-Venue",
-          "deviceStatus": "2_00_Operational",
-          "IP": "10.206.1.34",
-          "apMac": "34:20:E3:19:79:F0",
-          "apStatusData": {
-            "APRadio": [
+          serialNumber: '302002015736',
+          name: '302002015736-0802',
+          model: 'R550',
+          fwVersion: '6.2.0.103.500',
+          venueId: '4c778ed630394b76b17bce7fe230cf9f',
+          venueName: 'My-Venue',
+          deviceStatus: '2_00_Operational',
+          IP: '10.206.1.34',
+          apMac: '34:20:E3:19:79:F0',
+          apStatusData: {
+            APRadio: [
               {
-                "txPower": null,
-                "channel": 1,
-                "band": "2.4G",
-                "Rssi": null,
-                "radioId": 0
+                txPower: null,
+                channel: 1,
+                band: '2.4G',
+                Rssi: null,
+                radioId: 0
               },
               {
-                "txPower": null,
-                "channel": 36,
-                "band": "5G",
-                "Rssi": null,
-                "radioId": 1
+                txPower: null,
+                channel: 36,
+                band: '5G',
+                Rssi: null,
+                radioId: 1
               }
             ]
           },
-          "meshRole": "DISABLED",
-          "deviceGroupId": "99227979648c421c93c15c586e6ed80b",
-          "clients": 2,
-          "tags": "",
-          "deviceGroupName": "AP_GROUP"
+          meshRole: 'DISABLED',
+          deviceGroupId: '99227979648c421c93c15c586e6ed80b',
+          clients: 2,
+          tags: '',
+          deviceGroupName: 'AP_GROUP'
         },
         {
-          "serialNumber": "302002015732",
-          "name": "302002015736-0802",
-          "model": "R550",
-          "fwVersion": "6.2.0.103.500",
-          "venueId": "4c778ed630394b76b17bce7fe230cf9f",
-          "venueName": "My-Venue",
-          "deviceStatus": "2_00_Operational",
-          "IP": "10.206.1.34",
-          "apMac": "34:20:E3:19:79:F0",
-          "apStatusData": {
-            "APRadio": [
+          serialNumber: '302002015732',
+          name: '302002015736-0802',
+          model: 'R550',
+          fwVersion: '6.2.0.103.500',
+          venueId: '4c778ed630394b76b17bce7fe230cf9f',
+          venueName: 'My-Venue',
+          deviceStatus: '2_00_Operational',
+          IP: '10.206.1.34',
+          apMac: '34:20:E3:19:79:F0',
+          apStatusData: {
+            APRadio: [
               {
-                "txPower": null,
-                "channel": 1,
-                "band": "2.4G",
-                "Rssi": null,
-                "radioId": 0
+                txPower: null,
+                channel: 1,
+                band: '2.4G',
+                Rssi: null,
+                radioId: 0
               },
               {
-                "txPower": null,
-                "channel": 36,
-                "band": "5G",
-                "Rssi": null,
-                "radioId": 1
+                txPower: null,
+                channel: 36,
+                band: '5G',
+                Rssi: null,
+                radioId: 1
               }
             ]
           },
-          "meshRole": "DISABLED",
-          "deviceGroupId": "99227979648c421c93c15c586e6ed80b",
-          "clients": 2,
-          "tags": "",
-          "deviceGroupName": "AP_GROUP2"
+          meshRole: 'DISABLED',
+          deviceGroupId: '99227979648c421c93c15c586e6ed80b',
+          clients: 2,
+          tags: '',
+          deviceGroupName: 'AP_GROUP2'
         }
       ]
     }, {
 
-      "deviceGroupId": "", 
-      "deviceGroupName": "",
-      "deviceStatus": "3_RequiresAttention",
-      "model": "",
-      "networks": {
-        "count": 0,
-        "names": []
+      deviceGroupId: '',
+      deviceGroupName: '',
+      deviceStatus: '3_RequiresAttention',
+      model: '',
+      networks: {
+        count: 0,
+        names: []
       },
-      "members": 0,
-      "incidents": 0,
-      "clients": 0,
-      "aps": []
-     }, {
-      "deviceGroupId": "", 
-      "deviceGroupName": "",
-      "deviceStatus": "1_InSetupPhase",
-      "model": "",
-      "networks": {
-        "count": 0,
-        "names": []
+      members: 0,
+      incidents: 0,
+      clients: 0,
+      aps: []
+    }, {
+      deviceGroupId: '',
+      deviceGroupName: '',
+      deviceStatus: '1_InSetupPhase',
+      model: '',
+      networks: {
+        count: 0,
+        names: []
       },
-      "members": 0,
-      "incidents": 0,
-      "clients": 0,
-      "aps": []
-     }, {
-      "deviceGroupId": "", 
-      "deviceGroupName": "",
-      "deviceStatus": "1_InSetupPhase_Offline",
-      "model": "",
-      "networks": {
-        "count": 0,
-        "names": []
+      members: 0,
+      incidents: 0,
+      clients: 0,
+      aps: []
+    }, {
+      deviceGroupId: '',
+      deviceGroupName: '',
+      deviceStatus: '1_InSetupPhase_Offline',
+      model: '',
+      networks: {
+        count: 0,
+        names: []
       },
-      "members": 0,
-      "incidents": 0,
-      "clients": 0,
-      "aps": []
-     }  
+      members: 0,
+      incidents: 0,
+      clients: 0,
+      aps: []
+    }
   ]
 }
 
 const modelResponse: APExtendedGroupedResponse = {
-  "totalCount": 2,
-  "page": 1,
-  "data": [
-//List ALL model including
+  totalCount: 2,
+  page: 1,
+  data: [
+    //List ALL model including
     {
-     "deviceGroupId": "", 
-     "deviceGroupName": "",
-     "deviceStatus": "",
-     "model": "R550",
-     "networks": {
-        "count": 0,
-        "names": []
-      }, 
-      "members": 2,
-      "incidents": 2,
-      "clients": 3,
-      "aps": [ // List the ap list under this model
+      deviceGroupId: '',
+      deviceGroupName: '',
+      deviceStatus: '',
+      model: 'R550',
+      networks: {
+        count: 0,
+        names: []
+      },
+      members: 2,
+      incidents: 2,
+      clients: 3,
+      aps: [ // List the ap list under this model
         {
-          "serialNumber": "302002015736",
-          "name": "ap1",
-          "model": "R550",
-          "fwVersion": "6.2.0.103.500",
-          "venueId": "4c778ed630394b76b17bce7fe230cf9f",
-          "venueName": "My-Venue",
-          "deviceStatus": "2_00_Operational",
-          "IP": "10.206.1.34",
-          "apMac": "34:20:E3:19:79:F0",
-          "apStatusData": {
-            "APRadio": [
+          serialNumber: '302002015736',
+          name: 'ap1',
+          model: 'R550',
+          fwVersion: '6.2.0.103.500',
+          venueId: '4c778ed630394b76b17bce7fe230cf9f',
+          venueName: 'My-Venue',
+          deviceStatus: '2_00_Operational',
+          IP: '10.206.1.34',
+          apMac: '34:20:E3:19:79:F0',
+          apStatusData: {
+            APRadio: [
               {
-                "txPower": null,
-                "channel": 1,
-                "band": "2.4G",
-                "Rssi": null,
-                "radioId": 0
+                txPower: null,
+                channel: 1,
+                band: '2.4G',
+                Rssi: null,
+                radioId: 0
               },
               {
-                "txPower": null,
-                "channel": 36,
-                "band": "5G",
-                "Rssi": null,
-                "radioId": 1
+                txPower: null,
+                channel: 36,
+                band: '5G',
+                Rssi: null,
+                radioId: 1
               }
             ]
           },
-          "meshRole": "DISABLED",
-          "deviceGroupId": "99227979648c421c93c15c586e6ed80b",
-          "clients": 2,
-          "tags": "",
-          "deviceGroupName": "AP_GROUP"
+          meshRole: 'DISABLED',
+          deviceGroupId: '99227979648c421c93c15c586e6ed80b',
+          clients: 2,
+          tags: '',
+          deviceGroupName: 'AP_GROUP'
         },
         {
-          "serialNumber": "302002015732",
-          "name": "ap-test-2",
-          "model": "R550",
-          "fwVersion": "6.2.0.103.500",
-          "venueId": "4c778ed630394b76b17bce7fe230cf9f",
-          "venueName": "My-Venue",
-          "deviceStatus": "2_00_Operational",
-          "IP": "10.206.1.34",
-          "apMac": "34:20:E3:19:79:F0",
-          "apStatusData": {
-            "APRadio": [
+          serialNumber: '302002015732',
+          name: 'ap-test-2',
+          model: 'R550',
+          fwVersion: '6.2.0.103.500',
+          venueId: '4c778ed630394b76b17bce7fe230cf9f',
+          venueName: 'My-Venue',
+          deviceStatus: '2_00_Operational',
+          IP: '10.206.1.34',
+          apMac: '34:20:E3:19:79:F0',
+          apStatusData: {
+            APRadio: [
               {
-                "txPower": null,
-                "channel": 1,
-                "band": "2.4G",
-                "Rssi": null,
-                "radioId": 0
+                txPower: null,
+                channel: 1,
+                band: '2.4G',
+                Rssi: null,
+                radioId: 0
               },
               {
-                "txPower": null,
-                "channel": 36,
-                "band": "5G",
-                "Rssi": null,
-                "radioId": 1
+                txPower: null,
+                channel: 36,
+                band: '5G',
+                Rssi: null,
+                radioId: 1
               }
             ]
           },
-          "meshRole": "DISABLED",
-          "deviceGroupId": "99227979648c421c93c15c586e6ed80b",
-          "clients": 2,
-          "tags": "",
-          "deviceGroupName": "AP_GROUP"
+          meshRole: 'DISABLED',
+          deviceGroupId: '99227979648c421c93c15c586e6ed80b',
+          clients: 2,
+          tags: '',
+          deviceGroupName: 'AP_GROUP'
         }
       ]
-    },  {
-     "deviceGroupId": "", 
-     "deviceGroupName": "",
-     "deviceStatus": "",
-     "model": "",  //(Empty Model )
-     "networks": {
-        "count": 0,
-        "names": []
+    }, {
+      deviceGroupId: '',
+      deviceGroupName: '',
+      deviceStatus: '',
+      model: '',  //(Empty Model )
+      networks: {
+        count: 0,
+        names: []
       },
-      "members": 1,
-      "incidents": 2,
-      "clients": 3,
-      "aps": [ // List the ap list under this model
+      members: 1,
+      incidents: 2,
+      clients: 3,
+      aps: [ // List the ap list under this model
         {
-          "serialNumber": "302002015735",
-          "name": "ap2",
-          "model": " ",
-          "fwVersion": "6.2.0.103.500",
-          "venueId": "4c778ed630394b76b17bce7fe230cf9f",
-          "venueName": "My-Venue",
-          "deviceStatus": "2_00_Operational",
-          "IP": "10.206.1.34",
-          "apMac": "34:20:E3:19:79:00",
-          "apStatusData": {
-            "APRadio": [
+          serialNumber: '302002015735',
+          name: 'ap2',
+          model: ' ',
+          fwVersion: '6.2.0.103.500',
+          venueId: '4c778ed630394b76b17bce7fe230cf9f',
+          venueName: 'My-Venue',
+          deviceStatus: '2_00_Operational',
+          IP: '10.206.1.34',
+          apMac: '34:20:E3:19:79:00',
+          apStatusData: {
+            APRadio: [
               {
-                "txPower": null,
-                "channel": 1,
-                "band": "2.4G",
-                "Rssi": null,
-                "radioId": 0
+                txPower: null,
+                channel: 1,
+                band: '2.4G',
+                Rssi: null,
+                radioId: 0
               },
               {
-                "txPower": null,
-                "channel": 36,
-                "band": "5G",
-                "Rssi": null,
-                "radioId": 1
+                txPower: null,
+                channel: 36,
+                band: '5G',
+                Rssi: null,
+                radioId: 1
               }
             ]
           },
-          "meshRole": "DISABLED",
-          "deviceGroupId": "99227979648c421c93c15c586e6ed80b",
-          "clients": 2,
-          "tags": "",
-          "deviceGroupName": "AP_GROUP"
+          meshRole: 'DISABLED',
+          deviceGroupId: '99227979648c421c93c15c586e6ed80b',
+          clients: 2,
+          tags: '',
+          deviceGroupName: 'AP_GROUP'
         }
       ]
     }
@@ -481,7 +479,7 @@ export function GroupTable () {
   const [ currData, setCurrData ] = React.useState<typeof cleanedData>(() => cleanedData)
 
   const groupableCallback = (key: 'deviceStatus' | 'model' | 'deviceGroupName') => {
-    let response: APExtendedGroupedResponse;
+    let response: APExtendedGroupedResponse
     switch (key) {
       case 'deviceGroupName': {
         response = apGroupResponse
@@ -498,7 +496,6 @@ export function GroupTable () {
     }
     if (!response) return []
     const data = cleanResponse(response)
-    console.log(data, key)
     setCurrData(() => data)
     return data
   }
@@ -567,10 +564,6 @@ export function GroupTable () {
       filterable: true
     }
   ]
-  
-  React.useEffect(() => {
-    console.log(currData)
-  }, [currData])
 
   return (
     <>
@@ -582,7 +575,11 @@ export function GroupTable () {
         indentSize={6}
         columnEmptyText='-'
         groupable={{
-          selectors: [{ key: 'deviceGroupName', label: 'AP Group'}, { key: 'deviceStatus' , label: 'Status' }, { key: 'model', label: 'Model' }],
+          selectors: [
+            { key: 'deviceGroupName', label: 'AP Group' },
+            { key: 'deviceStatus' , label: 'Status' },
+            { key: 'model', label: 'Model' }
+          ],
           onChange: groupableCallback,
           onClear: () => setCurrData([])
         }}
