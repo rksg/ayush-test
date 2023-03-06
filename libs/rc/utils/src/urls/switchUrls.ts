4a645c9cda73ef7ef20893190cb5b5c618ae127a
Pinky Peng <pinky.peng@ruckuswireless.com>
2023-03-06 16:32:12 +0800

    ACX-23979 Replace add/update switch api

--- libs/rc/utils/src/urls/switchUrls.ts
+++ libs/rc/utils/src/urls/switchUrls.ts
@@ -110,16 +110,20 @@ export const SwitchUrlsInfo: { [key: string]: ApiInfo } = {
     newApi: true
   },
   addSwitch: {
+    // method: 'post',
+    // url: '/switches',
+    // oldUrl: '/api/switch/tenant/:tenantId/switch',
+    // newApi: true
     method: 'post',
-    url: '/switches',
-    oldUrl: '/api/switch/tenant/:tenantId/switch',
-    newApi: true
+    url: '/api/switch/tenant/:tenantId/switch'
   },
   updateSwitch: {
+    // method: 'put',
+    // url: '/switches/:switchId',
+    // oldUrl: '/api/switch/tenant/:tenantId/switch',
+    // newApi: true
     method: 'put',
-    url: '/switches/:switchId',
-    oldUrl: '/api/switch/tenant/:tenantId/switch',
-    newApi: true
+    url: '/api/switch/tenant/:tenantId/switch'
   },
   convertToStack: {
     method: 'post',
