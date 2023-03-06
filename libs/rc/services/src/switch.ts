56c525de3e0149483c72a47e08e5a6e80083e542
Karen Yeh <karen.yeh@ruckuswireless.com>
2023-03-06 12:23:07 +0800

    ACX-23979: Adjust the transformResponse for switch configbackup list

--- libs/rc/services/src/switch.ts
+++ libs/rc/services/src/switch.ts
@@ -418,18 +418,17 @@ export const switchApi = baseSwitchApi.injectEndpoints({
           body: payload
         }
       },
-      transformResponse: (res: ConfigurationBackup[]) => {
+      transformResponse: (res: TableResult<ConfigurationBackup>) => {
         return {
-          data: res
+          ...res,
+          data: res.data
             .sort((a, b) => b.createdDate.localeCompare(a.createdDate))
             .map(item => ({
               ...item,
               createdDate: formatter('dateTimeFormatWithSeconds')(item.createdDate),
               backupType: transformConfigBackupType(item.backupType),
               status: transformConfigBackupStatus(item) as ConfigurationBackupStatus
-            })),
-          totalCount: res.length,
-          page: 1
+            }))
         }
       },
       providesTags: [{ type: 'SwitchBackup', id: 'LIST' }]
