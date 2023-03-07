d2cddc458a37f6bb2115743781c792ef4537e156
Jacky Chiu <jacky.chiu@ruckuswireless.com>
2023-03-07 16:39:15 +0800

    ACX-19977: Fix the DPSK network form preserves the previous data issue

--- apps/rc/src/pages/Services/Dpsk/DpskDetail/DpskPassphraseManagement.tsx
+++ apps/rc/src/pages/Services/Dpsk/DpskDetail/DpskPassphraseManagement.tsx
@@ -245,6 +245,7 @@ export default function DpskPassphraseManagement () {
       visible={networkModalVisible}
       mask={true}
       children={networkForm}
+      destroyOnClose={true}
     />
     <Loader states={[tableQuery]}>
       <Table<NewDpskPassphrase>
