14e80a3d398ed13217a88c5e510336a82dcf7c58
yw1046 <Yc.Wong@ruckuswireless.com>
2023-03-09 11:16:35 +0800

    ACX-25032: Fix delete acl rule fail issue

--- apps/rc/src/pages/Networks/wired/profiles/AclSetting/ACLSettingDrawer/index.tsx
+++ apps/rc/src/pages/Networks/wired/profiles/AclSetting/ACLSettingDrawer/index.tsx
@@ -181,13 +181,13 @@ function ACLSettingForm (props: ACLSettingFormProps) {
     {
       label: $t({ defaultMessage: 'Delete' }),
       onClick: (selectedRows) => {
-        setRuleList(
-          ruleList?.filter((option: { sequence: number }) => {
-            return !selectedRows
-              .map((r) => r.sequence)
-              .includes(option.sequence)
-          })
-        )
+        const rules = ruleList?.filter((option: { sequence: number }) => {
+          return !selectedRows
+            .map((r) => r.sequence)
+            .includes(option.sequence)
+        })
+        setRuleList(rules)
+        form.setFieldValue('aclRules', rules)
         setSelected(undefined)
       }
     }
