ced21062dd9e4a18c419ecdbceeb945ecc066798
jeffrey.syu <jeffrey.syu@ruckuswireless.com>
2023-03-10 16:50:00 +0800

    ACX-25810:[Wifi Calling] update the test case WifiCallingSettingModal.spec.tsx

--- libs/rc/utils/src/types/index.ts
+++ libs/rc/utils/src/types/index.ts
@@ -378,8 +378,8 @@ export interface DnsProxyContextType {
 
 export interface WifiCallingSetting {
   id: string,
-  serviceName: string,
-  description: string | undefined,
+  serviceName?: string,
+  description?: string | undefined,
   qosPriority: QosPriorityEnum,
   tenantId?: string,
   name?: string,
