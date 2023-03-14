1053935bb77be72998e5230db1381308974147af
sean.huang <sean.huang@ruckuswireless.com>
2023-03-14 15:07:39 +0800

    ACX-22497: fix the Capacity column display error in DHCP detail page

--- apps/rc/src/pages/Services/DHCP/DHCPDetail/DHCPInstancesTable.tsx
+++ apps/rc/src/pages/Services/DHCP/DHCPDetail/DHCPInstancesTable.tsx
@@ -94,7 +94,9 @@ export default function DHCPInstancesTable (){
       render: function (_data, row) {
         const venueIDIndex = _.find(dhcpProfile?.usage, dhcp => dhcp.venueId===row.id)
         if(venueIDIndex) {
-          return (100-((venueIDIndex?.usedIpCount/venueIDIndex?.totalIpCount)*100)).toFixed(2)+'%'
+          // eslint-disable-next-line max-len
+          const percent = (100-((venueIDIndex?.usedIpCount/venueIDIndex?.totalIpCount)*100))
+          return (isNaN(percent) ? 0 : percent.toFixed(2)) + '%'
         }else{
           return ''
         }
