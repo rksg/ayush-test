6ff7066fbdfec95a1dd62da5ffe3216cb0cfe517
ki1003 <khairul.iman@ruckuswireless.com>
2023-03-15 14:25:54 +0800

    ACX-21224: wip: update SummaryBoxes snapshots

--- libs/analytics/components/src/Health/SummaryBoxes/index.spec.tsx
+++ libs/analytics/components/src/Health/SummaryBoxes/index.spec.tsx
@@ -1,12 +1,12 @@
 import userEvent         from '@testing-library/user-event'
 import { defineMessage } from 'react-intl'
 
-import { dataApiURL }                                                  from '@acx-ui/analytics/services'
-import { AnalyticsFilter }                                             from '@acx-ui/analytics/utils'
-import { BrowserRouter as Router }                                     from '@acx-ui/react-router-dom'
-import { Provider, store }                                             from '@acx-ui/store'
-import { render, waitForElementToBeRemoved, screen, mockGraphqlQuery } from '@acx-ui/test-utils'
-import { DateRange }                                                   from '@acx-ui/utils'
+import { dataApiURL }                                                       from '@acx-ui/analytics/services'
+import { AnalyticsFilter }                                                  from '@acx-ui/analytics/utils'
+import { BrowserRouter as Router }                                          from '@acx-ui/react-router-dom'
+import { Provider, store }                                                  from '@acx-ui/store'
+import { render, waitForElementToBeRemoved, screen, mockGraphqlQuery, act } from '@acx-ui/test-utils'
+import { DateRange }                                                        from '@acx-ui/utils'
 
 import { fakeSummary, fakeEmptySummary } from './__tests__/fixtures'
 import { api }                           from './services'
@@ -18,17 +18,21 @@ describe('box', () => {
     type: 'successCount',
     title: defineMessage({ defaultMessage: 'test box' }),
     suffix: '/suffix',
-    // isOpen: false,
-    value: '100'
+    value: '100',
+    toggleEnable: true
   }
-  it('should render correctly', async () => {
-    // TODO: post GA
-    // const onClick = jest.fn()
-    // const { asFragment } = render(<Box {...boxProps} isOpen onClick={onClick}/>)
-    // expect(asFragment()).toMatchSnapshot()
-    // await userEvent.click(screen.getByTestId('CaretDoubleUpOutlined'))
-    // expect(onClick).toBeCalledTimes(1)
-    const { asFragment } = render(<Box {...boxProps} />)
+  it('should render correctly with toggle enabled', async () => {
+    const onClick = jest.fn()
+    const { asFragment } = render(<Box {...boxProps} isOpen onClick={onClick}/>)
+    expect(asFragment()).toMatchSnapshot()
+    await userEvent.click(screen.getByTestId('CaretDoubleUpOutlined'))
+    expect(onClick).toBeCalledTimes(1)
+  })
+
+  it('should render correctly with toggle disabled', async () => {
+    const onClick = jest.fn()
+    const falseToggleBoxProps = { ...boxProps, toggleEnable: false }
+    const { asFragment } = render(<Box {...falseToggleBoxProps} isOpen onClick={onClick}/>)
     expect(asFragment()).toMatchSnapshot()
   })
 })
@@ -46,51 +50,105 @@ describe('Incidents Page', () => {
   })
   it('should match snapshot', async () => {
     mockGraphqlQuery(dataApiURL, 'HealthSummary', { data: fakeSummary })
+    const openType = 'none'
+    const toggleEnable = true
     const { asFragment } = render(
-      <Router><Provider><SummaryBoxes filters={filters}/></Provider></Router>
+      <Router><Provider><SummaryBoxes
+        filters={filters}
+        openType={openType}
+        setOpenType={jest.fn()}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>
     )
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
     expect(asFragment()).toMatchSnapshot()
   })
   it('should show - when no data', async () => {
     mockGraphqlQuery(dataApiURL, 'HealthSummary', { data: fakeEmptySummary })
+    const openType = 'none'
+    const toggleEnable = true
     const { asFragment } = render(
-      <Router><Provider><SummaryBoxes filters={filters}/></Provider></Router>
+      <Router><Provider><SummaryBoxes filters={filters}
+        openType={openType}
+        setOpenType={jest.fn()}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>
     )
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
     expect(asFragment()).toMatchSnapshot()
   })
 
-  // TODO: remove skip after feature available
-  describe.skip('toggle stats', () => {
+  describe('toggle stats', () => {
     it('should handle toggle stats', async () => {
       mockGraphqlQuery(dataApiURL, 'HealthSummary', { data: fakeSummary })
-      render(<Router><Provider><SummaryBoxes filters={filters}/></Provider></Router>)
+      let openType: 'connectionFailure' | 'ttc' | 'none' = 'none'
+      const setOpenType = jest.fn((val: typeof openType) => openType = val)
+      const toggleEnable = true
+      const { rerender } = render(<Router><Provider><SummaryBoxes
+        filters={filters}
+        openType={openType}
+        setOpenType={setOpenType}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>)
       await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
 
       const downArrows = screen.getAllByTestId('CaretDoubleDownOutlined')
       expect(downArrows).toHaveLength(4)
 
-      await userEvent.click(downArrows[0])
+      await act(async () => await userEvent.click(downArrows[0]))
+      rerender(<Router><Provider><SummaryBoxes
+        filters={filters}
+        openType={openType}
+        setOpenType={setOpenType}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>)
       const upArrows = screen.getAllByTestId('CaretDoubleUpOutlined')
       expect(upArrows).toHaveLength(3)
 
-      await userEvent.click(upArrows[1])
+      await act(async () => await userEvent.click(upArrows[0]))
+      rerender(<Router><Provider><SummaryBoxes
+        filters={filters}
+        openType={openType}
+        setOpenType={setOpenType}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>)
       expect(screen.getAllByTestId('CaretDoubleDownOutlined')).toHaveLength(4)
     })
 
     it('should handle toggle ttc', async () => {
       mockGraphqlQuery(dataApiURL, 'HealthSummary', { data: fakeSummary })
-      render(<Router><Provider><SummaryBoxes filters={filters}/></Provider></Router>)
+      let openType: 'connectionFailure' | 'ttc' | 'none' = 'none'
+      const setOpenType = jest.fn((val: typeof openType) => openType = val)
+      const toggleEnable = true
+      const { rerender } = render(<Router><Provider><SummaryBoxes
+        filters={filters}
+        openType={openType}
+        setOpenType={setOpenType}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>)
       await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
 
       expect(screen.getAllByTestId('CaretDoubleDownOutlined')).toHaveLength(4)
 
       const button = screen.getByRole('button', { name: /time to connect/i })
-      await userEvent.click(button)
+      await act(async () => await userEvent.click(button))
+      rerender(<Router><Provider><SummaryBoxes
+        filters={filters}
+        openType={openType}
+        setOpenType={setOpenType}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>)
+
       expect(screen.getAllByTestId('CaretDoubleUpOutlined')).toHaveLength(1)
 
-      await userEvent.click(button)
+      await act(async () => await userEvent.click(button))
+      rerender(<Router><Provider><SummaryBoxes
+        filters={filters}
+        openType={openType}
+        setOpenType={setOpenType}
+        toggleEnable={toggleEnable}
+      /></Provider></Router>)
+
       expect(screen.getAllByTestId('CaretDoubleDownOutlined')).toHaveLength(4)
     })
   })
