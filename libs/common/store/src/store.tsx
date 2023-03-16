46af3f9848d93002295215fa08724db41e6c9a97
Jerry Chen <Jerry.Chen@ruckuswireless.com>
2023-03-16 18:22:07 +0800

    ACX-23211: Fix conflict

--- libs/common/store/src/store.tsx
+++ libs/common/store/src/store.tsx
@@ -57,7 +57,8 @@ let isModalShown = false
 // TODO: workaround for skipping general error dialog
 const ignoreEndpointList = [
   'addAp', 'updateAp', 'inviteDelegation', 'addRecipient', 'updateRecipient', 'getDnsServers',
-  'addEdge', 'clientInfo', 'getClientDetails', 'createNetworkSegmentationGroup', 'getPropertyConfigs'
+  'addEdge', 'clientInfo', 'getClientDetails', 'createNetworkSegmentationGroup',
+  'getPropertyConfigs'
 ]
 const errorMessage = {
   SERVER_ERROR: {
