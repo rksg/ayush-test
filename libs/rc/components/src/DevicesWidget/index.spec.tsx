5c7eadbb7a5d94102a6338d373ef60ed39cba661
Asraf Ali <asraf.najmudeen@ruckuswireless.com>
2023-03-20 20:29:05 +0530

    ACX-26295 | chore: added test cases for clients widget v2

--- libs/rc/components/src/DevicesWidget/index.spec.tsx
+++ libs/rc/components/src/DevicesWidget/index.spec.tsx
@@ -47,8 +47,9 @@ describe('Devices widget V2', () => {
       </Provider>,
       { route: { params } })
     await screen.findByText('Devices')
-    expect(asFragment().querySelector('svg')).toBeDefined()
-    expect(asFragment()).toMatchSnapshot()
+    await screen.findByText('Access Points')
+    await screen.findByText('Switches')
+    expect(asFragment().querySelectorAll('svg').length).toBe(2)
   })
   it('should render proper data for zero devices', async () => {
     const params = {
@@ -63,8 +64,9 @@ describe('Devices widget V2', () => {
       </Provider>,
       { route: { params } })
     await screen.findByText('Devices')
-    expect(asFragment().querySelector('svg')).toBeDefined()
-    expect(asFragment()).toMatchSnapshot()
+    await screen.findByText('No Access Points')
+    await screen.findByText('No Switches')
+    expect(asFragment().querySelectorAll('svg').length).toBe(0)
   })
   it('should render correctly with right arrow enabled', async () => {
     const params = {
