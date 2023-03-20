c77781347fe940777a7f0647f55dc5723ad6e477
Li, Roil <Roil.Li@ruckuswireless.com>
2023-03-21 00:02:51 +0800

    ACX-26853: Implemetn create tunnel profile page

--- libs/rc/components/src/index.ts
+++ libs/rc/components/src/index.ts
@@ -67,6 +67,7 @@ export { SelectConnectedClientsTable, OSIconContainer } from './SelectConnectedC
 export { usePreference, countryCodes } from './usePreference'
 export { useUpdateGoogleMapRegion } from './usePreference/useUpdateGoogleMapRegion'
 export type { updatePreferenceProps } from './usePreference'
+export { TunnelProfileForm } from './TunnelProfileForm'
 
 export * from './services'
 export * from './EdgeStatisticWidget'
