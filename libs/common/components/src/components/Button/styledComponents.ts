631341ed07bff592a92898fdf40e1c2bdeb5a5a3
TLIN4 <Tsaihuei.Lin@ruckuswireless.com>
2023-03-20 13:18:45 +0800

    ACX-26998: adjust style of button on focus

--- libs/common/components/src/components/Button/styledComponents.ts
+++ libs/common/components/src/components/Button/styledComponents.ts
@@ -5,7 +5,13 @@ export const Button = styled(AntButton)<{ $customType: string | null }>`
   ${props => props.$customType === 'secondary' ? css`
     background-color: var(--acx-accents-orange-50);
     border-color: var(--acx-accents-orange-50);
-    &:hover, &:focus {
+    &:focus {
+      background-color: var(--acx-accents-orange-50);
+      border-color: var(--acx-accents-orange-50);      
+    }
+    &:hover,
+    &:focus-visible,
+    &.ant-btn-loading {
       background-color: var(--acx-accents-orange-60);
       border-color: var(--acx-accents-orange-60);
     }
