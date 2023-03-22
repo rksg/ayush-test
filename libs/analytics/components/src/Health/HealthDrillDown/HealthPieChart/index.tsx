799e7156f73c0225cb0ea41f3f283e3e138c4cd7
ki1003 <khairul.iman@ruckuswireless.com>
2023-03-23 00:23:24 +0800

    ACX-26398: add health pie chart and services

---
+++ libs/analytics/components/src/Health/HealthDrillDown/HealthPieChart/index.tsx
@@ -0,0 +1,156 @@
+import { useIntl } from 'react-intl'
+import AutoSizer   from 'react-virtualized-auto-sizer'
+
+import { useAnalyticsFilter } from '@acx-ui/analytics/utils'
+import {
+  Card,
+  ContentSwitcher,
+  ContentSwitcherProps,
+  DonutChart,
+  Loader,
+  qualitativeColorSet
+} from '@acx-ui/components'
+import { formatter } from '@acx-ui/formatter'
+
+import { ImpactedNodesAndWlans, usePieChartQuery } from './services'
+
+const transformData = (
+  data: ImpactedNodesAndWlans | undefined,
+  queryType: string
+): {
+  nodes: {
+    key: string;
+    value: number;
+    name: string;
+    color: string;
+  }[];
+  wlans: {
+    key: string;
+    value: number;
+    name: string;
+    color: string;
+  }[];
+} => {
+  if (data) {
+    const colors = qualitativeColorSet()
+    switch (queryType) {
+      case 'connectionFailure': {
+        const nodes = data.network.hierarchyNode.nodes.map((node, index) => ({
+          ...node,
+          name: node.key,
+          color: colors[index]
+        }))
+        const wlans = data.network.hierarchyNode.wlans.map((node, index) => ({
+          ...node,
+          name: node.key,
+          color: colors[index]
+        }))
+        return { nodes, wlans }
+      }
+      case 'timeToConnect': {
+        const nodes = data.network.hierarchyNode.nodes.map((node, index) => ({
+          ...node,
+          name: node.key,
+          color: colors[index]
+        }))
+        const wlans = data.network.hierarchyNode.wlans.map((node, index) => ({
+          ...node,
+          name: node.key,
+          color: colors[index]
+        }))
+        return { nodes, wlans }
+      }
+      default:
+        return { nodes: [], wlans: [] }
+    }
+  }
+  return { nodes: [], wlans: [] }
+}
+
+function getHealthPieChart (
+  data: { key: string; value: number; name: string; color: string }[],
+  height: number,
+  width: number,
+  dataFormatter: (value: unknown, tz?: string | undefined) => string
+) {
+  return (
+    <DonutChart
+      data={data}
+      style={{ height, width }}
+      showLegend={false}
+      dataFormatter={dataFormatter}
+    />
+  )
+}
+
+export const HealthPieChart = ({
+  queryType = 'connectionFailure',
+  queryFilter = 'connectionFailure'
+}) => {
+  const { $t } = useIntl()
+  const analyticsFilter = useAnalyticsFilter()
+  const { startDate: start, endDate: end, path } = analyticsFilter.filters
+  const queryResults = usePieChartQuery({
+    start,
+    end,
+    path,
+    queryType,
+    queryFilter
+  })
+
+  const { nodes, wlans } = transformData(queryResults.data, queryType)
+  const venues = nodes.length > 1 ? 'Venues' : 'Venue'
+  const networks = wlans.length > 1 ? 'Networks' : 'Network'
+
+  const tabDetails: ContentSwitcherProps['tabDetails'] = [
+    {
+      label: $t({ defaultMessage: '{venues}' }, { venues }),
+      value: 'nodes',
+      children: (
+        <Card
+          type='no-border'
+          title={$t(
+            { defaultMessage: 'Top {count} Impacted {venues}' },
+            { count: wlans.length, venues }
+          )}
+        >
+          <AutoSizer>
+            {({ height, width }) =>
+              getHealthPieChart(wlans, height, width, formatter('countFormat'))
+            }
+          </AutoSizer>
+        </Card>
+      )
+    },
+    {
+      label: $t({ defaultMessage: '{networks}' }, { networks }),
+      value: 'wlans',
+      children: (
+        <Card
+          type='no-border'
+          title={$t(
+            { defaultMessage: 'Top {count} Impacted {networks}' },
+            { count: nodes.length, networks }
+          )}
+        >
+          <AutoSizer>
+            {({ height, width }) =>
+              getHealthPieChart(
+                wlans,
+                height,
+                width,
+                formatter('durationFormat')
+              )
+            }
+          </AutoSizer>
+        </Card>
+      )
+    }
+  ]
+
+  return (
+    <Loader states={[queryResults]}>
+      <ContentSwitcher tabDetails={tabDetails} size='small' />
+    </Loader>
+  )
+}
---
+++ libs/analytics/components/src/Health/HealthDrillDown/HealthPieChart/services.ts
@@ -0,0 +1,78 @@
+import { gql } from 'graphql-request'
+import _       from 'lodash'
+
+import { dataApi }     from '@acx-ui/store'
+import { NetworkPath } from '@acx-ui/utils'
+
+
+export type RequestPayload = {
+  path: NetworkPath
+  start: string
+  end: string
+  queryType: string
+  queryFilter: string
+}
+
+export type ImpactedNodesAndWlans = {
+  network: {
+    hierarchyNode: {
+      nodes: Array<{ key: string, value: number, name: string | null }>,
+      wlans: Array<{ key: string, value: number }>
+    }
+  }
+}
+
+export const pieChartQuery = (
+  path: NetworkPath,
+  type: string,
+  filter: string
+) => {
+  const apNode = _.find(path, { type: 'AP' })
+  switch (type) {
+    case 'connectionFailure': {
+      return apNode
+        ? `wlans: topNSSIDbyConnFailure(n: 6, stage: "${filter}") { key value }`
+        : `nodes: topNNodebyConnFailure(n: 6, stage: "${filter}") { key value name }
+      wlans: topNSSIDbyConnFailure(n: 6, stage: "${filter}") { key value }`
+    }
+    case 'timeToConnect': {
+      return apNode
+        ? `wlans: topNSSIDbyAvgTTC(n: 6, stage: "${filter}") { key value }`
+        : `nodes: topNNodebyAvgTTC(n: 6, stage: "${filter}") { key value name }
+        wlans: topNSSIDbyAvgTTC(n: 6, stage: "${filter}") { key value }`
+    }
+    default: {
+      return ''
+    }
+  }
+}
+
+export const api = dataApi.injectEndpoints({
+  endpoints: (build) => ({
+    pieChart: build.query<ImpactedNodesAndWlans, RequestPayload>({
+      query: payload => {
+        const { path, queryType, queryFilter } = payload
+        const innerQuery = pieChartQuery(path, queryType, queryFilter.toLowerCase())
+        return ({
+          document: gql`
+            query Network($path: [HierarchyNodeInput], $start: DateTime, $end: DateTime) {
+              network(start: $start, end: $end) {
+                hierarchyNode(path: $path) {
+                  ${innerQuery}
+                    }
+                  }
+                }
+          `,
+          variables: {
+            start: payload.start,
+            end: payload.end,
+            path: payload.path
+          }
+        })
+      }
+    })
+  })
+})
+
+export const { usePieChartQuery } = api
+
