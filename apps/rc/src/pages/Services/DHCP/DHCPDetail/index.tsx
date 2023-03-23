89295b5b849530e4da30e39935bfe064afc8b2db
sean.huang <sean.huang@ruckuswireless.com>
2023-03-23 10:37:02 +0800

    ACX-27029: disabled DHCP edit button when it is bind to a venue.

--- apps/rc/src/pages/Services/DHCP/DHCPDetail/index.tsx
+++ apps/rc/src/pages/Services/DHCP/DHCPDetail/index.tsx
@@ -3,13 +3,16 @@ import { useParams } from 'react-router-dom'
 
 import { PageHeader, GridRow, GridCol, DisabledButton, Button, Tabs } from '@acx-ui/components'
 import { ClockOutlined }                                              from '@acx-ui/icons'
-import { useGetDHCPProfileQuery }                                     from '@acx-ui/rc/services'
+import {
+  useGetDHCPProfileQuery,
+  useVenuesListQuery } from '@acx-ui/rc/services'
 import {
   getServiceDetailsLink,
   getServiceRoutePath,
   ServiceOperation,
   ServiceType
 } from '@acx-ui/rc/utils'
+import { DHCPUsage }  from '@acx-ui/rc/utils'
 import { TenantLink } from '@acx-ui/react-router-dom'
 
 import { PoolTable } from '../DHCPForm/DHCPPool/PoolTable'
@@ -23,6 +26,12 @@ export default function DHCPServiceDetail () {
   const params = useParams()
 
   const { data } = useGetDHCPProfileQuery({ params })
+  const venuesList = useVenuesListQuery({ params, payload: {
+    fields: ['name', 'id'],
+    filters: {
+      id: data?.usage?.map((usage:DHCPUsage)=>usage.venueId)||['none']
+    }
+  } })
 
   return (
     <>
@@ -43,7 +52,9 @@ export default function DHCPServiceDetail () {
             oper: ServiceOperation.EDIT,
             serviceId: params.serviceId!
           })}>
-            <Button key='configure' type='primary'>{$t({ defaultMessage: 'Configure' })}</Button>
+            <Button key='configure'
+              disabled={venuesList.data && venuesList.data.data.length>0}
+              type='primary'>{$t({ defaultMessage: 'Configure' })}</Button>
           </TenantLink>
         ]}
       />
