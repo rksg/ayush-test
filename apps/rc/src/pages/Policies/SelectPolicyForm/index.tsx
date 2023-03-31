ac5c064dd297a503fa067aa4c1597e0a06c73252
rick yang <Rick.Yang@ruckuswireless.com>
2023-03-31 09:39:57 +0800

    ACX-19112: block create MAC_REGISTRATION_LIST card if ff is not enabled.

--- apps/rc/src/pages/Policies/SelectPolicyForm/index.tsx
+++ apps/rc/src/pages/Policies/SelectPolicyForm/index.tsx
@@ -27,6 +27,7 @@ export default function SelectPolicyForm () {
   const tenantBasePath: Path = useTenantLink('')
   const supportApSnmp = useIsSplitOn(Features.AP_SNMP)
   const isEdgeEnabled = useIsSplitOn(Features.EDGES)
+  const macRegistrationEnabled = useIsSplitOn(Features.MAC_REGISTRATION)
 
   const navigateToCreatePolicy = async function (data: { policyType: PolicyType }) {
     const policyCreatePath = getPolicyRoutePath({
@@ -46,8 +47,7 @@ export default function SelectPolicyForm () {
     { type: PolicyType.ROGUE_AP_DETECTION, categories: [RadioCardCategory.WIFI] },
     { type: PolicyType.AAA, categories: [RadioCardCategory.WIFI] },
     { type: PolicyType.SYSLOG, categories: [RadioCardCategory.WIFI] },
-    { type: PolicyType.CLIENT_ISOLATION, categories: [RadioCardCategory.WIFI] },
-    { type: PolicyType.MAC_REGISTRATION_LIST, categories: [RadioCardCategory.WIFI] }
+    { type: PolicyType.CLIENT_ISOLATION, categories: [RadioCardCategory.WIFI] }
   ]
 
   if (supportApSnmp) {
@@ -55,6 +55,10 @@ export default function SelectPolicyForm () {
   }
   if (isEdgeEnabled) {
     sets.push({ type: PolicyType.TUNNEL_PROFILE, categories: [RadioCardCategory.WIFI] })
+  }
+
+  if(macRegistrationEnabled) {
+    sets.push({ type: PolicyType.MAC_REGISTRATION_LIST, categories: [RadioCardCategory.WIFI] })
   }
 
   return (
