6d562c492932935b5fead5c8a507eecd95a81396
Li, Roil <Roil.Li@ruckuswireless.com>
2023-03-31 13:21:19 +0800

    ACX-26857: Implement tunnel profile list page

---
+++ apps/rc/src/pages/Policies/TunnelProfile/__tests__/fixtures.ts
@@ -0,0 +1,56 @@
+export const mockedTunnelProfileViewData = {
+  totalCount: 2,
+  page: 1,
+  data: [
+    {
+      id: 'tunnelProfileId1',
+      name: 'tunnelProfile1',
+      tag: 'tag1',
+      mtuType: 'MANUAL',
+      mtuSize: 1450,
+      forceFragmentation: true,
+      networkSegmentIds: ['nsg1', 'nsg2'],
+      networkIds: ['network1', 'network2']
+    },
+    {
+      id: 'tunnelProfileId2',
+      name: 'tunnelProfile2',
+      tag: 'tag2',
+      mtuType: 'AUTO',
+      mtuSize: 0,
+      forceFragmentation: false,
+      networkSegmentIds: ['nsg1', 'nsg2'],
+      networkIds: ['network1', 'network2']
+    }
+  ]
+}
+
+export const mockedNsgOptions = {
+  totalCount: 2,
+  page: 1,
+  data: [
+    {
+      id: 'nsgId1',
+      name: 'nsg1'
+    },
+    {
+      id: 'nsgId2',
+      name: 'nsg2'
+    }
+  ]
+}
+
+export const mockedNetworkOptions = {
+  totalCount: 2,
+  page: 1,
+  data: [
+    {
+      id: 'networkId1',
+      name: 'network1'
+    },
+    {
+      id: 'networkId2',
+      name: 'network2'
+    }
+  ]
+}
--- libs/rc/utils/src/urls/policies/tunnelProfileUrls.ts
+++ libs/rc/utils/src/urls/policies/tunnelProfileUrls.ts
@@ -5,5 +5,20 @@ export const TunnelProfileUrls: { [key: string]: ApiInfo } = {
     method: 'post',
     url: '/tunnelServiceProfiles',
     newApi: true
+  },
+  getTunnelProfileViewDataList: {
+    method: 'post',
+    url: '/tunnelServiceProfiles/query',
+    newApi: true
+  },
+  batchDeleteTunnelProfile: {
+    method: 'delete',
+    url: '/tunnelServiceProfiles',
+    newApi: true
+  },
+  deleteTunnelProfile: {
+    method: 'delete',
+    url: '/tunnelServiceProfiles/:id',
+    newApi: true
   }
 }
