3e4388154fd65e030f3d12485a4b65b3a515e15a
vivek.kandhvar <vivek.kandhvar@ruckuswireless.com>
2023-04-06 15:41:08 +0530

    ACX-20248: chore: refactor code

--- apps/analytics/src/pages/VideoCallQoe/index.tsx
+++ apps/analytics/src/pages/VideoCallQoe/index.tsx
@@ -4,10 +4,9 @@ import { PageHeader }             from '@acx-ui/components'
 import { Features, useIsSplitOn } from '@acx-ui/feature-toggle'
 
 function VideoCallQoeListPage () {
-  const isVideoCallQoeListPageEnabled = useIsSplitOn(Features.VIDEO_CALL_QOE)
   const { $t } = useIntl()
 
-  if (!isVideoCallQoeListPageEnabled) {
+  if (!useIsSplitOn(Features.VIDEO_CALL_QOE)) {
     return <span>{ $t({ defaultMessage: 'Video Call QoE is not enabled' }) }</span>
   }
 
--- apps/main/src/pages/Layout/menuConfig.tsx
+++ apps/main/src/pages/Layout/menuConfig.tsx
@@ -43,7 +43,7 @@ const PoliciesSolid = styled(PoliciesSolidBase)`${LayoutUI.iconSolidOverride}`
 export function useMenuConfig () {
   const { $t } = useIntl()
   const showSV = useIsSplitOn(Features.SERVICE_VALIDATION)
-  const showSVVCQ = useIsSplitOn(Features.VIDEO_CALL_QOE)
+  const showVideoCallQoe = useIsSplitOn(Features.VIDEO_CALL_QOE)
   const earlyBetaEnabled = useIsSplitOn(Features.EDGE_EARLY_BETA)
   const isEdgeEnabled = useIsSplitOn(Features.EDGES) || earlyBetaEnabled
   const isAdministrationEnabled = useIsSplitOn(Features.UNRELEASED) || earlyBetaEnabled
@@ -104,7 +104,7 @@ export function useMenuConfig () {
         {
           path: '/serviceValidation/videoCallQoe',
           name: $t({ defaultMessage: 'Video Call QoE' }),
-          disabled: !showSVVCQ
+          disabled: !showVideoCallQoe
         }
       ],
       disabled: !showSV
