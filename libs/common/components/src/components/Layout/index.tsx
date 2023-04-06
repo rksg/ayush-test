5afb9c1d7195af98882f267c1b66f94fdcc6909a
TLIN4 <Tsaihuei.Lin@ruckuswireless.com>
2023-04-06 15:37:38 +0800

    ACX-27567: Dashboard page is still visible for Guest use login

--- libs/common/components/src/components/Layout/index.tsx
+++ libs/common/components/src/components/Layout/index.tsx
@@ -11,6 +11,8 @@ import {
   useTenantLink,
   TenantNavLink
 }                          from '@acx-ui/react-router-dom'
+import { RolesEnum }       from '@acx-ui/types'
+import { hasRoles }        from '@acx-ui/user'
 import { notAvailableMsg } from '@acx-ui/utils'
 
 import { Tooltip } from '../Tooltip'
@@ -65,6 +67,9 @@ export function Layout ({
       }))
     }
   }))
+  const isGuestManager = hasRoles([RolesEnum.GUEST_MANAGER])
+  const indexPath = isGuestManager ? '/users/guestsManager' : '/dashboard'
+
   const menuRender = (item: MenuItem, dom: React.ReactNode) => {
     const link = <TenantNavLink to={item.uri!} tenantType={item.tenantType}>
       {({ isActive }) => {
@@ -100,8 +105,8 @@ export function Layout ({
       location={location}
       menuHeaderRender={() =>
         <TenantNavLink
-          to='/dashboard'
-          tenantType={routes.find(({ path })=> path === '/dashboard')?.tenantType || 't'}
+          to={indexPath}
+          tenantType={routes.find(({ path })=> path === indexPath)?.tenantType || 't'}
         >
           <Logo />
         </TenantNavLink>
