20e7416872ccf8a30bcfe125ff4ec9f734f8c153
cbokil <chinmay.bokil@ruckuswireless.com>
2023-04-06 15:45:21 +0800

    ACX-27369: env file path

--- tools/docker/nginx/start-nginx.sh
+++ tools/docker/nginx/start-nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst < /etc/nginx/env.json.template > /usr/share/nginx/html/env.json
+envsubst < /etc/nginx/env.json.template > /usr/share/nginx/html/tenant/t/env.json
 nginx -g 'daemon off;'
