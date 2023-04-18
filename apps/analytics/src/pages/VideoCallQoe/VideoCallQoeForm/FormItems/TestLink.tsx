8d9c96e334abf1fbd801bc24f54e9892f077dba6
jitendra.dangi <jitendra.dangi@ruckuswireless.com>
2023-04-18 10:35:14 +0530

    ACX-28121: Added Test Link form item

--- apps/analytics/src/pages/VideoCallQoe/VideoCallQoeForm/FormItems/TestLink.tsx
+++ apps/analytics/src/pages/VideoCallQoe/VideoCallQoeForm/FormItems/TestLink.tsx
@@ -1,31 +1,30 @@
-import { Form, Input, Space }     from 'antd'
+import { useState } from 'react'
+
+import { Form, Space }            from 'antd'
 import { defineMessage, useIntl } from 'react-intl'
 import { FormattedMessage }       from 'react-intl'
 
 import { Button }           from '@acx-ui/components'
 import { InformationSolid } from '@acx-ui/icons'
 
-//import { useDuplicateNameValidator } from '../../services'
+import { LinkButton } from '../styledComponents'
 
 const name = 'link' as const
 const label = defineMessage({ defaultMessage: 'Call Details' })
 
 export function TestLink ({ link }:{ link: string }) {
   const { $t } = useIntl()
-  //const { editMode, initialValues } = useStepFormContext<{ name: string }>()
-  //const duplicateNameValidator = useDuplicateNameValidator(editMode, initialValues?.name)
-
+  const [copied, setCopied] = useState(false)
   const onCopy = async () => {
     navigator.clipboard.writeText(link)
+    setCopied(true)
   }
   const linkDescription = <FormattedMessage
-  /* eslint-disable max-len */
     defaultMessage={`
     <highlight>
     The meeting is accessible by the URL above. Copy and share the URL with other participants. 
     </highlight>
   `}
-    /* eslint-enable */
     values={{
       highlight: (chunks) => <Space align='start'>
         <InformationSolid />
@@ -40,17 +39,28 @@ export function TestLink ({ link }:{ link: string }) {
     extra={
       linkDescription
     }
-    children={<div style={{ display: 'flex' }}>
-      <Input readOnly
-        value={link}
-      />
-      <Button
-        type='primary'
-        style={{ alignSelf: 'baseline', marginLeft: 7 }}
-        onClick={onCopy}
-      >
-        {$t({ defaultMessage: 'Copy Link' })}
-      </Button>
-    </div>}
+    children={
+      <Space>
+        <LinkButton type='link'>
+          <a
+            className='link'
+            target='_blank'
+            href={link}
+            rel='noreferrer'>
+            {link}
+          </a>
+        </LinkButton>
+        <Button
+          type='primary'
+          style={{ marginLeft: 7 }}
+          onClick={onCopy}
+        >
+          {
+            copied ? $t({ defaultMessage: 'Copied' }):
+              $t({ defaultMessage: 'Copy Link' })
+          }
+        </Button>
+      </Space>
+    }
   />
 }
