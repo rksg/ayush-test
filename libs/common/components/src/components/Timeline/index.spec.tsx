d5cd3e533651b813de24a23ca4116424fffad1c5
wilson.loh <wilson.loh@ruckuswireless.com>
2023-04-27 15:28:03 +0800

    ACX-26353: test - update unit tests

--- libs/common/components/src/components/Timeline/index.spec.tsx
+++ libs/common/components/src/components/Timeline/index.spec.tsx
@@ -1,25 +1,22 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import { IntlProvider }              from 'react-intl'
 
-import { sample, sampleFail } from './stories'
+import { sample } from './stories'
 
 import { Timeline } from '.'
 
 describe('Timeline', () => {
   it('should render', async () => {
     const { asFragment } = render(<IntlProvider locale='en'>
-      <Timeline items={sample}/>
+      <Timeline items={sample} status={'INPROGRESS'}/>
     </IntlProvider>)
     expect(asFragment()).toMatchSnapshot()
   })
   it('should handle when expand', async () => {
-    render(<IntlProvider locale='en'><Timeline items={sample}/></IntlProvider>)
+    render(<IntlProvider locale='en'><Timeline items={sample} status={'FAIL'}/></IntlProvider>)
     fireEvent.click((await screen.findAllByTestId('PlusSquareSolid'))[0])
     await screen.findByTestId('MinusSquareSolid')
     await screen.findAllByText('More fail content')
-  })
-  it('should remove startTime for in progress activities if not in current stage', async () => {
-    render(<IntlProvider locale='en'><Timeline items={sampleFail}/></IntlProvider>)
     const firstStartTime = screen.getByText(/12\/20\/2022 08:02:00/i)
     const secondStartTime = screen.queryAllByText(/12\/20\/2022 08:03:00/i)
     const thirdStartTime = screen.queryAllByText(/12\/20\/2022 08:04:00/i)
