75ea9824c3278c07bdf093c0dd5df30dace77bc5
tingan.chen <tingan.chen@ruckuswireless.com>
2023-04-27 15:04:31 +0800

    ACX-26213: feat: shell script for getting Unique Strings, Word Count and Unique Word Count

---
+++ tools/docker/locales/genCount.sh
@@ -0,0 +1,26 @@
+JSON_FILE="apps/main/src/locales/compiled/en-US.json"
+
+awk '{ \
+  sub(/[,]*$/,",")
+  for (i=2; i<=NF; i++) \
+    if(i == 2 && i == NF) { \
+      printf("%s\n", substr($i, 2, length($i)-3)) \
+    } \
+    else if(i == 2) { \
+      printf("%s ", substr($i, 2)) \
+    } \
+    else if(i == NF) { \
+      printf("%s\n", substr($i, 1, length($i)-2)) \
+    } \
+    else { \
+      printf("%s ",$i) } \
+}' $JSON_FILE > ./tmp
+
+UNIQUE_STRINGS=$(wc -l ./tmp | awk '{print $1}')
+echo "Unique Strings: ${UNIQUE_STRINGS}"
+
+WORD_COUNT=$(wc -w ./tmp | awk '{print $1}')
+echo "Word Count:  ${WORD_COUNT}"
+
+UNIQUE_WORD_COUNT=$(tr ' ' '\n' < ./tmp | sort | uniq -c | wc -l | awk '{print $1}')
+echo "Unique Word Count: ${UNIQUE_WORD_COUNT}"
