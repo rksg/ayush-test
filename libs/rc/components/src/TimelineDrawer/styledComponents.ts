ac35b0d17ad1221649865f4b1a69a21c010821e8
jason.cheow <jason.cheow@ruckuswireless.com>
2023-04-28 18:42:34 +0800

    ACX-26353: Share copyable textarea styles

--- libs/rc/components/src/TimelineDrawer/styledComponents.ts
+++ libs/rc/components/src/TimelineDrawer/styledComponents.ts
@@ -1,33 +1,13 @@
 import { Input as AntInput } from 'antd'
 import styled                from 'styled-components/macro'
 
-import { Button } from '@acx-ui/components'
+import { ActionModal } from '@acx-ui/components'
 
 const { TextArea } = AntInput
 
 export const FailureTextArea = styled(TextArea)`
-  background: var(--acx-neutrals-10);
-  border: 1px solid var(--acx-neutrals-30);
-  font-size: var(--acx-subtitle-6-font-size);
-  line-height: var(--acx-subtitle-6-line-height);
-  padding: 12px;
-  max-height: 200px;
-  resize: none;
-  &:hover,
-  &:focus {
-    border-color: var(--acx-neutrals-30);
-    box-shadow: none
-  }
+  ${ActionModal.copyableTextAreaStyle}
   margin-top: 10px;
 `
 
-export const CopyButton = styled(Button)`
-  display: inline-flex;
-  justify-content: flex-end;
-  font-size: var(--acx-body-5-font-size);
-  line-height: 16px;
-  color: var(--acx-accents-blue-50);
-  height: auto;
-  margin: 4px 0 4px auto;
-  justify-self: end;
-`
+export const CopyButton = ActionModal.CopyButton
