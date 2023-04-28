a1ae24ef536dce9a59d24fe4e124b653ab4ef030
cbokil <chinmay.bokil@ruckuswireless.com>
2023-04-28 19:24:55 +0800

    ACX-14956: added tests

--- apps/analytics/src/pages/IncidentDetails/Details/FailureTemplate.tsx
+++ apps/analytics/src/pages/IncidentDetails/Details/FailureTemplate.tsx
@@ -1,7 +1,8 @@
 import { unitOfTime } from 'moment-timezone'
+import { useIntl }    from 'react-intl'
 
-import { Incident }         from '@acx-ui/analytics/utils'
-import { GridRow, GridCol } from '@acx-ui/components'
+import { calculateSeverity, Incident, shortDescription } from '@acx-ui/analytics/utils'
+import { PageHeader, SeverityPill, GridRow, GridCol }    from '@acx-ui/components'
 
 import { IncidentAttributes, Attributes }    from '../IncidentAttributes'
 import { Insights }                          from '../Insights'
@@ -10,10 +11,11 @@ import { NetworkImpactChartTypes }           from '../NetworkImpact/config'
 import { TimeSeries }                        from '../TimeSeries'
 import { TimeSeriesChartTypes }              from '../TimeSeries/config'
 
-import * as UI from './styledComponents'
-import IncidentDetailsHeader from './IncidentDetailsHeader'
+import MuteIncident from './MuteIncident'
+import * as UI      from './styledComponents'
 
 export const FailureTemplate = (incident: Incident) => {
+  const { $t } = useIntl()
   const attributeList = [
     Attributes.ClientImpactCount,
     Attributes.IncidentCategory,
@@ -51,10 +53,18 @@ export const FailureTemplate = (incident: Incident) => {
     front: { value: 6, unit: 'hours' as unitOfTime.Base },
     back: { value: 6, unit: 'hours' as unitOfTime.Base }
   }
-  
+
   return (
     <>
-      <IncidentDetailsHeader incident={incident} />
+      <PageHeader
+        title={$t({ defaultMessage: 'Incident Details' })}
+        titleExtra={<SeverityPill severity={calculateSeverity(incident.severity)!} />}
+        breadcrumb={[
+          { text: $t({ defaultMessage: 'Incidents' }), link: '/analytics/incidents' }
+        ]}
+        subTitle={shortDescription(incident)}
+        extra={[<MuteIncident incident={incident} />]}
+      />
       <GridRow>
         <GridCol col={{ span: 4 }}>
           <UI.FixedAutoSizer>
