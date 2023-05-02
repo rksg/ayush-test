7c0ddb98178afeb3f0c649006c8c80e8b3788199
tingan.chen <tingan.chen@ruckuswireless.com>
2023-05-02 16:30:43 +0800

    ACX-29554: feat: add new disconnect event

--- libs/analytics/utils/src/index.ts
+++ libs/analytics/utils/src/index.ts
@@ -9,15 +9,19 @@ export * from './healthKPIConfig'
 export * from './incidents'
 export * from './incidentInformation'
 export { default as incidentSeverities } from './incidentSeverities.json'
+export { disconnectClientEvents } from './mapping/clientDisconnectEventsMap'
 export { getRootCauseAndRecommendations, codeToFailureTypeMap } from './rootCauseRecommendation'
 export {
   clientEventDescription,
+  mapDisconnectCode,
   mapDisconnectCodeToReason,
+  disconnectClientEventsMap,
   mapCodeToReason,
   mapCodeToAttempt,
   mapCodeToFailureText
 } from './reasonCodeMap'
 export * from './timeseries'
 export * from './sorters'
+export * from './types/clientEvent'
 export * from './types/incidents'
 export * from './types/timeseries'
---
+++ libs/analytics/utils/src/mapping/clientDisconnectEventsMap.ts
@@ -0,0 +1,69 @@
+/* eslint-disable max-len */
+import { defineMessage } from 'react-intl'
+
+import { ClientEventEnum } from '../types/clientEvent'
+
+const disconnects = [
+  {
+    id: 204,
+    code: ClientEventEnum.DISCONNECT,
+    text: defineMessage({ defaultMessage: 'Client disconnected' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when the client disconnects from WLAN on AP' })
+  },
+  {
+    id: 205,
+    code: ClientEventEnum.INACTIVITY_TIMEOUT,
+    text: defineMessage({ defaultMessage: 'Client connection timed out' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when client disconnects from WLAN on AP due to inactivity' })
+  },
+  {
+    id: 208,
+    code: ClientEventEnum.SESSION_EXPIRED,
+    text: defineMessage({ defaultMessage: 'Client session expired' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when the client exceeds the session time limit resulting in a session termination' })
+  },
+  {
+    id: 210,
+    code: ClientEventEnum.LOGGED_OUT,
+    text: defineMessage({ defaultMessage: 'Client logged out' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when a client session is logged out' })
+  },
+  {
+    id: 218,
+    code: ClientEventEnum.ROAMING_DISCONNECTED,
+    text: defineMessage({ defaultMessage: 'Client roaming disconnected' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when the client disconnects from the WLAN due to a smart roam policy' })
+  }
+]
+
+const isolatedDisconnects = [
+  {
+    id: 201,
+    code: ClientEventEnum.AUTHENTICATION_FAILURE,
+    text: defineMessage({ defaultMessage: 'Client authentication failed' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when the client fails to join WLAN on the AP due to an authentication failure' })
+  },
+  {
+    id: 219,
+    code: ClientEventEnum.BLOCKED,
+    text: defineMessage({ defaultMessage: 'Client blocked' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when a client is blocked by a device policy' })
+  },
+  {
+    id: 225,
+    code: ClientEventEnum.FORCE_DHCP_DISCONNECTED,
+    text: defineMessage({ defaultMessage: 'Force DHCP disconnected' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when the client disconnects by force dynamic host configuration protocol (DHCP)' })
+  },
+  {
+    id: 228,
+    code: ClientEventEnum.BLOCKED_BY_BARING,
+    text: defineMessage({ defaultMessage: 'Client is blocked because of barring UE rule' }),
+    reason: defineMessage({ defaultMessage: 'This event occurs when a client is temporally blocked by the UE barring rule' })
+  }
+]
+
+export const disconnectClientEvents = [
+  ...disconnects,
+  ...isolatedDisconnects
+]
