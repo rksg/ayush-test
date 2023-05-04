1d6bac587d1b0eff626cdeb634453f9a00448d91
Suraj <Suraj.Bande@ruckuswireless.com>
2023-05-04 13:40:05 +0530

    ACX-25486: snaps updated

--- libs/rc/components/src/Topology/SwitchDetailsCard.tsx
+++ libs/rc/components/src/Topology/SwitchDetailsCard.tsx
@@ -1,12 +1,12 @@
 import { Badge, Button, Typography } from 'antd'
 import { useIntl }                   from 'react-intl'
 
-import { IncidentsBySeverityData, useIncidentsBySeverityQuery } from '@acx-ui/analytics/components'
-import { AnalyticsFilter }                                      from '@acx-ui/analytics/utils'
-import { Card, Descriptions, Loader, StackedBarChart }          from '@acx-ui/components'
-import { DateFormatEnum, formatter }                            from '@acx-ui/formatter'
-import { SwitchStatusEnum, SwitchViewModel }                    from '@acx-ui/rc/utils'
-import { noDataDisplay, useDateFilter }                         from '@acx-ui/utils'
+import { IncidentsBySeverityData, useIncidentsBySeverityQuery }          from '@acx-ui/analytics/components'
+import { AnalyticsFilter }                                               from '@acx-ui/analytics/utils'
+import { Card, Descriptions, Loader, StackedBarChart, SuspenseBoundary } from '@acx-ui/components'
+import { DateFormatEnum, formatter }                                     from '@acx-ui/formatter'
+import { SwitchStatusEnum, SwitchViewModel }                             from '@acx-ui/rc/utils'
+import { noDataDisplay, useDateFilter }                                  from '@acx-ui/utils'
 
 import { getDeviceColor, switchStatus } from './utils'
 
@@ -31,6 +31,8 @@ export function SwitchDetailsCard (props: {
     }),
     skip: !switchDetail?.switchMac || !switchDetail?.venueId
   })
+
+  const { DefaultFallback: Spinner } = SuspenseBoundary
 
   const totalIncidents = incidentData?.data.P1
                   + incidentData?.data.P2
@@ -83,44 +85,53 @@ export function SwitchDetailsCard (props: {
         {/* Incidents */}
         <Descriptions.Item
           label={$t({ defaultMessage: 'Incidents (Last 24 hrs)' })}
-          style={{ alignItems: 'center' }}
-          contentStyle={{ display: 'inline-flex',
-            alignItems: 'center' }}
+          contentStyle={{
+            alignSelf: 'center',
+            display: 'inline-flex'
+          }}
           children={
-            incidentData?.data ?
-              <>{ !! totalIncidents &&
-                <StackedBarChart
-                  style={{ height: 16, width: 135, marginRight: 4 }}
-                  barWidth={12}
-                  showLabels={false}
-                  showTotal={false}
-                  data={[{
-                    category: 'Switch Incidents',
-                    series: [
-                      {
-                        name: 'P1',
-                        value: incidentData?.data.P1
-                      },
-                      {
-                        name: 'P2',
-                        value: incidentData?.data.P2
-                      },
-                      {
-                        name: 'P3',
-                        value: incidentData?.data.P3
-                      },
-                      {
-                        name: 'P4',
-                        value: incidentData?.data.P4
-                      }
-                    ]
-                  }]} />
+            <Loader
+              fallback={<Spinner size='small' />}
+              states={[
+                incidentData
+              ]}>
+              { incidentData?.data ?
+                <div style={{
+                  display: 'inline-flex'
+                }}>
+                  { !!totalIncidents && <StackedBarChart
+                    style={{ height: 16, width: 135, marginRight: 4 }}
+                    barWidth={12}
+                    showLabels={false}
+                    showTotal={false}
+                    data={[{
+                      category: 'AP Incidents',
+                      series: [
+                        {
+                          name: 'P1',
+                          value: incidentData?.data.P1
+                        },
+                        {
+                          name: 'P2',
+                          value: incidentData?.data.P2
+                        },
+                        {
+                          name: 'P3',
+                          value: incidentData?.data.P3
+                        },
+                        {
+                          name: 'P4',
+                          value: incidentData?.data.P4
+                        }
+                      ]
+                    }]} /> }
+                  <Typography.Text>
+                    { totalIncidents }
+                  </Typography.Text>
+                </div>
+                : noDataDisplay
               }
-              <Typography.Text>
-                { totalIncidents }
-              </Typography.Text>
-              </>
-              : noDataDisplay
+            </Loader>
           }
         />
 
