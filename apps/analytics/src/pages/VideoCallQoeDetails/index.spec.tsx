c727d5b7cbd5262276e6a2879f1726fee7fa1a3f
jitendra.dangi <jitendra.dangi@ruckuswireless.com>
2023-05-08 13:22:20 +0530

    ACX-29663: refact: mock search api

--- apps/analytics/src/pages/VideoCallQoeDetails/index.spec.tsx
+++ apps/analytics/src/pages/VideoCallQoeDetails/index.spec.tsx
@@ -1,22 +1,32 @@
 /* eslint-disable testing-library/no-node-access */
 import userEvent from '@testing-library/user-event'
 
-import { Provider, dataApiSearchURL, videoCallQoeURL } from '@acx-ui/store'
-import { mockGraphqlMutation, fireEvent,
+import { Provider, dataApiSearchURL, store, videoCallQoeURL } from '@acx-ui/store'
+import {
+  mockGraphqlMutation,
   mockGraphqlQuery, render, screen, waitFor,
-  waitForElementToBeRemoved } from '@acx-ui/test-utils'
+  waitForElementToBeRemoved
+} from '@acx-ui/test-utils'
 
 import { callQoeTestDetailsFixtures1, callQoeTestDetailsFixtures2, callQoeTestDetailsFixtures3, callQoeTestDetailsFixtures4, searchClientsFixture } from '../VideoCallQoe/__tests__/fixtures'
+import { clientSearchApi }                                                                                                                          from '../VideoCallQoe/services'
 
 import { VideoCallQoeDetails } from '.'
 
-describe('VideoCallQoe Details Page',()=>{
+describe('VideoCallQoe Details Page', () => {
   const params = {
     tenantId: 'tenant-id'
   }
-  it('render the page properly',async ()=>{
+  beforeEach(() => {
+    store.dispatch(clientSearchApi.util.resetApiState())
+    mockGraphqlQuery(dataApiSearchURL, 'Search', {
+      data: searchClientsFixture
+    })
+  })
+  it('render the page properly', async () => {
     mockGraphqlQuery(videoCallQoeURL, 'CallQoeTestDetails',
       { data: callQoeTestDetailsFixtures1 })
+
     const { asFragment } = render(
       <Provider>
         <VideoCallQoeDetails />
@@ -26,12 +36,13 @@ describe('VideoCallQoe Details Page',()=>{
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
     const fragment = asFragment()
     fragment.querySelectorAll('div[_echarts_instance_^="ec_"]')
-      .forEach((node:Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
+      .forEach((node: Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
     expect(fragment).toMatchSnapshot()
   })
-  it('render the page properly having bad/average quality',async ()=>{
+  it('render the page properly having bad/average quality', async () => {
     mockGraphqlQuery(videoCallQoeURL, 'CallQoeTestDetails',
       { data: callQoeTestDetailsFixtures2 })
+
     const { asFragment } = render(
       <Provider>
         <VideoCallQoeDetails />
@@ -41,12 +52,13 @@ describe('VideoCallQoe Details Page',()=>{
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
     const fragment = asFragment()
     fragment.querySelectorAll('div[_echarts_instance_^="ec_"]')
-      .forEach((node:Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
+      .forEach((node: Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
     expect(fragment).toMatchSnapshot()
   })
-  it('render the page properly without client MAC',async ()=>{
+  it('render the page properly without client MAC', async () => {
     mockGraphqlQuery(videoCallQoeURL, 'CallQoeTestDetails',
       { data: callQoeTestDetailsFixtures3 })
+
     const { asFragment } = render(
       <Provider>
         <VideoCallQoeDetails />
@@ -56,18 +68,15 @@ describe('VideoCallQoe Details Page',()=>{
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
     const fragment = asFragment()
     fragment.querySelectorAll('div[_echarts_instance_^="ec_"]')
-      .forEach((node:Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
+      .forEach((node: Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
     expect(fragment).toMatchSnapshot()
   })
-  it('should open drawer for client mac serach while click on edit icon',async ()=>{
+  it('should open drawer for client mac serach while click on edit icon', async () => {
     mockGraphqlQuery(videoCallQoeURL, 'CallQoeTestDetails',
       { data: callQoeTestDetailsFixtures1 })
     mockGraphqlMutation(videoCallQoeURL, 'UpdateCallQoeParticipant',
       { data: { updateCallQoeParticipant: 1 } })
 
-    mockGraphqlQuery(dataApiSearchURL, 'Search', {
-      data: searchClientsFixture
-    })
     render(
       <Provider>
         <VideoCallQoeDetails />
@@ -78,18 +87,15 @@ describe('VideoCallQoe Details Page',()=>{
     await userEvent.click(screen.getByTestId('EditOutlinedIcon'))
     await screen.findByText('Select Client MAC')
     await screen.findByText('A8:64:F1:1A:D0:33')
-    const radioButtons=screen.getAllByRole('radio')
+    const radioButtons = screen.getAllByRole('radio')
     await userEvent.click(radioButtons[2])
     await screen.findByText('1 selected')
     await userEvent.click(screen.getByText('Select'))
     await waitForElementToBeRemoved(() => screen.queryByText('Select'))
   })
-  it('should close client mac search drawer while click on cancel button',async ()=>{
+  it('should close client mac search drawer while click on cancel button', async () => {
     mockGraphqlQuery(videoCallQoeURL, 'CallQoeTestDetails',
       { data: callQoeTestDetailsFixtures1 })
-    mockGraphqlQuery(dataApiSearchURL, 'Search', {
-      data: searchClientsFixture
-    })
     render(
       <Provider>
         <VideoCallQoeDetails />
@@ -100,26 +106,23 @@ describe('VideoCallQoe Details Page',()=>{
     await userEvent.click(screen.getByTestId('EditOutlinedIcon'))
     await screen.findByText('Select Client MAC')
     await screen.findByText('A8:64:F1:1A:D0:33')
-    const radioButtons=screen.getAllByRole('radio')
+    const radioButtons = screen.getAllByRole('radio')
     await userEvent.click(radioButtons[2])
     await screen.findByText('1 selected')
     await userEvent.click(screen.getByTitle('Clear selection'))
     await userEvent.click(radioButtons[3])
     await userEvent.click(screen.getByText('Cancel'))
-    await waitFor(()=>{
+    await waitFor(() => {
       expect(screen.queryByText('Cancel')).not.toBeInTheDocument()
     })
   })
 
-  it('should search the client',async ()=>{
+  it('should search the client', async () => {
     mockGraphqlQuery(videoCallQoeURL, 'CallQoeTestDetails',
       { data: callQoeTestDetailsFixtures1 })
     mockGraphqlMutation(videoCallQoeURL, 'UpdateCallQoeParticipant',
       { data: { updateCallQoeParticipant: 1 } })
 
-    mockGraphqlQuery(dataApiSearchURL, 'Search', {
-      data: searchClientsFixture
-    })
     render(
       <Provider>
         <VideoCallQoeDetails />
@@ -130,13 +133,13 @@ describe('VideoCallQoe Details Page',()=>{
     await userEvent.click(screen.getByTestId('EditOutlinedIcon'))
     const searchInput = await screen.findByPlaceholderText(/search by mac, username or hostname/i)
     await userEvent.type(searchInput, 'DPSK_User_8709')
-    await waitFor(()=>{
+    await waitFor(() => {
       expect(screen.queryByRole('img', { name: 'loader' })).toBeVisible()
     })
     expect(await screen.findByText('A8:64:F1:1A:D0:33')).toBeVisible()
   })
 
-  it('render the page properly when call stats are null',async ()=>{
+  it('render the page properly when call stats are null', async () => {
     mockGraphqlQuery(videoCallQoeURL, 'CallQoeTestDetails',
       { data: callQoeTestDetailsFixtures4 })
     const { asFragment } = render(
@@ -148,7 +151,7 @@ describe('VideoCallQoe Details Page',()=>{
     await waitForElementToBeRemoved(() => screen.queryByRole('img', { name: 'loader' }))
     const fragment = asFragment()
     fragment.querySelectorAll('div[_echarts_instance_^="ec_"]')
-      .forEach((node:Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
+      .forEach((node: Element) => node.setAttribute('_echarts_instance_', 'ec_mock'))
     expect(fragment).toMatchSnapshot()
   })
 })
