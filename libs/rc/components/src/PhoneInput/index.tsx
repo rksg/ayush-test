6d4b6adfda11e0fc031f8f9498cc7e2c9f13c7dc
yw1046 <Yc.Wong@ruckuswireless.com>
2023-05-18 18:06:30 +0800

    ACX-29715: Fix the country dropdown list loading twice and overlapping.

--- libs/rc/components/src/PhoneInput/index.tsx
+++ libs/rc/components/src/PhoneInput/index.tsx
@@ -15,29 +15,35 @@ interface PhoneInputProps {
 
 export function PhoneInput ({ callback, name, onTop }: PhoneInputProps) {
   const inputRef = useRef<InputRef>(null)
-  const form = Form.useFormInstance()
 
   useEffect(() => {
     if (inputRef.current?.input) {
-      const iti = intlTelInput(inputRef.current.input, {
-        nationalMode: false,
-        hiddenInput: 'full_phone',
-        autoPlaceholder: 'aggressive',
-        placeholderNumberType: 'MOBILE',
-        preferredCountries: ['us'],
-        utilsScript: 'intl-tel-input/js/utils',
-        dropdownContainer: onTop ? document.body : undefined
-      })
+      const input = document.querySelector(`#phone_${name}`)
+      if(input && window.intlTelInputGlobals.getInstance(input)){
+        const iti = window.intlTelInputGlobals.getInstance(input)
+        initialise(iti)
+      }else{
+        const iti = intlTelInput(inputRef.current.input, {
+          nationalMode: false,
+          hiddenInput: 'full_phone',
+          autoPlaceholder: 'aggressive',
+          placeholderNumberType: 'MOBILE',
+          preferredCountries: ['us'],
+          utilsScript: 'intl-tel-input/js/utils',
+          dropdownContainer: onTop ? document.body : undefined
+        })
 
-      const handleChange = () => {
-        callback && callback(iti.getNumber())
+        initialise(iti)
       }
 
-      inputRef.current.input.addEventListener('change', handleChange)
-      inputRef.current.input.addEventListener('keyup', handleChange)
-
-      if(form?.getFieldValue(name)) {
-        iti.setNumber(form.getFieldValue(name))
+      function initialise (iti: intlTelInput.Plugin) {
+        if (inputRef.current?.input) {
+          const handleChange = () => {
+            callback && callback(iti.getNumber())
+          }
+          inputRef.current.input.addEventListener('change', handleChange)
+          inputRef.current.input.addEventListener('keyup', handleChange)
+        }
       }
     }
   }, [])
@@ -45,7 +51,7 @@ export function PhoneInput ({ callback, name, onTop }: PhoneInputProps) {
   return (
     <FlagContainer>
       <Form.Item name={name}>
-        <Input ref={inputRef} style={{ width: '100%' }} />
+        <Input ref={inputRef} style={{ width: '100%' }} id={`phone_${name}`} />
       </Form.Item>
     </FlagContainer>
   )
