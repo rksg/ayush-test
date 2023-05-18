5b9b86509766aa34e114139bf67ef936f951a0cf
Sander.Chen <Sander.Chen@ruckuswireless.com>
2023-05-18 16:43:37 +0800

    ACX-29976: change display format from self-defined to formatter, combine condition

--- libs/rc/components/src/EdgeStatisticWidget/EdgeUpTimeWidget.tsx
+++ libs/rc/components/src/EdgeStatisticWidget/EdgeUpTimeWidget.tsx
@@ -6,8 +6,10 @@ import { useIntl }   from 'react-intl'
 import { useParams } from 'react-router-dom'
 import AutoSizer     from 'react-virtualized-auto-sizer'
 
+
 import { calculateGranularity, TimeSeriesChartData, getSeriesData }                from '@acx-ui/analytics/utils'
 import { Card, Loader, NoData, MultiBarTimeSeriesChart, GridCol, cssStr, Tooltip } from '@acx-ui/components'
+import { formatter }                                                               from '@acx-ui/formatter'
 import { useGetEdgeUptimeMutation }                                                from '@acx-ui/rc/services'
 import { EdgeStatusTimeSeries, EdgeTimeSeriesPayload }                             from '@acx-ui/rc/utils'
 import { TimeStamp }                                                               from '@acx-ui/types'
@@ -26,12 +28,6 @@ interface EdfeUpTimePresentingData { timeSeries: [
   totalDowntime: number
 }
 
-const milliSecondsToHm = (milliSeconds: number) => {
-  const seconds = Math.floor(milliSeconds / 1000)
-  const h = Math.floor(seconds / 3600) + 'h'
-  const m = Math.floor((seconds % 3600) / 60) + 'm'
-  return h + ' ' + m
-}
 function getStartAndEndTimes (timeSeries: TimeSeriesChartData[]) {
   return timeSeries?.[0]?.data?.reduce((startEndTimes, dataPoint, index) => {
     const [time, value] = dataPoint
@@ -40,7 +36,7 @@ function getStartAndEndTimes (timeSeries: TimeSeriesChartData[]) {
         value === 1
           ? cssStr('--acx-semantics-green-50')
           : (cssStr('--acx-semantics-red-50') as string)
-      startEndTimes.push([time, 'switchStatus', time, value, seriesColor])
+      startEndTimes.push([time, 'edgeStatus', time, value, seriesColor])
     } else {
       const lastStartEndTime = startEndTimes[startEndTimes.length - 1]
       lastStartEndTime[2] = time
@@ -107,84 +103,84 @@ export function EdgeUpTimeWidget () {
     }
     initialWidget()
   }, [filters])
-
-  if (!queryResults || _.isEmpty(queryResults.timeSeries)) {
-    return (
-      <Loader states={[{ isLoading: loadingState }]}>
-        <UI.Wrapper>
-          <UI.EdgeStatusHeader col={{ span: 16 }}>
-            <Card.Title>
-              {$t({ defaultMessage: 'Edge Status' })}
-              <Tooltip
-                title={
-                  $t({ defaultMessage: 'Historical data is slightly delayed, and not real-time' })}>
-                <UI.HistoricalIcon />
-              </Tooltip>
-            </Card.Title>
-          </UI.EdgeStatusHeader>
-          <GridCol col={{ span: 24 }} style={{ height: '50px' }}>
-            <AutoSizer>
-              {() =><NoData />}
-            </AutoSizer>
-          </GridCol>
-        </UI.Wrapper>
-      </Loader>
-    )
-  }
-
   return (
     <Loader states={[{ isLoading: loadingState }]}>
       <UI.Wrapper>
-        <UI.EdgeStatusHeader col={{ span: 16 }}>
-          <Card.Title>
-            {$t({ defaultMessage: 'Edge Status' })}
-            <Tooltip
-              title={
-                $t({ defaultMessage: 'Historical data is slightly delayed, and not real-time' })}>
-              <UI.HistoricalIcon />
-            </Tooltip>
-          </Card.Title>
-        </UI.EdgeStatusHeader>
-        <UI.Status col={{ span: 4 }} style={{ height: '20px' }}>
-          {$t({ defaultMessage: 'Total Uptime' })}
-          {': '}
-          <UI.Duration>
-            {milliSecondsToHm((queryResults.totalUptime * 1000) || 0)}
-          </UI.Duration>
-        </UI.Status>
-        <UI.Status col={{ span: 4 }}>
-          {$t({ defaultMessage: 'Total Downtime' })}
-          {': '}
-          <UI.Duration>{milliSecondsToHm(queryResults?.totalDowntime * 1000 || 0)}</UI.Duration>
-        </UI.Status>
-        <GridCol col={{ span: 24 }} style={{ height: '50px' }}>
-          <AutoSizer>
-            {({ height, width }) =>
-              <MultiBarTimeSeriesChart
-                style={{ width, height }}
-                data={[
-                  {
-                    key: 'SwitchStatus',
-                    name: 'switch',
-                    color: cssStr('--acx-semantics-green-50'),
-                    data: queryResults?.timeSeries as [
+        {(!queryResults || _.isEmpty(queryResults.timeSeries)) ?
+          <>
+            <UI.EdgeStatusHeader col={{ span: 16 }}>
+              <Card.Title>
+                {$t({ defaultMessage: 'Edge Status' })}
+                <Tooltip
+                  title={$t({
+                    defaultMessage: 'Historical data is slightly delayed, and not real-time'
+                  })}>
+                  <UI.HistoricalIcon />
+                </Tooltip>
+              </Card.Title>
+            </UI.EdgeStatusHeader>
+            <GridCol col={{ span: 24 }} style={{ height: '50px' }}>
+              <AutoSizer>
+                {() =><NoData />}
+              </AutoSizer>
+            </GridCol>
+          </>:
+          <>
+            <UI.EdgeStatusHeader col={{ span: 16 }}>
+              <Card.Title>
+                {$t({ defaultMessage: 'Edge Status' })}
+                <Tooltip
+                  title={$t({
+                    defaultMessage: 'Historical data is slightly delayed, and not real-time'
+                  })}>
+                  <UI.HistoricalIcon />
+                </Tooltip>
+              </Card.Title>
+            </UI.EdgeStatusHeader>
+            <UI.Status col={{ span: 4 }} style={{ height: '20px' }}>
+              {$t({ defaultMessage: 'Total Uptime' })}
+              {': '}
+              <UI.Duration>
+                {formatter('durationFormat')((queryResults.totalUptime * 1000) || 0)}
+              </UI.Duration>
+            </UI.Status>
+            <UI.Status col={{ span: 4 }}>
+              {$t({ defaultMessage: 'Total Downtime' })}
+              {': '}
+              <UI.Duration>
+                {formatter('durationFormat')(queryResults?.totalDowntime * 1000 || 0)}
+              </UI.Duration>
+            </UI.Status>
+            <GridCol col={{ span: 24 }} style={{ height: '50px' }}>
+              <AutoSizer>
+                {({ height, width }) =>
+                  <MultiBarTimeSeriesChart
+                    style={{ width, height }}
+                    data={[
+                      {
+                        key: 'EdgeStatus',
+                        name: 'Edge',
+                        color: cssStr('--acx-semantics-green-50'),
+                        data: queryResults?.timeSeries as [
                         TimeStamp,
                         string,
                         TimeStamp,
                         number | null,
                         string
                       ][]
-                  }
-                ]}
-                chartBoundary={[
-                  moment(filters?.startDate).valueOf(),
-                  moment(filters?.endDate).valueOf()
-                ]}
-                hasXaxisLabel
-              />
-            }
-          </AutoSizer>
-        </GridCol>
+                      }
+                    ]}
+                    chartBoundary={[
+                      moment(filters?.startDate).valueOf(),
+                      moment(filters?.endDate).valueOf()
+                    ]}
+                    hasXaxisLabel
+                  />
+                }
+              </AutoSizer>
+            </GridCol>
+          </>
+        }
       </UI.Wrapper>
     </Loader>
   )
