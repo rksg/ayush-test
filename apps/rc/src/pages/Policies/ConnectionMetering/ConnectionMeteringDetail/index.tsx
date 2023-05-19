6ce3b263ef9bbd47145a7ab9892c35139a92c902
Jerry Chen <Jerry.Chen@ruckuswireless.com>
2023-05-19 14:58:31 +0800

    ACX-25763: Connection metering profile details page implementation

--- apps/rc/src/pages/Policies/ConnectionMetering/ConnectionMeteringDetail/index.tsx
+++ apps/rc/src/pages/Policies/ConnectionMetering/ConnectionMeteringDetail/index.tsx
@@ -1,196 +1,99 @@
 
 
-import { Descriptions } from 'antd'
-import { useIntl }      from 'react-intl'
+import { Typography } from 'antd'
+import { useIntl }    from 'react-intl'
+import { useParams }  from 'react-router-dom'
 
-import { Button, Card, Loader, PageHeader,  GridRow, GridCol, Subtitle, Table, TableProps } from '@acx-ui/components'
+import { Button, Card, Loader, PageHeader,  GridRow, GridCol } from '@acx-ui/components'
+import { useGetConnectionMeteringByIdQuery }                   from '@acx-ui/rc/services'
 import {
   getPolicyListRoutePath,
   getPolicyDetailsLink,
   PolicyType,
-  PolicyOperation
+  PolicyOperation, getPolicyRoutePath
 } from '@acx-ui/rc/utils'
-import { TenantLink } from '@acx-ui/react-router-dom'
+import { TenantLink }     from '@acx-ui/react-router-dom'
+import { filterByAccess } from '@acx-ui/user'
 
-import { DataConsumptionLabel }          from '../DataConsumptionHelper'
-import { VenueLink, PersonaDetailsLink } from '../LinkHelper'
-import { RateLimitingTableCell }         from '../RateLimitingHelper'
+import { DataConsumptionLabel }  from '../DataConsumptionHelper'
+import { RateLimitingTableCell } from '../RateLimitingHelper'
 
-function ConnectionMeteringDetailsPageHeader (props: {
-  id: string
-  title?: string
-}) {
-  const { $t } = useIntl()
-  const { id, title } = props
-
-  const extra = [
-    <TenantLink
-      key='add'
-      // eslint-disable-next-line max-len
-      to={getPolicyDetailsLink({ type: PolicyType.CONNECTION_METERING, oper: PolicyOperation.EDIT, policyId: id })}
-    >
-      <Button type='primary'>
-        { $t({ defaultMessage: 'Configure' }) }
-      </Button>
-    </TenantLink>
-  ]
-
-  return (
-    <PageHeader
-      title={title}
-      extra={extra}
-      breadcrumb={[
-        {
-          text: $t({ defaultMessage: 'Policies & Profiles' }),
-          link: getPolicyListRoutePath(true) }
-      ]}
-    />
-  )
-}
-
-interface Unit {
-  id: string
-  name: string
-}
-
-interface Persona {
-  id: string
-  name: string
-  groupId?: string
-}
-
-interface Venue {
-  id: string
-  name: string
-}
-
-interface Detail {
-  unit: Unit
-  personas: Persona[]
-  venue: Venue
-  segment?: number
-}
-
-interface ConnectionMeteringDetailTableProp {
-  data?: Detail[]
-}
-
-function ConnectionMeteringDetailTable (prop: ConnectionMeteringDetailTableProp) {
-  const { $t } = useIntl()
-  const colums: TableProps<Detail>['columns'] = [
-    {
-      title: $t({ defaultMessage: 'Unit' }),
-      dataIndex: 'unit',
-      key: 'unit',
-      render: (_, row)=><span>{row.unit.name}</span>
-    },
-    {
-      title: $t({ defaultMessage: 'Persona' }),
-      dataIndex: 'persona',
-      key: 'persona',
-      render: (_, row)=> {
-        return row.personas.map(persona=> <div><PersonaDetailsLink {...persona}/></div>)
-      }
-    },
-    {
-      title: $t({ defaultMessage: 'Venue' }),
-      dataIndex: 'venue',
-      key: 'venue',
-      render: (_, row)=><VenueLink venueId={row.venue.id} name={row.venue.name}/>
-    },
-    {
-      title: $t({ defaultMessage: 'Segment #' }),
-      dataIndex: 'segment',
-      key: 'segment'
-    }
-  ]
-
-  return(<Table
-    columns={colums}
-    dataSource={prop.data}
-    type={'compact'}
-  />)
-}
+import { ConnectionMeteringInstanceTable } from './ConnectionMeteringInstanceTable'
 
 
 export function ConnectionMeteringDetail () {
   const { $t } = useIntl()
+  const { policyId } = useParams()
+  const profileQuery = useGetConnectionMeteringByIdQuery({ params: { id: policyId } })
+
   const basicInfo = [
     {
-      title: $t({ defaultMessage: 'Tags' }),
-      value: ''
-    },
-    {
       title: $t({ defaultMessage: 'Rate Limiting' }),
-      value: <RateLimitingTableCell uploadRate={20} downloadRate={0} />
+      value: profileQuery.data && <RateLimitingTableCell
+        uploadRate={profileQuery.data.uploadRate}
+        downloadRate={profileQuery.data.downloadRate}
+      />
     },
     {
       title: $t({ defaultMessage: 'Data Consumption' }),
-      value: <DataConsumptionLabel onOffShow='OFF' dataCapacity={0} billingCycleRepeat={false}/>
+      value: profileQuery.data && <DataConsumptionLabel
+        onOffShow={$t({ defaultMessage: 'OFF' })}
+        {...profileQuery.data}
+      />
     }
   ]
 
-  const data: Detail[] = [{
-    unit: {
-      id: 'id',
-      name: 'name'
-    },
-    personas: [
-      {
-        id: 'test1',
-        name: 'name1'
-      },
-      {
-        id: 'test2',
-        name: 'name2'
-      }
-    ],
-    venue: {
-      id: 'venueId',
-      name: 'name'
-    }
-  }]
-
-  return <>
-    <ConnectionMeteringDetailsPageHeader
-      title={'test'}
-      id={'test'}
-    />
-    <GridRow>
-      <GridCol col={{ span: 24 }}>
-        <Loader>
-          <Card type={'solid-bg'}>
-            <Descriptions
-              layout={'vertical'}
-              column={6}
-              size={'small'}
-              colon={false}
-              style={{ padding: '8px 14px' }}
-            >
+  return (
+    <Loader states={[profileQuery]}>
+      <PageHeader
+        title={profileQuery.data?.name}
+        breadcrumb={[
+          {
+            text: $t({ defaultMessage: 'Policies & Profiles' }),
+            link: getPolicyListRoutePath(true)
+          },
+          {
+            text: $t({ defaultMessage: 'Connection Metering' }),
+            link: getPolicyRoutePath({
+              type: PolicyType.CONNECTION_METERING,
+              oper: PolicyOperation.LIST
+            })
+          }
+        ]}
+        extra={filterByAccess([
+          <TenantLink to={getPolicyDetailsLink({
+            policyId: policyId!,
+            type: PolicyType.CONNECTION_METERING,
+            oper: PolicyOperation.EDIT
+          })}>
+            <Button key='configure' type='primary'>{$t({ defaultMessage: 'Configure' })}</Button>
+          </TenantLink>
+        ])}
+      />
+      <GridRow>
+        <GridCol col={{ span: 24 }}>
+          <Card>
+            <GridRow>
               {
-                basicInfo.map(info =>
-                  <Descriptions.Item
-                    key={info.title}
-                    label={info.title}
-                  >
-                    {info.value}
-                  </Descriptions.Item>
-                )
+                basicInfo.map(({ title, value }) => {
+                  return (
+                    <GridCol col={{ span: 8 }} key={title}>
+                      <Card.Title>{title}</Card.Title>
+                      <Typography.Paragraph>{value}</Typography.Paragraph>
+                    </GridCol>
+                  )
+                })
               }
-            </Descriptions>
+            </GridRow>
           </Card>
-        </Loader>
-      </GridCol>
-      <GridCol col={{ span: 24 }}>
-        <div>
-          <Subtitle level={3}>
-            {/* eslint-disable-next-line max-len */}
-            {$t({ defaultMessage: 'Instances' })} ({data?.length ?? 0})
-          </Subtitle>
-          <ConnectionMeteringDetailTable data={data}/>
-        </div>
-      </GridCol>
-    </GridRow>
-  </>
+        </GridCol>
+        <GridCol col={{ span: 24 }}>
+          <Card>
+            <ConnectionMeteringInstanceTable data={profileQuery.data?.personas ?? []} />
+          </Card>
+        </GridCol>
+      </GridRow>
+    </Loader>
+  )
 }
 
