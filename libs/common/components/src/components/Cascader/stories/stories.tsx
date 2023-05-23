16c52cff3374b614bde807141cef0dab071d876b
jason.cheow <jason.cheow@ruckuswireless.com>
2023-05-23 16:47:37 +0800

    ACX-30729: Remove Single Select story for Cascader
    Should use antd's Select instead.

--- libs/common/components/src/components/Cascader/stories/stories.tsx
+++ libs/common/components/src/components/Cascader/stories/stories.tsx
@@ -3,7 +3,6 @@ import { storiesOf } from '@storybook/react'
 import { FlatListMulti }                                              from './FlatListMulti'
 import { LazyNested, LazyNestedWithBand, LazyNestedWithBandDisabled } from './LazyNested'
 import { NestedListSingle }                                           from './NestedListSingle'
-import { SingleSelect }                                               from './SingleSelect'
 
 storiesOf('Cascader', module)
   .add('With Nested List - single', NestedListSingle)
@@ -11,6 +10,5 @@ storiesOf('Cascader', module)
   .add('With Lazy Loading', LazyNested)
   .add('With Lazy Loading and Bands', LazyNestedWithBand)
   .add('With Lazy Loading having Bands disabled', LazyNestedWithBandDisabled)
-  .add('Single Select', SingleSelect)
 
 export {}
