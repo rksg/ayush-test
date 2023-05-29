d4d40320c0c9a724fd1d5efdeae618365b8e9592
Sander.Chen <Sander.Chen@ruckuswireless.com>
2023-05-29 20:02:27 +0800

    Revert "ACX-23204: Remove no-used-var"
    This reverts commit 65e0f5712a2a7a98bc1f9593413deee0a24ae182.

--- apps/rc/src/pages/Networks/wireless/NetworkForm/CaptivePortal/WISPrForm.tsx
+++ apps/rc/src/pages/Networks/wireless/NetworkForm/CaptivePortal/WISPrForm.tsx
@@ -42,7 +42,7 @@ export function WISPrForm () {
     cloneMode
   } = useContext(NetworkFormContext)
   const enableWISPREncryptMacIP = useIsSplitOn(Features.WISPR_ENCRYPT_MAC_IP)
-  const enableWISPRAlwaysAccept = true
+  const enableWISPRAlwaysAccept = useIsSplitOn(Features.WIFI_EDA_WISPR_ALWAYS_ACCEPT_TOGGLE)
   const { $t } = useIntl()
   const params = useParams()
   const { data: mspEcProfileData } = useGetMspEcProfileQuery({ params })
