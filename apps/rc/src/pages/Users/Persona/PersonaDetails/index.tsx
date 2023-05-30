4255ee45452638e6f01edf4e11f98837ad3c509c
TLIN4 <Tsaihuei.Lin@ruckuswireless.com>
2023-05-30 15:58:49 +0800

    ACX-23333: Implementation of the password hide/reveal control is wrong

--- apps/rc/src/pages/Users/Persona/PersonaDetails/index.tsx
+++ apps/rc/src/pages/Users/Persona/PersonaDetails/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 
-import { Col, Input, Row, Space, Tag, Typography } from 'antd'
-import { useIntl }                                 from 'react-intl'
-import { useParams }                               from 'react-router-dom'
+import { Col, Row, Space, Tag, Typography } from 'antd'
+import { useIntl }                          from 'react-intl'
+import {  useParams }                       from 'react-router-dom'
 
-import { Button, cssStr, Loader, PageHeader, showActionModal, Subtitle } from '@acx-ui/components'
-import { Features, useIsSplitOn, useIsTierAllowed }                      from '@acx-ui/feature-toggle'
-import { CopyOutlined }                                                  from '@acx-ui/icons'
+import { Button, cssStr, Loader, PageHeader, showActionModal, Subtitle, PasswordInput } from '@acx-ui/components'
+import { Features, useIsSplitOn, useIsTierAllowed }                                     from '@acx-ui/feature-toggle'
+import { CopyOutlined }                                                                 from '@acx-ui/icons'
 import {
   useLazyGetDpskQuery,
   useGetPersonaByIdQuery,
@@ -149,7 +149,7 @@ function PersonaDetails () {
     { label: $t({ defaultMessage: 'DPSK Passphrase' }),
       value:
         <>
-          <Input.Password
+          <PasswordInput
             readOnly
             bordered={false}
             value={personaDetailsQuery.data?.dpskPassphrase}
