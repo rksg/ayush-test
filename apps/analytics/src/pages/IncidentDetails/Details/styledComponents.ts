d934b944f2fe85d3f26c9716710986610e26a597
jason.cheow <jason.cheow@ruckuswireless.com>
2023-05-30 16:54:30 +0800

    ACX-30879: Standardize Dropdown overlays

--- apps/analytics/src/pages/IncidentDetails/Details/styledComponents.ts
+++ apps/analytics/src/pages/IncidentDetails/Details/styledComponents.ts
@@ -1,28 +1,14 @@
 import AutoSizer from 'react-virtualized-auto-sizer'
 import styled    from 'styled-components/macro'
 
-import { LayoutUI } from '@acx-ui/components'
+import { Dropdown } from '@acx-ui/components'
 
 export const FixedAutoSizer = styled(AutoSizer)`
   position: fixed;
 `
 
-export const MuteIncidentContainer = styled.div`
-  width: 200px;
-  .ant-switch {
-    width: 44px;
-    height: 22px;
-  }
-  .ant-switch-handle {
-    width: 16px;
-    height: 16px;
-  }
-  .ant-switch-handle::before {
-    border-radius: 8px;
-  }
-  .ant-switch-checked .ant-switch-handle {
-    left: calc(100% - 18px);
-  }
+export const MuteIncidentContainer = styled(Dropdown.OverlayContainer)`
+  width: 234px;
   p {
     margin: 10px 0 0 0 !important;
   }
