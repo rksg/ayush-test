b0dbfa545ab666709f99ee532c3e8c6f2cea32b7
mickael.dehez <mickael.dehez@ruckuswireless.com>
2023-05-31 13:56:38 +0800

    MLSA-6317: add redirection for local

--- apps/ra/src/AllRoutes.tsx
+++ apps/ra/src/AllRoutes.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-
-import { Route, rootRoutes } from '@acx-ui/react-router-dom'
+import { Route, rootRoutes, Navigate } from '@acx-ui/react-router-dom'
 
 import AnalyticsBase from './pages/Analytics'
 
 function AllRoutes () {
-  return rootRoutes(
+  return rootRoutes(<>
+    <Route path='/' element={<Navigate replace to='/analytics/next' />} />
     <Route path='analytics/next' element={<AnalyticsBase />}/>
-  )
+  </>)
 }
 
 export default AllRoutes
