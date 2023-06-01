5db7c5b9a9636ab432d7388476b429c50ef18e30
ny1012 <Nishanth.Yerramsetty@ruckuswireless.com>
2023-06-01 11:00:45 +0800

    ACX-6299 : HMR test changes

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,14 +1,44 @@
 const withModuleFederation = require('@nrwl/react/module-federation')
 const { merge } = require('webpack-merge')
-
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const modifyVars = require('./libs/common/components/src/theme/modify-vars')
+const webpack = require('webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 /**
  * @param {Parameters<typeof withModuleFederation>[0]} moduleFederationConfig
  */
  module.exports = async function webpackConfigWithModuleFederation (
   moduleFederationConfig,
-  additionalWebpackConfig = {}
+  additionalWebpackConfig = {   
+    cache: true,
+    experiments: {
+      topLevelAwait: true,
+      // Enable fast-refresh
+    },
+    watchOptions: {
+      // Ignore specific directories or files
+      ignored: /node_modules/,
+      // Check for changes every 300ms
+      aggregateTimeout: 150,
+      
+      // Enable polling for file changes
+      poll: 500,
+    },
+    plugins: [
+      new ReactRefreshWebpackPlugin(),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/node_modules\$/,
+        contextRegExp: /react-loadable/,
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        typescript: {
+          configFile: './tsconfig.json',
+        },
+      })
+    ],
+    devtool: 'eval',
+  }
 ) {
   const doModuleFederation = await withModuleFederation({
     ...moduleFederationConfig,
@@ -33,6 +63,7 @@ const modifyVars = require('./libs/common/components/src/theme/modify-vars')
 
     config.module.rules.push({
       test: /\.less$/,
+      use: 'thread-loader',
       use: ['style-loader', 'css-loader', {
         loader: 'less-loader',
         options: {
