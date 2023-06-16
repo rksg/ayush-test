9f0b4f79c4059e5e764c188a7c469e2443fc86cc
mickael.dehez <mickael.dehez@ruckuswireless.com>
2023-06-16 09:11:43 +0800

    MLSA-6390: fix lint in ra prod config

--- apps/ra/webpack.config.prod.js
+++ apps/ra/webpack.config.prod.js
@@ -4,7 +4,7 @@ const moduleFederationConfig = require('./module-federation.config')
 const setupProxy = require('./src/setupProxy')
 
 module.exports = webpackConfigWithModuleFederation({
-  ...moduleFederationConfig,
+  ...moduleFederationConfig
   /*
    * Remote overrides for production.
    * Each entry is a pair of an unique name and the URL where it is deployed.
