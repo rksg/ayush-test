426b2e0267814d76e4f5b50413a7edd879e08389
mickael.dehez <mickael.dehez@ruckuswireless.com>
2023-06-26 11:17:23 +0800

    MLSA-6314: fix: switch filter requires switchGroup in path + refactor

--- apps/rc/src/pages/Devices/Switch/SwitchDetails/SwitchIncidentsTab/index.tsx
+++ apps/rc/src/pages/Devices/Switch/SwitchDetails/SwitchIncidentsTab/index.tsx
@@ -1,20 +1,14 @@
-import { IncidentTabContent }            from '@acx-ui/analytics/components'
-import { AnalyticsFilter, pathToFilter } from '@acx-ui/analytics/utils'
-import { Loader }                        from '@acx-ui/components'
-import { useSwitchDetailHeaderQuery }    from '@acx-ui/rc/services'
-import { useParams  }                    from '@acx-ui/react-router-dom'
-import { useDateFilter }                 from '@acx-ui/utils'
+import { IncidentTabContent }         from '@acx-ui/analytics/components'
+import { Loader }                     from '@acx-ui/components'
+import { useSwitchDetailHeaderQuery } from '@acx-ui/rc/services'
+import { useParams  }                 from '@acx-ui/react-router-dom'
+
+import { useSwitchFilter } from '../switchFilter'
 
 export function SwitchIncidentsTab () {
-  const { dateFilter } = useDateFilter()
   const params = useParams()
   const switchDetailQuery = useSwitchDetailHeaderQuery({ params })
-  const filters = {
-    ...dateFilter,
-    filter: pathToFilter([
-      { type: 'switch', name: switchDetailQuery.data?.switchMac?.toUpperCase() as string }
-    ])
-  } as AnalyticsFilter
+  const filters = useSwitchFilter(switchDetailQuery.data)
   return <Loader states={[switchDetailQuery]}>
     <IncidentTabContent filters={filters} disableGraphs/>
   </Loader>
