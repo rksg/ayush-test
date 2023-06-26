9a78a7946936fb4f3e99fa3cac4e2be835621bdf
jason.cheow <jason.cheow@ruckuswireless.com>
2023-06-26 17:43:58 +0800

    ACX-28971: Align imports

--- apps/analytics/src/pages/AIAnalytics/index.tsx
+++ apps/analytics/src/pages/AIAnalytics/index.tsx
@@ -1,10 +1,10 @@
-import { Menu }    from  'antd'
+import { Menu }    from 'antd'
 import { useIntl } from 'react-intl'
 
 import { IncidentTabContent, useHeaderExtra } from '@acx-ui/analytics/components'
 import { PageHeader, Tabs, Button, Dropdown } from '@acx-ui/components'
-import { ClockOutlined }              from '@acx-ui/icons'
 import { useIsSplitOn, Features }             from '@acx-ui/feature-toggle'
+import { ClockOutlined }                      from '@acx-ui/icons'
 import { useNavigate, useTenantLink }         from '@acx-ui/react-router-dom'
 import { filterByAccess }                     from '@acx-ui/user'
 
