9826dd3547c30b3413802435480228c148d00216
jason.cheow <jason.cheow@ruckuswireless.com>
2023-06-26 19:35:25 +0800

    ACX-28971: Revert wrongly committed code

--- apps/analytics/src/pages/AIAnalytics/index.tsx
+++ apps/analytics/src/pages/AIAnalytics/index.tsx
@@ -2,9 +2,8 @@ import { Menu }    from 'antd'
 import { useIntl } from 'react-intl'
 
 import { IncidentTabContent, useHeaderExtra } from '@acx-ui/analytics/components'
-import { PageHeader, Tabs, Button, Dropdown } from '@acx-ui/components'
+import { PageHeader, Tabs }                   from '@acx-ui/components'
 import { useIsSplitOn, Features }             from '@acx-ui/feature-toggle'
-import { ClockOutlined }                      from '@acx-ui/icons'
 import { useNavigate, useTenantLink }         from '@acx-ui/react-router-dom'
 import { filterByAccess }                     from '@acx-ui/user'
 
@@ -30,31 +29,11 @@ const useTabs = () : Tab[] => {
     component: <IncidentTabContent/>,
     headerExtra: useHeaderExtra({ shouldQuerySwitch: true, withIncidents: true })
   }
-  const menuMap = {
-    last7Days: 'Last 24 Hours',
-    last30Days: 'Last 30 Days'
-  }
-  const menu = <Menu
-    selectable
-    defaultSelectedKeys={['last7Days']}
-    items={[
-      { key: 'last7Days', label: 'Last 24 Hours' },
-      { key: 'last30Days', label: 'Last 30 Days' }
-    ]}
-  />
   const configChangeTab = {
     key: AIAnalyticsTabEnum.CONFIG_CHANGE,
     title: $t({ defaultMessage: 'Config Change' }),
     component: <ConfigChange/>,
-    headerExtra: [
-      useHeaderExtra({ shouldQuerySwitch: true, withIncidents: false })[0],
-      <Dropdown overlay={menu}>{(selectedKeys) => {
-        const key = selectedKeys || ''
-        return <Button icon={<ClockOutlined />}>
-          {menuMap[key as keyof typeof menuMap]}
-        </Button>
-      }}</Dropdown>
-    ]
+    headerExtra: useHeaderExtra({ shouldQuerySwitch: true, withIncidents: false })
   }
   return [
     incidentsTab,
