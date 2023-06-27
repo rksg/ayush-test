dfd12c8853610e288ba9cfe6fd738a12eac0488e
mickael.dehez <mickael.dehez@ruckuswireless.com>
2023-06-27 09:24:27 +0800

    MLSA-6314: refactor and fix ap path in overview

--- apps/rc/src/pages/Devices/Wifi/ApDetails/ApAnalyticsTab/index.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApDetails/ApAnalyticsTab/index.tsx
@@ -1,21 +1,14 @@
-import { AnalyticsTabs }                                     from '@acx-ui/analytics/components'
-import { AnalyticsFilter, useAnalyticsFilter, pathToFilter } from '@acx-ui/analytics/utils'
-import { useApContext }                                      from '@acx-ui/rc/utils'
-import { NodeType }                                          from '@acx-ui/utils'
+import { AnalyticsTabs } from '@acx-ui/analytics/components'
+import { useApContext }  from '@acx-ui/rc/utils'
+
+import { useApFilter } from '../apFilter'
 
 export function ApAnalyticsTab () {
-  const { serialNumber, apMac, venueId } = useApContext()
-  const { filters } = useAnalyticsFilter()
-  const filter = {
-    ...filters,
-    filter: pathToFilter([
-      { type: 'zone' as NodeType, name: venueId as string },
-      { type: 'AP' as NodeType, name: apMac as string }
-    ])
-  } as AnalyticsFilter
+  const apContext = useApContext()
+  const filter = useApFilter(apContext)
   return <AnalyticsTabs
     incidentFilter={filter}
     healthFilter={filter}
-    healthPath={`devices/wifi/${serialNumber}/details/analytics/health`}
+    healthPath={`devices/wifi/${apContext.serialNumber}/details/analytics/health`}
   />
 }
