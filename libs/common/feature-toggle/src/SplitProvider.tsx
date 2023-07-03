a5279e33351c1389ba889ee66f17efbea5eefecd
jason.cheow <jason.cheow@ruckuswireless.com>
2023-07-03 15:14:05 +0800

    MLSA-6372: Revert "Pull request #1521: Feature/MLSA-6372- Feature flag for MLSA"
    This reverts commit f55eb8f6f82c1051840f29139a5d7fecb4bf2c55, reversing
    changes made to 17426fc02472c2ffb6a97b48f57519f40c3d84b6.

--- libs/common/feature-toggle/src/SplitProvider.tsx
+++ libs/common/feature-toggle/src/SplitProvider.tsx
@@ -3,43 +3,32 @@ import React from 'react'
 import { SplitFactory, SplitSdk } from '@splitsoftware/splitio-react'
 import SplitIO                    from '@splitsoftware/splitio-react/types/splitio/splitio'
 
-import {
-  useUserProfileContext
-} from '@acx-ui/analytics/utils'
 import { get }       from '@acx-ui/config'
 import { useParams } from '@acx-ui/react-router-dom'
 
 let factory: SplitIO.IBrowserSDK
 const splitKey = get('SPLIT_IO_KEY')
-const isMLISA =get('IS_MLISA_SA')
 const suffix = splitKey.substring(0, 5)
-
-
 
 function SplitProvider (props: Readonly<{ children: React.ReactElement }>) {
   const { tenantId } = useParams() as { tenantId: string }
-  const { data: userProfile } = useUserProfileContext()
-  const prefixKey = isMLISA ? 'MLISA' : 'ACX'
-  const tenantKey = isMLISA ? userProfile?.accountId as string : tenantId
-  if (!factory && userProfile?.accountId) {
+  if (!factory) {
     factory = SplitSdk({
       scheduler: {
         featuresRefreshRate: 30 // 30 sec
       },
       core: {
         authorizationKey: splitKey,
-        key: tenantKey
+        key: tenantId
       },
       storage: {
         type: 'LOCALSTORAGE',
-        prefix: prefixKey + suffix
+        prefix: 'ACX' + suffix
       },
       debug: false // set this value to true for running in debug mode for debugging in local development only
     })
   }
-  return tenantKey ? (
-    <SplitFactory factory={factory} children={props.children} />
-  ) : null
+  return <SplitFactory factory={factory} children={props.children}/>
 }
 
 export { SplitProvider }
