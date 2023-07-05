4504dd60800daad5e093c11b4c4e29a33c0ef7ba
Jerry Chen <Jerry.Chen@ruckuswireless.com>
2023-07-05 10:18:48 +0800

    ACX-33580: Fix lint

--- libs/rc/components/src/ResidentPortal/index.ts
+++ libs/rc/components/src/ResidentPortal/index.ts
@@ -1,3 +1,4 @@
 export * from './ResidentPortalForm/ResidentPortalForm'
 export * from './portalImageService'
+export * from './ResidentPortalForm/formParsing'
 export * from './residentPortalUtilities'
