34686de3685899b1db8f45557d466c0b1ea6b418
sp1148 <Sarada.Pachalla@ruckuswireless.com>
2023-07-07 17:17:49 -0700

    ACX-28982: To fix lint commented out incomplete test suite

--- apps/main/src/browser-dialog.spec.tsx
+++ apps/main/src/browser-dialog.spec.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react'
-
-import { browserDialog } from './browser-dialog'
-
-describe('BrowserDialog', () => {
-  // it('should render successfully', () => {
-  //   const { baseElement } = render(<BrowserDialog />)
-  //   expect(baseElement).toBeTruthy()
-  // })
-})
+// import { render } from '@testing-library/react'
+//
+// import { browserDialog } from './browser-dialog'
+//
+// describe('BrowserDialog', () => {
+//   it('should render successfully', () => {
+//     const { baseElement } = render(<BrowserDialog />)
+//     expect(baseElement).toBeTruthy()
+//   })
+// })
