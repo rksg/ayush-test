302e4de6ff8b46f214c0d69493f2881eb3da74c9
sb1212 <Suraj.Bande@commscope.com>
2023-07-16 15:28:48 +0530

    ACX-32199: list issue resolved

--- libs/common/components/src/components/IFrame/index.spec.tsx
+++ libs/common/components/src/components/IFrame/index.spec.tsx
@@ -1,9 +1,10 @@
-import { IFrame } from '@acx-ui/components'
 import { render, screen } from '@acx-ui/test-utils'
+
+import { IFrame } from '.'
 
 describe('IFrame', () => {
   it('should render iframe', async () => {
     await render(<IFrame title='test iframe' src='/api/a4rc/explorer/'/>)
-    expect(screen.getByTitle('test iframe')).toBeInTheDocument();
+    expect(screen.getByTitle('test iframe')).toBeInTheDocument()
   })
 })
