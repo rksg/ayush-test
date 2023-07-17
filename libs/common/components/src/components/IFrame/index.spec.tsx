773eab21a6c2ee8b125fa5bdb93ac639d0b1dad3
sb1212 <Suraj.Bande@commscope.com>
2023-07-17 10:42:03 +0530

    ACX-32199: test coverage

--- libs/common/components/src/components/IFrame/index.spec.tsx
+++ libs/common/components/src/components/IFrame/index.spec.tsx
@@ -4,7 +4,13 @@ import { IFrame } from '.'
 
 describe('IFrame', () => {
   it('should render iframe', async () => {
-    await render(<IFrame title='test iframe' src='/api/a4rc/explorer/'/>)
-    expect(screen.getByTitle('test iframe')).toBeInTheDocument()
+    await render(<IFrame title='test iframe' src='http://localhost/api/a4rc/explorer/'/>)
+    const iframe = screen.getByTitle('test iframe') as HTMLIFrameElement
+    expect(iframe).toBeInTheDocument()
+
+    await iframe.setAttribute('src', 'https://example.com')
+
+    await iframe.setAttribute('src', 'http://localhost/api/a4rc/explorer/')
+
   })
 })
