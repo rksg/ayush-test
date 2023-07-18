68b5eb99d835b7eefd65cbde8e2ee28cd8f87087
TCHEN7 <tingan.chen@commscope.com>
2023-07-18 13:38:54 +0800

    MLSA-6411: feat: adjust config change for RA

--- libs/analytics/components/src/ConfigChange/index.spec.tsx
+++ libs/analytics/components/src/ConfigChange/index.spec.tsx
@@ -1,5 +1,8 @@
+import { ReactNode } from 'react'
+
 import userEvent from '@testing-library/user-event'
 
+import { get }            from '@acx-ui/config'
 import { Provider }       from '@acx-ui/store'
 import { render, screen } from '@acx-ui/test-utils'
 
@@ -13,6 +16,18 @@ jest.mock('./NetworkFilter', () => ({
   NetworkFilter: () => <div data-testid='NetworkFilter' />
 }))
 
+jest.mock('@acx-ui/components', () => ({
+  ...jest.requireActual('@acx-ui/components'),
+  GridCol: (props: { children: ReactNode, style: Record<string, string> }) =>
+    <div {...props} data-testid='GridCol' />
+}))
+
+const mockGet = get as jest.Mock
+jest.mock('@acx-ui/config', () => ({
+  get: jest.fn()
+}))
+beforeEach(() => mockGet.mockReturnValue(''))
+
 describe('ConfigChange', () => {
   it('should render component correctly', async () => {
     const Component = () => {
@@ -23,6 +38,7 @@ describe('ConfigChange', () => {
     expect(await screen.findByTestId('Chart')).toBeVisible()
     expect(await screen.findByTestId('KPIs')).toBeVisible()
     expect(await screen.findByTestId('Table')).toBeVisible()
+    expect((await screen.findAllByTestId('GridCol'))[0]).toHaveStyle('minHeight: 170px')
   })
   it('should render header correctly', async () => {
     const Component = () => {
@@ -44,5 +60,17 @@ describe('ConfigChange', () => {
     await userEvent.click(trigger)
     await userEvent.click(screen.getByRole('menuitem', { name: 'Last 30 Days' }))
     expect(trigger).toHaveTextContent('Last 30 Days')
+  })
+  it('should render component correctly when IS_MLISA_SA', async () => {
+    mockGet.mockReturnValue('true')
+    const Component = () => {
+      const { component } = useConfigChange()
+      return component
+    }
+    render(<Component/>, { wrapper: Provider, route: {} })
+    expect(await screen.findByTestId('Chart')).toBeVisible()
+    expect(await screen.findByTestId('KPIs')).toBeVisible()
+    expect(await screen.findByTestId('Table')).toBeVisible()
+    expect((await screen.findAllByTestId('GridCol'))[0]).toHaveStyle('minHeight: 200px')
   })
 })
