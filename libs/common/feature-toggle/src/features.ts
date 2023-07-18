229a21910a2240e4954b2bbda4c4151eb5d7f300
lc1158 <Lish.Chen@commscope.com>
2023-07-18 10:35:41 +0800

    ACX-32430: Add multicast filter

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -59,7 +59,8 @@ export enum Features {
   WIFI_FR_6029_FG4_TOGGLE = 'wifi-fr-6029-fg4-toggle',
   WIFI_FR_6029_FG5_TOGGLE = 'wifi-fr-6029-fg5-toggle',
   RADIUS_CLIENT_CONFIG = 'radius-client-config-api-enabled',
-  SUPPORT_SWITCH_TWO_VERSION_UPGRADE = 'switch-consumer-support-two-fw-upgrade-toggle'
+  SUPPORT_SWITCH_TWO_VERSION_UPGRADE = 'switch-consumer-support-two-fw-upgrade-toggle',
+  WIFI_EDA_MULTICAST_FILTER_TOGGLE = 'wifi-eda-multicast-filter-toggle'
 }
 
 export enum TierFeatures { // for Tier (ex: Beta) feature flag
