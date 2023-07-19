c4d0c071de7ca341ed778104514db65ba1b69146
mdehez <mickael.dehez@commscope.com>
2023-07-19 15:27:53 +0800

    MLSA-6373: restore indent

--- libs/common/components/src/components/DatePicker/styledComponents.ts
+++ libs/common/components/src/components/DatePicker/styledComponents.ts
@@ -12,120 +12,120 @@ type RangePickerWrapperProps = {
 
 /* eslint-disable max-len */
 const datePickerStyle = `
-.ant-picker-input {
-  .ant-picker-clear {
-    span[role=img] {
-      svg { display: none; }
+  .ant-picker-input {
+    .ant-picker-clear {
+      span[role=img] {
+        svg { display: none; }
 
-      &::after {
-        content: '';
-        display: inline-block;
-        width: 14px;
-        height: 14px;
-        background-size: 12px 12px;
-        // encodeURIComponent(renderToStaticMarkup(<Close />))
-        background-image: url("data:image/svg+xml,%3Csvg%20width%3D%2218%22%20height%3D%2218%22%20viewBox%3D%220%200%2018%2018%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20class%3D%22NestedListSingle__CloseIcon-sc-1yefg1u-0%20lilJnS%22%3E%3Cpath%20d%3D%22M14.0625%203.9375L3.9375%2014.0625%22%20stroke%3D%22%23333333%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3C%2Fpath%3E%3Cpath%20d%3D%22M14.0625%2014.0625L3.9375%203.9375%22%20stroke%3D%22%23333333%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E");
-        background-repeat: no-repeat;
-        background-position: 50% 50%;
-        background-color: white;
-        margin-right: -4px;
+        &::after {
+          content: '';
+          display: inline-block;
+          width: 14px;
+          height: 14px;
+          background-size: 12px 12px;
+          // encodeURIComponent(renderToStaticMarkup(<Close />))
+          background-image: url("data:image/svg+xml,%3Csvg%20width%3D%2218%22%20height%3D%2218%22%20viewBox%3D%220%200%2018%2018%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20class%3D%22NestedListSingle__CloseIcon-sc-1yefg1u-0%20lilJnS%22%3E%3Cpath%20d%3D%22M14.0625%203.9375L3.9375%2014.0625%22%20stroke%3D%22%23333333%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3C%2Fpath%3E%3Cpath%20d%3D%22M14.0625%2014.0625L3.9375%203.9375%22%20stroke%3D%22%23333333%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E");
+          background-repeat: no-repeat;
+          background-position: 50% 50%;
+          background-color: white;
+          margin-right: -4px;
+        }
       }
     }
   }
-}
 
-.ant-picker-dropdown {
-  font-size: var(--acx-body-4-font-size);
-  padding: 10px 0;
-  & .ant-picker-panel {
-    border: none;
-  }
+  .ant-picker-dropdown {
+    font-size: var(--acx-body-4-font-size);
+    padding: 10px 0;
+    & .ant-picker-panel {
+      border: none;
+    }
 
-  .ant-picker-panel-container {
-    border-radius: 4px;
-    background: var(--acx-primary-white);
-    box-shadow: 0px 6px 16px rgba(51, 51, 51, 0.2);
-    padding-left: var(--acx-date-picker-ranges-width);
-  }
+    .ant-picker-panel-container {
+      border-radius: 4px;
+      background: var(--acx-primary-white);
+      box-shadow: 0px 6px 16px rgba(51, 51, 51, 0.2);
+      padding-left: var(--acx-date-picker-ranges-width);
+    }
 
-  .ant-picker-panel {
-    .ant-picker-header {
-      align-items: baseline;
-      border-bottom: none;
-      .ant-picker-header-view {
-        font-family: var(--acx-accent-brand-font);
-        font-size: var(--acx-headline-5-font-size);
-        font-weight: var(--acx-headline-5-font-weight-semi-bold);
-        color: var(--acx-neutrals-70);
-        button:hover {
-          color: inherit;
-          cursor: default;
+    .ant-picker-panel {
+      .ant-picker-header {
+        align-items: baseline;
+        border-bottom: none;
+        .ant-picker-header-view {
+          font-family: var(--acx-accent-brand-font);
+          font-size: var(--acx-headline-5-font-size);
+          font-weight: var(--acx-headline-5-font-weight-semi-bold);
+          color: var(--acx-neutrals-70);
+          button:hover {
+            color: inherit;
+            cursor: default;
+          }
+        }
+      }
+
+      .ant-picker-body {
+        th {
+          font-size: var(--acx-subtitle-6-font-size);
+          font-weight: var(--acx-subtitle-6-font-weight-bold);
+          color: var(--acx-neutrals-60);
+        }
+        td {
+          font-size: var(--acx-subtitle-5-font-size);
+          font-weight: var(--acx-subtitle-5-font-weight-semi-bold);
         }
       }
     }
 
-    .ant-picker-body {
-      th {
-        font-size: var(--acx-subtitle-6-font-size);
-        font-weight: var(--acx-subtitle-6-font-weight-bold);
-        color: var(--acx-neutrals-60);
-      }
-      td {
-        font-size: var(--acx-subtitle-5-font-size);
-        font-weight: var(--acx-subtitle-5-font-weight-semi-bold);
-      }
+    .ant-picker-cell-in-view.ant-picker-cell-selected .ant-picker-cell-inner,
+    .ant-picker-cell-in-view.ant-picker-cell-range-start .ant-picker-cell-inner,
+    .ant-picker-cell-in-view.ant-picker-cell-range-end .ant-picker-cell-inner {
+      color: var(--acx-primary-white);
+      background: var(--acx-accents-blue-50);
+      border-radius: 20px;
+    }
+    .ant-picker-cell-in-view. ant-picker-cell-range-hover .ant-picker-cell-inner::before,
+    .ant-picker-cell-in-view. ant-picker-cell-range-hover-start .ant-picker-cell-inner::before,
+    .ant-picker-cell-in-view. ant-picker-cell-range-hover-end .ant-picker-cell-inner::before {
+      background: var(--acx-accents-blue-10);
+    }
+    .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover::before,
+    .ant-picker-cell-in-view.ant-picker-cell-range-start.ant-picker-cell-range-hover::before,
+    .ant-picker-cell-in-view.ant-picker-cell-range-end.ant-picker-cell-range-hover::before,
+    .ant-picker-cell-in-view.ant-picker-cell-range-start:not(.ant-picker-cell-range-start-single).ant-picker-cell-range-hover-start::before,
+    .ant-picker-cell-in-view.ant-picker-cell-range-end:not(.ant-picker-cell-range-end-single).ant-picker-cell-range-hover-end::before,
+    .ant-picker-panel > :not(.ant-picker-date-panel) .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-start::before,
+    .ant-picker-panel > :not(.ant-picker-date-panel) .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-end::before,
+    .ant-picker-cell-in-view.ant-picker-cell-in-range::before,
+    .ant-picker-cell-in-view.ant-picker-cell-range-start:not(.ant-picker-cell-range-start-single)::before,
+    .ant-picker-cell-in-view.ant-picker-cell-range-end:not(.ant-picker-cell-range-end-single)::before {
+      background: var(--acx-accents-blue-10);
+    }
+    .ant-picker-date-panel .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-start .ant-picker-cell-inner::after,
+    .ant-picker-date-panel .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-end .ant-picker-cell-inner::after {
+      background: var(--acx-accents-blue-10);
+    }
+    .ant-picker-cell-in-view.ant-picker-cell-today .ant-picker-cell-inner::before {
+      border-color: var(--acx-accents-blue-50);
+    }
+    .ant-picker-cell-in-view.ant-picker-cell-range-hover:not(.ant-picker-cell-in-range)::after,
+    .ant-picker-cell-in-view.ant-picker-cell-range-hover-start:not(.ant-picker-cell-in-range)::after,
+    .ant-picker-cell-in-view.ant-picker-cell-range-hover-end:not(.ant-picker-cell-in-range)::after {
+      border-color: var(--acx-accents-blue-50);
+    }
+    .ant-picker-cell-in-view.ant-picker-cell-range-start:not(.ant-picker-cell-range-start-single):not(.ant-picker-cell-range-end) .ant-picker-cell-inner,
+    .ant-picker-cell-in-view.ant-picker-cell-range-end:not(.ant-picker-cell-range-end-single):not(.ant-picker-cell-range-start) .ant-picker-cell-inner {
+      border-radius: 20px;
+    }
+    .ant-picker-cell:hover .ant-picker-cell-inner,
+    .ant-picker-cell-in-view.ant-picker-cell-today .ant-picker-cell-inner::before {
+      border-radius: 20px;
+    }
+
+    .ant-picker-footer {
+      display: none;
     }
   }
-
-  .ant-picker-cell-in-view.ant-picker-cell-selected .ant-picker-cell-inner,
-  .ant-picker-cell-in-view.ant-picker-cell-range-start .ant-picker-cell-inner,
-  .ant-picker-cell-in-view.ant-picker-cell-range-end .ant-picker-cell-inner {
-    color: var(--acx-primary-white);
-    background: var(--acx-accents-blue-50);
-    border-radius: 20px;
-  }
-  .ant-picker-cell-in-view. ant-picker-cell-range-hover .ant-picker-cell-inner::before,
-  .ant-picker-cell-in-view. ant-picker-cell-range-hover-start .ant-picker-cell-inner::before,
-  .ant-picker-cell-in-view. ant-picker-cell-range-hover-end .ant-picker-cell-inner::before {
-    background: var(--acx-accents-blue-10);
-  }
-  .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover::before,
-  .ant-picker-cell-in-view.ant-picker-cell-range-start.ant-picker-cell-range-hover::before,
-  .ant-picker-cell-in-view.ant-picker-cell-range-end.ant-picker-cell-range-hover::before,
-  .ant-picker-cell-in-view.ant-picker-cell-range-start:not(.ant-picker-cell-range-start-single).ant-picker-cell-range-hover-start::before,
-  .ant-picker-cell-in-view.ant-picker-cell-range-end:not(.ant-picker-cell-range-end-single).ant-picker-cell-range-hover-end::before,
-  .ant-picker-panel > :not(.ant-picker-date-panel) .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-start::before,
-  .ant-picker-panel > :not(.ant-picker-date-panel) .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-end::before,
-  .ant-picker-cell-in-view.ant-picker-cell-in-range::before,
-  .ant-picker-cell-in-view.ant-picker-cell-range-start:not(.ant-picker-cell-range-start-single)::before,
-  .ant-picker-cell-in-view.ant-picker-cell-range-end:not(.ant-picker-cell-range-end-single)::before {
-    background: var(--acx-accents-blue-10);
-  }
-  .ant-picker-date-panel .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-start .ant-picker-cell-inner::after,
-  .ant-picker-date-panel .ant-picker-cell-in-view.ant-picker-cell-in-range.ant-picker-cell-range-hover-end .ant-picker-cell-inner::after {
-    background: var(--acx-accents-blue-10);
-  }
-  .ant-picker-cell-in-view.ant-picker-cell-today .ant-picker-cell-inner::before {
-    border-color: var(--acx-accents-blue-50);
-  }
-  .ant-picker-cell-in-view.ant-picker-cell-range-hover:not(.ant-picker-cell-in-range)::after,
-  .ant-picker-cell-in-view.ant-picker-cell-range-hover-start:not(.ant-picker-cell-in-range)::after,
-  .ant-picker-cell-in-view.ant-picker-cell-range-hover-end:not(.ant-picker-cell-in-range)::after {
-    border-color: var(--acx-accents-blue-50);
-  }
-  .ant-picker-cell-in-view.ant-picker-cell-range-start:not(.ant-picker-cell-range-start-single):not(.ant-picker-cell-range-end) .ant-picker-cell-inner,
-  .ant-picker-cell-in-view.ant-picker-cell-range-end:not(.ant-picker-cell-range-end-single):not(.ant-picker-cell-range-start) .ant-picker-cell-inner {
-    border-radius: 20px;
-  }
-  .ant-picker-cell:hover .ant-picker-cell-inner,
-  .ant-picker-cell-in-view.ant-picker-cell-today .ant-picker-cell-inner::before {
-    border-radius: 20px;
-  }
-
-  .ant-picker-footer {
-    display: none;
-  }
-}
 `
 /* eslint-enable */
 
