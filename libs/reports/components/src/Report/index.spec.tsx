acdd88ede1220697efdf127079597f947e09664f
jitendra.dangi <jitendra.dangi@commscope.com>
2023-07-25 12:02:59 +0530

    RSA-6390: chore: added test

---
+++ libs/reports/components/src/Report/index.spec.tsx
@@ -0,0 +1,36 @@
+import { ReportType }     from '@acx-ui/reports/components'
+import { Provider }       from '@acx-ui/store'
+import { render, screen } from '@acx-ui/test-utils'
+
+
+import { Report } from './index'
+
+jest.mock('./ReportHeader', () => ({
+  ReportHeader: () => <div data-testid='reportHeader' />
+}))
+jest.mock('@acx-ui/reports/components', () => ({
+  ...jest.requireActual('@acx-ui/reports/components'),
+  EmbeddedReport: () => <div data-testid='reportPage' />
+}))
+
+describe('Report', () => {
+  const params = { tenantId: 'tenant-id' }
+
+  it('should render the report with Header', async () => {
+    render(
+      <Provider>
+        <Report type={ReportType.ACCESS_POINT} withHeader/>
+      </Provider>, { route: { params } })
+    expect(await screen.findByTestId('reportHeader')).toBeTruthy()
+    expect(await screen.findByTestId('reportPage')).toBeTruthy()
+  })
+
+  it('should render the report without Header', async () => {
+    render(
+      <Provider>
+        <Report type={ReportType.OVERVIEW} withHeader={false}/>
+      </Provider>, { route: { params } })
+    expect(await screen.findByTestId('reportPage')).toBeTruthy()
+    expect(screen.queryByTestId('reportHeader')).toBeFalsy()
+  })
+})
