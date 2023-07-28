cc5f156696559fbb1b80bbf65268252b5183ae00
stan2 <shiawuen.tan@commscope.com>
2023-07-28 17:47:51 +0800

    MLSA-6505: add IncidentsCountBySeverities

---
+++ libs/analytics/components/src/IncidentsCountBySeverities/index.tsx
@@ -0,0 +1,60 @@
+import { useIntl } from 'react-intl'
+import AutoSizer   from 'react-virtualized-auto-sizer'
+
+import { IncidentFilter }             from '@acx-ui/analytics/utils'
+import { incidentSeverities }         from '@acx-ui/analytics/utils'
+import { Card, Loader, NoActiveData } from '@acx-ui/components'
+import { intlFormats }                from '@acx-ui/formatter'
+import { useNavigateToPath }          from '@acx-ui/react-router-dom'
+
+import { useIncidentsCountBySeveritiesQuery } from './services'
+import * as UI                                from './styledComponents'
+
+const { countFormat } = intlFormats
+
+type IncidentsCountBySeveritiesProps = {
+  filters: IncidentFilter
+}
+
+export function IncidentsCountBySeverities ({ filters }: IncidentsCountBySeveritiesProps) {
+  const { $t } = useIntl()
+  const onArrowClick = useNavigateToPath('/analytics/incidents/')
+
+  const response = useIncidentsCountBySeveritiesQuery(filters)
+  const noData = response.data?.total === 0
+
+  const title = {
+    title: $t({ defaultMessage: 'Incidents' }),
+    icon: <UI.TitleBadge
+      children={$t(countFormat, { value: response.data?.total ?? 0 })}
+    />
+  }
+
+  const items = response.data?.items.map(({ severity, incidentsCount, impactedClients }) => {
+    const style = { borderLeftColor: `var(${incidentSeverities[severity]?.color})` }
+    return <UI.Container key={severity} style={style}>
+      <UI.Count>{$t(countFormat, { value: incidentsCount })}</UI.Count>
+      {$t({ defaultMessage: 'Incident {severityKey}' }, { severityKey: severity })}
+      <UI.ImpactedClients>
+        {$t({ defaultMessage: '{impactedClients} clients impacted' },{
+          impactedClients: $t(countFormat, { value: impactedClients })
+        })}
+      </UI.ImpactedClients>
+    </UI.Container>
+  })
+
+  return <Loader states={[response]}>
+    <Card title={title} onArrowClick={onArrowClick}>
+      <AutoSizer>
+        {({ width, height }) => (
+          noData
+            ? <NoActiveData text={$t({ defaultMessage: 'No reported incidents' })} />
+            : <UI.Wrapper
+              style={{ width, height: height - 20 * 2, marginBlock: 20 }}
+              children={items}
+            />
+        )}
+      </AutoSizer>
+    </Card>
+  </Loader>
+}
