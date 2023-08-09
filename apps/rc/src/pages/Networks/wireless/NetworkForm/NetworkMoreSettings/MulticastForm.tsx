65dfb31c9eb95ddede1c8ed3a58edf0c40331e90
Ryan Wu <ryan.wu@commscope.com>
2023-08-09 11:57:30 +0800

    ACX-37824 Make enableMulticastRateLimiting get value in upper component useEffect

--- apps/rc/src/pages/Networks/wireless/NetworkForm/NetworkMoreSettings/MulticastForm.tsx
+++ apps/rc/src/pages/Networks/wireless/NetworkForm/NetworkMoreSettings/MulticastForm.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
 /* eslint-disable max-len */
 
-import { ReactNode, CSSProperties, useEffect } from 'react'
+import { ReactNode, CSSProperties } from 'react'
 
 import {
   Checkbox,
@@ -39,23 +39,13 @@ export function MulticastForm () {
     useWatch<boolean>(enableMulticastRateLimitingFieldName),
     useWatch<boolean>(enableMulticastUpLimitFieldName),
     useWatch<boolean>(enableMulticastDownLimitFieldName),
+    useWatch<boolean>(enableMulticastUpLimit6GFieldName),
+    useWatch<boolean>(enableMulticastDownLimit6GFieldName),
     useWatch<boolean>(enableMulticastFilterFieldName)
   ]
 
   const form = Form.useFormInstance()
   const getDownloadMaxValue = () => getDLMax(form.getFieldValue('bssMinimumPhyRate'))
-
-
-  useEffect(() => {
-    let multicastRateLimitingSwitch = false
-    if(form.getFieldValue(enableMulticastUpLimitFieldName) ||
-    form.getFieldValue(enableMulticastDownLimitFieldName) ||
-    form.getFieldValue(enableMulticastUpLimit6GFieldName) ||
-    form.getFieldValue(enableMulticastDownLimit6GFieldName)) {
-      multicastRateLimitingSwitch = true
-    }
-    form.setFieldValue(enableMulticastRateLimitingFieldName, multicastRateLimitingSwitch )
-  }, [])
 
   const multicastRateLimitFlag = useIsSplitOn(Features.MULTICAST_RATE_LIMIT_TOGGLE)
   const multicastFilterFlag = useIsSplitOn(Features.WIFI_EDA_MULTICAST_FILTER_TOGGLE)
