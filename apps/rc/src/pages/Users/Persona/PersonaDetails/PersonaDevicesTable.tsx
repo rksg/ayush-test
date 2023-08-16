ab36864e9d878c96d1bcb0a0425393936c4e4f32
Jerry Chen <Jerry.Chen@commscope.com>
2023-08-16 11:49:21 +0800

    ACX-38395: Remove unused dependency

--- apps/rc/src/pages/Users/Persona/PersonaDetails/PersonaDevicesTable.tsx
+++ apps/rc/src/pages/Users/Persona/PersonaDetails/PersonaDevicesTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import moment        from 'moment-timezone'
 import { useIntl }   from 'react-intl'
