c5e0e21f98e691c17c96891ef492b825051e649f
vivek.kandhvar <vivek.kandhvar@commscope.com>
2023-08-28 19:06:01 +0530

    RSA-6432: chore: refactor code

--- libs/reports/components/src/DataStudio/index.tsx
+++ libs/reports/components/src/DataStudio/index.tsx
@@ -1,29 +1,29 @@
 import { useState, useEffect } from 'react'
 
-import { getUserProfile as getUserProfileRA } from '@acx-ui/analytics/utils'
+import { getUserProfile }                     from '@acx-ui/analytics/utils'
 import { IFrame }                             from '@acx-ui/components'
 import { get }                                from '@acx-ui/config'
 import { useAuthenticateMutation }            from '@acx-ui/reports/services'
 import { getUserProfile as getUserProfileR1 } from '@acx-ui/user'
 
 export const getHostName = (origin: string) => {
-  if (process.env['NODE_ENV'] === 'development')
-    return get('IS_MLISA_SA') ?
-      'https://staging.mlisa.io'
-      :
-      'https://dev.ruckus.cloud'
-
+  if (process.env['NODE_ENV'] === 'development') {
+    return get('IS_MLISA_SA')
+      ? 'https://staging.mlisa.io'
+      : 'https://dev.ruckus.cloud'
+  }
   return origin
 }
 
 export function DataStudio () {
   const [url, setUrl] = useState<string>()
-  const [ authenticate ] = useAuthenticateMutation()
-  const { firstName, lastName, email } = get('IS_MLISA_SA')
-    ? getUserProfileRA()
-    : getUserProfileR1().profile
+  const [authenticate] = useAuthenticateMutation()
 
   useEffect(() => {
+    const { firstName, lastName, email } = get('IS_MLISA_SA')
+      ? getUserProfile()
+      : getUserProfileR1().profile
+
     authenticate({
       payload: {
         user: {
@@ -32,18 +32,21 @@ export function DataStudio () {
           email
         }
       }
-    }).unwrap().then(url => {
-      setUrl(url)
     })
-  }, [authenticate, email, firstName, lastName])
+      .unwrap()
+      .then(url => {
+        setUrl(url)
+      })
+  }, [authenticate])
 
   return (
     <div data-testid='data-studio'>
-      {url && (<IFrame
-        title='data-studio'
-        src={`${getHostName(window.location.origin)}${url}`}
-        style={{ width: '100%', height: '85vh', border: 'none' }}
-      />
+      {url && (
+        <IFrame
+          title='data-studio'
+          src={`${getHostName(window.location.origin)}${url}`}
+          style={{ width: '100%', height: '85vh', border: 'none' }}
+        />
       )}
     </div>
   )
--- libs/reports/components/src/EmbeddedReport/index.tsx
+++ libs/reports/components/src/EmbeddedReport/index.tsx
@@ -4,35 +4,43 @@ import { embedDashboard, EmbeddedDashboard } from '@superset-ui/embedded-sdk'
 import moment                                from 'moment'
 
 import { getUserProfile as getUserProfileRA } from '@acx-ui/analytics/utils'
+import { RadioBand, Loader }                  from '@acx-ui/components'
+import { get }                                from '@acx-ui/config'
+import { useParams }                          from '@acx-ui/react-router-dom'
 import {
-  RadioBand,
-  Loader
-} from '@acx-ui/components'
-import { get }                                          from '@acx-ui/config'
-import { useParams }                                    from '@acx-ui/react-router-dom'
-import { useGuestTokenMutation, useEmbeddedIdMutation } from '@acx-ui/reports/services'
-import { useReportsFilter }                             from '@acx-ui/reports/utils'
-import { REPORT_BASE_RELATIVE_URL }                     from '@acx-ui/store'
-import { getUserProfile as getUserProfileR1 }           from '@acx-ui/user'
-import { useDateFilter, getJwtToken, NetworkPath }      from '@acx-ui/utils'
+  useGuestTokenMutation,
+  useEmbeddedIdMutation
+} from '@acx-ui/reports/services'
+import { useReportsFilter }                        from '@acx-ui/reports/utils'
+import { REPORT_BASE_RELATIVE_URL }                from '@acx-ui/store'
+import { getUserProfile as getUserProfileR1 }      from '@acx-ui/user'
+import { useDateFilter, getJwtToken, NetworkPath } from '@acx-ui/utils'
 
-import { bandDisabledReports, ReportType, reportTypeDataStudioMapping, reportModeMapping } from '../mapping/reportsMapping'
+import {
+  bandDisabledReports,
+  ReportType,
+  reportTypeDataStudioMapping,
+  reportModeMapping
+} from '../mapping/reportsMapping'
 
 interface ReportProps {
-  reportName: ReportType
-  rlsClause?: string
-  hideHeader?: boolean
+  reportName: ReportType;
+  rlsClause?: string;
+  hideHeader?: boolean;
 }
 
 export function convertDateTimeToSqlFormat (dateTime: string): string {
   return moment.utc(dateTime).format('YYYY-MM-DD HH:mm:ss')
 }
 
-export const getSupersetRlsClause = (reportName:ReportType,
-  paths?:NetworkPath[],radioBands?:RadioBand[]) => {
-  const mode=reportModeMapping[reportName]
-  const isApReport = ['ap','both'].includes(mode)
-  const isSwitchReport = ['switch','both'].includes(mode)
+export const getSupersetRlsClause = (
+  reportName: ReportType,
+  paths?: NetworkPath[],
+  radioBands?: RadioBand[]
+) => {
+  const mode = reportModeMapping[reportName]
+  const isApReport = ['ap', 'both'].includes(mode)
+  const isSwitchReport = ['switch', 'both'].includes(mode)
   const isRadioBandDisabled = bandDisabledReports.includes(reportName)
   let radioBandClause = ''
   let zoneClause = ''
@@ -41,51 +49,61 @@ export const getSupersetRlsClause = (reportName:ReportType,
   let switchClause = ''
   let networkClause = ''
 
-  if(radioBands?.length && isApReport && !isRadioBandDisabled){
-    radioBandClause = ` "band" in (${radioBands.map(radioBand=>`'${radioBand}'`).join(', ')})`
+  if (radioBands?.length && isApReport && !isRadioBandDisabled) {
+    radioBandClause = ` "band" in (${radioBands
+      .map((radioBand) => `'${radioBand}'`)
+      .join(', ')})`
   }
 
-  if(paths?.length){
-    const zoneIds:string[] = []
-    const switchGroupIds:string[] = []
-    const apMacs:string[] = []
-    const switchMacs:string[]=[]
-    paths.forEach(path=>{
-      if(path.length === 2 && path[1].type === 'zone'){
-        zoneIds.push(`'${path[1].name}'`)
+  if (paths?.length) {
+    const zoneIds: string[] = []
+    const switchGroupIds: string[] = []
+    const apMacs: string[] = []
+    const switchMacs: string[] = []
+
+    paths.forEach((path) => {
+      if (path.length === 2) {
+        if (path[1].type === 'zone') zoneIds.push(`'${path[1].name}'`)
+        else if (path[1].type === 'switchGroup') {
+          switchGroupIds.push(`'${path[1].name}'`)
+        }
       }
-      else if(path.length === 2 && path[1].type === 'switchGroup'){
-        switchGroupIds.push(`'${path[1].name}'`)
-      }
-      else if(path.length === 3 && path[2].type === 'AP'){
-        apMacs.push(`'${path[2].name}'`)
-      }
-      else if(path.length === 3 && path[2].type === 'switch'){
-        switchMacs.push(`'${path[2].name}'`)
+      if (path.length === 3) {
+        if (path[2].type === 'AP') {
+          apMacs.push(`'${path[2].name}'`)
+        } else if (path[2].type === 'switch') {
+          switchMacs.push(`'${path[2].name}'`)
+        }
       }
     })
-    if(isApReport){
-      if(zoneIds.length){
+
+    if (isApReport) {
+      if (zoneIds.length) {
         zoneClause = `"zoneName" in (${zoneIds.join(', ')})`
       }
-
-      if(apMacs.length){
+      if (apMacs.length) {
         apClause = `"apMac" in (${apMacs.join(', ')})`
       }
     }
-    if(isSwitchReport){
-      if(switchGroupIds.length && isSwitchReport){
+
+    if (isSwitchReport) {
+      if (switchGroupIds.length) {
         switchGroupClause = `"switchGroupLevelOneName" in (${switchGroupIds.join(', ')})`
       }
-
-      if(switchMacs.length && isSwitchReport){
+      if (switchMacs.length) {
         switchClause = `"switchId" in (${switchMacs.join(', ')})`
       }
     }
 
-    if(zoneClause || apClause || switchGroupClause || switchClause){
-      networkClause = ` (${[zoneClause,apClause,switchGroupClause,switchClause]
-        .filter(item=>item!=='').join(' OR ')})`
+    if (zoneClause || apClause || switchGroupClause || switchClause) {
+      networkClause = ` (${[
+        zoneClause,
+        apClause,
+        switchGroupClause,
+        switchClause
+      ]
+        .filter((item) => item !== '')
+        .join(' OR ')})`
     }
   }
 
@@ -95,42 +113,50 @@ export const getSupersetRlsClause = (reportName:ReportType,
   }
 }
 
-
 export function EmbeddedReport (props: ReportProps) {
   const { reportName, rlsClause, hideHeader } = props
   const embedDashboardName = reportTypeDataStudioMapping[reportName]
   const params = useParams()
-  const [ guestToken ] = useGuestTokenMutation()
-  const [ embeddedId ] = useEmbeddedIdMutation()
+  const [guestToken] = useGuestTokenMutation()
+  const [embeddedId] = useEmbeddedIdMutation()
   const { startDate, endDate } = useDateFilter()
-  const [dashboardEmbeddedId, setDashboardEmbeddedId] = useState<string | null>(null)
-  const { filters: { paths, bands } } = useReportsFilter()
-  const { networkClause, radioBandClause } = getSupersetRlsClause(reportName,
-    paths,bands as RadioBand[])
+  const [dashboardEmbeddedId, setDashboardEmbeddedId] = useState<string | null>(
+    null
+  )
+  const {
+    filters: { paths, bands }
+  } = useReportsFilter()
+  const { networkClause, radioBandClause } = getSupersetRlsClause(
+    reportName,
+    paths,
+    bands as RadioBand[]
+  )
   const { firstName, lastName, email } = get('IS_MLISA_SA')
     ? getUserProfileRA()
     : getUserProfileR1().profile
 
   /**
-  * Hostname - Backend service where superset is running.
-  * For developement,
-  * Use https://dev.ruckus.cloud, for dev ruckus cloud.
-  * Use https://alto.local.mlisa.io, for minikube.
-  **/
-  const HOST_NAME = process.env['NODE_ENV'] === 'development'
-    ? get('IS_MLISA_SA') ?
-      'https://staging.mlisa.io'
-      :
-      'https://dev.ruckus.cloud'
-    : window.location.origin // Production
+   * Hostname - Backend service where superset is running.
+   * For developement,
+   * Use https://dev.ruckus.cloud, for dev ruckus cloud.
+   * Use https://alto.local.mlisa.io, for minikube.
+   **/
+  const HOST_NAME =
+    process.env['NODE_ENV'] === 'development'
+      ? get('IS_MLISA_SA')
+        ? 'https://staging.mlisa.io'
+        : 'https://dev.ruckus.cloud'
+      : window.location.origin // Production
 
   useEffect(() => {
     const embeddedData = {
       allowed_domains: [],
       dashboard_title: embedDashboardName
     }
-    embeddedId({ payload: embeddedData }).unwrap().then(uuid => setDashboardEmbeddedId(uuid))
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    embeddedId({ payload: embeddedData })
+      .unwrap()
+      .then((uuid) => setDashboardEmbeddedId(uuid))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [embedDashboardName])
 
   const guestTokenPayload = {
@@ -139,46 +165,65 @@ export function EmbeddedReport (props: ReportProps) {
       lastName,
       email
     },
-    resources: [{
-      type: 'dashboard',
-      id: dashboardEmbeddedId
-    }],
+    resources: [
+      {
+        type: 'dashboard',
+        id: dashboardEmbeddedId
+      }
+    ],
     rls: [
       {
-        clause: ['"__time"', '>=', `'${convertDateTimeToSqlFormat(startDate)}'`, 'AND',
-          '"__time"', '<', `'${convertDateTimeToSqlFormat(endDate)}'`, 'AND',
+        clause: [
+          '"__time"',
+          '>=',
+          `'${convertDateTimeToSqlFormat(startDate)}'`,
+          'AND',
+          '"__time"',
+          '<',
+          `'${convertDateTimeToSqlFormat(endDate)}'`,
+          'AND',
           `'${params?.tenantId}' = '${params?.tenantId}'`
         ].join(' ')
       },
-      ...((networkClause || radioBandClause || rlsClause)
-        ? [{
-          clause: rlsClause
-            ? rlsClause
-            : [networkClause.trim(),
-              networkClause && radioBandClause
-                ? ' AND ' + radioBandClause.trim()
-                : radioBandClause].join('')
-        }]
+      ...(networkClause || radioBandClause || rlsClause
+        ? [
+          {
+            clause: rlsClause
+              ? rlsClause
+              : [
+                networkClause.trim(),
+                networkClause && radioBandClause
+                  ? ' AND ' + radioBandClause.trim()
+                  : radioBandClause
+              ].join('')
+          }
+        ]
         : [])
     ]
   }
 
   const fetchGuestTokenFromBackend = async () => {
     // eslint-disable-next-line no-console
-    console.log('%c[%s][EmbeddedReport] -> Refreshing guest token for [%s]',
-      'color: cyan', new Date().toLocaleString(), embedDashboardName)
+    console.log(
+      '%c[%s][EmbeddedReport] -> Refreshing guest token for [%s]',
+      'color: cyan',
+      new Date().toLocaleString(),
+      embedDashboardName
+    )
     return await guestToken({ payload: guestTokenPayload }).unwrap()
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     let timer: ReturnType<typeof setInterval>
-    let embeddedObj :Promise<EmbeddedDashboard>
+    let embeddedObj: Promise<EmbeddedDashboard>
     const jwtToken = getJwtToken()
     if (dashboardEmbeddedId && dashboardEmbeddedId.length > 0) {
       embeddedObj = embedDashboard({
         id: dashboardEmbeddedId,
         supersetDomain: `${HOST_NAME}${REPORT_BASE_RELATIVE_URL}`,
-        mountPoint: document.getElementById(`acx-report-${embedDashboardName}`)!,
+        mountPoint: document.getElementById(
+          `acx-report-${embedDashboardName}`
+        )!,
         fetchGuestToken: () => fetchGuestTokenFromBackend(),
         dashboardUiConfig: {
           hideChartControls: true,
@@ -187,24 +232,28 @@ export function EmbeddedReport (props: ReportProps) {
         // debug: true
         authToken: jwtToken ? `Bearer ${jwtToken}` : undefined
       })
-      embeddedObj.then(async embObj =>{
+      embeddedObj.then(async (embObj) => {
         timer = setInterval(async () => {
           const { height } = await embObj.getScrollSize()
           if (height > 0) {
             const iframeElement = document.querySelector(
-              `div[id="acx-report-${embedDashboardName}"] > iframe`)
-            if(iframeElement){
-              iframeElement.setAttribute('style', `height: ${height}px !important`)
+              `div[id="acx-report-${embedDashboardName}"] > iframe`
+            )
+            if (iframeElement) {
+              iframeElement.setAttribute(
+                'style',
+                `height: ${height}px !important`
+              )
             }
           }
         }, 1000)
       })
     }
     return () => {
-      if(timer) clearTimeout(timer)
+      if (timer) clearTimeout(timer)
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[startDate, endDate, paths, bands, dashboardEmbeddedId])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [startDate, endDate, paths, bands, dashboardEmbeddedId])
 
   return (
     <Loader>
