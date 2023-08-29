6fa4cd13e0355e3c03c53f5be7ef7144ca3d01be
sp1148 <sarada.pachalla@commscope.com>
2023-08-28 22:34:42 -0700

    ACX-36033: code coverage wip

--- apps/main/src/browser-dialog/browser-dialog.spec.tsx
+++ apps/main/src/browser-dialog/browser-dialog.spec.tsx
@@ -1,55 +1,150 @@
-import { screen, fireEvent } from '@testing-library/react'
+// import { render, fireEvent, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 
-// import { BrowserDialog } from './browser-dialog' // Import the module you want to test
+import { showActionModal }              from '@acx-ui/components'
+import {
+  useUpdateUserProfileMutation,
+  UserProfile as UserProfileInterface
+} from '@acx-ui/user'
 
-jest.mock('react-router-dom', () => ({
-  useParams: () => ({ tenantId: 'mockTenantId' })
+import { BrowserDialog, LoadMessages, BrowserDialogProps } from './browser-dialog' // Replace with the actual path
+
+jest.mock('@acx-ui/utils', () => ({
+  getIntl: jest.fn(() => ({
+    $t: jest.fn((message) => message.defaultMessage)
+  })),
+  setUpIntl: jest.fn()
 }))
+const mockUserProfile = {
+  detailLevel: 'it',
+  dateFormat: 'mm/dd/yyyy',
+  preferredLanguage: 'en-US'
+}
+const params = { tenantId: 'tenant-id' }
 
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useState: jest.fn()
+jest.mock('@acx-ui/user', () => ({
+  ...jest.requireActual('@acx-ui/user'),
+  useUpdateUserProfileMutation: jest.fn(() =>
+    ({ data: { preferredLanguage: 'en-US' }, params: params }))
 }))
+jest.mock('@acx-ui/components', () => ({
+  showActionModal: jest.fn()
+}))
+const mockUseUpdateUserProfileMutation = useUpdateUserProfileMutation as jest.Mock
+const mockBrowserDialog = jest.fn().mockResolvedValue({
+  preferredLanguage: 'fr-FR' // Updated preferred language
+})
 
-// jest.mock('path-to-modal-module', () => ({
-//   showActionModal: jest.fn()
-// }))
+const setDialogOpen = jest.fn()
 
-describe.skip('BrowserDialog', () => {
-  it('renders correctly', () => {
-    // Mock the necessary values for the test
-    // useState.mockReturnValue([true, jest.fn()]) // Mock isOpen state
-
-    // Assertions
-    // Add your assertions here
+describe('BrowserDialog', () => {
+  beforeEach(() => {
+    jest.mocked(mockUseUpdateUserProfileMutation).mockReturnValue({ data: mockUserProfile })
   })
 
-  it('handles the confirmation action', () => {
-    // Mock the necessary values for the test
-    // useState.mockReturnValue([true, jest.fn()]) // Mock isOpen state
-    // useState.mockReturnValue(['false', jest.fn()]) // Mock isActionConfirmed state
-    const mockUpdateUserProfileMutation = jest.fn()
-
-    // Mock the dependencies' behaviors
-    require('path-to-your-api-module').useUpdateUserProfileMutation.mockReturnValue([
-      mockUpdateUserProfileMutation
-    ])
-    // require('path-to-intl-module').getIntl.mockReturnValue(mockGetIntl)
-
-    // Render the component
-    // render(<BrowserDialog browserLang='en-US' />)
-
-    // Simulate button click to trigger action
-    fireEvent.click(screen.getByText('Change to English'))
-
-    // Assertions
-    expect(mockUpdateUserProfileMutation).toHaveBeenCalledWith({
-      payload: {
-        dateFormat: 'mm/dd/yyyy',
-        detailLevel: 'it',
-        preferredLanguage: 'en-US'
-      },
-      params: { tenantId: 'mockTenantId' }
+  it.skip('should show action modal and handle confirm button click', async () => {
+    const setDialogOpen = jest.fn()
+    const updateUserProfile = jest.fn()
+    const bDialogProps = {
+      detailLevel: 'mockDetailLevel',
+      dateFormat: 'mockDateFormat',
+      preferredLanguage: 'mockPreferredLanguage',
+      isDialogOpen: true,
+      setDialogOpen: setDialogOpen
+    }
+    await act(async () => {
+      BrowserDialog('en-US', bDialogProps as BrowserDialogProps)
     })
+
+    // Assert modal is shown
+    expect(require('@acx-ui/components').showActionModal).toHaveBeenCalled()
+    const modalProps = require('@acx-ui/components').showActionModal.mock.calls[0][0]
+
+    // Simulate clicking the "Change to" button
+    await act(async () => {
+      modalProps.customContent.buttons[1].handler()
+    })
+
+    // Assert functions were called
+    expect(updateUserProfile).toHaveBeenCalledWith({
+      dateFormat: 'mockDateFormat',
+      detailLevel: 'mockDetailLevel',
+      preferredLanguage: 'en-US'
+    })
+    expect(setDialogOpen).toHaveBeenCalledWith(false)
+    expect(localStorage.setItem).toHaveBeenCalledWith('browserLang', 'en-US')
+    expect(localStorage.setItem).toHaveBeenCalledWith('isBrowserDialog', 'true')
+  })
+
+  it.skip('should open dialog and handle language change', async () => {
+    const props = {
+      detailLevel: 'it',
+      dateFormat: 'mm/dd/yyyy',
+      preferredLanguage: 'en-US',
+      isDialogOpen: true,
+      setDialogOpen
+    }
+    BrowserDialog('en-US', props as BrowserDialogProps)
+    expect(showActionModal).toHaveBeenCalled()
+
+    // expect(setDialogOpen).toHaveBeenCalledWith(true)
+    // Expectations related to API calls and localStorage can be added as well
+  })
+
+  it.skip('should call showActionModal when isDialogOpen is true', async () => {
+    const props = {
+      detailLevel: 'someDetailLevel',
+      dateFormat: 'someDateFormat',
+      preferredLanguage: 'en-US',
+      isDialogOpen: true,
+      setDialogOpen
+    }
+
+    await BrowserDialog('en-US', props as BrowserDialogProps)
+    expect(showActionModal).toHaveBeenCalled()
+  })
+})
+
+describe('LoadMessages', () => {
+  it('should not open dialog if language is the same', () => {
+    const userProfile = {
+      preferredLanguage: 'en-US',
+      detailLevel: 'it',
+      dateFormat: 'mm/dd/yyyy'
+    }
+
+    const result = LoadMessages(userProfile as UserProfileInterface)
+    expect(result).toBe('en-US')
+  })
+
+  it.skip('should open browser dialog and return updated preferred language', async () => {
+    const originalLocalStorage = { ...global.localStorage }
+    global.localStorage.getItem = jest.fn().mockReturnValue(null)
+    global.localStorage.setItem = jest.fn()
+
+    const userProfile = {
+      preferredLanguage: 'fr-FR',
+      detailLevel: 'mockDetailLevel',
+      dateFormat: 'mockDateFormat'
+    }
+
+    const bDialogProps = {
+      detailLevel: 'mockDetailLevel',
+      dateFormat: 'mockDateFormat',
+      isDialogOpen: true,
+      preferredLanguage: 'en-US',
+      setDialogOpen
+    }
+
+    jest.spyOn(require('./browser-dialog'), 'BrowserDialog').mockImplementation(mockBrowserDialog)
+    await Promise.resolve()
+    const result = await LoadMessages(userProfile as UserProfileInterface)
+
+    expect(mockBrowserDialog).toHaveBeenCalledWith('en-US', bDialogProps)
+
+    // await Promise.resolve()
+    expect(result).toBe('fr-FR')
+
+    global.localStorage = originalLocalStorage
   })
 })
