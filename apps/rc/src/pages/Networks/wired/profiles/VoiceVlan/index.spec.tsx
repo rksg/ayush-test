21cf8c366a92a4029dc93d25009bb39480196dc2
Guo, Yizhu <Yizhu.Guo@commscope.com>
2023-09-11 19:02:14 +0800

    ACX-38692: test: voice vlan tab on profile

--- apps/rc/src/pages/Networks/wired/profiles/VoiceVlan/index.spec.tsx
+++ apps/rc/src/pages/Networks/wired/profiles/VoiceVlan/index.spec.tsx
@@ -1,65 +1,62 @@
 import '@testing-library/jest-dom'
-import userEvent       from '@testing-library/user-event'
-import { Form, Modal } from 'antd'
-import { act }         from 'react-dom/test-utils'
+import userEvent from '@testing-library/user-event'
+import { Form }  from 'antd'
 
-import { SwitchConfigurationProfile }        from '@acx-ui/rc/utils'
-import { Provider }                          from '@acx-ui/store'
-import { fireEvent, render, screen, within } from '@acx-ui/test-utils'
+import { Provider }       from '@acx-ui/store'
+import { render, screen } from '@acx-ui/test-utils'
+import { noDataDisplay }  from '@acx-ui/utils'
 
 import { ConfigurationProfileFormContext, ConfigurationProfileType } from '../ConfigurationProfileFormContext'
 
-import { VlanSetting } from '.'
+import { VoiceVlan } from '.'
+
+jest.mock('./VoiceVlanDrawer', () => ({
+  VoiceVlanDrawer: () => <div data-testid={'rc-VoiceVlanDrawer'} />
+}))
 
 const currentData = {
   name: 'test-profile',
   description: '',
   acls: [],
-  vlans: [{ arpInspection: true, switchFamilyModels: [] }]
+  vlans: [{ arpInspection: true, switchFamilyModels: [] }],
+  voiceVlanOptions: [
+    {
+      model: 'ICX7150-24',
+      taggedVlans: [
+        {
+          vlanId: '4',
+          taggedPorts: [
+            '1/1/9',
+            '1/1/10'
+          ]
+        }
+      ]
+    },
+    {
+      model: 'ICX7650-48P',
+      taggedVlans: [
+        {
+          vlanId: '5',
+          taggedPorts: [
+            '1/1/6',
+            '1/1/7'
+          ]
+        }
+      ]
+    }
+  ],
+  voiceVlanConfigs: [
+    { model: 'ICX7150-24',voiceVlans: [{ vlanId: '4',taggedPorts: ['1/1/9'] }] },
+    { model: 'ICX7650-48P', voiceVlans: [] }
+  ]
 }
 
-describe('Wired - VlanSetting', () => {
+describe('Wired - VoiceVlan', () => {
   const configureProfileContextValues = {
     editMode: false,
     currentData
   } as unknown as ConfigurationProfileType
-
-  const vlans = [{
-    arpInspection: false,
-    id: '545d08c0e7894501846455233ad60cc5',
-    igmpSnooping: 'none',
-    ipv4DhcpSnooping: false,
-    spanningTreePriority: 32768,
-    spanningTreeProtocol: 'none',
-    vlanId: 2,
-    vlanName: 'vlan-01'
-  }, {
-    arpInspection: false,
-    id: '1af3d29b5dcc46a5a20a651fda55e2df',
-    igmpSnooping: 'none',
-    ipv4DhcpSnooping: false,
-    spanningTreePriority: 32768,
-    spanningTreeProtocol: 'none',
-    switchFamilyModels: [{
-      id: '9874453239bc479fac68bc050d0cf729',
-      model: 'ICX7550-24P',
-      slots: [
-        { slotNumber: 1, enable: true },
-        { slotNumber: 3, enable: true, option: '2X40G' },
-        { slotNumber: 2, enable: true, option: '2X40G' }
-      ],
-      taggedPorts: '1/2/2',
-      untaggedPorts: '1/1/20,1/3/2'
-    }],
-    vlanId: 3,
-    vlanName: 'vlan-02'
-  }]
-
-  afterEach(() => {
-    Modal.destroyAll()
-  })
-
-  it('should handle add VLAN correctly', async () => {
+  it('should render correctly', async () => {
     const params = {
       tenantId: 'tenant-id',
       action: 'add'
@@ -68,174 +65,21 @@ describe('Wired - VlanSetting', () => {
       <Provider>
         <ConfigurationProfileFormContext.Provider value={configureProfileContextValues}>
           <Form>
-            <VlanSetting />
+            <VoiceVlan />
           </Form>
         </ConfigurationProfileFormContext.Provider>
       </Provider>, {
         route: { params, path: '/:tenantId/networks/wired/profiles/:action' }
       })
 
-    await screen.findByRole('heading', { level: 3, name: /VLANs/ })
-
-    await userEvent.click(await screen.findByRole('button', { name: 'Add VLAN' }))
-    const vIdInput = await screen.findByLabelText('VLAN ID')
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    act(() => {
-      fireEvent.change(vIdInput, { target: { value: '1' } })
-    })
-    await userEvent.click(await screen.findByRole('button', { name: 'Add' }))
+    await screen.findByRole('heading', { level: 3, name: /Voice VLAN/ })
+    expect(await screen.findByText('ICX7150-24')).toBeVisible()
+    expect(await screen.findByText('ICX7650-48P')).toBeVisible()
+    expect(await screen.findByText('VLAN-ID: 4')).toBeVisible()
+    expect(await screen.findByText(noDataDisplay)).toBeVisible()
+    const button = await screen.findAllByRole('button', { name: 'Set Voice VLAN' })
+    await userEvent.click(button[0])
+    expect(await screen.findByTestId('rc-VoiceVlanDrawer')).toBeVisible()
   })
 
-  it('should handle edit VLAN correctly', async () => {
-    const params = {
-      tenantId: 'tenant-id',
-      action: 'edit'
-    }
-
-    render(
-      <Provider>
-        <ConfigurationProfileFormContext.Provider value={{
-          ...configureProfileContextValues,
-          editMode: true,
-          currentData: {
-            ...currentData,
-            vlans
-          } as unknown as SwitchConfigurationProfile
-        }}>
-          <Form>
-            <VlanSetting />
-          </Form>
-        </ConfigurationProfileFormContext.Provider>
-      </Provider>, {
-        route: { params, path: '/:tenantId/networks/wired/profiles/:action' }
-      })
-
-    await screen.findByRole('heading', { level: 3, name: /VLANs/ })
-
-    const row = await screen.findByRole('row', { name: /vlan-02/i })
-
-    const radio = await within(row).findByRole('radio')
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act(async () => {
-      fireEvent.click(radio)
-    })
-
-    const editButton = await screen.findByRole('button', { name: /Edit/i })
-
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act(async () => {
-      fireEvent.click(editButton)
-    })
-    const IGMPSnooping = await screen.findByLabelText('IGMP Snooping')
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act(async () => {
-      fireEvent.mouseDown(IGMPSnooping)
-    })
-    const active = await screen.findByText('Active')
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act(async () => {
-      fireEvent.click(active)
-      fireEvent.mouseDown(IGMPSnooping)
-    })
-    const passive = await screen.findByText('Passive')
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act(async () => {
-      fireEvent.click(passive)
-      fireEvent.mouseDown(IGMPSnooping)
-    })
-    const none = await screen.findByLabelText('NONE')
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act(async () => {
-      fireEvent.click(none)
-      fireEvent.click(await screen.findByRole('button', { name: /Save/i }))
-    })
-  })
-
-  it('should handle edit VLAN ports correctly', async () => {
-    const params = {
-      tenantId: 'tenant-id'
-    }
-
-    render(
-      <Provider>
-        <ConfigurationProfileFormContext.Provider value={{
-          ...configureProfileContextValues,
-          editMode: true,
-          currentData: {
-            ...currentData,
-            vlans
-          } as unknown as SwitchConfigurationProfile
-        }}>
-          <Form>
-            <VlanSetting />
-          </Form>
-        </ConfigurationProfileFormContext.Provider>
-      </Provider>, {
-        route: { params, path: '/:tenantId/networks/wired/profiles/add' }
-      })
-
-    await screen.findByRole('heading', { level: 3, name: /VLANs/ })
-
-    const row = await screen.findByRole('row', { name: /vlan-02/i })
-    fireEvent.click(await within(row).findByRole('radio'))
-    fireEvent.click(await screen.findByRole('button', { name: /Edit/i }))
-
-    const drawer = await screen.findByRole('dialog')
-    const row2 = await within(drawer).findByRole('row', { name: /ICX7550-24P/i })
-    fireEvent.click(await within(row2).findByRole('radio'))
-    fireEvent.click(await within(drawer).findByRole('button', { name: /Edit/i }))
-
-    const dialog = await screen.findByTestId('vlanSettingModal')
-    await userEvent.click(await within(dialog).findByText('Untagged Ports'))
-
-    await userEvent.click(await within(dialog).findByTestId('untagged_module1_1'))
-    await userEvent.click(await within(dialog).findByText('Tagged Ports'))
-
-    await userEvent.click(await within(dialog).findByTestId('tagged_module1_6'))
-    await userEvent.click(await within(dialog).findByRole('button', { name: 'Apply' }))
-    await userEvent.click(await screen.findByRole('button', { name: 'Save' }))
-  })
-
-  it('should handle delete VLAN correctly', async () => {
-    const params = {
-      tenantId: 'tenant-id'
-    }
-
-    render(
-      <Provider>
-        <ConfigurationProfileFormContext.Provider value={{
-          ...configureProfileContextValues,
-          editMode: true,
-          currentData: {
-            ...currentData,
-            vlans: vlans
-          } as unknown as SwitchConfigurationProfile
-        }}>
-          <Form>
-            <VlanSetting />
-          </Form>
-        </ConfigurationProfileFormContext.Provider>
-      </Provider>, {
-        route: { params, path: '/:tenantId/networks/wired/profiles/add' }
-      })
-
-    await screen.findByRole('heading', { level: 3, name: /VLANs/ })
-
-    const row = await screen.findByRole('row', { name: /vlan-01/i })
-    const radio = await within(row).findByRole('radio')
-    fireEvent.click(radio)
-
-    const deleteButton1 = await screen.findByRole('button', { name: /Delete/i })
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    act(() => {
-      fireEvent.click(deleteButton1)
-    })
-    await userEvent.click(await screen.findByRole('button', { name: 'Cancel' }))
-    const deleteButton2 = await screen.findByRole('button', { name: /Delete/i })
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    act(() => {
-      fireEvent.click(deleteButton2)
-    })
-    await userEvent.click(await screen.findByRole('button', { name: 'Delete Vlan' }))
-  })
 })
