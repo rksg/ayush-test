60ef2f692c7b53d5ce01c4d935ac05a23b2c9339
Eric Leu <eric.leu@commscope.com>
2023-09-13 12:41:07 -0700

    ACX-41014: Create FF for Application Key Management configuration

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -106,7 +106,8 @@ export enum Features {
   DPSK_PER_BOUND_PASSPHRASE_ALLOWED_DEVICE_INCREASED_LIMIT='dpsk-per-bound-passphrase-allowed-device-increased-limit',
   EDGES_SCHEDULE_UPGRADE_TOGGLE = 'acx-ui-edges-schedule-upgrade-toggle',
   WIFI_TRUNK_PORT_UNTAGGED_VLAN_TOGGLE = 'wifi-trunk-port-untagged-vlan-toggle',
-  MSP_AGGREGATE_NOTIFICATION_TOGGLE = 'aggregate-notification-toggle'
+  MSP_AGGREGATE_NOTIFICATION_TOGGLE = 'aggregate-notification-toggle',
+  IDM_APPLICATION_KEY_TOGGLE = 'ptenant-application-key-toggle'
 }
 
 export enum TierFeatures { // for Tier (ex: Beta) feature flag
