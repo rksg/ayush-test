4b560730d1278b792ac2ec2fb08dc8da69def6a1
Joe Kao <joe.kao@commscope.com>
2023-09-14 15:43:32 +0800

    ACX-40937: Extend the socket timeout time and use the new API to get CCD support AP groups

--- libs/rc/shared/utils/src/ccdSocket.ts
+++ libs/rc/shared/utils/src/ccdSocket.ts
@@ -4,11 +4,17 @@ import { getIndependentSocket } from './initialSocket'
 export const initCcdSocket = (subscriptionId: string, handler: (msg: string) => void): SocketIOClient.Socket => {
   let socket = getIndependentSocket(`/ccd?subscriptionId=${subscriptionId}`)
 
+  //socket.on('connect', () => {
+  //  console.log('CCD websocket connection')
+  //})
   socket.on('ccdEvent', handler)
+
+  //socket.on('disconnect', (reason: string) => {
+  //  console.log('CCD websocket disXConnection - reason', reason)
+  //})
 
   return socket
 }
-
 
 export const closeCcdSocket = (socket: SocketIOClient.Socket) => {
   if (socket.disconnected) return
