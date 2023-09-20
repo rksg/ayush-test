d5ea4d6d2545deb6a7ebccf23255a3a82efe6439
Jason Cheow <jason.cheow@commscope.com>
2023-09-20 16:37:41 +0800

    MLSA-6659: Change 'Ask Anything' -> 'Coming Soon'

--- libs/analytics/components/src/ChatWithMelissa/index.tsx
+++ libs/analytics/components/src/ChatWithMelissa/index.tsx
@@ -10,6 +10,10 @@ export function ChatWithMelissa () {
       Chat with <b>Melissa</b><br />
       and unlock the <b>power of AI</b>
     </p>
-    <Button size='small'>Ask anything</Button>
+    {
+      // TODO: open chatbot when it's ready with this button
+      // <Button size='small'>Ask Anything</Button>
+    }
+    <Button size='small' disabled>Coming Soon</Button>
   </Card></UI.Wrapper>
 }
