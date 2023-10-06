429025d96d688a62ad48ae5665131db3eb989586
TCHEN7 <tingan.chen@commscope.com>
2023-10-06 16:56:01 +0800

    Revert "ACX-43049: fix: tooltop text format"
    This reverts commit 5792c8d0f9693256e316389ed4de4cf2e4a2754e.

--- libs/common/components/src/components/CloudRRMGraph/helper.tsx
+++ libs/common/components/src/components/CloudRRMGraph/helper.tsx
@@ -58,30 +58,30 @@ export const tooltipFormatter = (params: TooltipFormatterProps) => {
               b: (contents) => <b>{contents}</b>
             }}
           />
-          {variables.map(vars => <li><FormattedMessage
+          <li>{variables.map(vars => <FormattedMessage
             key={`${vars.radio}-${vars.bandwidth}`}
             defaultMessage='Channel number (radio {radio}): <b>{channel}</b>'
             values={{
               ..._.pick(vars, ['radio', 'channel']),
               b: (contents) => <b>{contents}</b>
             }}
-          /></li>)}
-          {variables.map(vars => <li><FormattedMessage
+          />)}</li>
+          <li>{variables.map(vars => <FormattedMessage
             key={`${vars.radio}-${vars.bandwidth}`}
             defaultMessage='Bandwidth (radio {radio}): <b>{bandwidth}</b>'
             values={{
               ..._.pick(vars, ['radio', 'bandwidth']),
               b: (contents) => <b>{contents}</b>
             }}
-          /></li>)}
-          {showTxPower && variables.map(vars => <li><FormattedMessage
+          />)}</li>
+          {showTxPower && <li>{variables.map(vars => <FormattedMessage
             key={`${vars.radio}-${vars.txPower}`}
             defaultMessage='Tx Power (radio {radio}): <b>{txPower}</b>'
             values={{
               ..._.pick(vars, ['radio', 'txPower']),
               b: (contents) => <b>{contents}</b>
             }}
-          /></li>)}
+          />)}</li>}
         </ul>
       </TooltipWrapper>
     </RawIntlProvider>
