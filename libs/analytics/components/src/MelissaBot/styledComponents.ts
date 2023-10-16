e95bd6f1b3b7b7b2fe6f937244383a33e2d56513
Asraf Ali <asraf.najmudeen@commscope.com>
2023-10-16 17:21:50 +0530

    RSA-6506 | chore: css fix

---
+++ libs/analytics/components/src/MelissaBot/styledComponents.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro'
+
+import { Drawer } from '@acx-ui/components'
+
+export const MelissaDrawer = styled(Drawer)`
+    .ant-drawer-header{
+        background: linear-gradient(#2B4181,#223878,#263C7B);
+        margin: 0px !important;
+        padding: 8px 10px 8px 10px !important;
+    }
+    .ant-drawer-title {
+        color: white;
+    }
+    .ant-drawer-close > svg > path{
+        stroke: #FFF;
+    }
+`
