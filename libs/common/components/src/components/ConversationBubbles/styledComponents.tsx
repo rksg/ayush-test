1ae5196166d845a5ef978bb1f04680c36576e6dd
Sammanita <Sammanita.Pattnayak@commscope.com>
2023-10-18 17:30:44 +0530

    RSA-6505: Resolved conflict

--- libs/common/components/src/components/ConversationBubbles/styledComponents.tsx
+++ libs/common/components/src/components/ConversationBubbles/styledComponents.tsx
@@ -68,3 +68,61 @@ export const Bot = styled.div`
   text-align: left;
   padding: 10px;
 `
+
+const ChatTypingWrapper = styled.div`
+.typing {
+  align-items: center;
+  display: flex;
+  height: 17px;
+}
+.typing .dot {
+  animation: mercuryTypingAnimation 1.8s infinite ease-in-out;
+  background-color: #E47B01 ; //rgba(20,105,69,.7);
+  border-radius: 50%;
+  height: 7px;
+  margin-right: 4px;
+  vertical-align: middle;
+  width: 7px;
+  display: inline-block;
+}
+.typing .dot:nth-child(1) {
+  animation-delay: 200ms;
+}
+.typing .dot:nth-child(2) {
+  animation-delay: 300ms;
+}
+.typing .dot:nth-child(3) {
+  animation-delay: 400ms;
+}
+.typing .dot:last-child {
+  margin-right: 0;
+}
+
+@keyframes mercuryTypingAnimation {
+  0% {
+    transform: translateY(0px);
+    background-color: #E47B01;
+  }
+  28% {
+    transform: translateY(-7px);
+    background-color: #F7B41E;
+  }
+  44% {
+    transform: translateY(0px);
+    background-color: #FAD278;
+  }
+}
+`
+
+export const ChatTyping=()=>{
+  return(
+    <ChatTypingWrapper>
+      <div className='typing'>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+      </div>
+    </ChatTypingWrapper>
+  )
+}
+
