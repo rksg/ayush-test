eb564c79d6787bb306a1a79563d4538ca4ddba83
Sammanita <Sammanita.Pattnayak@commscope.com>
2023-10-18 16:59:16 +0530

    RSA-6505: Added accordion

--- libs/common/components/src/components/ConversationBubbles/styledComponents.ts
+++ libs/common/components/src/components/ConversationBubbles/styledComponents.ts
@@ -1,4 +1,33 @@
 import styled from 'styled-components/macro'
+import { Collapse as AcxCollapse } from '@acx-ui/components'
+
+export const Collapse = styled(AcxCollapse)`
+  width: 416px;
+  border: 1px solid #E3E4E5;
+  margin-top: 16px;
+  padding: 10px;
+  border-radius: 5px;
+  .ant-collapse-content {
+    background-color: var(--acx-primary-white) !important;
+  }
+  .ant-collapse-content-box {
+    padding: 0px 2px 2px 2px;
+  }
+  .ant-collapse-item .ant-collapse-header {
+    border-bottom: none !important;
+    padding: 10px !important;
+    font-size: 14px !important;
+  }
+`
+
+export const Panel = styled(AcxCollapse.Panel)`
+  .ant-collapse-header {
+    padding: 13px 10px !important;
+  }
+  .ant-collapse-header > .ant-collapse-expand-icon > .ant-collapse-arrow {
+    right: 18px !important;
+  }
+`
 
 export const Wrapper = styled.div`
   border-radius: 8px;
