b7e0e87414560fbafc37b189bf5a9706975f1c15
KYEH <karen.yeh@commscope.com>
2023-10-23 11:37:20 +0800

    ACX-41424: Enable split.io

--- apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/index.tsx
+++ apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/index.tsx
@@ -1,9 +1,11 @@
+import { Features, useIsSplitOn } from '@acx-ui/feature-toggle'
+
 import { VenueFirmwareList }       from './VenueFirmwareList'
 import { VenueFirmwareListLegacy } from './VenueFirmwareListLegacy'
 import VersionBanner               from './VersionBanner'
 
 const SwitchFirmware = () => {
-  const allowUpgradeBySwitch = true//useIsSplitOn(Features.SWITCH_UPGRADE_BY_SWITCH)***
+  const allowUpgradeBySwitch = useIsSplitOn(Features.SWITCH_UPGRADE_BY_SWITCH)
   return (
     <>
       <VersionBanner />
