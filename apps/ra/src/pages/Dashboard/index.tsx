4b4d64f8fa8f0357558b1d8dead4133eb8a0b0fc
wl1046 <wilson.loh@commscope.com>
2023-11-07 14:02:07 +0800

    MLSA-6807: fix typo

--- apps/ra/src/pages/Dashboard/index.tsx
+++ apps/ra/src/pages/Dashboard/index.tsx
@@ -152,7 +152,7 @@ const DashboardView = ({ filters, pathFilters }: DashboardViewProps) => {
         />
       </div>
       { enableAppInsights
-        ? <div style={{ gridArea: 'd3' }}>
+        ? <div style={{ gridArea: 'd2' }}>
           <SLA pathFilters={pathFilters} />
         </div>
         : <div style={{ gridArea: 'd2' }}>
