34d0c3f5e96b83c93967cfa562d9b03329c3fc0f
Chen, Sander <Sander.Chen@commscope.com>
2023-11-07 17:39:04 +0800

    ACX-41321: Revert commit

--- apps/rc/src/pages/Devices/Wifi/ApDetails/ApContextProvider.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApDetails/ApContextProvider.tsx
@@ -4,9 +4,9 @@ import { pick }              from 'lodash'
 import { useIntl }           from 'react-intl'
 import { useParams, Params } from 'react-router-dom'
 
-import { Loader }                                    from '@acx-ui/components'
-import { useApListQuery, useGetApCapabilitiesQuery } from '@acx-ui/rc/services'
-import { ApContext }                                 from '@acx-ui/rc/utils'
+import { Loader }         from '@acx-ui/components'
+import { useApListQuery } from '@acx-ui/rc/services'
+import { ApContext }      from '@acx-ui/rc/utils'
 
 export function ApContextProvider (props: { children: ReactNode }) {
   const params = useParams()
@@ -26,18 +26,10 @@ export function ApContextProvider (props: { children: ReactNode }) {
       ...rest
     })
   })
-
-  // eslint-disable-next-line
-  const apCapabilitiesQuery = useGetApCapabilitiesQuery({ params: { serialNumber: (results.data?.[0].serialNumber ?? '') } })
-
   const { data } = results
-  const values: Params<string> = {
-    ...params,
-    ...pick(data?.[0], fields) as Params<string>,
-    ...({ capabilities: apCapabilitiesQuery.data } as Params<string>)
-  }
+  const values: Params<string> = { ...params, ...pick(data?.[0], fields) as Params<string> }
   return <ApContext.Provider value={values}>
-    <Loader states={[results, apCapabilitiesQuery]}>{
+    <Loader states={[results]}>{
       data && data.length
         ? props.children
         : $t({ defaultMessage: 'Could not find AP {apId}' }, params)
