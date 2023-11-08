8d947f8ebd0d9898e19eef5ff00d759b8967787b
KYEH <karen.yeh@commscope.com>
2023-11-08 14:37:39 +0800

    ACX-41424: Workaround for INT

--- apps/main/src/errorMiddleware.tsx
+++ apps/main/src/errorMiddleware.tsx
@@ -163,7 +163,9 @@ export const getErrorContent = (action: ErrorAction) => {
     case 'FETCH_ERROR' as unknown as number: // no connection
       errorMsg = errorMessage.CHECK_YOUR_CONNECTION
       type = 'info'
-      callback = () => window.location.reload()
+      if (window.location.hostname !== 'int.ruckus.cloud') {
+        callback = () => window.location.reload()
+      }
       break
     case 422:
       const countryInvalid // TODO: check error format
@@ -249,7 +251,7 @@ export const errorMiddleware: Middleware = () => (next) => (action: ErrorAction)
     if (!shouldIgnoreErrorModal(action)) {
       showErrorModal(details)
     }
-    if (needLogout && !isDevModeOn) {
+    if (needLogout && !isDevModeOn && window.location.hostname !== 'int.ruckus.cloud') {
       userLogout()
     }
   }
