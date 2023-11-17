9d9efea07ac0cd968b6ff8497421ec1b2d188928
cbokil <chinmay.bokil@commscope.com>
2023-11-17 17:00:32 +0800

    MLSA-6909: address review comment

--- libs/analytics/components/src/GenericError/index.tsx
+++ libs/analytics/components/src/GenericError/index.tsx
@@ -2,9 +2,10 @@ import { useIntl } from 'react-intl'
 
 function GenericError ({ errMsg } : { errMsg?: string }) {
   const { $t } = useIntl()
-  const msg = errMsg || 'Please reduce the time period selection and try again.'
+  const msg = errMsg
+    || $t({ defaultMessage: 'Please reduce the time period selection and try again.' })
   return <div>
-    {$t({ defaultMessage: '{msg}' }, { msg })}
+    { msg }
   </div>
 }
 export default GenericError
