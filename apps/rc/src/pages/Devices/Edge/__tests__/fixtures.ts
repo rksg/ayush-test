266334cff62374bed877afd3f60705ed7f1bc2bb
Cheng, Amy <Amy.Cheng@commscope.com>
2023-11-17 17:23:39 +0800

    ACX-44742: implement SD-LAN detail in edge service tab

--- apps/rc/src/pages/Devices/Edge/__tests__/fixtures.ts
+++ apps/rc/src/pages/Devices/Edge/__tests__/fixtures.ts
@@ -1,4 +1,16 @@
-import { EdgeIpModeEnum, EdgePortTypeEnum, EdgePortStatus, EdgeServiceTypeEnum, ACLDirection, AccessAction, ProtocolType, AddressType, LeaseTimeUnit, EdgeLagTypeEnum, EdgeLagTimeoutEnum } from '@acx-ui/rc/utils'
+import {
+  EdgeIpModeEnum,
+  EdgePortTypeEnum,
+  EdgePortStatus,
+  EdgeServiceTypeEnum,
+  ACLDirection,
+  AccessAction,
+  ProtocolType,
+  AddressType,
+  LeaseTimeUnit,
+  EdgeLagTypeEnum,
+  EdgeLagTimeoutEnum
+} from '@acx-ui/rc/utils'
 
 export const mockVenueData = {
   fields: ['name', 'id'],
@@ -428,7 +440,7 @@ export const mockEdgeDhcpHostStats = {
 
 export const mockedEdgeServiceList = {
   fields: null,
-  totalCount: 3,
+  totalCount: 4,
   page: 1,
   data: [
     {
@@ -457,6 +469,15 @@ export const mockedEdgeServiceList = {
       status: 'Up',
       currentVersion: '1.1.1',
       targetVersion: '2.2.1'
+    },
+    {
+      edgeId: '96B341ADD6C16C11ED8B8B000C296600F5',
+      serviceName: 'SDLAN-1',
+      serviceId: 'sdlan-1',
+      serviceType: EdgeServiceTypeEnum.SD_LAN,
+      status: 'Up',
+      currentVersion: '1.1.1',
+      targetVersion: ''
     }
   ]
 }
@@ -938,3 +959,29 @@ export const mockedEdgeLagList = {
     totalCount: 2
   }
 }
+
+export const mockedSdLanDataList = [{
+  id: 'mocked-sd-lan-1',
+  name: 'Mocked_SDLAN_1',
+  tenantId: '0f18d1cf714b4bcf94bef4654f1ab29c',
+  venueId: 'a307d7077410456f8f1a4fc41d861567',
+  venueName: 'Mocked-Venue-1',
+  edgeId: '96B968BD2C76ED11EEA8E4B2E81F537A94',
+  edgeName: 'vSE-b490',
+  tunnelProfileId: 'aa3ecf6f283448d5bb8c0ce86790b843',
+  tunnelProfileName: 'Mocked_tunnel',
+  networkIds: ['8e22159cfe264ac18d591ea492fbc05a'],
+  networkInfos: [{
+    networkId: '8e22159cfe264ac18d591ea492fbc05a',
+    networkName: 'Mocked_network'
+  }],
+  corePortMac: 'c2:58:00:ae:63:f2',
+  edgeAlarmSummary: {
+    edgeId: 'mocked-edge-1',
+    severitySummary: {
+      critical: 1
+    },
+    totalCount: 1
+  },
+  serviceVersion: '1.0.0.100'
+}]
