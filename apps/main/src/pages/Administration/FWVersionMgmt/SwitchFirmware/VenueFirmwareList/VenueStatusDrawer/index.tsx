a01fef060c414d50de5a2d48416a3d6b8be8b17a
KYEH <karen.yeh@commscope.com>
2023-11-20 17:27:01 +0800

    ACX-44841: Fix the lint

--- apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/VenueFirmwareList/VenueStatusDrawer/index.tsx
+++ apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/VenueFirmwareList/VenueStatusDrawer/index.tsx
@@ -17,8 +17,7 @@ import {
   SwitchFwStatusEnum,
   SwitchStatusEnum,
   defaultSort,
-  sortProp,
-  transformSwitchUnitStatus
+  sortProp
 } from '@acx-ui/rc/utils'
 import { useParams }                         from '@acx-ui/react-router-dom'
 import { TABLE_QUERY_LONG_POLLING_INTERVAL } from '@acx-ui/utils'
