5b665adf5438db005dd7f6a3238453f470d389a4
mdehez <mickael.dehez@commscope.com>
2023-11-30 14:06:10 +0800

    MLSA-6934: add loader on save

--- libs/analytics/components/src/Brand360/SlaSliders.tsx
+++ libs/analytics/components/src/Brand360/SlaSliders.tsx
@@ -5,14 +5,14 @@ import { useIntl } from 'react-intl'
 
 import { useUpdateTenantSettingsMutation } from '@acx-ui/analytics/services'
 import type { Settings }                   from '@acx-ui/analytics/utils'
-import { Button, Card }                    from '@acx-ui/components'
+import { Button, Card, Loader }            from '@acx-ui/components'
 import { formatter, FormatterType }        from '@acx-ui/formatter'
 
 import { SliderLabel, Buttons } from './styledComponents'
 
 export const SlaSliders = ({ settings }: { settings: Partial<Settings> }) => {
   const { $t } = useIntl()
-  const [updateSlas] = useUpdateTenantSettingsMutation()
+  const [updateSlas, result] = useUpdateTenantSettingsMutation()
   const [slas, setSlas] = useState(settings)
   useEffect(() => setSlas(settings), [settings])
   const SlaSlider = ({ name, format }: { name: keyof Settings, format: FormatterType }) => <Slider
@@ -23,20 +23,22 @@ export const SlaSliders = ({ settings }: { settings: Partial<Settings> }) => {
     defaultValue={parseInt(slas[name]!, 10)}
     onAfterChange={(value: number) => setSlas({ ...slas, [name]: value.toString() })}
   />
-  return <Card title={$t({ defaultMessage: 'Service Level Agreements' })}>
-    <SliderLabel>{$t({ defaultMessage: 'P1 Incidents' })}</SliderLabel>
-    <SlaSlider name='sla-p1-incidents-count' format='countFormat' />
-    <SliderLabel>{$t({ defaultMessage: 'Guest Experience' })}</SliderLabel>
-    <SlaSlider name='sla-guest-experience' format='percent' />
-    <SliderLabel>{$t({ defaultMessage: 'SSID Compliance' })}</SliderLabel>
-    <SlaSlider name='sla-brand-ssid-compliance' format='percent' />
-    <Buttons>
-      <Button onClick={() => updateSlas(slas)} size={'small'} type='primary'>
-        {$t({ defaultMessage: 'Apply' })}
-      </Button>
-      <Button onClick={() => setSlas(settings)} size={'small'}>
-        {$t({ defaultMessage: 'Reset' })}
-      </Button>
-    </Buttons>
-  </Card>
+  return <Loader states={[result]}>
+    <Card title={$t({ defaultMessage: 'Service Level Agreements' })}>
+      <SliderLabel>{$t({ defaultMessage: 'P1 Incidents' })}</SliderLabel>
+      <SlaSlider name='sla-p1-incidents-count' format='countFormat' />
+      <SliderLabel>{$t({ defaultMessage: 'Guest Experience' })}</SliderLabel>
+      <SlaSlider name='sla-guest-experience' format='percent' />
+      <SliderLabel>{$t({ defaultMessage: 'SSID Compliance' })}</SliderLabel>
+      <SlaSlider name='sla-brand-ssid-compliance' format='percent' />
+      <Buttons>
+        <Button onClick={() => updateSlas(slas)} size='small' type='primary'>
+          {$t({ defaultMessage: 'Apply' })}
+        </Button>
+        <Button onClick={() => setSlas(settings)} size='small'>
+          {$t({ defaultMessage: 'Reset' })}
+        </Button>
+      </Buttons>
+    </Card>
+  </Loader>
 }
