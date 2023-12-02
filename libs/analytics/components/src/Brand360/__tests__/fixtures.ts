71a23df872dfb40f6c109032896add69bb350df7
cbokil <chinmay.bokil@commscope.com>
2023-12-02 14:50:07 +0800

    MLSA-6932: Update fixture

--- libs/analytics/components/src/Brand360/__tests__/fixtures.ts
+++ libs/analytics/components/src/Brand360/__tests__/fixtures.ts
@@ -2,11 +2,18 @@ import { sample } from 'lodash'
 export const fetchBrandProperties = (sliceType: string) => {
   const properties = [
     'AC Hotels', 'Courtyard', 'Fairfield', 'Element' , 'Potea Hotels',
-    'Four Seasons', 'Hayatt', 'Shereton', 'J W Marriott' , 'Raddison'
+    'Four Seasons', 'Hyatt', 'Sheraton', 'J W Marriott' , 'Ramada',
+    'Ritz-Carlton', 'Le Meridien', 'Accor', 'Best Western' , 'Holiday Inn',
+    'Hilton', 'IHG Hotels & Resorts', 'St Regis Hotels', 'Wyndham Hotels & Resorts',
+    'Shangri-La Hotels and Resorts', 'Ibis'
   ]
-  const lsps = ['Art-integrator', 'B-Network', 'NetOps', 'Single Digit']
+  const lsps = [
+    'Single Digit', 'DeepBlue', 'Delion',
+    'Cloud5', 'Hotel Internet Services'
+  ]
   const pcts = [ .54, .73, .64, .5, .6, .78, .88, .92, .75, .84 ]
-  const nums = [ 10, 20, 33, 56, 64, 87, 94, 24, 89, 99 ]
+  const nums = [ 3, 5, 6, 10, 20, 33, 56, 64, 87, 94, 24, 89, 99 ]
+  const pCnt = [ 3, 4, 5, 2, 7 ]
   return sliceType === 'lsp'
     ? lsps.map(lsp => {
       const [
@@ -14,13 +21,14 @@ export const fetchBrandProperties = (sliceType: string) => {
         avgTTC = 0,
         avgClientThroughput = 0
       ] = [ sample(pcts), sample(pcts), sample(pcts) ]
+      const propertyCount = sample(pCnt) || 0
       return {
-        propertyCount: Math.ceil(Math.random() * 10 / properties.length),
+        propertyCount,
         lsp,
         p1Incidents: sample(nums) || 0,
         guestExp: (avgConnSuccess + avgTTC + avgClientThroughput) / 3,
         ssidComplaince: sample(pcts) || 0,
-        deviceCount: sample(nums) || 0,
+        deviceCount: (sample(nums) || 0) * propertyCount,
         avgConnSuccess,
         avgTTC,
         avgClientThroughput
