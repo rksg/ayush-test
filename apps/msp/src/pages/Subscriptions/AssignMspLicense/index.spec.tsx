354e19c378767572a41c31b6346e7330c620771c
mdehez <mickael.dehez@commscope.com>
2023-12-04 10:32:32 +0800

    MLSA-6870: disable test until we mock date

--- apps/msp/src/pages/Subscriptions/AssignMspLicense/index.spec.tsx
+++ apps/msp/src/pages/Subscriptions/AssignMspLicense/index.spec.tsx
@@ -582,7 +582,7 @@ describe('AssignMspLicense', () => {
       })
     })
   })
-  it('datepicker should work correctly', async () => {
+  it.skip('datepicker should work correctly', async () => {
     jest.mocked(useIsSplitOn).mockReturnValue(false)
     render(
       <Provider>
