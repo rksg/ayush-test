441c19618ae2531ac73b33fb706a52fb4972f75e
lc1158 <Lish.Chen@commscope.com>
2023-12-08 14:28:10 +0800

    ACX-46753: Add filter for network

--- libs/rc/shared/components/src/ConnectedClientsTable/index.tsx
+++ libs/rc/shared/components/src/ConnectedClientsTable/index.tsx
@@ -5,7 +5,7 @@ import { Space }   from 'antd'
 import { useIntl } from 'react-intl'
 
 import { Subtitle, Tooltip, Table, TableProps, Loader  }                                  from '@acx-ui/components'
-import { useGetClientListQuery, useVenuesListQuery, useApListQuery }                      from '@acx-ui/rc/services'
+import { useGetClientListQuery, useVenuesListQuery, useApListQuery, useNetworkListQuery } from '@acx-ui/rc/services'
 import { ClientList, getDeviceTypeIcon, getOsTypeIcon, TableQuery, usePollingTableQuery } from '@acx-ui/rc/utils'
 import { TenantLink, useParams }                                                          from '@acx-ui/react-router-dom'
 import { RequestPayload }                                                                 from '@acx-ui/types'
@@ -41,6 +41,20 @@ function GetApFilterOptions (tenantId: string|undefined, venueId: string|undefin
     })
   })
   return apFilterOptions
+}
+
+function GetNetworkFilterOptions (tenantId: string|undefined) {
+  const { networkFilterOptions } = useNetworkListQuery({ params: { tenantId }, payload: {
+    fields: ['name', 'id'],
+    pageSize: 10000,
+    sortField: 'name',
+    sortOrder: 'ASC'
+  } }, {
+    selectFromResult: ({ data }) => ({
+      networkFilterOptions: data?.data?.map(v=>({ key: v.id, value: v.name })) || true
+    })
+  })
+  return networkFilterOptions
 }
 
 function GetCols (intl: ReturnType<typeof useIntl>, showAllColumns?: boolean) {
@@ -197,6 +211,8 @@ function GetCols (intl: ReturnType<typeof useIntl>, showAllColumns?: boolean) {
       title: intl.$t({ defaultMessage: 'Network' }),
       dataIndex: 'ssid',
       sorter: true,
+      filterKey: 'ssid',
+      filterable: networkId ? false : GetNetworkFilterOptions(tenantId),
       render: (_: React.ReactNode, row: ClientList) => {
         if (!row.healthCheckStatus) {
           return row.ssid
