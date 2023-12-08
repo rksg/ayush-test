cac9c1447e0180bac031ca3b9933a3ed6478d224
Jacky Chiu <jacky.chiu@commscope.com>
2023-12-08 16:03:11 +0800

    ACX-46503: Refactor AAA form

--- apps/rc/src/pages/Policies/AAA/AAAForm/AAAForm.tsx
+++ apps/rc/src/pages/Policies/AAA/AAAForm/AAAForm.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState, useMemo } from 'react'
+import { useRef, useEffect, useState } from 'react'
 
 import { useIntl } from 'react-intl'
 
@@ -11,17 +11,14 @@ import { useAddAAAPolicyTemplateMutation }                                      
 import { useAaaPolicyQuery, useAddAAAPolicyMutation, useUpdateAAAPolicyMutation } from '@acx-ui/rc/services'
 import {
   AAAPolicyType,
-  CONFIG_TEMPLATE_LIST_PATH,
-  generateConfigTemplateBreadcrumb,
   generatePolicyPageHeaderTitle,
-  getPolicyListRoutePath,
-  getPolicyRoutePath,
   PolicyOperation,
   PolicyType,
-  policyTypeLabelMapping,
-  useConfigTemplate
+  useConfigTemplate,
+  usePolicyBreadcrumb,
+  usePolicyInstanceListPath
 } from '@acx-ui/rc/utils'
-import { useNavigate, useParams, useTenantLink } from '@acx-ui/react-router-dom'
+import { useNavigate, useParams } from '@acx-ui/react-router-dom'
 
 import AAASettingForm from './AAASettingForm'
 
@@ -35,27 +32,23 @@ type AAAFormProps = {
 const AAAForm = (props: AAAFormProps) => {
   const { $t } = useIntl()
   const navigate = useNavigate()
-  const linkToInstanceList = useInstanceListPath()
+  const linkToInstanceList = usePolicyInstanceListPath(PolicyType.AAA, PolicyOperation.LIST)
   const params = useParams()
   const { type, edit, networkView, backToNetwork } = props
   const isEdit = edit && !networkView
   const formRef = useRef<StepsFormLegacyInstance<AAAPolicyType>>()
   const { data } = useAaaPolicyQuery({ params }, { skip: !isEdit })
   const { isTemplate } = useConfigTemplate()
+  const breadcrumb = usePolicyBreadcrumb(PolicyType.AAA, PolicyOperation.LIST)
   const createAAAPolicy = useAddInstance()
-  const breadcrumb = useBreadcrumb()
-
   const [ updateAAAPolicy ] = useUpdateAAAPolicyMutation()
   const [saveState, updateSaveState] = useState<AAAPolicyType>({ name: '' })
-  const updateSaveData = (saveData: Partial<AAAPolicyType>) => {
-    updateSaveState({ ...saveState, ...saveData })
-  }
 
   useEffect(() => {
     if (data) {
       formRef?.current?.resetFields()
       formRef?.current?.setFieldsValue(data)
-      updateSaveData(data)
+      updateSaveState({ ...saveState, ...data })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data])
@@ -110,32 +103,4 @@ function useAddInstance () {
   const [ createAAAPolicyTemplate ] = useAddAAAPolicyTemplateMutation()
 
   return isTemplate ? createAAAPolicyTemplate : createAAAPolicy
-}
-
-function useBreadcrumb () {
-  const { isTemplate } = useConfigTemplate()
-  const tablePath = getPolicyRoutePath({ type: PolicyType.AAA, oper: PolicyOperation.LIST })
-  const { $t } = useIntl()
-  const breadcrumb = useMemo(() => {
-    return isTemplate
-      ? generateConfigTemplateBreadcrumb()
-      : [
-        { text: $t({ defaultMessage: 'Network Control' }) },
-        {
-          text: $t({ defaultMessage: 'Policies & Profiles' }),
-          link: getPolicyListRoutePath(true)
-        },
-        { text: $t(policyTypeLabelMapping[PolicyType.AAA]), link: tablePath }
-      ]
-  }, [isTemplate])
-
-  return breadcrumb
-}
-
-function useInstanceListPath () {
-  const { isTemplate } = useConfigTemplate()
-  const aaaTablePath = getPolicyRoutePath({ type: PolicyType.AAA, oper: PolicyOperation.LIST })
-
-  // eslint-disable-next-line max-len
-  return useTenantLink(isTemplate ? CONFIG_TEMPLATE_LIST_PATH : aaaTablePath, isTemplate ? 'v' : 't')
 }
--- libs/rc/shared/utils/src/features/policy/policyPageUtils.ts
+++ libs/rc/shared/utils/src/features/policy/policyPageUtils.ts
@@ -1,12 +1,46 @@
-import { getIntl } from '@acx-ui/utils'
+import { useMemo } from 'react'
 
-import { generatePageHeaderTitle } from '../../pages'
-import { PolicyType }              from '../../types'
+import { useIntl } from 'react-intl'
 
-import { policyTypeLabelMapping } from './contentsMap'
+import { useTenantLink } from '@acx-ui/react-router-dom'
+import { getIntl }       from '@acx-ui/utils'
+
+import { CONFIG_TEMPLATE_LIST_PATH, generateConfigTemplateBreadcrumb, useConfigTemplate } from '../../configTemplate'
+import { generatePageHeaderTitle }                                                        from '../../pages'
+import { PolicyType }                                                                     from '../../types'
+
+import { policyTypeLabelMapping }                                      from './contentsMap'
+import { PolicyOperation, getPolicyListRoutePath, getPolicyRoutePath } from './policyRouteUtils'
 
 // eslint-disable-next-line max-len
 export function generatePolicyPageHeaderTitle (isEdit: boolean, isTemplate: boolean, policyType: PolicyType) {
   const { $t } = getIntl()
   return generatePageHeaderTitle(isEdit, isTemplate, $t(policyTypeLabelMapping[policyType]))
+}
+
+export function usePolicyBreadcrumb (type: PolicyType, oper: PolicyOperation) {
+  const { isTemplate } = useConfigTemplate()
+  const tablePath = getPolicyRoutePath({ type, oper })
+  const { $t } = useIntl()
+  const breadcrumb = useMemo(() => {
+    return isTemplate
+      ? generateConfigTemplateBreadcrumb()
+      : [
+        { text: $t({ defaultMessage: 'Network Control' }) },
+        {
+          text: $t({ defaultMessage: 'Policies & Profiles' }),
+          link: getPolicyListRoutePath(true)
+        },
+        { text: $t(policyTypeLabelMapping[type]), link: tablePath }
+      ]
+  }, [isTemplate])
+
+  return breadcrumb
+}
+
+export function usePolicyInstanceListPath (type: PolicyType, oper: PolicyOperation) {
+  const { isTemplate } = useConfigTemplate()
+  const tablePath = getPolicyRoutePath({ type, oper })
+
+  return useTenantLink(isTemplate ? CONFIG_TEMPLATE_LIST_PATH : tablePath, isTemplate ? 'v' : 't')
 }
