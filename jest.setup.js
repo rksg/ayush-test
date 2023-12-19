55f5c7b7e385590241ab14207611a1424bf55717
TLIN4 <Tsaihuei.Lin@commscope.com>
2023-12-19 14:24:11 +0800

    ACX-48414: Fix test warnings

--- jest.setup.js
+++ jest.setup.js
@@ -142,7 +142,6 @@ window.crypto = {
 window.open = jest.fn()
 window.scrollTo = jest.fn()
 console.log = jest.fn()
-console.warn = jest.fn()
 
 jest.mock('libs/common/components/src/theme/helper', () => ({
   __esModule: true,
