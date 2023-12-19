fd7145a7eb999f5f4c2cc2b932b4889ca01ae04a
Eric Leu <eric.leu@commscope.com>
2023-12-19 10:23:05 -0800

    ACX-41369: add test coverage

--- apps/main/src/AllRoutes.spec.tsx
+++ apps/main/src/AllRoutes.spec.tsx
@@ -21,11 +21,6 @@ jest.mock('@acx-ui/rc/services', () => ({
   useGetPreferencesQuery: () => ({ data: {} }),
   useGetTenantDetailsQuery: () => ({ data: {} })
 }))
-jest.mock('@acx-ui/msp/services', () => ({
-  ...jest.requireActual('@acx-ui/msp/services'),
-  useStreamActivityMessagesQuery: jest.fn(),
-  useInviteCustomerListQuery: () => ({ data: {} })
-}))
 jest.mock('@acx-ui/main/components', () => ({
   ...jest.requireActual('@acx-ui/main/components'),
   LicenseBanner: () => <div data-testid='license-banner' />,
