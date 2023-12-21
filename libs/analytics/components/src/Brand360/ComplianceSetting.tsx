3e2b0c262e3e10298d16fb7b85228157e1932dab
khairul.iman <khairul.iman@commscope.com>
2023-12-21 23:28:35 +0800

    MLSA-6939: fix: fix textarea test id attribute

--- libs/analytics/components/src/Brand360/ComplianceSetting.tsx
+++ libs/analytics/components/src/Brand360/ComplianceSetting.tsx
@@ -62,7 +62,7 @@ export function ComplianceSetting ({ settings }: { settings: Settings }) {
               />
             </>}
             children={<textarea
-              data-testId='ssidRegex'
+              data-testid='ssidRegex'
               style={{ width: '100%', outline: 'none' }}
               rows={5}
               defaultValue={ssidPattern || ssidRegex}
