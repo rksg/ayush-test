f5bbbe84bfa063db061781963f5db39440e3187b
khairul.iman <khairul.iman@commscope.com>
2023-12-22 10:08:06 +0800

    MLSA-6933: refactor: simplify all timerange for overall query

--- libs/analytics/components/src/Brand360/helpers.spec.ts
+++ libs/analytics/components/src/Brand360/helpers.spec.ts
@@ -1,5 +1,3 @@
-import { DateRange } from '@acx-ui/utils'
-
 import { computePastRange } from './helpers'
 
 
@@ -7,38 +5,43 @@ describe('helpers', () => {
   describe('computePastRange', () => {
     it('should handle last8hours', () => {
       const startDate = '2023-12-12T00:00:00+00:00'
-      const range = computePastRange(startDate, DateRange.last8Hours)
+      const endDate = '2023-12-12T08:00:00+00:00'
+      const range = computePastRange(startDate, endDate)
       expect(range[0]).toMatch('2023-12-11T16:00:00+00:00')
       expect(range[1]).toMatch(startDate)
     })
     it('should handle last24hours', () => {
       const startDate = '2023-12-12T00:00:00+00:00'
-      const range = computePastRange(startDate, DateRange.last24Hours)
+      const endDate = '2023-12-13T00:00:00+00:00'
+      const range = computePastRange(startDate, endDate)
       expect(range[0]).toMatch('2023-12-11T00:00:00+00:00')
       expect(range[1]).toMatch(startDate)
     })
     it('should handle last7days', () => {
       const startDate = '2023-12-12T00:00:00+00:00'
-      const range = computePastRange(startDate, DateRange.last7Days)
+      const endDate = '2023-12-19T00:00:00+00:00'
+      const range = computePastRange(startDate, endDate)
       expect(range[0]).toMatch('2023-12-05T00:00:00+00:00')
       expect(range[1]).toMatch(startDate)
     })
     it('should handle last30days', () => {
       const startDate = '2023-12-12T00:00:00+00:00'
-      const range = computePastRange(startDate, DateRange.last30Days)
+      const endDate = '2024-01-11T00:00:00+00:00'
+      const range = computePastRange(startDate, endDate)
       expect(range[0]).toMatch('2023-11-12T00:00:00+00:00')
       expect(range[1]).toMatch(startDate)
     })
     it('should handle custom', () => {
       const startDate = '2023-12-10T00:00:00+00:00'
       const endDate = '2023-12-12T00:00:00+00:00'
-      const range = computePastRange(startDate, DateRange.custom, endDate)
+      const range = computePastRange(startDate, endDate)
       expect(range[0]).toMatch('2023-12-08T00:00:00+00:00')
       expect(range[1]).toMatch(startDate)
     })
     it('should handle allTime', () => {
       const startDate = '2023-12-12T00:00:00+00:00'
-      const range = computePastRange(startDate, DateRange.allTime)
+      const endDate = '2024-03-12T00:00:00+00:00'
+      const range = computePastRange(startDate, endDate)
       expect(range[0]).toMatch('2023-09-12T00:00:00+00:00')
       expect(range[1]).toMatch(startDate)
     })
