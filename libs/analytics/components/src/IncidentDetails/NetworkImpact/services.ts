92cfa30b472f2e4ce551de11810c248f0df553ce
TCHEN7 <tingan.chen@commscope.com>
2023-12-26 14:22:50 +0800

    MLSA-6887: chore: rename getPeak to peak, getDistribution to distribution

--- libs/analytics/components/src/IncidentDetails/NetworkImpact/services.ts
+++ libs/analytics/components/src/IncidentDetails/NetworkImpact/services.ts
@@ -46,10 +46,10 @@ export const networkImpactChartsApi = dataApi.injectEndpoints({
           switch(query){
             case NetworkImpactQueryTypes.Distribution:
               return [
-                gql`${chart}: getDistribution(by: "${dimension}", type: "${type}") {
+                gql`${chart}: distribution(by: "${dimension}", type: "${type}") {
                   summary data { key value }
                 }`,
-                gql`${chart}Peak: getPeak(by: "${dimension}", type: "${type}")`
+                gql`${chart}Peak: peak(by: "${dimension}", type: "${type}")`
               ]
             case  NetworkImpactQueryTypes.TopN:
             default:
