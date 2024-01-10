4887fec611e760f236f409210e351e193f567d00
KYEH <karen.yeh@commscope.com>
2024-01-10 14:36:54 +0800

    ACX-50118: Revised firmware status description

--- apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/VenueFirmwareList/VenueStatusDrawer/index.tsx
+++ apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/VenueFirmwareList/VenueStatusDrawer/index.tsx
@@ -95,7 +95,7 @@ export function VenueStatusDrawer (props: VenueStatusDrawerProps) {
             [SwitchFwStatusEnum.FW_UPD_FAIL]:
               $t({ defaultMessage: 'Firmware Update - Failed' }),
             [SwitchFwStatusEnum.FW_UPD_WAITING_RESPONSE]:
-              $t({ defaultMessage: 'Firmware Upgrade - Waiting for Switch Response' }),
+              $t({ defaultMessage: 'Firmware Upgrade - Awaiting Response from Switch' }),
             [SwitchStatusEnum.DISCONNECTED]:
               $t({ defaultMessage: 'Disconnected from cloud' })
           }
