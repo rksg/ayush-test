f03753da9c86dfef6cd72291bbd86c7a1317069b
khairul.iman <khairul.iman@commscope.com>
2024-01-18 11:28:52 +0800

    MLSA-7095: feat: add tooltip to users page in RAI

--- apps/ra/src/pages/Users/Table.spec.tsx
+++ apps/ra/src/pages/Users/Table.spec.tsx
@@ -1,9 +1,9 @@
 import '@testing-library/react'
-import { ManagedUser }    from '@acx-ui/analytics/utils'
-import { Provider }       from '@acx-ui/store'
-import { render, screen } from '@acx-ui/test-utils'
+import { Provider }                          from '@acx-ui/store'
+import { findTBody, render, screen, within } from '@acx-ui/test-utils'
 
-import { UsersTable } from './Table'
+import { mockMangedUsers } from './__fixtures__'
+import { UsersTable }      from './Table'
 
 jest.mock('@acx-ui/analytics/utils', () => ({
   ...jest.requireActual('@acx-ui/analytics/utils'),
@@ -12,87 +12,9 @@ jest.mock('@acx-ui/analytics/utils', () => ({
   }))
 }))
 
-const data: ManagedUser[] = [
-  {
-    id: '1',
-    firstName: 'firstName dog1',
-    lastName: 'lastName dog1',
-    email: 'dog1@ruckuswireless.com.uat',
-    accountId: '0015000000GlI7SAAV',
-    accountName: 'RUCKUS NETWORKS, INC',
-    role: 'admin' as const,
-    tenantId: '0015000000GlI7SAAV',
-    resourceGroupId: '087b6de8-953f-405e-b2c2-000000000000',
-    resourceGroupName: 'default',
-    updatedAt: '2023-09-22T07:31:11.844Z',
-    type: null,
-    invitation: null
-  },
-  {
-    id: '0032h00000LUqTmAAL',
-    firstName: 'FisrtName 1062',
-    lastName: 'LastName 1062',
-    email: 'dog1062@email.com',
-    accountId: '0012h00000NrlBdAAJ',
-    accountName: 'Dog Company 1062',
-    role: 'network-admin' as const,
-    tenantId: '0015000000GlI7SAAV',
-    resourceGroupId: '1aac20e7-c7db-4bab-a9d4-28b26995c691',
-    resourceGroupName: 'bugbash-12',
-    type: 'tenant',
-    invitation: {
-      state: 'accepted',
-      inviterUser: {
-        firstName: 'userFirst',
-        lastName: 'userSecond'
-      }
-    }
-  },
-  {
-    id: '0032h00000LUqYCAA1',
-    firstName: 'FisrtName 12',
-    lastName: 'LastName 12',
-    email: 'dog12@email.com',
-    accountId: '0012h00000NrlKrAAJ',
-    accountName: 'Dog Company 12',
-    role: 'report-only' as const,
-    tenantId: '0015000000GlI7SAAV',
-    resourceGroupId: '087b6de8-953f-405e-b2c2-000000000000',
-    resourceGroupName: 'default',
-    type: 'super-tenant',
-    invitation: {
-      state: 'pending',
-      inviterUser: {
-        firstName: 'userThird',
-        lastName: 'userFourth'
-      }
-    }
-  },
-  {
-    id: 'rejected user',
-    firstName: 'FisrtName rej',
-    lastName: 'LastName rej',
-    email: 'dog12@email.com',
-    accountId: '0012h00000NrlKrAAJ',
-    accountName: 'Dog Company 12',
-    role: 'report-only' as const,
-    tenantId: '0015000000GlI7SAAV',
-    resourceGroupId: '087b6de8-953f-405e-b2c2-000000000000',
-    resourceGroupName: 'default',
-    type: 'super-tenant',
-    invitation: {
-      state: 'rejected',
-      inviterUser: {
-        firstName: 'userRej',
-        lastName: 'userRej'
-      }
-    }
-  }
-]
-
 describe('UsersTable', () => {
   it('should render table correctly', async () => {
-    render(<UsersTable data={data} />, { wrapper: Provider })
+    render(<UsersTable data={mockMangedUsers} />, { wrapper: Provider })
     expect(await screen.findByText('firstName dog1')).toBeVisible()
     expect(await screen.findByText('FisrtName 1062')).toBeVisible()
     expect(await screen.findByText('FisrtName 12')).toBeVisible()
@@ -100,6 +22,7 @@ describe('UsersTable', () => {
   })
   it('should render undefined data table correctly', async () => {
     render(<UsersTable data={undefined} />, { wrapper: Provider })
-    expect(await screen.findByText('No Data')).toBeVisible()
+    const tbody = await findTBody()
+    expect(await within(tbody).findAllByRole('row')).toHaveLength(1)
   })
 })
--- apps/ra/src/pages/Users/index.tsx
+++ apps/ra/src/pages/Users/index.tsx
@@ -1,12 +1,23 @@
 import { defineMessage, useIntl } from 'react-intl'
 
-import { useGetUsersQuery }   from '@acx-ui/analytics/services'
-import { Loader, PageHeader } from '@acx-ui/components'
+import { useGetUsersQuery }            from '@acx-ui/analytics/services'
+import { Loader, PageHeader, Tooltip } from '@acx-ui/components'
 
 import { UsersTable } from './Table'
 
 const title = defineMessage({
   defaultMessage: '{usersCount, plural, one {User} other {Users}}'
+})
+
+const info = defineMessage({
+  defaultMessage: `"Invite 3rd Party" allows you to invite a user who does not
+  belong to your organisation into this RUCKUS AI account.
+  {br}
+  "Add internal User" allows you to include a user who belongs to your
+  organisation into this RUCKUS AI account.
+  {br}
+  In all cases, please note that the invitee needs to have an existing
+  Ruckus Support account.`
 })
 
 export default function Users () {
@@ -15,7 +26,10 @@ export default function Users () {
   const usersCount = usersQuery.data?.length || 0
   return <Loader states={[usersQuery]}>
     <PageHeader
-      title={<>{$t(title,{ usersCount })} ({usersCount})</>}
+      title={<>
+        {$t(title,{ usersCount })} ({usersCount})
+        <Tooltip.Info title={$t(info, { br: <br/> })} />
+      </>}
     />
     <UsersTable data={usersQuery.data} />
   </Loader>
