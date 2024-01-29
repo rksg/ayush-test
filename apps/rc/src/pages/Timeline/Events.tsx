488838813e47d755799c6cfc0a7297474d37c0ec
sb1212 <Suraj.Bande@commscope.com>
2024-01-29 15:45:33 +0530

    ACX-50933: search string to array

--- apps/rc/src/pages/Timeline/Events.tsx
+++ apps/rc/src/pages/Timeline/Events.tsx
@@ -2,6 +2,8 @@ import { EventTable, useEventsTableQuery } from '@acx-ui/rc/components'
 
 const Events = () => {
   const tableQuery = useEventsTableQuery()
-  return <EventTable settingsId='timeline-event-table' tableQuery={tableQuery}/>
+  return <EventTable settingsId='timeline-event-table'
+    tableQuery={tableQuery}
+    showScheduleExport={true}/>
 }
 export { Events }
