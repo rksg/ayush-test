5a65441cbf708be43ab90e13ab96821f78ffcb18
yw1046 <Yc.Wong@commscope.com>
2024-02-20 15:36:41 +0800

    ACX-52382: Apply hasAccess() to determine the access rights for redirecting incidents

--- libs/analytics/components/src/IncidentsDashboardv2/styledComponents.tsx
+++ libs/analytics/components/src/IncidentsDashboardv2/styledComponents.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.div<{ hasAccess: boolean }>`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   svg {
-    cursor: pointer
+    cursor: ${(props) => props.hasAccess ? 'pointer' : 'auto'}
   }
 `
