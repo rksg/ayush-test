3f33082d7d133122c91c5718cfb1d5c0592f1fc1
wl1046 <wilson.loh@commscope.com>
2024-02-22 16:00:24 +0800

    MLSA-7086: added CRRMStates to states file

--- libs/analytics/components/src/Recommendations/states.ts
+++ libs/analytics/components/src/Recommendations/states.ts
@@ -1,3 +1,13 @@
+export enum CRRMStates {
+  optimized = 'optimized',
+  nonOptimized = 'nonOptimized',
+  verified = 'verified',
+  insufficientLicenses = 'insufficientLicenses',
+  unqualifiedZone = 'unqualifiedZone',
+  noAps = 'noAps',
+  verificationError = 'verificationError'
+}
+
 export const states = {
   new: 'new',
   applyScheduled: 'applyscheduled',
