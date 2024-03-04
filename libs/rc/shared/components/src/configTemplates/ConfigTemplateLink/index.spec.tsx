72ea22251d67a89ad6f14d9711475173bb009c74
Jacky Chiu <jacky.chiu@commscope.com>
2024-03-04 12:01:09 +0800

    ACX-51210: Add unit tests to rc-components for the coverage threshold

--- libs/rc/shared/components/src/configTemplates/ConfigTemplateLink/index.spec.tsx
+++ libs/rc/shared/components/src/configTemplates/ConfigTemplateLink/index.spec.tsx
@@ -3,22 +3,31 @@ import {
   getConfigTemplatePath,
   getPolicyDetailsLink,
   getPolicyRoutePath,
+  getServiceDetailsLink,
   PolicyDetailsLinkProps,
   PolicyOperation,
-  PolicyType
+  PolicyType,
+  ServiceDetailsLinkProps,
+  ServiceOperation,
+  ServiceType
 } from '@acx-ui/rc/utils'
-import { render, screen } from '@acx-ui/test-utils'
+import { TenantType, To } from '@acx-ui/react-router-dom'
+import { render, renderHook, screen } from '@acx-ui/test-utils'
 
 import {
   ConfigTemplateLink,
   PolicyConfigTemplateDetailsLink,
   PolicyConfigTemplateLink,
-  renderConfigTemplateDetailsComponent
+  renderConfigTemplateDetailsComponent,
+  ServiceConfigTemplateLinkSwitcher,
+  usePathBasedOnConfigTemplate
 } from '.'
 
 const mockedMspTenantLinkStateFn = jest.fn()
 const mockedLocation = { pathname: 'previous/path' }
 const mockedLocationFn = jest.fn().mockReturnValue(mockedLocation)
+// eslint-disable-next-line max-len
+const mockedUseTenantLink = jest.fn().mockImplementation((to: To, tenantType: TenantType) => ({ to, tenantType }))
 jest.mock('@acx-ui/react-router-dom', () => ({
   ...jest.requireActual('@acx-ui/react-router-dom'),
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -26,12 +35,26 @@ jest.mock('@acx-ui/react-router-dom', () => ({
     mockedMspTenantLinkStateFn(props.state)
     return <div>{props.to}</div>
   },
-  useLocation: () => mockedLocationFn()
+  TenantLink: (props: { to: string }) => <div>{props.to}</div>,
+  useLocation: () => mockedLocationFn(),
+  useTenantLink: (to: To, tenantType: TenantType) => mockedUseTenantLink(to, tenantType)
+}))
+
+const mockedUseConfigTemplate = jest.fn()
+jest.mock('@acx-ui/rc/utils', () => ({
+  ...jest.requireActual('@acx-ui/rc/utils'),
+  useConfigTemplate: () => mockedUseConfigTemplate()
 }))
 
 describe('ConfigTemplateLink', () => {
+  beforeEach(() => {
+    mockedUseConfigTemplate.mockReturnValue({ isTemplate: false })
+  })
+
   afterEach(() => {
     mockedMspTenantLinkStateFn.mockClear()
+    mockedUseConfigTemplate.mockRestore()
+    mockedUseTenantLink.mockClear()
   })
 
   it('renders ConfigTemplateLink with state prop', () => {
@@ -87,5 +110,51 @@ describe('ConfigTemplateLink', () => {
 
     const targetPath = getConfigTemplatePath('venues/venue_id/venue-details/services')
     expect(screen.getByText(targetPath)).toBeInTheDocument()
+  })
+
+  it('should render the correct service link with the config template flag', async () => {
+    mockedUseConfigTemplate.mockReturnValue({ isTemplate: true })
+
+    // eslint-disable-next-line max-len
+    const targetProps: ServiceDetailsLinkProps = { type: ServiceType.DHCP, oper: ServiceOperation.EDIT, serviceId: '123456' }
+    const targetPath = getServiceDetailsLink(targetProps)
+
+    const { rerender } = render(<ServiceConfigTemplateLinkSwitcher
+      type={targetProps.type}
+      oper={targetProps.oper}
+      serviceId={targetProps.serviceId}
+      children={'DHCP Edit Page'}
+    />, { route: { path: '/tenantId/t/test' } })
+
+    expect(await screen.findByText(`configTemplates/${targetPath}`)).toBeInTheDocument()
+
+    mockedUseConfigTemplate.mockReturnValue({ isTemplate: false })
+
+    rerender(<ServiceConfigTemplateLinkSwitcher
+      type={targetProps.type}
+      oper={targetProps.oper}
+      serviceId={targetProps.serviceId}
+      children={'DHCP Edit Page'}
+    />)
+
+    expect(await screen.findByText(targetPath)).toBeInTheDocument()
+  })
+
+  it('should render the correct base path with the config template flag', async () => {
+    mockedUseConfigTemplate.mockReturnValue({ isTemplate: true })
+    // eslint-disable-next-line max-len
+    const { result: templateResult } = renderHook(() => usePathBasedOnConfigTemplate('regular', 'template'))
+    expect(templateResult.current).toEqual({
+      to: 'configTemplates/template',
+      tenantType: 'v'
+    })
+
+    mockedUseConfigTemplate.mockReturnValue({ isTemplate: false })
+    // eslint-disable-next-line max-len
+    const { result: regularResult } = renderHook(() => usePathBasedOnConfigTemplate('regular', 'template'))
+    expect(regularResult.current).toEqual({
+      to: 'regular',
+      tenantType: undefined
+    })
   })
 })
