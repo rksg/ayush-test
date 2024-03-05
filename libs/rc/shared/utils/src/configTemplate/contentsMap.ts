5eb0dbcfa9bb13084564922a98efff921fe7df4e
JSYU <jeffrey.syu@commscope.com>
2024-03-04 20:01:54 -0800

    ACX-51168: remove the unnecessary configTemplateType

--- libs/rc/shared/utils/src/configTemplate/contentsMap.ts
+++ libs/rc/shared/utils/src/configTemplate/contentsMap.ts
@@ -3,11 +3,7 @@ import { PolicyType, ConfigTemplateType } from '../types'
 
 export const configTemplatePolicyTypeMap: Partial<Record<ConfigTemplateType, PolicyType>> = {
   [ConfigTemplateType.RADIUS]: PolicyType.AAA,
-  [ConfigTemplateType.ACCESS_CONTROL_SET]: PolicyType.ACCESS_CONTROL,
-  [ConfigTemplateType.LAYER_2_POLICY]: PolicyType.LAYER_2_POLICY,
-  [ConfigTemplateType.LAYER_3_POLICY]: PolicyType.LAYER_3_POLICY,
-  [ConfigTemplateType.DEVICE_POLICY]: PolicyType.DEVICE_POLICY,
-  [ConfigTemplateType.APPLICATION_POLICY]: PolicyType.APPLICATION_POLICY
+  [ConfigTemplateType.ACCESS_CONTROL_SET]: PolicyType.ACCESS_CONTROL
 }
 
 export const configTemplateServiceTypeMap: Partial<Record<ConfigTemplateType, ServiceType>> = {
