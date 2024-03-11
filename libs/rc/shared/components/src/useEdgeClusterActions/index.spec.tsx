a52a400b15022b8bfdf5825174aa517ba9e1d6b8
Hsiao, Cooper <cooper.hsiao@commscope.com>
2024-03-11 09:58:33 +0800

    COMMENT=add test case DEV_TREE= feature/ACX-53988 CODEBASE=SCG BUG_NUMBER= ACX-53988 COMPONENT= UNIT_TESTS=yes SSR_ID=

--- libs/rc/shared/components/src/useEdgeClusterActions/index.spec.tsx
+++ libs/rc/shared/components/src/useEdgeClusterActions/index.spec.tsx
@@ -11,6 +11,7 @@ const mockedDeleteApi = jest.fn()
 const mockedBuckDeleteApi = jest.fn()
 const mockedDeleteClusterApi = jest.fn()
 const mockedRebootApi = jest.fn()
+const mockedSendOtp = jest.fn()
 
 const { mockEdgeList, mockEdgeClusterList } = EdgeGeneralFixtures
 
@@ -40,6 +41,13 @@ describe('useEdgeClusterActions', () => {
         EdgeUrlsInfo.reboot.url,
         (req, res, ctx) => {
           mockedRebootApi()
+          return res(ctx.status(202))
+        }
+      ),
+      rest.patch(
+        EdgeUrlsInfo.sendOtp.url,
+        (req, res, ctx) => {
+          mockedSendOtp()
           return res(ctx.status(202))
         }
       )
@@ -94,6 +102,34 @@ describe('useEdgeClusterActions', () => {
     })
     await waitFor(() => {
       expect(mockedDeleteClusterApi).toBeCalledTimes(2)
+    })
+    await waitFor(() => {
+      expect(mockedCallback).toBeCalledTimes(1)
+    })
+    await waitFor(() => {
+      expect(screen.queryByRole('dialog')).toBeNull()
+    })
+  })
+
+  it('should send otp successfully', async () => {
+    const mockedCallback = jest.fn()
+    const { result } = renderHook(() => useEdgeClusterActions(), {
+      wrapper: ({ children }) => <Provider children={children} />
+    })
+
+    const { sendEdgeOnboardOtp } = result.current
+    const clusterData = mockEdgeClusterList.data
+      .map(item => ({ ...item, isFirstLevel: true })).slice(2,4)
+    const nodeData = mockEdgeList.data[4]
+    sendEdgeOnboardOtp(
+      [...clusterData, nodeData] as EdgeClusterTableDataType[],
+      mockedCallback
+    )
+    const dialog = await screen.findByRole('dialog')
+    expect(dialog).toHaveTextContent('Are you sure you want to send OTP?')
+    await userEvent.click(within(dialog).getByRole('button', { name: 'OK' }))
+    await waitFor(() => {
+      expect(mockedSendOtp).toBeCalledTimes(1)
     })
     await waitFor(() => {
       expect(mockedCallback).toBeCalledTimes(1)
