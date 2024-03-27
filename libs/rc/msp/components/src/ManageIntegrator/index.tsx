0759787f3322af5ee746abefa4d5bef55711676e
Eric Leu <eric.leu@commscope.com>
2024-03-27 13:12:16 -0700

    ACX-53527: [UI] - MSP: Manage EC admin delegation based on privilege group

--- libs/rc/msp/components/src/ManageIntegrator/index.tsx
+++ libs/rc/msp/components/src/ManageIntegrator/index.tsx
@@ -519,7 +519,9 @@ export function ManageIntegrator () {
     return <>
       {mspAdmins.map(admin =>
         <UI.AdminList key={admin.id}>
-          {admin.email} ({intl.$t(roleDisplayText[admin.role])})
+          {admin.email} ({roleDisplayText[admin.role]
+            ? intl.$t(roleDisplayText[admin.role]) : admin.role})
+
         </UI.AdminList>
       )}
     </>
@@ -569,14 +571,20 @@ export function ManageIntegrator () {
             ? intl.$t({ defaultMessage: 'Privilege Group' })
             : intl.$t({ defaultMessage: 'Role' })}
         >
-          <Paragraph>{intl.$t(roleDisplayText[mspEcAdmins[0].role])}</Paragraph>
+          <Paragraph>
+            {/* {intl.$t(roleDisplayText[mspEcAdmins[0].role])} */}
+            {roleDisplayText[mspEcAdmins[0].role]
+              ? intl.$t(roleDisplayText[mspEcAdmins[0].role]) : mspEcAdmins[0].role}
+          </Paragraph>
         </Form.Item>
       </>
     }
     return <div style={{ marginTop: '5px', marginBottom: '30px' }}>
       {mspEcAdmins.map(admin =>
         <UI.AdminList>
-          {admin.email} ({intl.$t(roleDisplayText[admin.role])}
+          {admin.email} {roleDisplayText[admin.role]
+            ? intl.$t(roleDisplayText[admin.role]) : admin.role}
+          {/* {admin.email} ({intl.$t(roleDisplayText[admin.role])} */}
         </UI.AdminList>
       )}
     </div>
@@ -701,7 +709,8 @@ export function ManageIntegrator () {
               Object.entries(RolesEnum).map(([label, value]) => (
                 <Option
                   key={label}
-                  value={value}>{intl.$t(roleDisplayText[value])}
+                  value={value}>
+                  {roleDisplayText[value] ? intl.$t(roleDisplayText[value]) : value}
                 </Option>
               ))
             }
@@ -964,7 +973,10 @@ export function ManageIntegrator () {
             : intl.$t({ defaultMessage: 'Role' })}
         >
           {formData?.admin_role &&
-          <Paragraph>{intl.$t(roleDisplayText[formData.admin_role as RolesEnum])}</Paragraph>}
+          <Paragraph>
+            {roleDisplayText[formData.admin_role]
+              ? intl.$t(roleDisplayText[formData.admin_role]) : formData.admin_role}
+          </Paragraph>}
         </Form.Item>
 
         {!isDeviceAgnosticEnabled && <div>
