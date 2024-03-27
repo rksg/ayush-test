ddba397528a529ce30ca96f26a27c13a69a3ebba
Sharon Chen <sharon.chen2@commscope.com>
2024-03-27 11:02:42 +0800

    ACX-41563: Update hotspot 20

--- libs/rc/shared/utils/src/models/Hotspot20FriendlyName.ts
+++
@@ -1,8 +0,0 @@
-import { Hotspot20FriendlyNameLanguageEnum } from './Hotspot20FriendlyNameLanguageEnum'
-import { Hotspot20Operator }                 from './Hotspot20Operator'
-
-export class Hotspot20FriendlyName {
-  name?: string
-  language?: Hotspot20FriendlyNameLanguageEnum
-  operator?: Hotspot20Operator
-}
--- libs/rc/shared/utils/src/models/Hotspot20Operator.ts
+++
@@ -1,8 +0,0 @@
-import { Hotspot20FriendlyName } from './Hotspot20FriendlyName'
-
-export class Hotspot20Operator {
-  name?: string
-  description?: string
-  domainNames?: string
-  friendlyNames?: Hotspot20FriendlyName
-}
