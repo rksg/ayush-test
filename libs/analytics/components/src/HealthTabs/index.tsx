019cff9912c168c0fb82307c040017306604d358
vivek.kandhvar <vivek.kandhvar@commscope.com>
2024-04-01 19:45:28 +0530

    RSA-6647: chore: fix route issue

--- libs/analytics/components/src/HealthTabs/index.tsx
+++ libs/analytics/components/src/HealthTabs/index.tsx
@@ -1,7 +1,7 @@
 import { useIntl } from 'react-intl'
 
-import { Tabs }                                from '@acx-ui/components'
-import { useLocation, useNavigate, useParams } from '@acx-ui/react-router-dom'
+import { Tabs }                                               from '@acx-ui/components'
+import { useLocation, useNavigate, useParams, useTenantLink } from '@acx-ui/react-router-dom'
 
 import { HealthPage } from '..'
 
@@ -12,12 +12,12 @@ export function HealthTabs () {
   const location = useLocation()
   const navigate = useNavigate()
   const { activeSubTab } = useParams()
+  const basePath = useTenantLink('/analytics/health/')
+
   const onTabChange = (tab: string) => {
     navigate({
       ...location,
-      pathname: activeSubTab
-        ? location.pathname.replace(activeSubTab as string, tab)
-        : `${location.pathname}/${tab}`
+      pathname: `${basePath.pathname}/${tab}`
     })
   }
   return <Tabs
