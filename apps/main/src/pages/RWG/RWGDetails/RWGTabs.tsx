acd9e95102b7ef4d12f218968323793f3ed34c22
sb1212 <Suraj.Bande@commscope.com>
2024-04-08 17:53:16 +0530

    ACX-54869: RWG DNS records code cleanup

--- apps/main/src/pages/RWG/RWGDetails/RWGTabs.tsx
+++ apps/main/src/pages/RWG/RWGDetails/RWGTabs.tsx
@@ -4,10 +4,10 @@ import { Tabs }                                  from '@acx-ui/components'
 import { RWG }                                   from '@acx-ui/rc/utils'
 import { useNavigate, useParams, useTenantLink } from '@acx-ui/react-router-dom'
 
-function RWGTabs (props:{ gatewayDetail: RWG, dnsRecordsCount: number }) {
+function RWGTabs (props:{ gatewayDetail: RWG }) {
   const { $t } = useIntl()
   const params = useParams()
-  const { gatewayDetail, dnsRecordsCount } = props
+  const { gatewayDetail } = props
   const basePath = useTenantLink(`/ruckus-wan-gateway/${gatewayDetail?.id}/gateway-details`)
   const navigate = useNavigate()
 
@@ -20,10 +20,6 @@ function RWGTabs (props:{ gatewayDetail: RWG, dnsRecordsCount: number }) {
   return (
     <Tabs onChange={onTabChange} activeKey={params.activeTab}>
       <Tabs.TabPane tab={$t({ defaultMessage: 'Overview' })} key='overview' />
-      <Tabs.TabPane
-        tab={$t({ defaultMessage: 'DNS Records ({dnsRecordsCount})' }, { dnsRecordsCount })}
-        key='dnsRecords'
-      />
     </Tabs>
   )
 }
