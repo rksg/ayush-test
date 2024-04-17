c9077b83a41d510fda1dbaa712b128df60c8ab80
KYEH <karen.yeh@commscope.com>
2024-04-17 18:17:04 +0800

    ACX-56039: Fix the latest version type

--- apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/VersionBanner/index.tsx
+++ apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmware/VersionBanner/index.tsx
@@ -1,10 +1,13 @@
+import _             from 'lodash'
 import { useIntl }   from 'react-intl'
 import { useParams } from 'react-router-dom'
 
 import { useSwitchFirmwareUtils }       from '@acx-ui/rc/components'
 import {
+  useGetSwitchDefaultFirmwareListQuery,
   useGetSwitchLatestFirmwareListQuery
 } from '@acx-ui/rc/services'
+import { FirmwareCategory } from '@acx-ui/rc/utils'
 
 import { FirmwareBanner }     from '../../FirmwareBanner'
 import { getReleaseFirmware } from '../../FirmwareUtils'
@@ -14,11 +17,25 @@ export const VersionBanner = () => {
 
   const { $t } = useIntl()
   const { data: latestReleaseVersions } = useGetSwitchLatestFirmwareListQuery({ params })
+  const { data: defaultReleaseVersions } = useGetSwitchDefaultFirmwareListQuery({ params })
   const { parseSwitchVersion } = useSwitchFirmwareUtils()
+
 
   const versions = getReleaseFirmware(latestReleaseVersions)
   const firmware = versions.filter(v => v.id.startsWith('090'))[0]
   const rodanFirmware = versions.filter(v => v.id.startsWith('100'))[0]
+
+
+
+  const isDefaultVersion = function (currentVersion: string) {
+    if(_.isEmpty(currentVersion)) return false
+    const defaultVersions = getReleaseFirmware(defaultReleaseVersions)
+    const defaultFirmware = defaultVersions.filter(v => v.id.startsWith('090'))[0]
+    const defaultRodanFirmware = defaultVersions.filter(v => v.id.startsWith('100'))[0]
+    return (currentVersion === defaultFirmware?.id ||
+      currentVersion === defaultRodanFirmware?.id)
+  }
+
 
   if (!firmware && !rodanFirmware) return null
 
@@ -29,7 +46,8 @@ export const VersionBanner = () => {
       label: $t({ defaultMessage: 'For ICX Models (8200):' }),
       firmware: {
         version: parseSwitchVersion(rodanFirmware?.name),
-        category: rodanFirmware?.category,
+        category: isDefaultVersion(rodanFirmware?.name) ?
+          FirmwareCategory.RECOMMENDED: FirmwareCategory.REGULAR,
         releaseDate: rodanFirmware?.createdDate
       }
     })
@@ -40,7 +58,8 @@ export const VersionBanner = () => {
       label: $t({ defaultMessage: 'For ICX Models (7150-7850):' }),
       firmware: {
         version: parseSwitchVersion(firmware?.name),
-        category: firmware?.category,
+        category: isDefaultVersion(rodanFirmware?.name) ?
+          FirmwareCategory.RECOMMENDED: FirmwareCategory.REGULAR,
         releaseDate: firmware?.createdDate
       }
     })
