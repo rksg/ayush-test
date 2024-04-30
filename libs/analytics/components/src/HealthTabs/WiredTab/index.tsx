cdb7ac156f2801cc849f055c6691f918739ff7ad
vivek.kandhvar <vivek.kandhvar@commscope.com>
2024-04-30 12:41:37 +0530

    RSA-6725: Add template for wired sub tabs and fix network filter path issue

--- libs/analytics/components/src/HealthTabs/WiredTab/index.tsx
+++ libs/analytics/components/src/HealthTabs/WiredTab/index.tsx
@@ -1,20 +1,51 @@
+import { useIntl } from 'react-intl'
 
-import { useAnalyticsFilter }   from '@acx-ui/analytics/utils'
-import { GridCol, GridRow }     from '@acx-ui/components'
-import type { AnalyticsFilter } from '@acx-ui/utils'
+import { useAnalyticsFilter, categoryTabs }      from '@acx-ui/analytics/utils'
+import { GridCol, GridRow, Tabs }                from '@acx-ui/components'
+import { useNavigate, useParams, useTenantLink } from '@acx-ui/react-router-dom'
+import type { AnalyticsFilter }                  from '@acx-ui/utils'
+
+import * as UI from '../../Health/styledComponents'
 
 import { SummaryBoxes } from './SummaryBoxes'
 
-const WiredTab = (props: { filters? : AnalyticsFilter }) => {
+const WiredTab = (props: { filters?: AnalyticsFilter, path?: string }) => {
+  const { $t } = useIntl()
   const { filters: widgetFilters } = props
   const { filters } = useAnalyticsFilter()
   const healthPageFilters = widgetFilters ? widgetFilters : filters
+
+  const params = useParams()
+  const selectedTab = params['categoryTab'] ?? categoryTabs[0].value
+  const navigate = useNavigate()
+  const basePath = useTenantLink(props.path ?? '/analytics/health/wired/tab/')
+
+  const onTabChange = (tab: string) =>
+    navigate({
+      ...basePath,
+      pathname: `${basePath.pathname}/${tab}`
+    })
   return (
     <GridRow>
       <GridCol col={{ span: 24 }} style={{ minHeight: '105px' }}>
         <SummaryBoxes
           filters={healthPageFilters}
         />
+      </GridCol>
+      <GridCol col={{ span: 16 }}>
+        <UI.TabTitle activeKey={selectedTab} onChange={onTabChange}>
+          {categoryTabs.map(({ value, label }) => (
+            <Tabs.TabPane tab={$t(label)} key={value} />
+          ))}
+        </UI.TabTitle>
+      </GridCol>
+      <GridCol col={{ span: 8 }}>
+        <UI.ThresholdTitle>
+          {$t({ defaultMessage: 'Customized SLA Threshold' })}
+        </UI.ThresholdTitle>
+      </GridCol>
+      <GridCol col={{ span: 24 }}>
+        KPI page go here
       </GridCol>
     </GridRow>
   )
