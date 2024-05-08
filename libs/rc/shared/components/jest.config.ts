58a7bab53134bb8714a5687ce941943a082c0c5b
JSYU <jeffrey.syu@commscope.com>
2024-05-08 03:42:06 -0700

    ACX-57625: adjust the code coverage

--- libs/rc/shared/components/jest.config.ts
+++ libs/rc/shared/components/jest.config.ts
@@ -8,10 +8,10 @@ module.exports = {
   coverageDirectory: '../../../../coverage/libs/rc/shared/components',
   coverageThreshold: {
     global: {
-      statements: 86,
-      branches: 74,
+      statements: 86.5,
+      branches: 75,
       functions: 85,
-      lines: 86
+      lines: 87
     }
   }
 }
