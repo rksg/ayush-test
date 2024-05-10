bd66a7adf2da872dab33e8efd8faa6ef2b5560f3
mdehez <mickael.dehez@commscope.com>
2024-05-10 15:40:40 +0800

    Revert "Pull request #3303: MLSA-7691: add new user roles for RAI"
    This reverts commit d17a2c84efcee209b7c35c13692dcb9e50b2a8fe, reversing
    changes made to eed2824d952b57e1c999db24a792d43d089c4563.

--- libs/analytics/components/src/Users/RoleSelection.tsx
+++ libs/analytics/components/src/Users/RoleSelection.tsx
@@ -3,7 +3,7 @@
 import { Select }  from 'antd'
 import { useIntl } from 'react-intl'
 
-import { useRoles } from '@acx-ui/analytics/utils'
+import { RolesEnum, roleStringMap } from '@acx-ui/analytics/utils'
 
 export const RoleSelection = ({
   onChange,
@@ -13,14 +13,13 @@ export const RoleSelection = ({
   onChange: (value: string) => void;
 }) => {
   const { $t } = useIntl()
-  const roles = useRoles(false)
   return (
     <Select
       style={{ width: 350 }}
-      options={Object.entries(roles).map(([role, label]) => ({
-        label: $t(label),
+      options={Object.values(RolesEnum).map((role, i) => ({
+        label: $t(roleStringMap[role]),
         value: role,
-        key: role
+        key: i
       }))}
       placeholder={$t({ defaultMessage: 'Select a role' })}
       value={selectedValue}
