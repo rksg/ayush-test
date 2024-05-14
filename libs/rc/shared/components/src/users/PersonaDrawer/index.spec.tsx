156ca7a570f29294fbedbfc7de487d78ac88a914
Jerry Chen <Jerry.Chen@commscope.com>
2024-05-14 13:54:34 +0800

    ACX-57436: Adjust test case

--- libs/rc/shared/components/src/users/PersonaDrawer/index.spec.tsx
+++ libs/rc/shared/components/src/users/PersonaDrawer/index.spec.tsx
@@ -31,8 +31,8 @@ describe('Persona Drawer', () => {
         PersonaUrls.getPersonaGroupById.url,
         (req, res, ctx) => res(ctx.json(mockPersonaGroup))
       ),
-      rest.get(
-        NewPersonaBaseUrl,
+      rest.post(
+        PersonaUrls.searchPersonaGroupList.url.split('?')[0],
         (req, res, ctx) => res(ctx.json(mockPersonaGroupList))
       ),
       rest.post(
@@ -47,8 +47,8 @@ describe('Persona Drawer', () => {
         PersonaUrls.addPersona.url,
         (req, res, ctx) => res(ctx.json({}))
       ),
-      rest.get(
-        MacRegListUrlsInfo.getMacRegistrationPool.url,
+      rest.post(
+        MacRegListUrlsInfo.searchMacRegistrationPools.url.split('?')[0],
         (req, res, ctx) => res(ctx.json(mockMacRegistration))
       ),
       rest.patch(
