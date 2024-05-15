a2169db1d69026e0d6208b880c1848891ca63437
yw1046 <Yc.Wong@commscope.com>
2024-05-15 18:39:40 +0800

    ACX-58019: Revise the storybook to provide instructions on configuring validation settings

--- libs/common/components/src/components/Input/stories.tsx
+++ libs/common/components/src/components/Input/stories.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { storiesOf } from '@storybook/react'
 import { Form }      from 'antd'
 
@@ -16,37 +18,54 @@ storiesOf('Input', module).add('Password Input', () => (
   </Wrapper>
 ))
 
-storiesOf('Input', module).add('Password Input Strength', () => (
-  <Wrapper>
-    <Form.Item
-      className='password-input-strength'
-      style={{ width: '350px' }}>
-      <PasswordInputStrength />
-    </Form.Item>
+storiesOf('Input', module).add('Password Input Strength', () => {
+  const [conditionMet, setConditionMet] = React.useState(false)
+
+  return <Wrapper>
+    <Form>
+      <Form.Item
+        name='passwordfield'
+        className='password-input-strength'
+        style={{ width: '350px' }}
+        rules={[
+          { validator: () => conditionMet ? Promise.resolve() : Promise.reject() }
+        ]}>
+        <PasswordInputStrength onConditionCountMet={setConditionMet} />
+      </Form.Item>
+    </Form>
   </Wrapper>
-))
+})
 
 
-storiesOf('Input', module).add('Password Input Strength - Custom rules', () => (
-  <Wrapper>
-    <Form.Item
-      className='password-input-strength'
-      style={{ width: '350px' }}>
-      <PasswordInputStrength
-        regExRules={[
-          /^.{8,}$/,
-          /(?=.*[a-z])(?=.*[A-Z])/,
-          /(?=.*\d)/
-        ]}
-        regExErrorMessages={[
-          '8 characters',
-          'One uppercase and one lowercase letters',
-          'One number'
-        ]}
-        isAllConditionsMet={3}
-      />
-    </Form.Item>
+storiesOf('Input', module).add('Password Input Strength - Custom rules', () => {
+  const [conditionMet, setConditionMet] = React.useState(false)
+
+  return <Wrapper>
+    <Form>
+      <Form.Item
+        name='passwordfield'
+        className='password-input-strength'
+        style={{ width: '350px' }}
+        rules={[
+          { validator: () => conditionMet ? Promise.resolve() : Promise.reject() }
+        ]}>
+        <PasswordInputStrength
+          regExRules={[
+            /^.{8,}$/,
+            /(?=.*[a-z])(?=.*[A-Z])/,
+            /(?=.*\d)/
+          ]}
+          regExErrorMessages={[
+            '8 characters',
+            'One uppercase and one lowercase letters',
+            'One number'
+          ]}
+          isAllConditionsMet={3}
+          onConditionCountMet={setConditionMet}
+        />
+      </Form.Item>
+    </Form>
   </Wrapper>
-))
+})
 
 export {}
