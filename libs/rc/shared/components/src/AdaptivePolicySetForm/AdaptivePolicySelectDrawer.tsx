c9c13ed6a80f9baec0365a7c2583a0fe49a40626
rick yang <rick.yang@commscope.com>
2024-05-15 13:48:13 +0800

    ACX-57431: added filterByAccess to AdaptivePoliciesSelectDrawer.

--- libs/rc/shared/components/src/AdaptivePolicySetForm/AdaptivePolicySelectDrawer.tsx
+++ libs/rc/shared/components/src/AdaptivePolicySetForm/AdaptivePolicySelectDrawer.tsx
@@ -14,8 +14,9 @@ import {
   PolicyOperation, SEARCH,
   useTableQuery
 } from '@acx-ui/rc/utils'
-import { TenantLink } from '@acx-ui/react-router-dom'
-import { WifiScopes } from '@acx-ui/types'
+import { TenantLink }     from '@acx-ui/react-router-dom'
+import { WifiScopes }     from '@acx-ui/types'
+import { filterByAccess } from '@acx-ui/user'
 
 import { SimpleListTooltip } from '../SimpleListTooltip'
 
@@ -175,13 +176,13 @@ export function AdaptivePoliciesSelectDrawer (props: AdaptivePoliciesSelectDrawe
             pagination={adaptivePolicyListTableQuery.pagination}
             onChange={adaptivePolicyListTableQuery.handleTableChange}
             onFilterChange={handleFilterChange}
-            actions={[{
+            actions={filterByAccess([{
               label: $t({ defaultMessage: 'Add Policy' }),
               onClick: () => {
                 setAdaptivePolicyDrawerVisible(true)
               },
               scopeKey: [WifiScopes.CREATE]
-            }]}
+            }])}
           />
         </Form.Item>
       </Loader>
