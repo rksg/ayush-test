0f990518094cdc4e893f0189bdbad4126f78d44f
JSYU <jeffrey.syu@commscope.com>
2024-05-16 01:05:51 -0700

    ACX-57625: update the apgroup test case

--- libs/rc/shared/components/src/ApGroupEdit/ApGroupGeneralTab/index.tsx
+++ libs/rc/shared/components/src/ApGroupEdit/ApGroupGeneralTab/index.tsx
@@ -179,7 +179,7 @@ export function ApGroupGeneralTab () {
         payload.apSerialNumbers = payload.apSerialNumbers.map(i => { return { serialNumber: i } })
       }
 
-      if (!editContextData.isDirty) {
+      if (!editContextData || !editContextData.isDirty) {
         if (isEditMode) {
           await updateApGroup({ params: { tenantId, apGroupId }, payload }).unwrap()
         } else {
