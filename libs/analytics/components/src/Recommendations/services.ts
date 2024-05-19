8cc3280a108ba89c14b9fed5e3bf8da535bf9d70
cbokil <chinmay.bokil@commscope.com>
2024-05-19 12:50:39 +0530

    MLSA-7767: fix: cancel rec makes the rec disappear from the list

--- libs/analytics/components/src/Recommendations/services.ts
+++ libs/analytics/components/src/Recommendations/services.ts
@@ -8,10 +8,10 @@ import {
   formattedPath,
   productNames
 } from '@acx-ui/analytics/utils'
-import { DateFormatEnum, formatter }      from '@acx-ui/formatter'
-import { recommendationApi }              from '@acx-ui/store'
-import { NodeType, getIntl, NetworkPath } from '@acx-ui/utils'
-import type { PathFilter }                from '@acx-ui/utils'
+import { DateFormatEnum, formatter }                          from '@acx-ui/formatter'
+import { recommendationApi }                                  from '@acx-ui/store'
+import { NodeType, getIntl, NetworkPath, computeRangeFilter } from '@acx-ui/utils'
+import type { PathFilter }                                    from '@acx-ui/utils'
 
 import {
   states,
@@ -402,7 +402,15 @@ export const api = recommendationApi.injectEndpoints({
           }
         }
         `,
-        variables: _.pick(payload, ['path', 'startDate', 'endDate', 'crrm'])
+        variables: {
+          ...(_.pick(payload,['path', 'crrm'])),
+          ...(_.pick(
+            computeRangeFilter({
+              dateFilter: _.pick(payload, ['startDate', 'endDate', 'range'])
+            }),
+            ['startDate', 'endDate']
+          ))
+        }
       }),
       transformResponse: (response: Response<Recommendation>) => {
         const { $t } = getIntl()
