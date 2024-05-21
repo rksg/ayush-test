36c2ba36d89614dd70a357cf102a9723774e680c
lc1158 <Lish.Chen@commscope.com>
2024-05-21 14:50:07 +0800

    ACX-57492: Add AP Client Admission Control RBAC API test

--- apps/rc/src/pages/Devices/Wifi/ApEdit/RadioTab/ClientAdmissionControlSettings/ClientAdmissionControlSettings.spec.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApEdit/RadioTab/ClientAdmissionControlSettings/ClientAdmissionControlSettings.spec.tsx
@@ -61,10 +61,16 @@ describe('Ap Client Admission Control', () => {
       rest.delete(
         WifiUrlsInfo.deleteApClientAdmissionControl.url,
         (_, res, ctx) => res(ctx.json({}))),
-      // rbac
+      // RBAC API
       rest.get(
         WifiRbacUrlsInfo.getVenueClientAdmissionControl.url,
-        (_, res, ctx) => res(ctx.json(mockVenueClientAdmissionControl)))
+        (_, res, ctx) => res(ctx.json(mockVenueClientAdmissionControl))),
+      rest.get(
+        WifiRbacUrlsInfo.getApClientAdmissionControl.url,
+        (_, res, ctx) => res(ctx.json(mockApClientAdmissionControl))),
+      rest.put(
+        WifiRbacUrlsInfo.updateApClientAdmissionControl.url,
+        (_, res, ctx) => res(ctx.json({})))
     )
   })
 
