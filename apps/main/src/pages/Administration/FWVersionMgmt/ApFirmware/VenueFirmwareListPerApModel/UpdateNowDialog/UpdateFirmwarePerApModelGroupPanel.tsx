9fafd48ace23445d5b6db49816eae45b2e1536cb
Jacky Chiu <jacky.chiu@commscope.com>
2024-05-21 10:07:04 +0800

    ACX-57412: Fix lint error

--- apps/main/src/pages/Administration/FWVersionMgmt/ApFirmware/VenueFirmwareListPerApModel/UpdateNowDialog/UpdateFirmwarePerApModelGroupPanel.tsx
+++ apps/main/src/pages/Administration/FWVersionMgmt/ApFirmware/VenueFirmwareListPerApModel/UpdateNowDialog/UpdateFirmwarePerApModelGroupPanel.tsx
@@ -9,8 +9,8 @@ import { useGetAllApModelFirmwareListQuery } from '@acx-ui/rc/services'
 import { FirmwareVenuePerApModel }           from '@acx-ui/rc/utils'
 import { getIntl }                           from '@acx-ui/utils'
 
-import { compareVersions, getVersionLabel }                from '../../../FirmwareUtils'
-import * as UI                            from '../../VenueFirmwareList/styledComponents'
+import { compareVersions, getVersionLabel } from '../../../FirmwareUtils'
+import * as UI                              from '../../VenueFirmwareList/styledComponents'
 import {
   ApFirmwareUpdateGroupType,
   convertApModelFirmwaresToUpdateGroups,
