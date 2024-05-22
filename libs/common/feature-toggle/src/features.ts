aa48716a85aaade301211d92235bb4f80378c088
jl1203 <julie.lin@commscope.com>
2024-05-22 16:58:25 +0800

    ACX-56925 Remove wifi-eda-mesh-enhancements-toggle

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -73,7 +73,6 @@ export enum Features {
   LIST_OF_CLIENTS_PER_WLAN = 'acx-ui-list-of-clients-per-wlan-toggle',
   MDNS_FENCING = 'wifi-consumer-bonjour-fencing-toggle',
   LOGIN_SSO_SIGNATURE_TOGGLE = 'login-sso-saml-signature',
-  MESH_ENHANCEMENTS = 'wifi-eda-mesh-enhancements-toggle',
   MSP_AGGREGATE_NOTIFICATION_TOGGLE = 'aggregate-notification-toggle',
   MSP_BRAND_360 = 'acx-ui-msp-brand360-toggle',
   MSP_DATA_STUDIO = 'acx-ui-msp-data-studio-toggle',
