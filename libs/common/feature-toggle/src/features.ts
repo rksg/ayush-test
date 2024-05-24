abb6446e3dec55d3948d2d076625827dcebd5925
jl1203 <julie.lin@commscope.com>
2024-05-24 16:03:29 +0800

    ACX-56924 Remove wifi-eda-additional-regulatory-domains-toggle

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -3,7 +3,6 @@ import { defineMessage, MessageDescriptor } from 'react-intl'
 /* eslint-disable max-len */
 export enum Features {
   ABAC_POLICIES_TOGGLE = 'abac-policies-toggle',
-  ADDITIONAL_REGULATORY_DOMAINS_TOGGLE = 'wifi-eda-additional-regulatory-domains-toggle',
   AI_CRRM = 'acx-ui-ai-crrm-toggle',
   AI_RECOMMENDATIONS = 'acx-ui-ai-recommendations-toggle',
   ALLOWED_OPERATIONS_TOGGLE = 'allowed-operations-toggle',
