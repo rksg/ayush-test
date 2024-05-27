c773fa2be7b44041cfc7a14f545df9fee74dc7c4
mdehez <mickael.dehez@commscope.com>
2024-05-27 10:24:10 +0800

    MLSA-7422: add health page for ithelpdesk + fix translation

--- apps/ra/src/pages/SearchResults/NoData/index.tsx
+++ apps/ra/src/pages/SearchResults/NoData/index.tsx
@@ -11,19 +11,46 @@ import * as UI from './styledComponents'
 
 const useLinkData = () => {
   const { $t } = useIntl()
-  const links: { title: string, to: string, permission: RaiPermission }[] = [
-    { title: 'Data Studio', to: '/dataStudio', permission: 'READ_DATA_STUDIO' },
-    { title: 'Reports', to: '/reports', permission: 'READ_REPORTS' },
-    { title: 'Dashboard', to: '/dashboard', permission: 'READ_DASHBOARD' },
-    { title: 'Incidents', to: '/incidents', permission: 'READ_INCIDENTS' },
-    { title: 'Clients', to: '/users/wifi/clients', permission: 'READ_WIRELESS_CLIENTS_LIST' },
-    { title: 'APs', to: '/devices/wifi', permission: 'READ_ACCESS_POINTS_LIST' },
-    { title: 'Switches', to: '/devices/switch', permission: 'READ_SWITCH_LIST' },
-    { title: 'Wi-Fi Networks', to: '/networks/wireless', permission: 'READ_WIFI_NETWORKS_LIST' }
-  ]
+  const links: { title: string, to: string, permission: RaiPermission }[] = [{
+    title: $t({ defaultMessage: 'Data Studio' }),
+    to: '/dataStudio',
+    permission: 'READ_DATA_STUDIO'
+  }, {
+    title: $t({ defaultMessage: 'Reports' }),
+    to: '/reports',
+    permission: 'READ_REPORTS'
+  }, {
+    title: $t({ defaultMessage: 'Dashboard' }),
+    to: '/dashboard',
+    permission: 'READ_DASHBOARD'
+  }, {
+    title: $t({ defaultMessage: 'Incidents' }),
+    to: '/incidents',
+    permission: 'READ_INCIDENTS'
+  }, {
+    title: $t({ defaultMessage: 'Clients' }),
+    to: '/users/wifi/clients',
+    permission: 'READ_WIRELESS_CLIENTS_LIST'
+  }, {
+    title: $t({ defaultMessage: 'Health' }),
+    to: '/health',
+    permission: 'READ_HEALTH'
+  }, {
+    title: $t({ defaultMessage: 'APs' }),
+    to: '/devices/wifi',
+    permission: 'READ_ACCESS_POINTS_LIST'
+  }, {
+    title: $t({ defaultMessage: 'Switches' }),
+    to: '/devices/switch',
+    permission: 'READ_SWITCH_LIST'
+  }, {
+    title: $t({ defaultMessage: 'Wi-Fi Networks' }),
+    to: '/networks/wireless',
+    permission: 'READ_WIFI_NETWORKS_LIST'
+  }]
   return links.reduce((elements, { title, to, permission }) => {
     if (hasPermission({ permission })) {
-      elements.push(<TenantLink to={to}>{$t({ defaultMessage: '{title}' }, { title })}</TenantLink>)
+      elements.push(<TenantLink to={to}>{title}</TenantLink>)
     }
     return elements
   }, [] as React.ReactNode[])
