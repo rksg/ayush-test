b5a8e853772ad4e8c418352ade674b8cd6452a4f
Eric Leu <eric.leu@commscope.com>
2024-06-09 22:13:17 -0700

    ACX-57207: [UI] notification channel selection

--- apps/main/src/pages/Administration/Notifications/R1NotificationSettings.tsx
+++ apps/main/src/pages/Administration/Notifications/R1NotificationSettings.tsx
@@ -1,29 +1,25 @@
 import { useState, useEffect, Dispatch, SetStateAction, MutableRefObject } from 'react'
 
-import { Form, Checkbox }              from 'antd'
-import {  cloneDeep, get, set, unset } from 'lodash'
-import { defineMessage, useIntl }      from 'react-intl'
-import { useParams }                   from 'react-router-dom'
+import { Form, Checkbox }         from 'antd'
+import {  cloneDeep, get, set }   from 'lodash'
+import { defineMessage, useIntl } from 'react-intl'
+import { useParams }              from 'react-router-dom'
 
 import {  showToast }                                            from '@acx-ui/components'
 import { useGetTenantDetailsQuery, useUpdateTenantSelfMutation } from '@acx-ui/rc/services'
 import { NotificationPreference }                                from '@acx-ui/rc/utils'
 
-type PreferenceType = 'notificationType'
-
 const labels = {
-  notificationType: {
-    DEVICE_AP_FIRMWARE: defineMessage({ defaultMessage: 'AP Firmware' }),
-    DEVICE_SWITCH_FIRMWARE: defineMessage({ defaultMessage: 'Switch Firmware' }),
-    DEVICE_EDGE_FIRMWARE: defineMessage({ defaultMessage: 'Edge Firmware' }),
-    DEVICE_API_CHANGES: defineMessage({ defaultMessage: 'API Changes' })
-  }
+  DEVICE_AP_FIRMWARE: defineMessage({ defaultMessage: 'AP Firmware' }),
+  DEVICE_SWITCH_FIRMWARE: defineMessage({ defaultMessage: 'Switch Firmware' }),
+  DEVICE_EDGE_FIRMWARE: defineMessage({ defaultMessage: 'Edge Firmware' }),
+  DEVICE_API_CHANGES: defineMessage({ defaultMessage: 'API Changes' })
 }
 
 const defaultNotification: NotificationPreference = {
   DEVICE_AP_FIRMWARE: true,
   DEVICE_SWITCH_FIRMWARE: true,
-  DEVICE_EDGE_FIRMWARE: true,
+  DEVICE_EDGE_FIRMWARE: false,
   DEVICE_API_CHANGES: true
 }
 const getApplyMsg = (success?: boolean) => {
@@ -32,28 +28,21 @@ const getApplyMsg = (success?: boolean) => {
     : defineMessage({ defaultMessage: 'Update failed, please try again later.' })
 }
 
-function OptionsList ({ preferences, setState, type }: {
+function OptionsList ({ preferences, setState }: {
   preferences: NotificationPreference,
-  setState: Dispatch<SetStateAction<NotificationPreference>>,
-  type: PreferenceType
+  setState: Dispatch<SetStateAction<NotificationPreference>>
 }) {
   const { $t } = useIntl()
-  return <>{Object.entries(labels[type]).map(([key, label]) => <div key={key}>
+  return <>{Object.entries(labels).map(([key, label]) => <div key={key}>
     <Checkbox
       id={key}
       name={key}
       style={{ padding: '5px' }}
-      checked={get(preferences, [type, key], []).includes('email')}
+      checked={get(preferences, [key])}
       onChange={(e: { target: { checked: boolean } }) =>
         setState((p: NotificationPreference) => {
           const preferences = cloneDeep(p)
-          const path = [type, key]
-          e.target.checked
-            ? set(preferences, path, ['email'])
-            : unset(preferences, path)
-          // if (isEmpty(preferences[type])) {
-          //   delete preferences[type]
-          // }
+          set(preferences, [key], e.target.checked)
           return preferences
         })
       } />
@@ -82,7 +71,6 @@ export const R1NotificationSettings = ({ tenantId, apply }: {
     return updatePrefrences({ params, payload: payload })
       .unwrap()
       .then(() => {
-        // showToast({ type: success ? 'success' : 'error', content: $t(getApplyMsg(success)) })
         return true
       })
       .catch(() => {
@@ -92,6 +80,6 @@ export const R1NotificationSettings = ({ tenantId, apply }: {
   }
 
   return <Form.Item label={$t({ defaultMessage: 'Notification Type' })}>
-    <OptionsList preferences={preferences} setState={setState} type='notificationType' />
+    <OptionsList preferences={preferences} setState={setState} />
   </Form.Item>
 }
