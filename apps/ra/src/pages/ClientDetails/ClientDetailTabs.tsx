7e2c88f73381be4ad4e48b8207b4edf47663c433
ychang3 <yy.chang@commscope.com>
2024-06-11 20:03:37 +0800

    MLSA-7886: hide tabs when there is a single item

--- apps/ra/src/pages/ClientDetails/ClientDetailTabs.tsx
+++ apps/ra/src/pages/ClientDetails/ClientDetailTabs.tsx
@@ -4,8 +4,44 @@ import { Tabs }                                  from '@acx-ui/components'
 import { useNavigate, useParams, useTenantLink } from '@acx-ui/react-router-dom'
 import { hasPermission }                         from '@acx-ui/user'
 
-function ClientDetailTabs () {
+enum ClientDetailTabEnum {
+  TROUBLESHOOTING = 'troubleshooting',
+  REPORTS = 'reports'
+}
+
+interface Tab {
+  key: ClientDetailTabEnum,
+  title: string
+}
+
+const useTabs = () : Tab[] => {
   const { $t } = useIntl()
+  const troubleshootingTab = {
+    key: ClientDetailTabEnum.TROUBLESHOOTING,
+    title: $t({ defaultMessage: 'Troubleshooting' })
+  }
+  const reportsTab = {
+    key: ClientDetailTabEnum.REPORTS,
+    title: $t({ defaultMessage: 'Reports' })
+  }
+
+  const getTabsWithPermissions = () => {
+    let tabsWithPermissions = [] as Tab[]
+    if (hasPermission({ permission: 'READ_CLIENT_TROUBLESHOOTING' })) {
+      tabsWithPermissions.push(troubleshootingTab as Tab)
+    }
+    if (hasPermission({ permission: 'READ_WIRELESS_CLIENTS_REPORT' })) {
+      tabsWithPermissions.push(reportsTab as Tab)
+    }
+    return tabsWithPermissions
+  }
+
+  return [
+    ...getTabsWithPermissions()
+  ]
+}
+
+function ClientDetailTabs () {
   const params = useParams()
   const basePath = useTenantLink(`/users/wifi/clients/${params.clientId}/details/`)
   const navigate = useNavigate()
@@ -14,23 +50,12 @@ function ClientDetailTabs () {
       ...basePath,
       pathname: `${basePath.pathname}/${tab}`
     })
-  return (
+  const tabs = useTabs()
+  return (tabs.length <= 1
+    ? <></>
+    :
     <Tabs onChange={onTabChange} activeKey={params.activeTab}>
-      {
-        hasPermission({ permission: 'READ_CLIENT_TROUBLESHOOTING' })
-          ? <Tabs.TabPane
-            tab={$t({ defaultMessage: 'Troubleshooting' })}
-            key='troubleshooting'
-          />
-          : <></>
-      } {
-        hasPermission({ permission: 'READ_WIRELESS_CLIENTS_REPORT' })
-          ? <Tabs.TabPane
-            tab={$t({ defaultMessage: 'Reports' })}
-            key='reports'
-          />
-          : <></>
-      }
+      {tabs.map(({ key, title }) => <Tabs.TabPane tab={title} key={key} />)}
     </Tabs>
   )
 }
