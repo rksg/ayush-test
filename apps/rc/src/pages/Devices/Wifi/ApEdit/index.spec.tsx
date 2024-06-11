1b67840227e1e40a8c192199830db07a10cab6ae
KYEH <karen.yeh@commscope.com>
2024-06-11 15:15:10 +0800

    ACX-55960: Fix the flaky tests

--- apps/rc/src/pages/Devices/Wifi/ApEdit/index.spec.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApEdit/index.spec.tsx
@@ -94,6 +94,8 @@ describe('ApEdit', () => {
         (_, res, ctx) => res(ctx.json(venueCaps))),
       rest.get(CommonUrlsInfo.getApGroupListByVenue.url,
         (_, res, ctx) => res(ctx.json(apGrouplist))),
+      rest.get(FirmwareUrlsInfo.getVenueApModelFirmwares.url,
+        (_, res, ctx) => res(ctx.json([]))),
       rest.post(WifiUrlsInfo.addAp.url,
         (_, res, ctx) => res(ctx.json(successResponse))),
       rest.get(WifiUrlsInfo.getAp.url.replace('?operational=false', ''),
@@ -358,6 +360,8 @@ describe('ApEdit', () => {
           (_, res, ctx) => res(ctx.json(venueSetting))),
         rest.get(CommonUrlsInfo.getVenueLanPorts.url,
           (_, res, ctx) => res(ctx.json(venueLanPorts))),
+        rest.get(FirmwareUrlsInfo.getVenueApModelFirmwares.url,
+          (_, res, ctx) => res(ctx.json([]))),
         rest.post(CommonUrlsInfo.getApsList.url,
           (_, res, ctx) => res(ctx.json(deviceAps)))
       )
