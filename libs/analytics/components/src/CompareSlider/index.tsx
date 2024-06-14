82e31a6a047d5c72c105556b38a8539121f869b2
ychang3 <yy.chang@commscope.com>
2024-06-14 19:50:19 +0800

    MLSA-7921: add CompareSlider component

---
+++ libs/analytics/components/src/CompareSlider/index.tsx
@@ -0,0 +1,24 @@
+import { ReactCompareSlider, ReactCompareSliderDetailedProps } from 'react-compare-slider'
+
+// https://react-compare-slider.vercel.app/?path=/docs/docs-api--docs
+export type CompareSliderProps = ReactCompareSliderDetailedProps
+
+export const CompareSlider = (props: CompareSliderProps) => {
+  const { itemOne, itemTwo,
+    disabled = false, portrait = false, boundsPadding = 0, position = 50,
+    changePositionOnHover = false, keyboardIncrement = 0, onlyHandleDraggable = true
+  } = props
+  return (
+    <ReactCompareSlider
+      itemOne={itemOne}
+      itemTwo={itemTwo}
+      disabled={disabled}
+      portrait={portrait}
+      boundsPadding={boundsPadding}
+      position={position}
+      changePositionOnHover={changePositionOnHover}
+      keyboardIncrement={keyboardIncrement}
+      onlyHandleDraggable={onlyHandleDraggable}
+    />
+  )
+}
---
+++ libs/analytics/components/src/CompareSlider/stories/stories.tsx
@@ -0,0 +1,95 @@
+import { ComponentStory } from '@storybook/react'
+import { scalePow }       from 'd3-scale'
+
+import { Graph, deriveInterfering } from '@acx-ui/components'
+import { BandEnum }                 from '@acx-ui/components'
+
+import { CompareSlider, CompareSliderProps } from '..'
+
+import { ComponentOne, ComponentTwo } from './CustomComponents'
+import { sample }                     from './mockData'
+
+const story = {
+  title: 'CompareSlider',
+  component: CompareSlider
+}
+export default story
+
+const Template: ComponentStory<typeof CompareSlider> = (props: CompareSliderProps) => {
+  return (
+    <CompareSlider {...props} />
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  itemOne:
+    <img alt='ImageOne'
+      // eslint-disable-next-line max-len
+      src='https://raw.githubusercontent.com/nerdyman/stuff/main/libs/react-compare-slider/demo-images/seattle-space-needle-1.jpg'
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+      }}
+    />,
+  itemTwo:
+    <img alt='ImageTwo'
+      // eslint-disable-next-line max-len
+      src='https://raw.githubusercontent.com/nerdyman/stuff/main/libs/react-compare-slider/demo-images/seattle-space-needle-2.jpg'
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+      }}
+    />
+}
+
+export const TwoComponents = Template.bind({})
+TwoComponents.args = {
+  itemOne: <ComponentOne />,
+  itemTwo: <ComponentTwo />,
+  disabled: false,
+  portrait: false,
+  boundsPadding: 0,
+  position: 50,
+  changePositionOnHover: false,
+  keyboardIncrement: 0,
+  onlyHandleDraggable: false
+}
+
+const zoomScale = scalePow()
+  .exponent(0.01)
+  .domain([3, 10, 20, 30, 63, 125, 250, 375, 500, 750])
+  .range([1.75, 0.6, 0.4, 0.35, 0.2, 0.15, 0.11, 0.09, 0.075, 0.06])
+
+export const TwoGraphs = Template.bind({})
+TwoGraphs.args = {
+  ...TwoComponents.args,
+  itemOne:
+    <Graph
+      chartRef={() => {}}
+      title='Current'
+      data={deriveInterfering(sample, BandEnum._5_GHz)}
+      zoomScale={zoomScale}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: 1100
+      }}
+    />,
+  itemTwo:
+    <Graph
+      chartRef={() => {}}
+      title='Future'
+      data={deriveInterfering({ ...sample, interferingLinks: [] }, BandEnum._5_GHz)}
+      zoomScale={zoomScale}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: 1100
+      }}
+    />
+}
