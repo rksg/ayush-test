5230540d8c75ccaf273fac2c0b652b47a17a54a2
ychang3 <yy.chang@commscope.com>
2024-06-19 12:23:01 +0800

    MLSA-7921: fix main and main-components component test issue for 'react-compare-slider'

--- apps/main/jest.config.ts
+++ apps/main/jest.config.ts
@@ -2,6 +2,7 @@ module.exports = {
   displayName: 'main',
   preset: '../../jest.preset.ts',
   transform: {
+    '^.+\\.mjs?$': 'babel-jest',
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest'
   },
