c1bdc3b5b82fd9c412001b4562e35a5464545b9f
Jerry Chen <Jerry.Chen@commscope.com>
2024-06-28 18:16:30 +0800

    ACX-60510: Remove unused code for uploading file

--- libs/rc/shared/components/src/policies/WorkflowCanvas/StepDrawer/WorkflowActionSettingForm/AupSettings.tsx
+++ libs/rc/shared/components/src/policies/WorkflowCanvas/StepDrawer/WorkflowActionSettingForm/AupSettings.tsx
@@ -1,52 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-import { Checkbox, Form, Input, Upload } from 'antd'
-import { RcFile }                        from 'antd/lib/upload'
-import { useIntl }                       from 'react-intl'
+import { Checkbox, Form, Input } from 'antd'
+import { useIntl }               from 'react-intl'
 
-import { Button }               from '@acx-ui/components'
-import { UploadDocument }       from '@acx-ui/icons'
 import { whitespaceOnlyRegExp } from '@acx-ui/rc/utils'
-import { loadImageWithJWT }     from '@acx-ui/utils'
 
 import { CommonActionSettings } from './CommonActionSettings'
 
 export function AupSettings () {
   const { $t } = useIntl()
-  const [downloadUrl, setDownloadUrl] = useState<string>()
-  const form = Form.useFormInstance()
-  const aupFileLocation = Form.useWatch<RcFile|string>('aupFileLocation', form)
-
-  useEffect(() => {
-    if (typeof aupFileLocation === 'string') {
-      if (aupFileLocation.trim() === '') return
-      loadImageWithJWT(aupFileLocation)
-        .then(res => {
-          setDownloadUrl(res)
-        })
-        .catch(() => {
-          // FIXME: Try to display the error result to the user
-        })
-    }
-  }, [aupFileLocation])
-
-  const setFile = (file: RcFile) => {
-    form.setFieldValue('aupFileLocation', file)
-    form.setFieldValue('useAupFile', true)
-
-    return Upload.LIST_IGNORE
-  }
-
-  const onRemoveFile = (): boolean => {
-    form.setFieldValue('aupFileLocation', undefined)
-    form.setFieldValue('useAupFile', false)
-    return true
-  }
 
   return (<>
     <CommonActionSettings />
 
-    {/* TODO: Split content if the user want to upload the file as the template (Need HTML file) */}
     <Form.Item
       name={'title'}
       label={$t({ defaultMessage: 'Title' })}
@@ -133,7 +99,6 @@ export function AupSettings () {
       <Input />
     </Form.Item>
 
-
     {/* Different type of AUP settings */}
     <Form.Item
       hidden
@@ -143,74 +108,6 @@ export function AupSettings () {
     >
       <Checkbox />
     </Form.Item>
-
-    <Form.Item
-      name={'aupFileLocation'}
-      label={$t({ defaultMessage: 'Aup File' })}
-    >
-      {downloadUrl
-        ? <>
-          <Button
-            type={'link'}
-            target={'_blank'}
-            href={downloadUrl}
-          >
-            {$t({ defaultMessage: 'Download Policy File' })}
-          </Button>
-          {/*<Button*/}
-          {/*  onClick={onRemoveFile}*/}
-          {/*  icon={<DeleteOutlined />}*/}
-          {/*/>*/}
-        </>
-        : <div>
-          {$t({ defaultMessage: 'No File Chosen' })}
-        </div>
-      }
-    </Form.Item>
-
-    {/*<Form.Item*/}
-    {/*  hidden*/}
-    {/*  name={'useContentFile'}*/}
-    {/*  label={$t({ defaultMessage: 'useContentFile' })}*/}
-    {/*  valuePropName={'checked'}*/}
-    {/*>*/}
-    {/*  <Checkbox />*/}
-    {/*</Form.Item>*/}
-
-    {/*<Form.Item*/}
-    {/*  name={'contentFileLocation'}*/}
-    {/*  label={$t({ defaultMessage: 'contentFileLocation' })}*/}
-    {/*>*/}
-    {/*  {downloadUrl*/}
-    {/*    ? <>*/}
-    {/*      <Button*/}
-    {/*        type={'link'}*/}
-    {/*        target={'_blank'}*/}
-    {/*        href={downloadUrl}*/}
-    {/*      >*/}
-    {/*        {$t({ defaultMessage: 'Download Policy File' })}*/}
-    {/*      </Button>*/}
-    {/*      <Button*/}
-    {/*        onClick={onRemoveFile}*/}
-    {/*        icon={<DeleteOutlined />}*/}
-    {/*      />*/}
-    {/*    </>*/}
-    {/*    : <Input hidden disabled />*/}
-    {/*  }*/}
-    {/*</Form.Item>*/}
-
-    <Upload
-      showUploadList
-      name={'file'}
-      accept={'txt'}
-      beforeUpload={setFile}
-      onRemove={onRemoveFile}
-      maxCount={1}
-    >
-      <Button icon={<UploadDocument />}>
-        {$t({ defaultMessage: 'Upload AUP File' })}
-      </Button>
-    </Upload>
 
   </>)
 }
