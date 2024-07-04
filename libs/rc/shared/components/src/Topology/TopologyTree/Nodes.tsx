ac4f47a19c3dce49fe2b044d93ce3602e649491d
jl1203 <julie.lin@commscope.com>
2024-07-04 14:30:10 +0800

    ACX-61978 Set leaficon according to node name length

--- libs/rc/shared/components/src/Topology/TopologyTree/Nodes.tsx
+++ libs/rc/shared/components/src/Topology/TopologyTree/Nodes.tsx
@@ -92,6 +92,9 @@ const Nodes: React.FC<NodeProps> = (props) => {
             (node.data?._children && node.data._children?.length > 0 ?
               `(${node.data._children.length})` : '')
           const deviceType = node.data.meshRole === 'EMAP' ? DeviceTypes.ApWired : node.data.type
+          const nodeName = children !== '' && node.data.id !== 'Cloud' ?
+            node.data.name.substring(0,8)+children+'...'
+            :truncateLabel(node.data.name || node.data.id, 13)
           return (
             <g
               transform={coordinateTransform(node)}
@@ -131,9 +134,7 @@ const Nodes: React.FC<NodeProps> = (props) => {
                     text-anchor='middle'
                     dy='13'
                   >
-                    {children !== '' && node.data.id !== 'Cloud' ?
-                      node.data.name.substring(0,8)+children+'...'
-                      :truncateLabel(node.data.name || node.data.id, 13)}
+                    {nodeName}
                   </text>
                 </g>
               </g>
@@ -180,8 +181,8 @@ const Nodes: React.FC<NodeProps> = (props) => {
                   placement='bottom'
                 >
                   <LeafSolidIcon
-                    x='20'
-                    y='8'
+                    x={nodeName.length + 6}
+                    y={8}
                     width={8}
                     height={8}
                   />
