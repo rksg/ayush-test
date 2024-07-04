b9a79d0a47af7671cbe32d2fb8c956cd815fbe41
ychang3 <yy.chang@commscope.com>
2024-07-04 19:57:00 +0800

    MLSA-7958: format date without link

--- libs/analytics/components/src/IntentAI/Table.tsx
+++ libs/analytics/components/src/IntentAI/Table.tsx
@@ -1,14 +1,11 @@
-import { Typography }                from 'antd'
-import { useIntl, FormattedMessage } from 'react-intl'
+import { useIntl } from 'react-intl'
 
 import { isSwitchPath } from '@acx-ui/analytics/utils'
 import {
   Loader,
-  TableProps,
-  Tooltip }        from '@acx-ui/components'
+  TableProps }        from '@acx-ui/components'
 import { get }                       from '@acx-ui/config'
 import { DateFormatEnum, formatter } from '@acx-ui/formatter'
-import { TenantLink }                from '@acx-ui/react-router-dom'
 import { noDataDisplay, PathFilter } from '@acx-ui/utils'
 
 import {
@@ -16,30 +13,6 @@ import {
   IntentAIRecommendationListItem
 } from './services'
 import * as UI from './styledComponents'
-
-
-const Link = (
-  { text, to, disabled }: { text: string, to: string, disabled: boolean }
-) => {
-  return disabled
-    ? <Tooltip
-      title={<FormattedMessage defaultMessage='Not available' />}
-      children={<Typography.Text disabled children={text} />}
-    />
-    : <TenantLink to={to} children={text} />
-}
-
-const DateLink = (
-  { value }: { value: IntentAIRecommendationListItem }
-) => {
-  const { updatedAt } = value
-  const text = formatter(DateFormatEnum.DateTimeFormat)(updatedAt)
-  return <Link
-    disabled={false}
-    text={text}
-    to={'TBD'}
-  />
-}
 
 export function IntentAIRecommendationTable (
   { pathFilters }: { pathFilters: PathFilter }
@@ -91,9 +64,7 @@ export function IntentAIRecommendationTable (
       width: 130,
       dataIndex: 'updatedAt',
       key: 'updatedAt',
-      render: (_, value) => <DateLink
-        value={value}
-      />
+      render: (_, record) => formatter(DateFormatEnum.DateTimeFormat)(record.updatedAt)
     }
   ]
 
