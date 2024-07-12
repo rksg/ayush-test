b3ed4d6f830b9b46e907c0461144df8679f9ba2e
gchiu <george.chiu@commscope.com>
2024-07-12 11:01:51 +0800

    ACX-62911: Add RBAC check for devices/switch/add

--- libs/rc/shared/components/src/FloorPlan/index.tsx
+++ libs/rc/shared/components/src/FloorPlan/index.tsx
@@ -23,8 +23,9 @@ import {
   FloorPlanDto, FloorPlanFormDto, NetworkDevice, NetworkDevicePayload,
   NetworkDevicePosition, NetworkDeviceType, TypeWiseNetworkDevices
 } from '@acx-ui/rc/utils'
-import { TenantLink } from '@acx-ui/react-router-dom'
-import { hasAccess }  from '@acx-ui/user'
+import { TenantLink }               from '@acx-ui/react-router-dom'
+import { SwitchScopes, WifiScopes } from '@acx-ui/types'
+import { hasAccess, hasPermission } from '@acx-ui/user'
 
 import AddEditFloorplanModal from './FloorPlanModal'
 import GalleryView           from './GalleryView/GalleryView'
@@ -416,13 +417,15 @@ export function FloorPlan () {
               showIcon
               action={
                 <Space direction='horizontal'>
-                  { hasAccess() && <TenantLink to='devices/wifi/add'>
+                  { hasPermission({ scopes: [WifiScopes.CREATE] }) &&
+                  <TenantLink to='devices/wifi/add'>
                     <Button size='small' type='primary'>
                       {$t({ defaultMessage: 'Add AP' })}
                     </Button>
                   </TenantLink>
                   }
-                  { hasAccess() && <TenantLink to='devices/switch/add'>
+                  { hasPermission({ scopes: [SwitchScopes.CREATE] }) &&
+                  <TenantLink to='devices/switch/add'>
                     <Button size='small' type='primary'>
                       {$t({ defaultMessage: 'Add Switch' })}
                     </Button>
