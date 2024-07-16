b882d6d4b9af528ab6619fb68e33e8a82dc67652
yw1046 <Yc.Wong@commscope.com>
2024-07-16 11:13:51 +0800

    ACX-63124: Optimize the startup time of the development environment

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,6 +1,33 @@
 const withModuleFederation = require('@nrwl/react/module-federation')
 const { merge } = require('webpack-merge')
 const modifyVars = require('./libs/common/components/src/theme/modify-vars')
+const path = require('path')
+const fs = require('fs')
+
+// Function to read dependencies from package.json
+function getDependenciesFromPackageJson() {
+  const packageJsonPath = path.resolve(__dirname, 'package.json')
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+  const allDependencies = new Set([
+    ...Object.keys(packageJson.dependencies || {}),
+    '@acx-ui/components',
+    '@acx-ui/config',
+    '@acx-ui/feature-toggle',
+    '@acx-ui/formatter',
+    '@acx-ui/icons',
+    '@acx-ui/react-router-dom',
+    '@acx-ui/store',
+    '@acx-ui/theme',
+    '@acx-ui/types',
+    '@acx-ui/user',
+    '@acx-ui/utils'
+  ])
+
+  return allDependencies
+}
+
+// Get core libraries from package.json
+const coreLibraries = getDependenciesFromPackageJson()
 
 /**
  * @param {Parameters<typeof withModuleFederation>[0]} moduleFederationConfig
@@ -9,7 +36,23 @@ const modifyVars = require('./libs/common/components/src/theme/modify-vars')
   moduleFederationConfig,
   additionalWebpackConfig = {}
 ) {
-  const doModuleFederation = await withModuleFederation(moduleFederationConfig)
+  const extendedModuleFederationConfig = {
+    ...moduleFederationConfig,
+    shared: (libraryName, defaultConfig) => {
+      if (coreLibraries.has(libraryName)) {
+        return {
+          ...defaultConfig,
+          singleton: true,
+          requiredVersion: defaultConfig.requiredVersion,
+        }
+      }
+      // You can add additional logic here for non-core libraries if needed
+      return false
+    }
+  }
+
+  const doModuleFederation = await withModuleFederation(process.env.NODE_ENV === 'production' ?
+  moduleFederationConfig : extendedModuleFederationConfig)
   return function (config) {
     config = merge(doModuleFederation(config), additionalWebpackConfig)
     if (process.env.NODE_ENV === 'production') {
@@ -17,7 +60,14 @@ const modifyVars = require('./libs/common/components/src/theme/modify-vars')
       config.mode = 'production'
       config.optimization.minimize = true
     } else {
-      config.cache = true
+      // config.cache = true
+      config.cache = {
+        type: 'filesystem',
+        allowCollectingMemory: true,
+        buildDependencies: {
+          config: [__filename],
+        },
+      }
       config.watchOptions = {
         ignored: /node_modules/
       }
