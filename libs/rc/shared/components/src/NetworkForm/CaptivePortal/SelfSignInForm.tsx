bd44172f014e0d23c5089048533f6c4e623b8fa4
Chen, Sander <Sander.Chen@commscope.com>
2024-07-18 18:23:55 +0800

    ACX-61982: move sms token check box to a new componet

--- libs/rc/shared/components/src/NetworkForm/CaptivePortal/SelfSignInForm.tsx
+++ libs/rc/shared/components/src/NetworkForm/CaptivePortal/SelfSignInForm.tsx
@@ -7,14 +7,13 @@ import {
   Form,
   Select,
   Space,
-  Input,
-  Button
+  Input
 } from 'antd'
 import { FormattedMessage, useIntl } from 'react-intl'
 
-import { cssStr, GridCol, GridRow, StepsFormLegacy, Tooltip } from '@acx-ui/components'
-import { get }                                                from '@acx-ui/config'
-import { Features, useIsSplitOn }                             from '@acx-ui/feature-toggle'
+import { GridCol, GridRow, StepsFormLegacy, Tooltip } from '@acx-ui/components'
+import { get }                                        from '@acx-ui/config'
+import { Features, useIsSplitOn }                     from '@acx-ui/feature-toggle'
 import {
   QuestionMarkCircleOutlined
 } from '@acx-ui/icons'
@@ -23,8 +22,8 @@ import {
   domainsNameRegExp, NetworkSaveData,
   GuestNetworkTypeEnum, NetworkTypeEnum, SmsProviderType
 } from '@acx-ui/rc/utils'
-import { useParams, TenantLink } from '@acx-ui/react-router-dom'
-import { validationMessages }    from '@acx-ui/utils'
+import { useParams }          from '@acx-ui/react-router-dom'
+import { validationMessages } from '@acx-ui/utils'
 
 import { NetworkDiagram }          from '../NetworkDiagram/NetworkDiagram'
 import { MLOContext }              from '../NetworkForm'
@@ -38,6 +37,7 @@ import GoogleSetting                             from './GoogleSetting'
 import LinkedInSetting                           from './LinkedInSetting'
 import { RedirectUrlInput }                      from './RedirectUrlInput'
 import { BypassCaptiveNetworkAssistantCheckbox } from './SharedComponent/BypassCNA/BypassCaptiveNetworkAssistantCheckbox'
+import { SMSTokenCheckbox, isSMSTokenAvailable } from './SharedComponent/SMSToken/SMSTokenCheckbox'
 import { WalledGardenTextArea }                  from './SharedComponent/WalledGarden/WalledGardenTextArea'
 import TwitterSetting                            from './TwitterSetting'
 
@@ -100,29 +100,11 @@ export function SelfSignInForm () {
   const isGracePeriodEnabled = useIsSplitOn(Features.NUVO_SMS_GRACE_PERIOD_TOGGLE)
   const params = useParams()
   const smsUsage = useGetNotificationSmsQuery({ params }, { skip: !isSmsProviderEnabled })
-  const isSMSTokenAvailable = () : boolean=> {
-    const provider = smsUsage?.data?.provider
-    const usedSMS = smsUsage?.data?.ruckusOneUsed ?? 0
-    if (!isSmsProviderEnabled) {
-      return true
-    }
-    // if Provider unset, disable SMS option even used SMS is not over 100
-    if (provider === SmsProviderType.SMSProvider_UNSET){
-      return false
-    }
-    if (provider === SmsProviderType.RUCKUS_ONE){
-      // When Provider is R1 and Grace Period is on, Client can still use R1 SMS even used SMS over 100
-      if (isGracePeriodEnabled) {
-        return true
-      }
-      // When Provider is R1 and used SMS over 100, disable the SMS option
-      if (usedSMS >= 100) {
-        return false
-      }
-    }
-    return true
-  }
-  const isRestEnableSmsLogin = cloneMode && !isSMSTokenAvailable()
+  const provider = smsUsage?.data?.provider ?? SmsProviderType.RUCKUS_ONE
+  const usedSMS = smsUsage?.data?.ruckusOneUsed ?? 0
+  const isRestEnableSmsLogin = cloneMode && !isSMSTokenAvailable(usedSMS, provider, {
+    isSmsProviderEnabled, isGracePeriodEnabled
+  })
 
   const updateAllowSign = (checked: boolean, name: Array<string>) => {
     form.setFieldValue(name, checked)
@@ -150,78 +132,6 @@ export function SelfSignInForm () {
     setAllowedSignValue(allowedSignValueTemp)
   }
 
-  const displaySMSTokenToolTips = () => {
-    const provider = smsUsage?.data?.provider
-    const usedPools = smsUsage?.data?.ruckusOneUsed ?? 0
-    // eslint-disable-next-line max-len
-    const defaultMessage = $t({ defaultMessage: 'Captive Portal Self-sign-in via SMS One-time Passcode.' })
-
-    // when FF is off
-    if (!isSmsProviderEnabled) {
-      // eslint-disable-next-line max-len
-      return $t({ defaultMessage: 'Self-service signup using one time token sent to a mobile number' })
-    }
-
-    // when provider is not ruckus one, no needs to consider used pools at this condition
-    if (provider !== SmsProviderType.RUCKUS_ONE && provider !== SmsProviderType.SMSProvider_UNSET) {
-      return defaultMessage
-    }
-
-    // when provider is ruckus one and grace period is on, no needs to consider the used SMS count
-    if(provider === SmsProviderType.RUCKUS_ONE && isGracePeriodEnabled){
-      return defaultMessage
-    }
-
-    // when provider is ruckus one but there's pool still remains
-    if(provider === SmsProviderType.RUCKUS_ONE && usedPools < 100) {
-      return (
-        <FormattedMessage
-          defaultMessage={
-            `{defaultMessage}<br></br>
-              You have {poolCount} messages remaining in the RUCKUS-provided pool. 
-              To ensure uninterrupted service, kindly set up an SMS provider on the 
-              <SMSLink></SMSLink> page.`}
-          values={{
-            defaultMessage: defaultMessage,
-            poolCount: 100 - usedPools,
-            br: () => <br/>,
-            SMSLink: () => {
-              return (<TenantLink to='/administration/accountSettings'>
-                <Button
-                  data-testid='button-has-pool'
-                  type='link'
-                  style={{ fontSize: 'var(--acx-body-4-font-size)' }}>
-                  { $t({ defaultMessage: 'Administration > Settings' }) }
-                </Button>
-              </TenantLink>)}
-          }}
-        />
-      )
-    } else {
-      // when provider is ruckus one, no pool remains
-      return (
-        <FormattedMessage
-          defaultMessage={
-            `{defaultMessage}<br></br>
-              To activate the SMS option, configure an SMS provider 
-              on the <SMSLink></SMSLink> page.`}
-          values={{
-            defaultMessage,
-            br: () => <br/>,
-            SMSLink: () => {
-              return (<TenantLink to='/administration/accountSettings'>
-                <Button
-                  data-testid='button-no-pool'
-                  type='link'
-                  style={{ fontSize: 'var(--acx-body-4-font-size)' }}>
-                  { $t({ defaultMessage: 'Administration > Settings' }) }
-                </Button>
-              </TenantLink>)}
-          }}
-        />
-      )
-    }
-  }
   const checkSocial = (value: string | string[]) => {
     if (!value || value.length < 1) {
       return Promise.reject($t({ defaultMessage: 'Please configure sign-in option' }))
@@ -303,55 +213,7 @@ export function SelfSignInForm () {
             {$t({ defaultMessage: 'Allow Sign-In Using: (At least one option must be selected)' })}
           </>}
         ><>
-            <Form.Item name={['guestPortal', 'enableSmsLogin']}
-              initialValue={false}
-              style={SelfSignInAppStyle}>
-              <>
-                <UI.RedAlertCheckbox onChange={(e) => updateAllowSign(e.target.checked,
-                  ['guestPortal', 'enableSmsLogin'])}
-                checked={enableSmsLogin}
-                disabled={!editMode && !isSMSTokenAvailable()}
-                >
-                  <UI.SMSToken />
-                  {$t({ defaultMessage: 'SMS Token' })}
-                </UI.RedAlertCheckbox>
-                <Tooltip
-                  title={displaySMSTokenToolTips()}
-                  placement='bottom'>
-                  <QuestionMarkCircleOutlined style={{ marginLeft: -5, marginBottom: -3 }} />
-                </Tooltip>
-              </>
-            </Form.Item>
-            <Form.Item style={SelfSignInAppStyle}>
-              <div style={{
-                paddingLeft: '24px',
-                width: '600px',
-                color: cssStr('--acx-semantics-red-70')
-              }} >
-                <div style={{ height: '100%',float: 'left' }}>
-                  <UI.WarningTriangleSolid/>
-                </div>
-                <div style={{ fontSize: '12px', paddingLeft: '3px', float: 'left' }}>
-                  <FormattedMessage
-                    defaultMessage={
-                      `To keep this option enabled, configure an SMS provider on the<br></br>
-                      <SMSLink></SMSLink> or set a social login option.`}
-                    values={{
-                      br: () => <br/>,
-                      SMSLink: () => {
-                        return (<TenantLink to='/administration/accountSettings'>
-                          <Button
-                            data-testid='red-alert-message'
-                            type='link'
-                            style={{ fontSize: 'var(--acx-body-4-font-size)' }}>
-                            { $t({ defaultMessage: 'Administration > Settings' }) }
-                          </Button>
-                        </TenantLink>)}
-                    }}
-                  />
-                </div>
-              </div>
-            </Form.Item>
+            <SMSTokenCheckbox SMSUsage={smsUsage.data} onChange={updateAllowSign} />
             { isEnabledEmailOTP && <Form.Item name={['guestPortal', 'enableEmailLogin']}
               initialValue={false}
               style={SelfSignInAppStyle}>
