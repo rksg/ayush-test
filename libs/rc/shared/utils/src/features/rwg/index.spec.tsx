5c01450ca47e927dbb84be9f3fcd6fa6e8da13af
sb1212 <Suraj.Bande@commscope.com>
2024-07-22 22:21:48 +0530

    ACX-61511: corrected test cases

--- libs/rc/shared/utils/src/features/rwg/index.spec.tsx
+++ libs/rc/shared/utils/src/features/rwg/index.spec.tsx
@@ -4,6 +4,6 @@ import { getRwgStatus } from '.'
 
 describe('seriesMappingRWG', () => {
   it('should correctly recognize OWE transition network', async () => {
-    expect(getRwgStatus(RWGStatusEnum.ONLINE).name).toBe('Operational')
+    expect(getRwgStatus(RWGStatusEnum.ONLINE).name).toBe('Online')
   })
 })
