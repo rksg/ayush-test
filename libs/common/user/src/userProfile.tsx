ee5f63c5c21775b2877bb941e4e89f50faa31503
KYEH <karen.yeh@commscope.com>
2024-07-23 16:43:04 +0800

    ACX-63706: Remove testing code

--- libs/common/user/src/userProfile.tsx
+++ libs/common/user/src/userProfile.tsx
@@ -71,10 +71,10 @@ export const getShowWithoutRbacCheckKey = (id:string) => {
  * DO NOT use hasAccess. It will be private after RBAC feature release.
  */
 
-export function hasAccess (id?: string, roles?: RolesEnum[]) {
+export function hasAccess (id?: string) {
   if (get('IS_MLISA_SA')) return true
   // measure to permit all undefined id for admins
-  if (!id) return hasRoles(roles || [Role.PRIME_ADMIN, Role.ADMINISTRATOR, Role.DPSK_ADMIN])
+  if (!id) return hasRoles([Role.PRIME_ADMIN, Role.ADMINISTRATOR, Role.DPSK_ADMIN])
   return hasAllowedOperations(id)
 }
 
@@ -116,24 +116,23 @@ export function hasPermission (props?: {
     // RAI
     permission?: RaiPermission,
     // R1
-    roles?: RolesEnum[],
     scopes?: ScopeKeys,
     allowedOperations?:string
 }): boolean {
-  const { scopes = [], allowedOperations, permission, roles } = props || {}
+  const { scopes = [], allowedOperations, permission } = props || {}
   if (get('IS_MLISA_SA')) {
     return !!(permission && permissions[permission])
   } else {
     const { abacEnabled, isCustomRole } = getUserProfile()
     if(!abacEnabled) {
-      return hasAccess(allowedOperations, roles)
+      return hasAccess(allowedOperations)
     }else {
       if(isCustomRole){
         const isScopesValid = scopes.length > 0 ? hasScope(scopes): true
         const isOperationsValid = allowedOperations ? hasAllowedOperations(allowedOperations): true
         return !!(isScopesValid && isOperationsValid)
       } else {
-        return hasAccess(allowedOperations, roles)
+        return hasAccess(allowedOperations)
       }
     }
   }
