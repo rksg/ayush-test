5a6cc6860bbc825c4e05def3e46a18b34aaae6a7
wilson.loh <wilson.loh@commscope.com>
2024-07-24 15:20:47 +0800

    MLSA-7983: update unit test

--- libs/analytics/components/src/IntentAI/IntentAIDetails/AIDrivenRRM/CrrmBenefits.spec.tsx
+++ libs/analytics/components/src/IntentAI/IntentAIDetails/AIDrivenRRM/CrrmBenefits.spec.tsx
@@ -3,8 +3,8 @@ import { mockGraphqlQuery, render,screen } from '@acx-ui/test-utils'
 
 import { transformDetailsResponse } from '../../IntentAIForm/services'
 
-import { mockedCRRMGraphs, mockedRecommendationCRRM, mockedRecommendationCRRMnew } from './__tests__/fixtures'
-import { CrrmBenefits }                                                            from './CrrmBenefits'
+import { mockedCRRMGraphs, mockedRecommendationCRRM } from './__tests__/fixtures'
+import { CrrmBenefits }                               from './CrrmBenefits'
 
 describe('CrrmBenefits', () => {
   beforeEach(() => {
@@ -21,20 +21,5 @@ describe('CrrmBenefits', () => {
     expect(await screen.findByText('Average interfering links per AP')).toBeVisible()
     expect(await screen.findByText('-200%')).toBeVisible()
     expect(await screen.findByText('-100%')).toBeVisible()
-  })
-
-  it('should render correctly with no interfering link', async () => {
-    const crrmDetails = transformDetailsResponse(mockedRecommendationCRRMnew)
-    render(<CrrmBenefits details={crrmDetails} />, { wrapper: Provider })
-
-    expect(await screen.findByText('Benefits')).toBeVisible()
-    const interferingLinks = expect(await screen.findByTestId('interfering-links'))
-    interferingLinks.toBeVisible()
-    interferingLinks.toHaveTextContent('0')
-    interferingLinks.toHaveTextContent('0%')
-    const interferingLinksPerAp = expect(await screen.findByTestId('interfering-links-per-ap'))
-    interferingLinksPerAp.toBeVisible()
-    interferingLinksPerAp.toHaveTextContent('0')
-    interferingLinksPerAp.toHaveTextContent('0%')
   })
 })
