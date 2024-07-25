596fa0d8519ddad5589a892ce1b626d766f6ed6f
wilson.loh <wilson.loh@commscope.com>
2024-07-25 13:34:56 +0800

    MLSA-7983: update unit test

--- libs/analytics/components/src/IntentAI/IntentAIDetails/AIDrivenRRM/CrrmBenefits.spec.tsx
+++ libs/analytics/components/src/IntentAI/IntentAIDetails/AIDrivenRRM/CrrmBenefits.spec.tsx
@@ -11,6 +11,7 @@ describe('CrrmBenefits', () => {
     mockGraphqlQuery(recommendationUrl, 'CloudRRMGraph', {
       data: { recommendation: mockedCRRMGraphs }
     })
+    jest.spyOn(require('../../utils'), 'isDataRetained').mockImplementation(() => true)
   })
   it('should render correctly', async () => {
     const crrmDetails = transformDetailsResponse(mockedRecommendationCRRM)
@@ -21,5 +22,12 @@ describe('CrrmBenefits', () => {
     expect(await screen.findByText('Average interfering links per AP')).toBeVisible()
     expect(await screen.findByText('-200%')).toBeVisible()
     expect(await screen.findByText('-100%')).toBeVisible()
+  })
+  it('should handle when beyond data retention', async () => {
+    jest.spyOn(require('../../utils'), 'isDataRetained').mockImplementation(() => false)
+    const crrmDetails = transformDetailsResponse(mockedRecommendationCRRM)
+    render(<CrrmBenefits details={crrmDetails} />, { wrapper: Provider })
+    expect(await screen.findByText('Benefits')).toBeVisible()
+    expect(await screen.findByText('Beyond data retention period')).toBeVisible()
   })
 })
