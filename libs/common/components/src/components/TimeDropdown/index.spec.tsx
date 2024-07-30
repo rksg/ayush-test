7ec1534be25123805bbe6720dc6bdd53e620bba7
Jason Cheow <jason.cheow@commscope.com>
2024-07-30 18:40:36 +0800

    MLSA-8104: Fix TimeDropdown test to use enum

--- libs/common/components/src/components/TimeDropdown/index.spec.tsx
+++ libs/common/components/src/components/TimeDropdown/index.spec.tsx
@@ -3,24 +3,24 @@ import { ReactElement } from 'react'
 import { render, screen } from '@acx-ui/test-utils'
 
 import '@testing-library/jest-dom/extend-expect'
-import { getDisplayTime, TimeDropdown } from '.'
+import { getDisplayTime, TimeDropdown, TimeDropdownTypes } from '.'
 
 describe('TimeDropdown', () => {
   it('renders Daily dropdown correctly', async () => {
-    render(<TimeDropdown type='daily' name='daily' />)
+    render(<TimeDropdown type={TimeDropdownTypes.Daily} name='daily' />)
 
     expect(screen.getByText('Select hour')).toBeInTheDocument()
   })
 
   it('renders Weekly dropdown correctly', () => {
-    render(<TimeDropdown type='weekly' name='weekly' />)
+    render(<TimeDropdown type={TimeDropdownTypes.Weekly} name='weekly' />)
 
     expect(screen.getByText('Select day')).toBeInTheDocument()
     expect(screen.getByText('Select hour')).toBeInTheDocument()
   })
 
   it('renders Month dropdown correctly', () => {
-    render(<TimeDropdown type='monthly' name='weekly' />)
+    render(<TimeDropdown type={TimeDropdownTypes.Monthly} name='weekly' />)
 
     expect(screen.getByText('Select day')).toBeInTheDocument()
     expect(screen.getByText('Select hour')).toBeInTheDocument()
@@ -30,7 +30,7 @@ describe('TimeDropdown', () => {
 describe ('getDisplayTime', () => {
   it('should return a function that formats time for "daily" type', () => {
     type DailyFunction = (hour: number) => React.ReactNode
-    const displayTime = getDisplayTime('daily') as DailyFunction
+    const displayTime = getDisplayTime(TimeDropdownTypes.Daily) as DailyFunction
     expect(displayTime).toBeInstanceOf(Function)
 
     const result = displayTime(12)
@@ -38,7 +38,7 @@ describe ('getDisplayTime', () => {
   })
 
   it('should return a function that formats time for "weekly" type', () => {
-    const displayWeeklyFunction = getDisplayTime('weekly')
+    const displayWeeklyFunction = getDisplayTime(TimeDropdownTypes.Weekly)
     expect(displayWeeklyFunction).toBeInstanceOf(Function)
 
     const { container } = render(displayWeeklyFunction(1, 12) as ReactElement)
@@ -46,7 +46,7 @@ describe ('getDisplayTime', () => {
   })
 
   it('should return a function that formats time for "monthly" type', () => {
-    const displayMonthlyFunction = getDisplayTime('monthly')
+    const displayMonthlyFunction = getDisplayTime(TimeDropdownTypes.Monthly)
     expect(displayMonthlyFunction).toBeInstanceOf(Function)
 
     const { container } = render(displayMonthlyFunction(15, 12) as ReactElement)
