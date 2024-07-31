adcb9ed7b06d5951a76eaec1f3c40869cf224072
Jason Cheow <jason.cheow@commscope.com>
2024-07-31 17:12:58 +0800

    MLSA-8104: Wrap TimeDropdown with Form to remove warnings

--- libs/common/components/src/components/TimeDropdown/stories.tsx
+++ libs/common/components/src/components/TimeDropdown/stories.tsx
@@ -4,20 +4,20 @@ import { Form, Row } from 'antd'
 import { TimeDropdown, TimeDropdownTypes } from '.'
 
 storiesOf('TimeDropdown', module)
-  .add('Daily', () => <Form.Item style={{ width: '30%' }}>
+  .add('Daily', () => <Form><Form.Item style={{ width: '30%' }}>
     <Row align='middle' justify='center'>
       <TimeDropdown type={TimeDropdownTypes.Daily} name='daily' />
     </Row>
-  </Form.Item>)
-  .add('Weekly', () => <Form.Item style={{ width: '30%' }}>
+  </Form.Item></Form>)
+  .add('Weekly', () => <Form><Form.Item style={{ width: '30%' }}>
     <Row align='middle' justify='center'>
       <TimeDropdown type={TimeDropdownTypes.Weekly} name='weekly' />
     </Row>
-  </Form.Item>)
-  .add('Monthly', () => <Form.Item style={{ width: '30%' }}>
+  </Form.Item></Form>)
+  .add('Monthly', () => <Form><Form.Item style={{ width: '30%' }}>
     <Row align='middle' justify='center'>
       <TimeDropdown type={TimeDropdownTypes.Monthly} name='monthly' />
     </Row>
-  </Form.Item>)
+  </Form.Item></Form>)
 
 export {}
