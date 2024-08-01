5621411f07eea5b05d612806c09678bf142bb612
ychang3 <yy.chang@commscope.com>
2024-08-01 17:08:45 +0800

    MLSA-7992: Refine code

--- libs/analytics/components/src/IntentAI/IntentAIWidget/index.tsx
+++ libs/analytics/components/src/IntentAI/IntentAIWidget/index.tsx
@@ -18,84 +18,6 @@ type IntentAIWidgetProps = {
   pathFilters: PathFilter
 }
 
-function getHighlightList (data: IntentHighlight | undefined) {
-  const highlightList: HighlightCardProps[] = []
-  if (data?.rrm) {
-    highlightList.push({ type: 'rrm', new: data.rrm.new, active: data.rrm.active })
-  }
-  if (data?.airflex) {
-    highlightList.push({ type: 'airflex', new: data.airflex.new, active: data.airflex.active })
-  }
-  if (data?.ops) {
-    highlightList.push({ type: 'operations', new: data.ops.new, active: data.ops.active })
-  }
-  return highlightList
-}
-
-export function IntentAIWidget ({
-  pathFilters
-}: IntentAIWidgetProps) {
-  const { $t } = useIntl()
-  // pass selectedTenants to query to prevent error on account switch
-  const [search] = useSearchParams()
-  const selectedTenants = search.get('selectedTenants')
-  const queryResults = useIntentHighlightQuery(
-    { ...pathFilters, n: 12, selectedTenants }
-  )
-  const data = queryResults?.data
-
-  const title = $t({ defaultMessage: 'IntentAI' })
-
-  const firstParagraph = defineMessage({ defaultMessage:
-  'Revolutionize your Network Optimization'
-  })
-  const secondParagraph = defineMessage({
-    defaultMessage: `Automates configuration, monitors task based, and optimizes your network
-      performance with IntentAI's advanced AI and ML technologies.
-      ` })
-
-  const responseData = data
-  const highlightList: HighlightCardProps[] = getHighlightList(responseData)
-  const hasData = highlightList.length > 0
-
-  return <Loader states={[queryResults]}>
-    <Card title={title}>
-      <UI.ContentWrapper>
-        <AutoSizer>
-          {({ width, height }) => (
-            !hasData
-              ? <div style={{ width, height }}>
-                <p><b>{$t(firstParagraph)}</b></p>
-                <p>{$t(secondParagraph)}</p>
-                <UI.NoDataWrapper>
-                  <NoDataIconOnly />
-                </UI.NoDataWrapper>
-              </div>
-              : <div style={{ width, height }}>
-                <p><b>{$t(firstParagraph)}</b></p>
-                <p>{$t(secondParagraph)}</p>
-                <Row gutter={[24, 24]}>
-                  {
-                    highlightList.map((detail, index) => {
-                      return <Col span={12} key={index} >
-                        <HighlightCard
-                          key={index}
-                          type={detail.type}
-                          new={detail.new}
-                          active={detail.active} />
-                      </Col>
-                    })
-
-                  }
-                </Row>
-              </div>
-          )}
-        </AutoSizer>
-      </UI.ContentWrapper>
-    </Card>
-  </Loader>
-}
-
 type HighlightCardProps = HighlightItem & {
   type: 'rrm' | 'airflex' | 'operations'
 }
@@ -145,4 +67,81 @@ function HighlightCard (props: HighlightCardProps) {
   return (<Card cardIcon={getCardIcon(type)} title={title}>
     {content}
   </Card>)
+}
+
+function getHighlightList (data: IntentHighlight | undefined) {
+  const highlightList: HighlightCardProps[] = []
+  if (data?.rrm) {
+    highlightList.push({ type: 'rrm', new: data.rrm.new, active: data.rrm.active })
+  }
+  if (data?.airflex) {
+    highlightList.push({ type: 'airflex', new: data.airflex.new, active: data.airflex.active })
+  }
+  if (data?.ops) {
+    highlightList.push({ type: 'operations', new: data.ops.new, active: data.ops.active })
+  }
+  return highlightList
+}
+
+export function IntentAIWidget ({
+  pathFilters
+}: IntentAIWidgetProps) {
+  const { $t } = useIntl()
+  // pass selectedTenants to query to prevent error on account switch
+  const [search] = useSearchParams()
+  const selectedTenants = search.get('selectedTenants')
+  const queryResults = useIntentHighlightQuery(
+    { ...pathFilters, n: 12, selectedTenants }
+  )
+  const data = queryResults?.data
+
+  const title = $t({ defaultMessage: 'IntentAI' })
+
+  const firstParagraph = defineMessage({ defaultMessage:
+  'Revolutionize your Network Optimization'
+  })
+  const secondParagraph = defineMessage({
+    defaultMessage: `Automates configuration, monitors task based, and optimizes your network
+      performance with IntentAI's advanced AI and ML technologies.
+      ` })
+
+  const highlightList: HighlightCardProps[] = getHighlightList(data)
+  const hasData = highlightList.length > 0
+
+  return <Loader states={[queryResults]}>
+    <Card title={title}>
+      <UI.ContentWrapper>
+        <AutoSizer>
+          {({ width, height }) => (
+            !hasData
+              ? <div style={{ width, height }}>
+                <p><b>{$t(firstParagraph)}</b></p>
+                <p>{$t(secondParagraph)}</p>
+                <UI.NoDataWrapper>
+                  <NoDataIconOnly />
+                </UI.NoDataWrapper>
+              </div>
+              : <div style={{ width, height }}>
+                <p><b>{$t(firstParagraph)}</b></p>
+                <p>{$t(secondParagraph)}</p>
+                <Row gutter={[24, 24]}>
+                  {
+                    highlightList.map((detail, index) => {
+                      return <Col span={12} key={index} >
+                        <HighlightCard
+                          key={index}
+                          type={detail.type}
+                          new={detail.new}
+                          active={detail.active} />
+                      </Col>
+                    })
+
+                  }
+                </Row>
+              </div>
+          )}
+        </AutoSizer>
+      </UI.ContentWrapper>
+    </Card>
+  </Loader>
 }
