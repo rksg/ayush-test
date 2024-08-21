a593d362ef830db027e10c57f6c33d6b3c2572ed
stan2 <shiawuen.tan@commscope.com>
2024-08-21 15:03:20 +0800

    MLSA-8094: fix mistake in attempt to remove warning

--- libs/common/components/src/components/CloudRRMGraph/index.tsx
+++ libs/common/components/src/components/CloudRRMGraph/index.tsx
@@ -3,7 +3,6 @@ import { RefCallback, useImperativeHandle, useRef } from 'react'
 import { ScalePower }                      from 'd3'
 import { scalePow }                        from 'd3-scale'
 import ReactECharts, { EChartsReactProps } from 'echarts-for-react'
-import { omit }                            from 'lodash'
 import PropTypes                           from 'prop-types'
 
 import { cssStr, cssNumber } from '../../theme/helper'
@@ -132,12 +131,7 @@ export function Graph (props: GraphProps) {
     ]
   }
 
-  return <ReactECharts
-    {...omit(props, ['backgroundColor'])}
-    ref={eChartsRef}
-    opts={{ renderer: 'svg' }}
-    option={option}
-  />
+  return <ReactECharts {...props} ref={eChartsRef} opts={{ renderer: 'svg' }} option={option} />
 }
 
 Graph.propTypes = {
