64646f3a6173e84538f41752e7ec88a682bb7b4d
Joe Kao <joe.kao@commscope.com>
2024-08-22 18:02:22 +0800

    ACX-65957: Support RBAC Venue filter for the Wifi Pages.

--- apps/rc/src/pages/Networks/wireless/NetworksTable/index.tsx
+++ apps/rc/src/pages/Networks/wireless/NetworksTable/index.tsx
@@ -9,6 +9,7 @@ import { useNetworkTableQuery, useWifiNetworkTableQuery }                       
 import { Network, usePollingTableQuery, WifiNetwork }                                        from '@acx-ui/rc/utils'
 import { TenantLink }                                                                        from '@acx-ui/react-router-dom'
 import { WifiScopes }                                                                        from '@acx-ui/types'
+import { hasCrossVenuesPermission }                                                          from '@acx-ui/user'
 
 export default function useNetworksTable () {
   const isWifiRbacEnabled = useIsSplitOn(Features.WIFI_RBAC_API)
@@ -32,12 +33,12 @@ export default function useNetworksTable () {
     description: 'Translation strings - Network List'
   })
 
-  const extra = [
+  const extra = hasCrossVenuesPermission()? [
     <TenantLink to='/networks/wireless/add'
       scopeKey={[WifiScopes.CREATE]}>
       <Button type='primary'>{ $t({ defaultMessage: 'Add Wi-Fi Network' }) }</Button>
     </TenantLink>
-  ]
+  ] : []
 
   const component = <NetworkTabContext.Provider value={{ setNetworkCount }}>
     <NetworkTable tableQuery={tableQuery} selectable={true} settingsId={settingsId} />
