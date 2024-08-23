71b0f342c76115fd2f1f2586b5c1ccb997d25608
Jean Lu <pinchun.lu@commscope.com>
2024-08-23 09:55:28 +0800

    ACX-63746: removed unusable mock data in unit tests for softGreForm

--- libs/rc/shared/components/src/policies/SoftGre/SoftGreForm/__tests__/fixtures.ts
+++ libs/rc/shared/components/src/policies/SoftGre/SoftGreForm/__tests__/fixtures.ts
@@ -1,112 +1,9 @@
 import { MtuTypeEnum, SoftGreViewData } from '@acx-ui/rc/utils'
 
-export const mockSoftGreData = {
-  id: 'test-policyId',
-  name: 'UNKNOWN-SOFTGRE-NAME',
-  description: 'mockSoftGreDescription',
-  mtuType: MtuTypeEnum.AUTO,
-  disassociateClientEnabled: false,
-  primaryGatewayAddress: '128.0.0.1',
-  secondaryGatewayAddress: '128.0.0.0',
-  keepAliveInterval: 10,
-  keepAliveRetryTimes: 5
-}
-
-export const mockSoftGreDetail = {
-  fields: null,
-  totalCount: 1,
-  page: 1,
-  data: [
-    {
-      id: '0d89c0f5596c4689900fb7f5f53a0859',
-      name: 'the ID of tunnel profile, 222',
-      description: 'mockSoftGreDetail in __tests__fixture',
-      mtuType: MtuTypeEnum.AUTO,
-      disassociateClientEnabled: true,
-      primaryGatewayAddress: '128.0.0.1',
-      secondaryGatewayAddress: '128.0.0.0',
-      keepAliveInterval: 100,
-      keepAliveRetryTimes: 8,
-      activationInformations: [
-        {
-          venueId: '0e2f68ab79154ffea64aa52c5cc48826',
-          networkIds: [
-            '9b33509cc0a1464cad9447778a72006f',
-            '797a1f499c254260b7a1aedafba524a3',
-            'b946294426b8413d819751cb3d320a20'
-          ]
-        },
-        {
-          venueId: 'eef3d9913bcc4deea43300804281c2c6',
-          networkIds: [
-            '9b33509cc0a1464cad9447778a72006f',
-            'dca03b33dc354c90b89b1ef68a7f93e1'
-          ]
-        }
-      ]
-    }
-  ] as SoftGreViewData[]
-}
-
-export const mockSoftGreNamesByQuery =
-{
-  status: 'fulfilled',
-  endpointName: 'getTunnelProfileViewDataList',
-  requestId: 'il-v5uqZMa7NKkTcgjPZt',
-  originalArgs: {
-    params: {
-      tenantId: 'b338eaa6796443829192a61093e143f9',
-      policyId: '0d89c0f5596c4689900fb7f5f53a0859'
-    },
-    payload: {
-      searchString: '5555',
-      fields: [
-        'name',
-        'id'
-      ],
-      searchTargetFields: [
-        'name'
-      ],
-      filters: {},
-      pageSize: 10000
-    }
-  },
-  startedTimeStamp: 1723702740547,
-  data: {
-    fields: [
-      'centralizedForwardingIds',
-      'name',
-      'networkSegmentationIds',
-      'id'
-    ],
-    totalCount: 2,
-    page: 1,
-    data: [
-      {
-        id: '7edd6664c2e84e009e38e5949ee5151f',
-        name: '5555',
-        personalIdentityNetworkIds: [],
-        sdLanIds: []
-      },
-      {
-        id: '14adffb071604b409a76da7d296333c6',
-        name: '55555',
-        personalIdentityNetworkIds: [],
-        sdLanIds: []
-      }
-    ]
-  },
-  fulfilledTimeStamp: 1723702740895,
-  isUninitialized: false,
-  isLoading: false,
-  isSuccess: true,
-  isError: false
-}
-
 export const mockSoftGreTable = {
   data: {
     fields: null,
-    totalCount: 2,
+    totalCount: 3,
     page: 1,
     data: [
       {
@@ -119,10 +16,10 @@ export const mockSoftGreTable = {
         secondaryGatewayAddress: '128.0.0.0',
         keepAliveInterval: 100,
         keepAliveRetryTimes: 8,
-        activationInformations: [
+        activations: [
           {
             venueId: '0e2f68ab79154ffea64aa52c5cc48826',
-            networkIds: [
+            wifiNetworkIds: [
               '9b33509cc0a1464cad9447778a72006f',
               '797a1f499c254260b7a1aedafba524a3',
               'b946294426b8413d819751cb3d320a20'
@@ -149,7 +46,7 @@ export const mockSoftGreTable = {
         secondaryGatewayAddress: '128.0.0.5',
         keepAliveInterval: 100,
         keepAliveRetryTimes: 8,
-        activationInformations: [
+        activations: [
           {
             venueId: '0e2f68ab79154ffea64aa52c5cc48826'
           }
