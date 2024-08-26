1bd393c5d37a1a060bf860b0e84b5c8e6bc0b631
Cheng, Amy <Amy.Cheng@commscope.com>
2024-08-26 17:23:39 +0800

    ACX-56306: fix UT

--- libs/rc/shared/components/src/Compatibility/CompatibilityDrawer/FeatureCrossDeviceTypeCompatibility.spec.tsx
+++ libs/rc/shared/components/src/Compatibility/CompatibilityDrawer/FeatureCrossDeviceTypeCompatibility.spec.tsx
@@ -1,27 +1,17 @@
 /* eslint-disable max-len */
-import userEvent          from '@testing-library/user-event'
-import { cloneDeep, get } from 'lodash'
-import { rest }           from 'msw'
+import { IncompatibilityFeatures, CompatibilityDeviceEnum } from '@acx-ui/rc/utils'
+import { Provider }                                         from '@acx-ui/store'
+import { render, screen, within }                           from '@acx-ui/test-utils'
 
-import { venueApi, networkApi, apApi }                           from '@acx-ui/rc/services'
-import { WifiUrlsInfo, CommonUrlsInfo, IncompatibilityFeatures } from '@acx-ui/rc/utils'
-import { Provider, store }                                       from '@acx-ui/store'
-import { act, mockServer, render, screen, within }               from '@acx-ui/test-utils'
+import { mockApCompatibilitiesVenue }              from '../ApCompatibilityDrawer/__test__/fixtures'
+import { CompatibilityItemProps }                  from '../CompatibilityDrawer/CompatibilityItem'
+import { FeatureItemProps }                        from '../CompatibilityDrawer/CompatibilityItem/FeatureItem'
+import { transformedMockEdgeCompatibilitiesVenue } from '../EdgeCompatibilityDrawer/__test__/fixtures'
 
+import { FeatureCrossDeviceTypeCompatibility } from './FeatureCrossDeviceTypeCompatibility'
 
-import { CompatibilityItemProps } from '../CompatibilityDrawer/CompatibilityItem'
-import { FeatureItemProps }       from '../CompatibilityDrawer/CompatibilityItem/FeatureItem'
-
-import {
-  mockApCompatibilitiesVenue,
-  mockApCompatibilitiesNetwork,
-  mockFeatureCompatibilities
-} from './__test__/fixtures'
-
-import { ApGeneralCompatibilityDrawer, ApCompatibilityType } from '.'
-
-jest.mock('../CompatibilityDrawer/CompatibilityItem', () => {
-  const CompatibilityItemComp = jest.requireActual('../CompatibilityDrawer/CompatibilityItem')
+jest.mock('./CompatibilityItem', () => {
+  const CompatibilityItemComp = jest.requireActual('./CompatibilityItem')
   return {
     ...CompatibilityItemComp,
     CompatibilityItem: (props: CompatibilityItemProps) => <div data-testid='CompatibilityItem'>
@@ -29,8 +19,8 @@ jest.mock('../CompatibilityDrawer/CompatibilityItem', () => {
     </div>
   }
 })
-jest.mock('../CompatibilityDrawer/CompatibilityItem/FeatureItem', () => {
-  const FeatureItemComp = jest.requireActual('../CompatibilityDrawer/CompatibilityItem/FeatureItem')
+jest.mock('./CompatibilityItem/FeatureItem', () => {
+  const FeatureItemComp = jest.requireActual('./CompatibilityItem/FeatureItem')
   return {
     ...FeatureItemComp,
     FeatureItem: (props: FeatureItemProps) => <div data-testid='FeatureItem'>
@@ -39,236 +29,50 @@ jest.mock('../CompatibilityDrawer/CompatibilityItem/FeatureItem', () => {
   }
 })
 
-describe('ApGeneralCompatibilityDrawer', () => {
+describe('FeatureCrossDeviceTypeCompatibility', () => {
   const venueId = '8caa8f5e01494b5499fa156a6c565138'
-  const networkId = 'c9d5f4c771c34ad2898f7078cebbb191'
   const tenantId = 'ecc2d7cf9d2342fdb31ae0e24958fcac'
-  const featureName = IncompatibilityFeatures.BETA_DPSK3
-  let params = { tenantId, venueId, featureName: IncompatibilityFeatures.BETA_DPSK3 }
-  const venueName = 'Test Venue'
+  let params = { tenantId, venueId }
 
-  const mockedCloseDrawer = jest.fn()
-  const mockGetVenue = jest.fn()
-
-  beforeEach(() => {
-    act(() => {
-      store.dispatch(venueApi.util.resetApiState())
-      store.dispatch(networkApi.util.resetApiState())
-      store.dispatch(apApi.util.resetApiState())
-    })
-
-    mockGetVenue.mockClear()
-
-    mockServer.use(
-      rest.post(
-        WifiUrlsInfo.getApCompatibilitiesVenue.url,
-        (_, res, ctx) => res(ctx.json(mockApCompatibilitiesVenue))),
-      rest.post(
-        WifiUrlsInfo.getApCompatibilitiesNetwork.url,
-        (_, res, ctx) => res(ctx.json(mockApCompatibilitiesNetwork))),
-      rest.get(
-        WifiUrlsInfo.getApFeatureSets.url.split('?')[0],
-        (_, res, ctx) => res(ctx.json(mockFeatureCompatibilities))),
-      rest.get(
-        CommonUrlsInfo.getVenue.url.split('?')[0],
-        (_, res, ctx) => {
-          mockGetVenue()
-          return res(ctx.json({ name: venueName }))
-        }),
-      rest.get(
-        CommonUrlsInfo.getVenuesList.url.split('?')[0],
-        (_, res, ctx) => res(ctx.json({ name: venueName })))
-    )
-  })
-  it('should fetch and display render venue correctly', async () => {
-    mockServer.use(
-      rest.post(
-        WifiUrlsInfo.getApCompatibilitiesVenue.url,
-        (_, res, ctx) => res(ctx.json({ apCompatibilities: mockApCompatibilitiesVenue.apCompatibilities.slice(1, 2) })))
-    )
+  it('should render correctly', async () => {
+    const edgeData = transformedMockEdgeCompatibilitiesVenue.compatibilities[0]
+    edgeData.incompatibleFeatures = edgeData.incompatibleFeatures?.filter(i => i.featureName === IncompatibilityFeatures.SD_LAN)
 
     render(
       <Provider>
-        <ApGeneralCompatibilityDrawer
-          visible={true}
-          type={ApCompatibilityType.VENUE}
-          venueId={venueId}
-          featureName={IncompatibilityFeatures.BETA_DPSK3}
-          venueName={venueName}
-          onClose={mockedCloseDrawer}
-        />
-      </Provider>, {
-        route: { params: { tenantId, venueId }, path: '/:tenantId' }
-      })
-
-    const compatibilityItems = await screen.findAllByTestId('CompatibilityItem')
-    expect(compatibilityItems.length).toBe(1)
-
-    const features = screen.getAllByTestId('FeatureItem')
-    expect(features.length).toBe(1)
-
-    const wifi = compatibilityItems[0]
-    expect(await within(wifi).findByText(/AP Firmware/)).toBeInTheDocument()
-    expect(within(wifi).getByText(/Test Venue/)).toBeInTheDocument()
-    expect(within(wifi).getByText('6.2.3.103.250')).toBeValid()
-    expect(within(wifi).getByText('1 / 1')).toBeValid()
-
-    const icon = await screen.findByTestId('CloseSymbol')
-    expect(icon).toBeVisible()
-    expect(mockGetVenue).toBeCalledTimes(0)
-  })
-
-  it('should fetch and display render network correctly', async () => {
-    render(
-      <Provider>
-        <ApGeneralCompatibilityDrawer
-          visible={true}
-          type={ApCompatibilityType.NETWORK}
-          networkId={networkId}
-          featureName={IncompatibilityFeatures.BETA_DPSK3}
-          onClose={mockedCloseDrawer}
-        />
-      </Provider>, {
-        route: { params: { tenantId, networkId }, path: '/:tenantId' }
-      })
-    expect(await screen.findByText('6.2.3.103.251')).toBeInTheDocument()
-    expect(await screen.findByText('Wi-Fi 6')).toBeInTheDocument()
-    const icon = await screen.findByTestId('CloseSymbol')
-    expect(icon).toBeVisible()
-    expect(mockGetVenue).toBeCalledTimes(0)
-  })
-
-  it('should fetch and display render alone correctly', async () => {
-    render(
-      <Provider>
-        <ApGeneralCompatibilityDrawer
-          visible={true}
-          type={ApCompatibilityType.ALONE}
-          featureName={IncompatibilityFeatures.BETA_DPSK3}
-          onClose={mockedCloseDrawer}
-        />
-      </Provider>, {
-        route: { params: { tenantId, featureName }, path: '/:tenantId' }
-      })
-    await screen.findByText(/To use the/)
-    screen.getByText('DSAE')
-    expect(screen.getByText('6.2.3.103.251')).toBeInTheDocument()
-    const icon = screen.getByTestId('CloseSymbol')
-    expect(icon).toBeVisible()
-    expect(mockGetVenue).toBeCalledTimes(0)
-  })
-
-  it('should direct display render correctly(Devices of Venue banner) with given data', async () => {
-    mockedCloseDrawer.mockClear()
-    render(
-      <Provider>
-        <ApGeneralCompatibilityDrawer
-          isMultiple
-          visible={true}
-          data={mockApCompatibilitiesVenue.apCompatibilities}
-          onClose={mockedCloseDrawer}
-        />
-      </Provider>, {
-        route: { params, path: '/:tenantId' }
-      })
-
-    expect(await screen.findByText('Incompatibility Details')).toBeInTheDocument()
-    const descriptions = await screen.findAllByText(/Some features are not enabled on specific access points in this venue/)
-    expect(descriptions.length).toBe(2)
-    expect(screen.getByText('7.0.0.0.123')).toBeInTheDocument()
-    const icon = screen.getByTestId('CloseSymbol')
-    expect(icon).toBeVisible()
-    await userEvent.click(icon)
-    expect(mockedCloseDrawer).toBeCalledTimes(1)
-  })
-
-  it('should direct display render correctly(Device of Venue)', async () => {
-    const apName = 'AP-Test'
-    mockedCloseDrawer.mockClear()
-    mockServer.use(
-      rest.post(
-        WifiUrlsInfo.getApCompatibilitiesVenue.url,
-        (_, res, ctx) => res(ctx.json({ apCompatibilities: mockApCompatibilitiesVenue.apCompatibilities.slice(0, 1) })))
-    )
-
-    render(
-      <Provider>
-        <ApGeneralCompatibilityDrawer
-          isMultiple
-          visible={true}
-          venueId={params.venueId}
-          apId={'001001001'}
-          apName={apName}
-          onClose={mockedCloseDrawer}
-        />
-      </Provider>, {
-        route: { params, path: '/:tenantId' }
-      })
-
-    expect(await screen.findByText(`Incompatibility Details: ${apName}`)).toBeInTheDocument()
-    const compatibilityItems = await screen.findAllByTestId('CompatibilityItem')
-    expect(compatibilityItems.length).toBe(1)
-    const features = screen.getAllByTestId('FeatureItem')
-    expect(features.length).toBe(1)
-    expect(await screen.findByText(/The following features are not enabled on this access point/)).toBeInTheDocument()
-    expect(screen.getByText('7.0.0.0.123')).toBeInTheDocument()
-    const icon = screen.getByTestId('CloseSymbol')
-    expect(icon).toBeVisible()
-    await userEvent.click(icon)
-    expect(mockedCloseDrawer).toBeCalledTimes(1)
-  })
-
-  it('should also fetch requiredFeatures and merge data', async () => {
-    const mockData = cloneDeep(mockApCompatibilitiesVenue)
-    mockData.apCompatibilities[0].incompatibleFeatures![0].featureName = IncompatibilityFeatures.SD_LAN
-    mockData.apCompatibilities[1].incompatibleFeatures![0].featureName = IncompatibilityFeatures.TUNNEL_PROFILE
-
-    mockServer.use(
-      rest.post(
-        WifiUrlsInfo.getApCompatibilitiesVenue.url,
-        (req, res, ctx) => {
-          const payload = get(req.body, 'featureName')
-          let returnVal
-          if (payload === IncompatibilityFeatures.TUNNEL_PROFILE) {
-            returnVal = mockData.apCompatibilities.slice(1, 2)
-          } else {
-            returnVal = mockData.apCompatibilities.slice(0, 1)
-          }
-          return res(ctx.json({ apCompatibilities: returnVal }))
-        })
-    )
-
-    render(
-      <Provider>
-        <ApGeneralCompatibilityDrawer
-          visible={true}
-          type={ApCompatibilityType.VENUE}
-          venueId={venueId}
-          venueName={venueName}
+        <FeatureCrossDeviceTypeCompatibility
+          data={{
+            [CompatibilityDeviceEnum.AP]: mockApCompatibilitiesVenue.apCompatibilities[0],
+            [CompatibilityDeviceEnum.EDGE]: edgeData
+          }}
           featureName={IncompatibilityFeatures.SD_LAN}
-          requiredFeatures={[IncompatibilityFeatures.TUNNEL_PROFILE]}
-          isFeatureEnabledRegardless
-          onClose={mockedCloseDrawer}
         />
       </Provider>, {
-        route: { params: { tenantId, venueId }, path: '/:tenantId' }
+        route: { params, path: '/:tenantId' }
       })
 
+    const description = screen.getByText(/To enable the/)
+    expect(description).toHaveTextContent('SD-LAN feature completely')
     const compatibilityItems = await screen.findAllByTestId('CompatibilityItem')
-    expect(compatibilityItems.length).toBe(1)
-    expect(await within(compatibilityItems[0]).findByText(/AP Firmware/)).toBeInTheDocument()
+    expect(compatibilityItems.length).toBe(2)
+    screen.getByText('SmartEdge')
+    screen.getByText('Wi-Fi')
 
-    const features = screen.getAllByTestId('FeatureItem')
-    expect(features.length).toBe(2)
+    expect(screen.getByRole('link', { name: /AP Firmware/ })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /SmartEdge Firmware/ })).toBeInTheDocument()
 
-    const feature1 = features[0]
-    expect(within(feature1).getByText('SD-LAN')).toBeValid()
-    expect(within(feature1).getByText('7.0.0.0.123')).toBeValid()
-    expect(within(feature1).getByText('1 / 1')).toBeValid()
+    const wifiBlock = compatibilityItems[0]
+    const features = within(wifiBlock).getAllByTestId('FeatureItem')
+    expect(features.length).toBe(1)
+    within(features[0]).getByText(/Minimum required version/)
+    within(features[0]).getByText('7.0.0.0.123')
+    within(features[0]).getByText('Incompatible Access Points (Currently)')
 
-    const feature2 = features[1]
-    expect(within(feature2).getByText('Tunnel Profile')).toBeValid()
-    expect(within(feature2).getByText('6.2.3.103.250')).toBeValid()
-    expect(within(feature2).getByText('1 / 1')).toBeValid()
+    const edgeBlock = compatibilityItems[1]
+    const features2 = within(edgeBlock).getAllByTestId('FeatureItem')
+    expect(features2.length).toBe(1)
+    within(features2[0]).getByText(/Minimum required version/)
+    within(features2[0]).getByText('2.1.0.200')
+    within(features2[0]).getByText('Incompatible SmartEdges (Currently)')
   })
 })
