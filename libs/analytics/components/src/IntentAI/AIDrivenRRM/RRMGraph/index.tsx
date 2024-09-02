09c561f7a444686d7909fbd8821e23f73bfdcb35
AaronJoelTan <aaron.tan@commscope.com>
2024-09-02 11:05:18 +0800

    MLSA-8278: Enable conditional blurring

--- libs/analytics/components/src/IntentAI/AIDrivenRRM/RRMGraph/index.tsx
+++ libs/analytics/components/src/IntentAI/AIDrivenRRM/RRMGraph/index.tsx
@@ -110,6 +110,10 @@ export const IntentAIRRMGraph = ({
     summaryUrlAfter?: string,
     blurData: boolean
   }) => {
+
+  // eslint-disable-next-line max-len
+  const STATIC_SUMMARY_URL='data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22350%22%20height%3D%22300%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20viewBox%3D%220%200%20350%20300%22%3E%0A%3Crect%20width%3D%22350%22%20height%3D%22300%22%20x%3D%220%22%20y%3D%220%22%20id%3D%220%22%20fill%3D%22none%22%20fill-opacity%3D%221%22%3E%3C%2Frect%3E%0A%3Cpath%20d%3D%22M209.9%20152.4L181.6%2094.5%22%20transform%3D%22matrix(0.405%2C0%2C0%2C0.405%2C104.125%2C89.25)%22%20fill%3D%22none%22%20stroke%3D%22%23ACAEB0%22%20stroke-width%3D%224.938271604938271%22%20stroke-opacity%3D%220.5%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M209.9%20152.4L239.1%20209.5%22%20transform%3D%22matrix(0.405%2C0%2C0%2C0.405%2C104.125%2C89.25)%22%20fill%3D%22none%22%20stroke%3D%22%23ACAEB0%22%20stroke-width%3D%224.938271604938271%22%20stroke-opacity%3D%220.5%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M-22.1%2056.8L-36.9%20114.8%22%20transform%3D%22matrix(0.405%2C0%2C0%2C0.405%2C104.125%2C89.25)%22%20fill%3D%22none%22%20stroke%3D%22%23ACAEB0%22%20stroke-width%3D%224.938271604938271%22%20stroke-opacity%3D%220.5%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C270.0256%2C188.998)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C187.4%2C247.6979)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C189.1212%2C150.9755)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C111.9058%2C219.6111)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C89.1963%2C135.7297)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C177.6692%2C127.5285)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C95.1573%2C112.2739)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C173.3462%2C51.3092)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C255.211%2C91.7876)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M1%200A1%201%200%201%201%201%20-0.0001%22%20transform%3D%22matrix(7.716%2C0%2C0%2C7.716%2C200.9675%2C174.0886)%22%20fill%3D%22%23ACAEB0%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%220.12960082944530846%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M-5%20-5l10%200l0%2010l-10%200Z%22%20transform%3D%22translate(20%2020)%22%20fill%3D%22rgb(0%2C0%2C0)%22%20fill-opacity%3D%220%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%220%22%3E%3C%2Fpath%3E%0A%3Cpath%20d%3D%22M0%200l0%200l0%200l0%200Z%22%20transform%3D%22translate(20%2020)%22%20fill%3D%22none%22%3E%3C%2Fpath%3E%0A%3C%2Fsvg%3E'
+
   const { $t } = useIntl()
   const title = $t({ defaultMessage: 'Key Performance Indications' })
   const [ visible, setVisible ] = useState<boolean>(false)
@@ -125,11 +129,14 @@ export const IntentAIRRMGraph = ({
     <Card>
       <UI.GraphWrapper key={'graph-details'}>
         {useGraph(crrmData, detailsZoomScale, false,
-          summaryUrlBefore, summaryUrlAfter)}
+          blurData ? STATIC_SUMMARY_URL : summaryUrlBefore,
+          blurData ? STATIC_SUMMARY_URL : summaryUrlAfter)}
         <GraphTitle details={details} />
       </UI.GraphWrapper>
     </Card>
-    <UI.ViewMoreButton onClick={showDrawer} children={$t({ defaultMessage: 'View More' })} />
+    {!blurData &&
+      <UI.ViewMoreButton onClick={showDrawer} children={$t({ defaultMessage: 'View More' })} />
+    }
     <Drawer
       key={key}
       drawerType={DrawerTypes.FullHeight}
