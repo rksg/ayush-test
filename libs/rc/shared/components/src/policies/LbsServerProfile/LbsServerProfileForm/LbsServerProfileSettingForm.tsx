291b511035578e2c55c44d93f9265c634d479e75
whsu1 <weiwen.hsu@commscope.com>
2024-09-02 13:51:51 +0800

    ACX-66516: Revise field 'lbsVenueName' to 'lbsServerVenueName

--- libs/rc/shared/components/src/policies/LbsServerProfile/LbsServerProfileForm/LbsServerProfileSettingForm.tsx
+++ libs/rc/shared/components/src/policies/LbsServerProfile/LbsServerProfileForm/LbsServerProfileSettingForm.tsx
@@ -4,7 +4,7 @@ import { useIntl }                  from 'react-intl'
 import { PasswordInput }      from '@acx-ui/components'
 import {
   checkObjectNotExists,
-  lbsVenueNameRegExp,
+  lbsServerVenueNameRegExp,
   servicePolicyNameRegExp,
   networkWifiSecretRegExp,
   domainNameRegExp,
@@ -54,13 +54,13 @@ const LbsServerProfileSettingForm = ( props: { list : TableResult<LbsServerProfi
       />
       <div>
         <Form.Item
-          name='lbsVenueName'
+          name='lbsServerVenueName'
           label={$t({ defaultMessage: 'LBS <VenueSingular></VenueSingular> Name' })}
           rules={[
             { required: true },
             { min: 2 },
             { max: 16 },
-            { validator: (_, value) => lbsVenueNameRegExp(value) }
+            { validator: (_, value) => lbsServerVenueNameRegExp(value) }
           ]}
           initialValue={''}
           children={<Input />}
