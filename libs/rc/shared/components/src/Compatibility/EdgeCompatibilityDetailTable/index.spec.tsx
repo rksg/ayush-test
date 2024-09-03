559e4abb8645a78f0b4e2acc793cdec65b51d80d
Cheng, Amy <Amy.Cheng@commscope.com>
2024-09-04 02:08:25 +0800

    ACX-62016: add update now/schedule update on edge compatibility

---
+++ libs/rc/shared/components/src/Compatibility/EdgeCompatibilityDetailTable/index.spec.tsx
@@ -0,0 +1,176 @@
+import userEvent     from '@testing-library/user-event'
+import { cloneDeep } from 'lodash'
+import moment        from 'moment'
+import { rest }      from 'msw'
+
+import { firmwareApi } from '@acx-ui/rc/services'
+import {
+  EdgeCompatibilityFixtures,
+  EdgeFirmwareFixtures,
+  FirmwareUrlsInfo
+} from '@acx-ui/rc/utils'
+import { Provider, store }                             from '@acx-ui/store'
+import { mockServer, render, screen, waitFor, within } from '@acx-ui/test-utils'
+
+import { EdgeCompatibilityDetailTable } from '.'
+
+const { mockEdgeCompatibilitiesVenue } = EdgeCompatibilityFixtures
+const mockAvailableVersions = cloneDeep(EdgeFirmwareFixtures.mockAvailableVersions).slice(0, 1)
+mockAvailableVersions.forEach(item => {
+  item.id = '2.1.0.600'
+  item.name = '2.1.0.600'
+})
+const mockedVenueFirmwareList = cloneDeep(EdgeFirmwareFixtures.mockedVenueFirmwareList)
+mockedVenueFirmwareList[0].id = 'mock_venue_id'
+
+jest.spyOn(Date, 'now').mockImplementation(() => {
+  return new Date('2023-01-20T12:33:37.101+00:00').getTime()
+})
+
+const mockedUpdateNow = jest.fn()
+const mockedUpdateSchedule = jest.fn()
+describe('EdgeCompatibilityDetailTable', () => {
+  let params: { tenantId: string }
+  beforeEach(() => {
+    params = {
+      tenantId: 'ecc2d7cf9d2342fdb31ae0e24958fcac'
+    }
+
+    store.dispatch(firmwareApi.util.resetApiState())
+
+    mockServer.use(
+      rest.post(
+        FirmwareUrlsInfo.getVenueEdgeFirmwareList.url,
+        (_req, res, ctx) => res(ctx.json(mockedVenueFirmwareList))
+      ),
+      rest.get(
+        FirmwareUrlsInfo.getAvailableEdgeFirmwareVersions.url,
+        (_req, res, ctx) => res(ctx.json(mockAvailableVersions))
+      ),
+      rest.patch(
+        FirmwareUrlsInfo.updateEdgeFirmware.url,
+        (req, res, ctx) => {
+          mockedUpdateNow(req.body)
+          return res(ctx.status(202))
+        }
+      ),
+      rest.post(
+        FirmwareUrlsInfo.updateEdgeVenueSchedules.url,
+        (req, res, ctx) => {
+          mockedUpdateSchedule(req.body)
+          return res(ctx.status(202))
+        }
+      )
+    )
+  })
+
+  it('should create table successfully', async () => {
+    render(
+      <Provider>
+        <EdgeCompatibilityDetailTable
+          data={mockEdgeCompatibilitiesVenue.compatibilities[0].incompatibleFeatures}
+          venueId='mock_venue_id'
+        />
+      </Provider>
+    )
+
+    const rows = await basicCheck()
+    expect(rows.length).toBe(3) // including header
+    expect(screen.getByRole('columnheader', { name: 'Incompatible SmartEdges' })).toBeVisible()
+
+    const row1 = screen.getByRole('row', { name: /SD-LAN 1 2.1.0.200/i })
+    screen.getByRole('row', { name: /Tunnel Profile 2 2.1.0.400/i })
+    await userEvent.click(within(row1).getByRole('checkbox'))
+    await screen.findByRole('button', { name: 'Update Version Now' })
+  })
+
+  it('should be able to do update now', async () => {
+    render(
+      <Provider>
+        <EdgeCompatibilityDetailTable
+          data={mockEdgeCompatibilitiesVenue.compatibilities[0].incompatibleFeatures}
+          venueId='mock_venue_id'
+        />
+      </Provider>
+    )
+
+    const rows = await basicCheck()
+    expect(rows.length).toBe(3) // including header
+    expect(screen.getByRole('columnheader', { name: 'Incompatible SmartEdges' })).toBeVisible()
+
+    const row1 = screen.getByRole('row', { name: /SD-LAN 1 2.1.0.200/i })
+    screen.getByRole('row', { name: /Tunnel Profile 2 2.1.0.400/i })
+    await userEvent.click(within(row1).getByRole('checkbox'))
+    await userEvent.click(await screen.findByRole('button', { name: 'Update Version Now' }))
+    const submitBtn = await screen.findByRole('button', { name: 'Run Update' })
+    await userEvent.click(submitBtn)
+    await waitFor(() => expect(mockedUpdateNow).toBeCalledWith({
+      state: 'UPDATE_NOW',
+      version: '2.1.0.600'
+    }))
+    await waitFor(() => expect(submitBtn).not.toBeVisible())
+  })
+
+  it('should be able to do change update schedule', async () => {
+    render(
+      <Provider>
+        <EdgeCompatibilityDetailTable
+          data={mockEdgeCompatibilitiesVenue.compatibilities[0].incompatibleFeatures}
+          venueId='mock_venue_id'
+        />
+      </Provider>
+    )
+
+    const rows = await basicCheck()
+    expect(rows.length).toBe(3) // including header
+    expect(screen.getByRole('columnheader', { name: 'Incompatible SmartEdges' })).toBeVisible()
+
+    const row1 = screen.getByRole('row', { name: /SD-LAN 1 2.1.0.200/i })
+    screen.getByRole('row', { name: /Tunnel Profile 2 2.1.0.400/i })
+    await userEvent.click(within(row1).getByRole('checkbox'))
+    await userEvent.click(await screen.findByRole('button', { name: 'Schedule Version Update' }))
+
+    const nowDateStr = moment().add(2, 'days').format('YYYY-MM-DD')
+    const nowDayStr = nowDateStr.split('-')[2]
+    await userEvent.click(await screen.findByRole('radio',
+      { name: '2.1.0.600 (Release - Recommended) - 02/23/2023' }
+    ))
+    await userEvent.click(screen.getByPlaceholderText('Select date'))
+    const cell = await screen.findByRole('cell', { name: new RegExp(nowDateStr) })
+    await userEvent.click(within(cell).getByText(new RegExp(nowDayStr)))
+    await userEvent.click(await screen.findByRole('radio', { name: /12 am \- 02 am/i }))
+    const submitBtn = await screen.findByRole('button', { name: 'Save' })
+    await userEvent.click(submitBtn)
+    await waitFor(() => expect(mockedUpdateSchedule).toBeCalledWith({
+      date: '2023-01-22',
+      time: '00:00-02:00',
+      version: '2.1.0.600'
+    }))
+    await waitFor(() => expect(submitBtn).not.toBeVisible())
+  })
+
+  it('should not have incompatible count when requirementOnly', async () => {
+    render(
+      <Provider>
+        <EdgeCompatibilityDetailTable
+          data={mockEdgeCompatibilitiesVenue.compatibilities[0].incompatibleFeatures}
+          requirementOnly
+        />
+      </Provider>, {
+        route: { params, path: '/:tenantId/services/edgeMvSdLan/list' }
+      }
+    )
+
+    const rows = await basicCheck()
+    expect(rows.length).toBe(3) // including header
+    expect(screen.queryByRole('columnheader', { name: 'Incompatible SmartEdges' })).toBeNull()
+
+    screen.getByRole('row', { name: /SD-LAN 2.1.0.200/i })
+    screen.getByRole('row', { name: /Tunnel Profile 2.1.0.400/i })
+  })
+})
+
+const basicCheck= async () => {
+  const rows = await screen.findAllByRole('row')
+  return rows
+}
---
+++ libs/rc/shared/components/src/Compatibility/EdgeCompatibilityDetailTable/index.tsx
@@ -0,0 +1,171 @@
+import { useState } from 'react'
+
+import { sumBy }   from 'lodash'
+import moment      from 'moment'
+import { useIntl } from 'react-intl'
+
+import { Table, TableProps }                                                                                                                                 from '@acx-ui/components'
+import { useGetAvailableEdgeFirmwareVersionsQuery, useGetVenueEdgeFirmwareListQuery, useUpdateEdgeFirmwareNowMutation, useUpdateEdgeVenueSchedulesMutation } from '@acx-ui/rc/services'
+import { EdgeFirmwareVersion, EdgeUpdateScheduleRequest, EntityCompatibility }                                                                               from '@acx-ui/rc/utils'
+import { EdgeScopes }                                                                                                                                        from '@acx-ui/types'
+import { filterByAccess, hasPermission }                                                                                                                     from '@acx-ui/user'
+import { compareVersions }                                                                                                                                   from '@acx-ui/utils'
+
+import { EdgeChangeScheduleDialog } from '../../EdgeFirmware/ChangeScheduleDialog'
+import { EdgeUpdateNowDialog }      from '../../EdgeFirmware/UpdateNowDialog'
+
+interface EdgeCompatibilityDetailTableData {
+  featureName: string,
+  requiredFw: string,
+  incompatible?: number,
+}
+
+interface EdgeCompatibilityDetailTableProps {
+  data: EntityCompatibility['incompatibleFeatures'],
+  requirementOnly?: boolean,
+  venueId?: string,  // is required when `requirementOnly === false`
+}
+
+const useColumns = () => {
+  const { $t } = useIntl()
+
+  const defaultColumns: TableProps<EdgeCompatibilityDetailTableData>['columns'] = [{
+    title: $t({ defaultMessage: 'Incompatible Feature' }),
+    key: 'featureName',
+    dataIndex: 'featureName',
+    defaultSortOrder: 'ascend'
+  }, {
+    title: $t({ defaultMessage: 'Incompatible SmartEdges' }),
+    key: 'incompatible',
+    dataIndex: 'incompatible',
+    align: 'center'
+  }, {
+    title: $t({ defaultMessage: 'Min. Required Version' }),
+    key: 'requiredFw',
+    dataIndex: 'requiredFw'
+  }]
+
+  return defaultColumns
+}
+export const EdgeCompatibilityDetailTable = (props: EdgeCompatibilityDetailTableProps) => {
+  const { $t } = useIntl()
+
+  const { data, requirementOnly = false, venueId } = props
+  const [updateNowVisible, setUpdateNowVisible] = useState<string|undefined>()
+  const [scheduleUpdateVisible, setScheduleUpdateVisible] = useState<string|undefined>()
+  const [selectedRowKeys, setSelectedRowKeys] = useState([])
+
+  const [updateNow] = useUpdateEdgeFirmwareNowMutation()
+  const [updateSchedule] = useUpdateEdgeVenueSchedulesMutation()
+  const { data: availableVersions } = useGetAvailableEdgeFirmwareVersionsQuery({},
+    { skip: requirementOnly })
+  const { data: venueFirmwareList } = useGetVenueEdgeFirmwareListQuery({},
+    { skip: requirementOnly })
+
+  const columns = useColumns()
+
+  const handleUpdateNowSubmit = async (data: string) => {
+    const payload = { version: data }
+
+    try {
+      await updateNow({
+        params: { venueId },
+        payload
+      })
+      setSelectedRowKeys([])
+    } catch (error) {
+      console.log(error) // eslint-disable-line no-console
+    }
+  }
+
+  const handleScheduleSubmit = async (value: EdgeUpdateScheduleRequest) => {
+    const payload = {
+      ...value,
+      date: moment(value.date).format('yyyy-MM-DD')
+    }
+
+    try {
+      await updateSchedule({
+        params: { venueId },
+        payload
+      })
+      setSelectedRowKeys([])
+    } catch (error) {
+      console.log(error) // eslint-disable-line no-console
+    }
+  }
+
+  const rowActions: TableProps<EdgeCompatibilityDetailTableData>['rowActions'] = [{
+    label: $t({ defaultMessage: 'Update Version Now' }),
+    scopeKey: [EdgeScopes.UPDATE],
+    visible: (selectedRows) => {
+      if (!selectedRows?.length) return false
+
+      const maxTargetVersion = getMaxMinVersion(selectedRows)
+      return (getFilteredVersions(availableVersions, maxTargetVersion)?.length ?? 0) > 0
+    },
+    onClick: (selectedRows: EdgeCompatibilityDetailTableData[]) => {
+      const maxTargetVersion = getMaxMinVersion(selectedRows)
+      setUpdateNowVisible(maxTargetVersion)
+    }
+  }, {
+    label: $t({ defaultMessage: 'Schedule Version Update' }),
+    scopeKey: [EdgeScopes.UPDATE],
+    visible: (selectedRows) => {
+      if (!selectedRows?.length) return false
+
+      const targetData = venueFirmwareList?.filter(i => i.id === venueId)
+      if (!targetData?.[0]?.nextSchedule) return false
+
+      const maxTargetVersion = getMaxMinVersion(selectedRows)
+      return (getFilteredVersions(availableVersions, maxTargetVersion)?.length ?? 0) > 0
+    },
+    onClick: (selectedRows: EdgeCompatibilityDetailTableData[]) => {
+      const maxTargetVersion = getMaxMinVersion(selectedRows)
+      setScheduleUpdateVisible(maxTargetVersion)
+    }
+  }]
+
+  const transformed = data.map(feature => ({
+    ...feature.featureRequirement,
+    incompatible: sumBy(feature.incompatibleDevices, (d) => d.count)
+  })) as EdgeCompatibilityDetailTableData[]
+
+  const showCheckbox = hasPermission({ scopes: [EdgeScopes.UPDATE] }) && !requirementOnly
+
+  return <>
+    <Table
+      rowKey='featureName'
+      columns={columns.filter(i => requirementOnly ? i.dataIndex !== 'incompatible' : true)}
+      dataSource={transformed}
+      rowActions={filterByAccess(rowActions)}
+      rowSelection={showCheckbox && {
+        type: 'checkbox',
+        selectedRowKeys
+      }}
+    />
+
+    <EdgeUpdateNowDialog
+      visible={!!updateNowVisible}
+      onCancel={() => setUpdateNowVisible(undefined)}
+      onSubmit={handleUpdateNowSubmit}
+      availableVersions={getFilteredVersions(availableVersions, updateNowVisible)}
+    />
+    <EdgeChangeScheduleDialog
+      visible={!!scheduleUpdateVisible}
+      onCancel={() => setScheduleUpdateVisible(undefined)}
+      onSubmit={handleScheduleSubmit}
+      availableVersions={getFilteredVersions(availableVersions, scheduleUpdateVisible)}
+    />
+  </>
+}
+
+const getMaxMinVersion = (selectedRows: EdgeCompatibilityDetailTableData[]) => {
+  const sorted = selectedRows.sort((a, b) => compareVersions(a?.requiredFw, b?.requiredFw))
+  return sorted[0].requiredFw
+}
+
+const getFilteredVersions = (availableVersions?: EdgeFirmwareVersion[], minVer?: string) => {
+  if (!availableVersions || !minVer) return availableVersions
+  return availableVersions.filter(fw => compareVersions(fw.id, minVer) >= 0)
+}
