7ed8be24d8314acc321883f6a1b12fa6b28ac4b4
ychang3 <yy.chang@commscope.com>
2024-09-03 19:03:28 +0800

    Revert "MLSA-8150: Temporary remove form files to fix lint & UT issues"
    This reverts commit b4db5dcb143c9ccdd455b46402066daee9b3233f.

--- libs/analytics/components/src/IntentAI/AirFlexAI/IntentAIForm/index.tsx
+++ libs/analytics/components/src/IntentAI/AirFlexAI/IntentAIForm/index.tsx
@@ -1,61 +1,61 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 
-// import _           from 'lodash'
-// import { useIntl } from 'react-intl'
+import _           from 'lodash'
+import { useIntl } from 'react-intl'
 
-// import { StepsForm } from '@acx-ui/components'
+import { StepsForm } from '@acx-ui/components'
 
-// import { IntentWizardHeader } from '../../common/IntentWizardHeader'
-// import { useIntentContext }   from '../../IntentContext'
+import { IntentWizardHeader } from '../../common/IntentWizardHeader'
+import { useIntentContext }   from '../../IntentContext'
 
-// import { Introduction } from './Introduction'
-// import { Priority }     from './Priority'
-// import { Settings }     from './Settings'
-// import { Summary }      from './Summary'
+import { Introduction } from './Introduction'
+import { Priority }     from './Priority'
+import { Settings }     from './Settings'
+import { Summary }      from './Summary'
 
-// export function IntentAIForm () {
-//   const { intent } = useIntentContext()
-//   const { $t } = useIntl()
+export function IntentAIForm () {
+  const { intent } = useIntentContext()
+  const { $t } = useIntl()
 
-//   //const queryResult = useIntentAICRRMQuery()
-//   //const crrmData = queryResult.data!
+  //const queryResult = useIntentAICRRMQuery()
+  //const crrmData = queryResult.data!
 
 
-//   const defaultValue = {
-//     preferences: {
-//       crrmFullOptimization: true
-//     }
-//   }
+  const defaultValue = {
+    preferences: {
+      crrmFullOptimization: true
+    }
+  }
 
-//   return (<>
-//     <IntentWizardHeader />
+  return (<>
+    <IntentWizardHeader />
 
-//     <StepsForm
-//       buttonLabel={{
-//         submit: $t({ defaultMessage: 'Apply' })
-//       }}
-//       initialValues={_.merge(defaultValue, intent)}
-//     >
-//       <StepsForm.StepForm
-//         title={$t({ defaultMessage: 'Introduction' })}
-//         children={<Introduction/>}
-//       />
-//       <StepsForm.StepForm
-//         title={$t({ defaultMessage: 'Intent Priority' })}
-//         children={<Priority />}
-//       />
-//       <StepsForm.StepForm
-//         title={$t({ defaultMessage: 'Settings' })}
-//         children={<Settings />}
-//       />
-//       {/* <StepsForm.StepForm
-//         title={$t({ defaultMessage: 'Summary' })}
-//         children={<Summary
-//           summaryUrlBefore={summaryUrlBefore}
-//           summaryUrlAfter={summaryUrlAfter}
-//           crrmData={crrmData}
-//         />}
-//       /> */}
-//     </StepsForm>
-//   </>)
-// }
+    <StepsForm
+      buttonLabel={{
+        submit: $t({ defaultMessage: 'Apply' })
+      }}
+      initialValues={_.merge(defaultValue, intent)}
+    >
+      <StepsForm.StepForm
+        title={$t({ defaultMessage: 'Introduction' })}
+        children={<Introduction/>}
+      />
+      <StepsForm.StepForm
+        title={$t({ defaultMessage: 'Intent Priority' })}
+        children={<Priority />}
+      />
+      <StepsForm.StepForm
+        title={$t({ defaultMessage: 'Settings' })}
+        children={<Settings />}
+      />
+      {/* <StepsForm.StepForm
+        title={$t({ defaultMessage: 'Summary' })}
+        children={<Summary
+          summaryUrlBefore={summaryUrlBefore}
+          summaryUrlAfter={summaryUrlAfter}
+          crrmData={crrmData}
+        />}
+      /> */}
+    </StepsForm>
+  </>)
+}
