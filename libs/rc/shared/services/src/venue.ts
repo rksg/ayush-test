cede50baf85451ac31afa538ef2a7a51e8d78528
KYEH <karen.yeh@commscope.com>
2024-09-06 16:56:14 +0800

    ACX-65245: Disabled the settings when the profile is CLI

--- libs/rc/shared/services/src/venue.ts
+++ libs/rc/shared/services/src/venue.ts
@@ -1061,6 +1061,15 @@ export const venueApi = baseVenueApi.injectEndpoints({
         return{
           ...req
         }
+      },
+      providesTags: [{ type: 'Venue', id: 'SWITCH_SETTING' }],
+      async onCacheEntryAdded (requestArgs, api) {
+        await onSocketActivityChanged(requestArgs, api, (msg) => {
+          onActivityMessageReceived(msg,
+            ['UpdateVenueSwitchSetting'], () => {
+              api.dispatch(venueApi.util.invalidateTags([{ type: 'Venue', id: 'SWITCH_SETTING' }]))
+            })
+        })
       }
     }),
     updateVenueSwitchSetting: build.mutation<Venue, RequestPayload>({
