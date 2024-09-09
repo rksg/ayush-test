dfafba6aca5b5792eb32204321f69d6c8c75c739
Jerry Chen <Jerry.Chen@commscope.com>
2024-09-09 16:31:31 +0800

    ACX-61172: Add i18n support.

--- libs/rc/shared/components/src/WorkflowActionPreviewModal/WorkflowActionPreview/AupPreview.tsx
+++ libs/rc/shared/components/src/WorkflowActionPreviewModal/WorkflowActionPreview/AupPreview.tsx
@@ -1,4 +1,5 @@
 import { Typography, Space, Checkbox } from 'antd'
+import { useIntl }                     from 'react-intl'
 
 import { AupAction, GenericActionPreviewProps } from '@acx-ui/rc/utils'
 
@@ -6,8 +7,10 @@ import { ContentPreview } from './ContentPreview'
 
 
 export function AupPreview (props: GenericActionPreviewProps<AupAction>) {
+  const { $t } = useIntl()
   const { Text, Link } = Typography
   const { data, ...rest } = props
+
   return <ContentPreview
     title={data?.title}
     body={
@@ -16,7 +19,12 @@ export function AupPreview (props: GenericActionPreviewProps<AupAction>) {
         <br/>
         <Text >{data?.messageHtml}</Text>
         <br/>
-        <Checkbox><Text>I agree to the <Link>Terms & Conditions</Link></Text></Checkbox>
+        <Checkbox>
+          <Text>
+            {$t({ defaultMessage: 'I agree to the' })}{' '}
+            <Link>{$t({ defaultMessage: 'Terms & Conditions' })}</Link>
+          </Text>
+        </Checkbox>
       </Space>
     }
     {...rest}
