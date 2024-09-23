1221a3bb519d00d47ecd72d9a08dd7bd1853e8ad
cl1092 <Bess.Leng@commscope.com>
2024-09-23 22:19:57 +0800

    MLSA-8384: Updated UT

--- libs/common/components/src/components/ScheduleTipsModal/index.spec.tsx
+++ libs/common/components/src/components/ScheduleTipsModal/index.spec.tsx
@@ -1,20 +1,20 @@
 
-import React from 'react'
-
 import userEvent from '@testing-library/user-event'
 
-import { render, screen } from '@acx-ui/test-utils'
+import { render, screen, waitFor } from '@acx-ui/test-utils'
 
 import { BasicTipsModal } from './stories'
 
 describe('ScheduleTipsModal', () => {
   it('should render correct', async () => {
     render(<BasicTipsModal />)
+
     userEvent.click(await screen.findByText('Open'))
+    expect(await screen.findByRole('dialog')).toBeVisible()
     // eslint-disable-next-line max-len
-    expect(await screen.findByText('You can set custom schedule using the following options')).toBeVisible()
-    userEvent.click(await screen.findByText('Open'))
+    expect(await screen.findByText('You can set custom schedule using the following options:')).toBeVisible()
+    userEvent.click(await screen.findByText('OK'))
     // eslint-disable-next-line max-len
-    expect(await screen.findByText('You can set custom schedule using the following options')).not.toBeVisible()
+    await waitFor(async() => expect(await screen.findByRole('dialog')).toBeVisible())    
   })
 })
