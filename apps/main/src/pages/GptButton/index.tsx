378f4e9f188a66db0788c2b6195ca2864ffe4a33
KYEH <karen.yeh@commscope.com>
2024-09-23 18:45:15 +0800

    ACX-67458: Add modal for gpt

--- apps/main/src/pages/GptButton/index.tsx
+++ apps/main/src/pages/GptButton/index.tsx
@@ -1,57 +1,34 @@
 import { useState } from 'react'
 
-import { Badge } from 'antd'
 
-import { AlarmsDrawer }   from '@acx-ui/rc/components'
-import {
-  useGetAlarmCountQuery }  from '@acx-ui/rc/services'
-import { useParams } from '@acx-ui/react-router-dom'
+import { useIntl } from 'react-intl'
+
+import { Modal, ModalType } from '@acx-ui/components'
 
 import { ReactComponent as Logo } from './assets/gptDog.svg'
 import * as UI                    from './styledComponents'
 
 export default function RuckusGptButton () {
-  const params = useParams()
-
-  const payload = { filters: { } }
-  const { data } = useGetAlarmCountQuery({ params, payload })
-
   const [visible, setVisible] = useState(false)
+  const { $t } = useIntl()
 
-  const getCount = function () {
-    if (data?.summary?.alarms?.totalCount) {
-      const clearedAlarms = data.summary.alarms.summary?.clear || 0
-      return data.summary.alarms.totalCount - clearedAlarms
-    } else {
-      return 0
-    }
-  }
 
-  const getOffset = function (totalCount: number) {
-    if(totalCount >= 1000){
-      return -9
-    }else if(totalCount >= 100){
-      return -6
-    }
-    return -3
-  }
 
   return <>
-    <Badge
-      count={getCount()}
-      overflowCount={999}
-      offset={[getOffset(getCount()), 0]}
-      children={<UI.ButtonSolid
-        icon={<Logo />}
-        onClick={()=> {
-          setVisible(!visible)
-          const event = new CustomEvent('showAlarmDrawer',
-            { detail: { data: { name: 'all' } } })
-          window.dispatchEvent(event)
-        }}
-      />}
+    <UI.ButtonSolid
+      icon={<Logo />}
+      onClick={()=> {
+        setVisible(!visible)
+      }}
     />
-    <AlarmsDrawer visible={visible} setVisible={setVisible}/>
+    <Modal
+      title={$t({ defaultMessage: 'Add Guest Pass Network' })}
+      type={ModalType.ModalStepsForm}
+      visible={visible}
+      mask={true}
+      children={<div/>}
+    />
+    {/* <AlarmsDrawer visible={visible} setVisible={setVisible}/> */}
   </>
 }
 
