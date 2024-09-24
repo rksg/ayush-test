0cab19108cb5081dc5fef697c8e61cdb64210e7b
Chu-Hsuan.Lee <Chu-Hsuan.Lee@commscope.com>
2024-09-24 15:29:30 +0800

    ACX-68369: fix lint

--- libs/rc/shared/components/src/EnrollmentPortalLink/index.tsx
+++ libs/rc/shared/components/src/EnrollmentPortalLink/index.tsx
@@ -3,6 +3,9 @@ import { ChatbotLink } from '@acx-ui/icons'
 export function EnrollmentPortalLink (props: { url: string }) {
   const { url } = props
   return <div>
-    <a href={url} target='_blank' rel='noreferrer'>{url.length > 55 ? url.substring(0, 55) + '...' : url}  <ChatbotLink/></a>
+    <a href={url} target='_blank' rel='noreferrer'>
+      {url.length > 55 ? url.substring(0, 55) + '...' : url}
+      <ChatbotLink/>
+    </a>
   </div>
 }
