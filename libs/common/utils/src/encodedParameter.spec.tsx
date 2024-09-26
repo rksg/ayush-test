18acac7eb4e0bae4a961427aaa4900eba37ce0f7
AaronJoelTan <aaron.tan@commscope.com>
2024-09-26 13:50:29 +0800

    MLSA-8365: Restructure encodedParameter

--- libs/common/utils/src/encodedParameter.spec.tsx
+++ libs/common/utils/src/encodedParameter.spec.tsx
@@ -1,20 +1,8 @@
 import { BrowserRouter, MemoryRouter } from '@acx-ui/react-router-dom'
 import { renderHook }                  from '@acx-ui/test-utils'
 
-import { fixedEncodeURIComponent, useEncodedParameter, encodeParameter } from './encodedParameter'
+import { useEncodedParameter } from './encodedParameter'
 
-describe('fixedEncodeURIComponent', () => {
-  it('Should return encoded string', () => {
-    expect(fixedEncodeURIComponent('test~!@#$%^&*()'))
-      .toEqual('test~%21%40%23%24%25%5E%26%2A%28%29')
-  })
-})
-describe('encodeParameter', () => {
-  it('encodes a value', () => {
-    expect(encodeParameter<{ value: string }>({ value: 'test~!@#$%^&*()' }))
-      .toEqual('%7B%22value%22%3A%22test~%21%40%23%24%25%5E%26%2A%28%29%22%7D')
-  })
-})
 describe('useEncodedParameter', () => {
   it('reads and writes search parameters', () => {
     const { result } = renderHook(
