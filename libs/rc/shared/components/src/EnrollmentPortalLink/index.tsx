5e4d3a6a7bd0b1def984c178e2a2d6407afa8ada
Chu-Hsuan.Lee <Chu-Hsuan.Lee@commscope.com>
2024-09-30 17:09:05 +0800

    ACX-68369: fix the id for portal link

--- libs/rc/shared/components/src/EnrollmentPortalLink/index.tsx
+++ libs/rc/shared/components/src/EnrollmentPortalLink/index.tsx
@@ -6,7 +6,7 @@ import { ChatbotLink } from '@acx-ui/icons'
 export function EnrollmentPortalLink (props: { url: string }) {
   const { Link } = Typography
   const { url } = props
-  const id = 'portalLink' + url
+  const id = 'portalLink_' + url
   return <div style={{ display: 'flex' }}>
     <Link id={id} ellipsis={true} href={url} target='_blank' rel='noreferrer'>{url}</Link>
     <div style={{ cursor: 'pointer' }} onClick={()=>document.getElementById(id)?.click()}>
