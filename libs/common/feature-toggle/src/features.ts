ef27fa6243643ea7276f69dd55b1d7300b664078
Daniel Fowler <daniel.fowler@commscope.com>
2024-10-07 14:50:32 -0600

    ACX-69021: Fix workflow Tier name

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -199,7 +199,7 @@ export enum TierFeatures { // for Tier (ex: Beta) feature flag
   BETA_DPSK3 = 'BETA-DPSK3',
   SMART_EDGES = 'PLCY-EDGE',
   CONFIG_TEMPLATE = 'CONFIG-TEMPLATE',
-  WORKFLOW_ONBOARD = 'WORKFLOW_ONBOARD',
+  WORKFLOW_ONBOARD = 'WORKFLOW-ONBOARD',
 }
 
 interface BetaList {
