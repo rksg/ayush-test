930c062c760627409558bb88e1c74035b0a96640
Jack Lin <jack.lin@commscope.com>
2024-10-17 14:44:57 +0800

    ACX-69614: Client Isolation Allowlist by Venue option in wlan should can be disabled

--- libs/rc/shared/components/src/NetworkForm/NetworkForm.tsx
+++ libs/rc/shared/components/src/NetworkForm/NetworkForm.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars, max-len */
 import { useEffect, useRef, useState, createContext } from 'react'
 
-import { Form }                                                          from 'antd'
-import { get, isEqual, isNil, isNull, isUndefined, merge, omit, omitBy } from 'lodash'
-import { defineMessage, useIntl }                                        from 'react-intl'
+import { Form }                                                             from 'antd'
+import _, { get, isEqual, isNil, isNull, isUndefined, merge, omit, omitBy } from 'lodash'
+import { defineMessage, useIntl }                                           from 'react-intl'
 
 import { PageHeader, StepsForm, StepsFormLegacy, StepsFormLegacyInstance } from '@acx-ui/components'
 import { Features, useIsSplitOn }                                          from '@acx-ui/feature-toggle'
@@ -881,6 +881,7 @@ export function NetworkForm (props:{
   const handleEditNetwork = async (formData: NetworkSaveData) => {
     try {
       processEditData(formData)
+      const oldData = _.cloneDeep(saveContextRef.current)
       const payload = updateClientIsolationAllowlist(saveContextRef.current as NetworkSaveData)
       await updateNetworkInstance({
         params,
@@ -916,7 +917,7 @@ export function NetworkForm (props:{
           allRequests.push(handleNetworkVenues(payload.id, payload.venues, data?.venues))
         }
       }
-      allRequests.push(updateClientIsolationActivations(payload, data, payload.id))
+      allRequests.push(updateClientIsolationActivations(payload, oldData, payload.id))
 
       // eslint-disable-next-line max-len
       if (isEdgeSdLanMvEnabled && form.getFieldValue('sdLanAssociationUpdate') && payload.id && payload.venues) {
