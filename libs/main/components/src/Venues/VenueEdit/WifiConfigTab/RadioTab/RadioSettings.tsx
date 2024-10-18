9257ab2df85c64e87828438606e2264cd427b3d9
Sharon Chen <sharon.chen2@commscope.com>
2024-10-18 14:55:44 +0800

    ACX-70252: Update correct radio channels

--- libs/main/components/src/Venues/VenueEdit/WifiConfigTab/RadioTab/RadioSettings.tsx
+++ libs/main/components/src/Venues/VenueEdit/WifiConfigTab/RadioTab/RadioSettings.tsx
@@ -550,8 +550,8 @@ export function RadioSettings () {
       if (is6gChannelSeparation) {
         radioParams6G.allowedIndoorChannels =
           curForm?.getFieldValue(['radioParams6G', 'allowedIndoorChannels'])
-        radioParams6G.allowedOutdoorChannels = supportRadioChannels[ApRadioTypeEnum.Radio6G]?.['outdoor'] ?
-          curForm?.getFieldValue(['radioParams6G', 'allowedOutdoorChannels']) : undefined
+        radioParams6G.allowedOutdoorChannels =
+          curForm?.getFieldValue(['radioParams6G', 'allowedOutdoorChannels'])
       } else {
         radioParams6G.allowedChannels = curForm?.getFieldValue(['radioParams6G', 'allowedChannels'])
       }
