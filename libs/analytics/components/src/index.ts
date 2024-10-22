57962762c116ab28956dfa1faf47b4a7bc5c7f9d
stan2 <shiawuen.tan@commscope.com>
2024-10-22 12:08:23 +0800

    ACX-69145: move AppInsights icons into its contex and lazy load component

--- libs/analytics/components/src/index.ts
+++ libs/analytics/components/src/index.ts
@@ -1,9 +1,10 @@
+import React from 'react'
+
 export * from './DescriptionSection'
 export { AccountManagement, AccountManagementTabEnum } from './AccountManagement'
 export { AnalyticsTabs } from './AnalyticsTabs'
 export { AIDrivenRRM } from './AIDrivenRRM'
 export { AIOperations } from './AIOperations'
-export { AppInsights } from './AppInsights'
 export { Brand360 } from './Brand360'
 export { ChatWithMelissa } from './ChatWithMelissa'
 export { ClientHealth } from './ClientTroubleshooting/ClientHealth'
@@ -88,3 +89,5 @@ export * from './Webhooks'
 export { IntentAIForm } from './IntentAI/IntentAIForm'
 export { IntentAIDetails } from './IntentAI/IntentAIDetails'
 export { IntentAIWidget } from './IntentAI/IntentAIWidget'
+
+export const AppInsights = React.lazy(() => import('./AppInsights'))
