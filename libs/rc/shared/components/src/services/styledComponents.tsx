a42bc589e23b2ec78274a1a96c5b849864987bfe
Cheng, Amy <Amy.Cheng@commscope.com>
2024-10-25 22:02:58 +0800

    ACX-60627: fix CSS issue

--- libs/rc/shared/components/src/services/styledComponents.tsx
+++ libs/rc/shared/components/src/services/styledComponents.tsx
@@ -4,11 +4,12 @@ export const toolTipClassName = 'rks-rc-table-class-tooltip-popover'
 
 export const ToolTipStyle = createGlobalStyle`
   .${toolTipClassName} {
-    .ant-table-thead>tr>th,.ant-table-tbody {
+    .ant-table-thead > tr > th, 
+    .ant-table-tbody,
+    .ant-table-tbody > tr > td {
       background-color:var(--acx-primary-black);
       color:var(--acx-primary-white)
     }
-
 
     .ant-table-thead > tr > th.ant-table-column-has-sorters {
       & .ant-table-column-sorters span.ant-table-column-sorter {
@@ -17,9 +18,9 @@ export const ToolTipStyle = createGlobalStyle`
         }
       }
     }
-    
+
+    .ant-table-thead > tr:hover > th.ant-table-cell,
     .ant-table-tbody > tr.ant-table-row:hover > td,
-    .ant-table-thead > tr > td.ant-table-cell-row-hover,
     .ant-table-tbody > tr > td.ant-table-cell-row-hover {
       background-color:var(--acx-neutrals-70) !important;
     }
