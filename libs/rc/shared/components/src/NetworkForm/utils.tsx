9c7482c3cb37722e501db2917a933b3fa60af95d
Jack Lin <jack.lin@commscope.com>
2024-10-31 18:35:30 +0800

    ACX-71153: fix client isolation allowlist by venue bug

--- libs/rc/shared/components/src/NetworkForm/utils.tsx
+++ libs/rc/shared/components/src/NetworkForm/utils.tsx
@@ -394,6 +394,7 @@ export function useClientIsolationActivations (shouldSkipMode: boolean,
       const activations = _.filter(item.activations, { wifiNetworkId: networkId })
       activations.forEach(activation => venueClientIsolationMap.set(activation.venueId, item.id))
     })
+
     const venueData = saveState?.venues?.map(v => ({ ...v,
       clientIsolationAllowlistId: venueClientIsolationMap.get(v.venueId!) }))
     const fullNetworkSaveData = { ...saveState, venues: venueData }
