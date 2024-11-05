a80fb332dae77bb62f24db47c1e1b7ca1c62d573
sb1212 <Suraj.Bande@commscope.com>
2024-11-05 20:07:18 +0530

    ACX-69259: correctd test cases

--- libs/rc/shared/components/src/policies/CertificateTemplate/ServerClientCertificateForm/CertificateStepForms/UploadCertificate.spec.tsx
+++ libs/rc/shared/components/src/policies/CertificateTemplate/ServerClientCertificateForm/CertificateStepForms/UploadCertificate.spec.tsx
@@ -33,12 +33,13 @@ describe('Upload Certificate', () => {
     render(<Provider><Form><UploadCertificate /></Form></Provider>)
 
     const publicFileInput = screen.getByTestId('public-key-upload')
-    const file = new File([''], 'public.pem')
+    const file = new File([''], 'public.pem', { type: 'application/x-pem-file' })
+    // file['type'] = 'application/x-pem-file'
     await userEvent.upload(publicFileInput, file)
     expect(screen.getByText('public.pem')).toBeVisible()
 
     const privateFileInput = screen.getByTestId('private-key-upload')
-    const privateFile = new File([''], 'private.pem')
+    const privateFile = new File([''], 'private.pem', { type: 'application/x-pem-file' })
     await userEvent.upload(privateFileInput, privateFile)
     expect(screen.getByText('private.pem')).toBeVisible()
   })
@@ -47,7 +48,7 @@ describe('Upload Certificate', () => {
     render(<Provider><Form><UploadCertificate /></Form></Provider>)
 
     const publicFileInput = screen.getByTestId('public-key-upload')
-    const file = new File([''], 'public.pem')
+    const file = new File([''], 'public.pem', { type: 'application/x-pem-file' })
     Object.defineProperty(file, 'size', { value: 1024 * 11 })
     await userEvent.upload(publicFileInput, file)
     expect(screen.getByText('File size (11 KB) is too big.')).toBeInTheDocument()
