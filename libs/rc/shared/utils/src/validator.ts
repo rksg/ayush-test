0fcf8d28cedadaa1a244a8978076f56c3ff66351
Joe Kao <joe.kao@commscope.com>
2024-11-06 15:14:41 +0800

    ACX-71285: Modify to fit reviewer's suggestion.

--- libs/rc/shared/utils/src/validator.ts
+++ libs/rc/shared/utils/src/validator.ts
@@ -1160,16 +1160,11 @@ export function ipv6RegExp (value: string) {
   return Promise.resolve()
 }
 
-export function servicePolicyNameRegExp (value: string, maxLength?: number) {
+export function servicePolicyNameRegExp (value: string, maxLength: number=32) {
   const { $t } = getIntl()
 
-  let namePattern = '(?=^((?!(`|\\$\\()).){2,32}$)^(\\S.*\\S)$'
-  if (maxLength) {
-    namePattern = namePattern.replace('32', `${maxLength}`)
-  }
-
   // regex from service and policy backend
-  const re = new RegExp(namePattern)
+  const re = new RegExp(`(?=^((?!(\`|\\$\\()).){2,${maxLength}}$)^(\\S.*\\S)$`)
 
   // make sure there is no special character in value
   if ([...value].length !== JSON.stringify(value).normalize().slice(1, -1).length) {
