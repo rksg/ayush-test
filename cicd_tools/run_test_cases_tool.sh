5e999494f7e78bcb1650eb5df4219daa8fd58906
Dylan Wang <dylan.wang2@commscope.com>
2024-11-19 23:56:53 +0800

    CICD-727: [ACX-UI] Extract test cases into scripts to increase usability.

--- cicd_tools/run_test_cases_tool.sh
+++ cicd_tools/run_test_cases_tool.sh
@@ -3,15 +3,12 @@
 set -xe
 
 GROUP=$1
-PRECOMMIT=$2
-GIT_COMMIT_BRANCH=$3
 RUN_COMMAND="node ./node_modules/.bin/nx"
 FILE="./test_cases.table"
 
 # Source the test cases file
 source $FILE
 
-# Check PRECOMMIT and GIT_COMMIT_BRANCH conditions
     
 # Dynamically reference the group array
 declare -n nx_params=$GROUP
@@ -20,8 +17,8 @@ declare -n nx_params=$GROUP
 for nx_param in "${nx_params[@]}"; do
     cmd="${RUN_COMMAND} ${nx_param}"
     echo "Executing: $cmd"
-    eval "node ./node_modules/.bin/nx run common-formatter:test --coverage --maxWorkers=50%"
-    eval "node ./node_modules/.bin/nx run common-icons:test --coverage --maxWorkers=50%"
+    eval "$cmd"
 done
+
 eval "mkdir -p /app/coverage"
 
