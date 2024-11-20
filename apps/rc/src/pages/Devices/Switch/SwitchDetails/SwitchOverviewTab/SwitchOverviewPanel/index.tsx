409ba35713ca6ffd3fab70eb27ee8915dcc696ec
jitendra.dangi <jitendra.dangi@commscope.com>
2024-11-20 10:21:45 +0530

    RSA-7098: chore: added filters and switch serial check

--- apps/rc/src/pages/Devices/Switch/SwitchDetails/SwitchOverviewTab/SwitchOverviewPanel/index.tsx
+++ apps/rc/src/pages/Devices/Switch/SwitchDetails/SwitchOverviewTab/SwitchOverviewPanel/index.tsx
@@ -80,7 +80,7 @@ export function SwitchOverviewPanel (props:{
       </GridCol>
     </GridRow>
     <GridRow>
-      { switchDetail?.serialNumber &&
+      { switchDetail?.serialNumber && filters &&
       <SwitchWidgets filters={{ ...filters }} switchDetailHeader={switchDetail} /> }
     </GridRow>
 
