c72a5828e3d17689c129bbb3da67bc7947935c31
Dylan Wang <dylan.wang2@commscope.com>
2024-11-20 15:47:54 +0800

    CICD-727

--- cicd_tools/run_test_cases_tool.sh
+++ cicd_tools/run_test_cases_tool.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-trap 'echo "Caught SIGTERM, exiting gracefully"; exit 0' SIGTERM SIGINT
+RUN_COMMAND="node ./node_modules/.bin/nx"
 
 set -xe
 
-node ./node_modules/.bin/nx run rc:test --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run rc-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run-many --target=test --projects=main,main-components --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run analytics-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run-many --target=test --projects=msp,msp-components --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components --coverage --maxWorkers=30% --noStackTrace --bail
+${RUN_COMMAND} run rc:test --coverage --maxWorkers=30% --noStackTrace --bail
+${RUN_COMMAND} run rc-components:test --coverage --maxWorkers=30% --noStackTrace --bail
+${RUN_COMMAND} run-many --target=test --projects=main,main-components --coverage --maxWorkers=30% --noStackTrace --bail
+${RUN_COMMAND} run analytics-components:test --coverage --maxWorkers=30% --noStackTrace --bail
+${RUN_COMMAND} run-many --target=test --projects=msp,msp-components --coverage --maxWorkers=30% --noStackTrace --bail
+${RUN_COMMAND} run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components --coverage --maxWorkers=30% --noStackTrace --bail
 
 mkdir -p /app/coverage
-
