4f450d121dbd2b36f6e151506eebdcde4d16b181
Dylan Wang <dylan.wang2@commscope.com>
2024-11-20 12:03:20 +0800

    CICD-727

--- cicd_tools/run_test_cases_tool.sh
+++ cicd_tools/run_test_cases_tool.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-trap 'echo "Received SIGTERM, exiting"; exit 143' SIGTERM
-
 set -xe
-
-node ./node_modules/.bin/nx run rc:test --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run rc-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run-many --target=test --projects=main,main-components --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run analytics-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run-many --target=test --projects=msp,msp-components --coverage --maxWorkers=30% --noStackTrace --bail
-node ./node_modules/.bin/nx run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components --coverage --maxWorkers=30% --noStackTrace --bail
-
+exec \
+node ./node_modules/.bin/nx run rc:test --coverage --maxWorkers=30% --noStackTrace --bail; \
+node ./node_modules/.bin/nx run rc-components:test --coverage --maxWorkers=30% --noStackTrace --bail; \
+node ./node_modules/.bin/nx run-many --target=test --projects=main,main-components --coverage --maxWorkers=30% --noStackTrace --bail; \
+node ./node_modules/.bin/nx run analytics-components:test --coverage --maxWorkers=30% --noStackTrace --bail; \
+node ./node_modules/.bin/nx run-many --target=test --projects=msp,msp-components --coverage --maxWorkers=30% --noStackTrace --bail; \
+node ./node_modules/.bin/nx run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components --coverage --maxWorkers=30% --noStackTrace --bail; \
 mkdir -p /app/coverage
 
