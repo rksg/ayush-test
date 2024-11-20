7dab2f209141c91d68874092da3651dc31a51bdc
Dylan Wang <dylan.wang2@commscope.com>
2024-11-20 11:33:42 +0800

    CICD-727

--- cicd_tools/run_test_cases_tool.sh
+++ cicd_tools/run_test_cases_tool.sh
@@ -2,14 +2,13 @@
 
 set -xe
 
-RUN_COMMAND="node ./node_modules/.bin/nx"
 
-${RUN_COMMAND} run rc:test --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run rc-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run-many --target=test --projects=main,main-components --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run analytics-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run-many --target=test --projects=msp,msp-components --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components --coverage --maxWorkers=30% --noStackTrace --bail
+node ./node_modules/.bin/nx run rc:test --coverage --maxWorkers=30% --noStackTrace --bail
+node ./node_modules/.bin/nx run rc-components:test --coverage --maxWorkers=30% --noStackTrace --bail
+node ./node_modules/.bin/nx run-many --target=test --projects=main,main-components --coverage --maxWorkers=30% --noStackTrace --bail
+node ./node_modules/.bin/nx run analytics-components:test --coverage --maxWorkers=30% --noStackTrace --bail
+node ./node_modules/.bin/nx run-many --target=test --projects=msp,msp-components --coverage --maxWorkers=30% --noStackTrace --bail
+node ./node_modules/.bin/nx run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components --coverage --maxWorkers=30% --noStackTrace --bail
 
-eval "mkdir -p /app/coverage"
+mkdir -p /app/coverage
 
