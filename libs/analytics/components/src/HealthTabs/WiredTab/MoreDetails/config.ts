39b786f6f72c1c5d09d14e42c6eed535668367c1
vivek.kandhvar <vivek.kandhvar@commscope.com>
2024-11-21 16:20:47 +0530

    RSA-7096: chore: fix lint

--- libs/analytics/components/src/HealthTabs/WiredTab/MoreDetails/config.ts
+++ libs/analytics/components/src/HealthTabs/WiredTab/MoreDetails/config.ts
@@ -3,10 +3,10 @@ import { IntlShape } from 'react-intl'
 import { NodesFilter } from '@acx-ui/utils'
 
 export interface PieChartResult {
-  topNSwitchesByCpuUsage: TopNByCPUUsageResult[];
-  topNSwitchesByDhcpFailure: TopNByDHCPFailureResult[];
-  topNSwitchesByPortCongestion: TopNByPortCongestionResult[];
-  topNSwitchesByStormPortCount: TopNByStormPortCountResult[];
+  topNSwitchesByCpuUsage: TopNByCPUUsageResult[]
+  topNSwitchesByDhcpFailure: TopNByDHCPFailureResult[]
+  topNSwitchesByPortCongestion: TopNByPortCongestionResult[]
+  topNSwitchesByStormPortCount: TopNByStormPortCountResult[]
 }
 export type WidgetType =
   | 'dhcpFailure'
@@ -15,67 +15,65 @@ export type WidgetType =
   | 'cpuUsage'
 
 export type Switch = {
-  mac: string;
-  name: string | null;
+  mac: string
+  name: string | null
 }
 export type SwitchDetails = {
-  serial: string;
-  model: string;
-  status: string;
-  firmware: string;
-  numOfPorts: number;
+  serial: string
+  model: string
+  status: string
+  firmware: string
+  numOfPorts: number
 } & Switch
 
 export type ImpactedClients = {
-  switchName: string;
-  switchId: string;
-  switchSerial: string;
-  deviceName: string;
-  deviceMac: string;
-  devicePort: string;
-  devicePortMac: string;
-  devicePortType: string;
-  isRuckusAp: boolean;
-  localPortName: string;
-  metricValue: number;
-  metricName: string;
+  switchName: string,
+  switchId: string,
+  switchSerial: string,
+  deviceName: string,
+  deviceMac: string,
+  devicePort: string,
+  devicePortMac: string,
+  devicePortType: string,
+  isRuckusAp: boolean,
+  localPortName: string,
+  metricValue: number,
+  metricName: string
 }
 
 export type ImpactedClientsResult = {
-  wiredDevicesExpStorm: ImpactedClients[];
-  wiredDevicesExpCongestion: ImpactedClients[];
+  wiredDevicesExpStorm: ImpactedClients[],
+  wiredDevicesExpCongestion: ImpactedClients[]
 }
 
 export type TopNByCPUUsageResult = {
-  cpuUtilization: number;
+  cpuUtilization: number
 } & SwitchDetails
 
 export type TopNByDHCPFailureResult = {
-  dhcpFailureCount: number;
+  dhcpFailureCount: number
 } & SwitchDetails
 
 export type TopNByPortCongestionResult = {
-  congestedPortCount: number;
+  congestedPortCount: number
 } & Switch
 
 export type TopNByStormPortCountResult = {
-  stormPortCount: number;
+  stormPortCount: number
 } & Switch
 
 export interface RequestPayload {
-  filter: NodesFilter;
-  start: string;
-  end: string;
-  type: WidgetType;
-  switchIds?: string[];
-  n?: number;
+  filter: NodesFilter
+  start: string
+  end: string
+  type: WidgetType
+  switchIds?: string[]
+  n?: number
 }
 
 export const topImpactedSwitchesLimit = 10
 
-export const showTopNTableResult = (
-  $t: IntlShape['$t'],
-  count: number,
-  limit: number
-) =>
-  count === limit ? $t({ defaultMessage: 'Top {limit}' }, { limit }) : count
+export const showTopNTableResult = ($t: IntlShape['$t'], count: number, limit: number) =>
+  count === limit
+    ? $t({ defaultMessage: 'Top {limit}' }, { limit })
+    : count
