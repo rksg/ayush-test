8cc8d9e4be254597f206d6985b151ec6dff9fe14
Chen, Sander <Sander.Chen@commscope.com>
2024-11-26 19:08:43 +0800

    ACX-70851: Add Softgre profile drawer

--- libs/rc/shared/components/src/SoftGRETunnelSettings/SoftGREProfileSettings.tsx
+++ libs/rc/shared/components/src/SoftGRETunnelSettings/SoftGREProfileSettings.tsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from 'react'
+
 import { Form, Select, Button, Divider, Space } from 'antd'
+import { DefaultOptionType }                    from 'antd/lib/select'
 import { useIntl }                              from 'react-intl'
+
+import { useGetSoftGreViewDataListQuery } from '@acx-ui/rc/services'
+import { useParams }                      from '@acx-ui/react-router-dom'
+
+
+import SoftGreDrawer from '../policies/SoftGre/SoftGreForm/SoftGreDrawer'
 
 import * as UI from './styledComponents'
 
@@ -7,41 +16,84 @@ import * as UI from './styledComponents'
 export const SoftGREProfileSettings = () => {
 
   const { $t } = useIntl()
+  const params = useParams()
+
+
+  const [ detailDrawerVisible, setDetailDrawerVisible ] = useState<boolean>(false)
+  const [ detailDrawerReadMode, setDetailDrawerReadMode ] = useState<boolean>(true)
+  const [ softGREProfileOptionList, setsoftGREProfileOptionList] = useState<DefaultOptionType[]>([])
+  const [ softGREProfile, setSoftGREProfile ] = useState<DefaultOptionType>({
+    label: '',
+    value: ''
+  })
+
+  const softGreViewDataList = useGetSoftGreViewDataListQuery({
+    params,
+    payload: {}
+  })
 
   const onChange = (value: string) => {
-
+    setSoftGREProfile(softGREProfileOptionList.find((profile) => profile.value === value)!)
   }
 
+  useEffect(() => {
+    const softGreProfileList = softGreViewDataList.data?.data ?? []
+
+    if(softGreProfileList.length > 0) {
+      setsoftGREProfileOptionList(softGreProfileList.map((softGreProfile) => {
+        return { label: softGreProfile.name, value: softGreProfile.id }
+      }))
+    }
+
+  }, [softGreViewDataList])
+
   return (
-    <Space>
-      <Form.Item
-        label={$t({ defaultMessage: 'SoftGRE Profile' })}
-        initialValue=''
-        children={
-          <Select
-            style={{ width: '100%' }}
-            data-testid={'directory-server-select'}
-            value={''}
-            onChange={onChange}
-            options={[
-              {
-                label: $t({ defaultMessage: 'Select...' }), value: ''
-              }
-            ]}
-            placeholder={$t({ defaultMessage: 'Select...' })}
-          />}
+    <>
+      <Space>
+        <Form.Item
+          label={$t({ defaultMessage: 'SoftGRE Profile' })}
+          initialValue=''
+          children={
+            <Select
+              style={{ width: '100%' }}
+              data-testid={'directory-server-select'}
+              value={softGREProfile.id}
+              onChange={onChange}
+              options={[
+                {
+                  label: $t({ defaultMessage: 'Select...' }), value: ''
+                },
+                ...softGREProfileOptionList
+              ]}
+              placeholder={$t({ defaultMessage: 'Select...' })}
+            />}
+        />
+        <UI.TypeSpace split={<Divider type='vertical' />}>
+          <Button type='link'
+            disabled={false}
+            onClick={() => {
+              setDetailDrawerVisible(true)
+              setDetailDrawerReadMode(true)
+            }}>
+            {$t({ defaultMessage: 'Profile Details' })}
+          </Button>
+          <Button type='link'
+            onClick={() => {
+              setDetailDrawerVisible(true)
+              setDetailDrawerReadMode(false)
+            }}>
+            {$t({ defaultMessage: 'Add Profile' })}
+          </Button>
+        </UI.TypeSpace>
+      </Space>
+      <SoftGreDrawer
+        visible={detailDrawerVisible}
+        setVisible={setDetailDrawerVisible}
+        policyId={softGREProfile.value as string}
+        policyName={softGREProfile.label as string}
+        readMode={detailDrawerReadMode}
+        editMode={true}
       />
-      <UI.TypeSpace split={<Divider type='vertical' />}>
-        <Button type='link'
-          disabled={false}
-          onClick={() => {}}>
-          {$t({ defaultMessage: 'Profile Details' })}
-        </Button>
-        <Button type='link'
-          onClick={() => {}}>
-          {$t({ defaultMessage: 'Add Profile' })}
-        </Button>
-      </UI.TypeSpace>
-    </Space>
+    </>
   )
 }
