abc224d9b40fd46f76c105b42fc494d1a0b179ff
Dylan Wang <dylan.wang2@commscope.com>
2024-11-26 11:24:54 +0800

    CICD-727

--- cicd_tools/run_test_cases_tool.sh
+++ cicd_tools/run_test_cases_tool.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 RUN_COMMAND="node ./node_modules/.bin/nx"
+NX_RUN_OPTIONS="--coverage --maxWorkers=30% --noStackTrace --bail"
+
+export NX_DAEMON=false
 
 set -xe
 
-${RUN_COMMAND} run rc:test --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run rc-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run-many --target=test --projects=main,main-components --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run analytics-components:test --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run-many --target=test --projects=msp,msp-components --coverage --maxWorkers=30% --noStackTrace --bail
-${RUN_COMMAND} run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components --coverage --maxWorkers=30% --noStackTrace --bail
+${RUN_COMMAND} run rc:test ${NX_RUN_OPTIONS}
+${RUN_COMMAND} run rc-components:test ${NX_RUN_OPTIONS}
+${RUN_COMMAND} run-many --target=test --projects=main,main-components ${NX_RUN_OPTIONS}
+${RUN_COMMAND} run analytics-components:test ${NX_RUN_OPTIONS}
+${RUN_COMMAND} run-many --target=test --projects=msp,msp-components ${NX_RUN_OPTIONS}
+${RUN_COMMAND} run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components ${NX_RUN_OPTIONS}
 
 mkdir -p /app/coverage
