071988dfb431fbb5ab946b77bdf794bd560bda61
Dylan Wang <dylan.wang2@commscope.com>
2024-11-26 15:59:01 +0800

    CICD-727

--- cicd_tools/run_test_cases_tool.sh
+++ cicd_tools/run_test_cases_tool.sh
@@ -9,11 +9,13 @@ set -xe
 
 echo "Start executing unit tests script file - ./cicd_tools/run_test_cases_tool.sh"
 
+## All unit tests. ##
 ${RUN_COMMAND} run rc:test ${NX_RUN_OPTIONS}
 ${RUN_COMMAND} run rc-components:test ${NX_RUN_OPTIONS}
 ${RUN_COMMAND} run-many --target=test --projects=main,main-components ${NX_RUN_OPTIONS}
 ${RUN_COMMAND} run analytics-components:test ${NX_RUN_OPTIONS}
 ${RUN_COMMAND} run-many --target=test --projects=msp,msp-components ${NX_RUN_OPTIONS}
 ${RUN_COMMAND} run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components ${NX_RUN_OPTIONS}
+## All unit tests. ##
 
 mkdir -p /app/coverage
