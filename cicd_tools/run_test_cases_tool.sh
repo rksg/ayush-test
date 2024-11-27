3792c992ca65d9b72346ab8f45285ca56661a731
Dylan Wang <dylan.wang2@commscope.com>
2024-11-27 10:40:46 +0800

    CICD-727 [ACX-UI] Extract test cases into scripts to increase usability.

--- cicd_tools/run_test_cases_tool.sh
+++ cicd_tools/run_test_cases_tool.sh
@@ -3,20 +3,23 @@
 RUN_COMMAND="node ./node_modules/.bin/nx"
 NX_RUN_OPTIONS="--coverage --maxWorkers=30% --noStackTrace --bail"
 
-## Disrupt NX daemon exception.
-${RUN_COMMAND} run --help > /dev/null 2>&1 & 
+if [ -z "${PRECOMMIT}" ] || [ "${PRECOMMIT}" == "true" ] || [ "${GIT_COMMIT_BRANCH}" == "master" ]; then
 
-set -xe
+    ## Disrupt NX daemon exception.
+    ${RUN_COMMAND} run --help > /dev/null 2>&1 & 
 
-echo "Start executing unit tests script file - ./cicd_tools/run_test_cases_tool.sh"
+    set -xe
 
-## All unit tests. ##
-${RUN_COMMAND} run rc:test ${NX_RUN_OPTIONS}
-${RUN_COMMAND} run rc-components:test ${NX_RUN_OPTIONS}
-${RUN_COMMAND} run-many --target=test --projects=main,main-components ${NX_RUN_OPTIONS}
-${RUN_COMMAND} run analytics-components:test ${NX_RUN_OPTIONS}
-${RUN_COMMAND} run-many --target=test --projects=msp,msp-components ${NX_RUN_OPTIONS}
-${RUN_COMMAND} run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components ${NX_RUN_OPTIONS}
-## All unit tests. ##
+    echo "Start executing unit tests script file - ./cicd_tools/run_test_cases_tool.sh"
+
+    ## All unit tests. ##
+    ${RUN_COMMAND} run rc:test ${NX_RUN_OPTIONS}
+    ${RUN_COMMAND} run rc-components:test ${NX_RUN_OPTIONS}
+    ${RUN_COMMAND} run-many --target=test --projects=main,main-components ${NX_RUN_OPTIONS}
+    ${RUN_COMMAND} run analytics-components:test ${NX_RUN_OPTIONS}
+    ${RUN_COMMAND} run-many --target=test --projects=msp,msp-components ${NX_RUN_OPTIONS}
+    ${RUN_COMMAND} run-many --target=test --all --exclude=rc,rc-components,main,analytics-components,msp,msp-components,main-components ${NX_RUN_OPTIONS}
+    ## All unit tests. ##
+fi
 
 mkdir -p /app/coverage
