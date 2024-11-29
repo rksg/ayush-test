98a64bfcd12d8aa182c76da8a0e784465177d2d8
mdehez <mickael.dehez@commscope.com>
2024-11-29 10:15:07 +0800

    MLSA-8799: fix type in middlewares

--- apps/ra/src/errorMiddleware.tsx
+++ apps/ra/src/errorMiddleware.tsx
@@ -1,11 +1,12 @@
 import { Middleware, isAction } from '@reduxjs/toolkit'
+import { FetchBaseQueryMeta }   from '@reduxjs/toolkit/query'
 
 import { showExpiredSessionModal } from '@acx-ui/analytics/components'
 
 export const errorMiddleware: Middleware = () => next => action => {
   if (isAction(action)) {
-    // @ts-ignore
-    if (action.meta?.baseQueryMeta?.response?.status === 401) {
+    const { meta } = action as unknown as { meta?: { baseQueryMeta?: FetchBaseQueryMeta } }
+    if (meta?.baseQueryMeta?.response?.status === 401) {
       showExpiredSessionModal()
     } else {
       return next(action)
