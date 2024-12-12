1a7cc2fa88da641ef9d1afbedbfa9b47b3de1499
Chen, Sander <Sander.Chen@commscope.com>
2024-12-12 17:46:14 +0800

    ACX-70852: delete context

--- libs/rc/shared/components/src/SoftGRETunnelSettings/index.tsx
+++ libs/rc/shared/components/src/SoftGRETunnelSettings/index.tsx
@@ -1,6 +1,2 @@
 export { SoftGRETunnelSettings } from './SoftGRETunnelSettings'
 export { SoftGREProfileSettings } from './SoftGREProfileSettings'
-export {
-  SoftgreProfileProvider,
-  SoftgreProfileAndDHCP82Context
-} from './SoftGREProfileAndDHCP82Context'
