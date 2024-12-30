f365fc398cf8a40d01ff2bff677746c9a3df0429
Asraf Ali <asraf.najmudeen@commscope.com>
2024-12-30 13:11:27 +0530

    RSA-7163 | chore: added y-axis and x-axis buffer based on bins

--- libs/analytics/components/src/IncidentDetails/Details/SwitchTcpSynDDoS.tsx
+++ libs/analytics/components/src/IncidentDetails/Details/SwitchTcpSynDDoS.tsx
@@ -1,8 +1,9 @@
 import { unitOfTime } from 'moment-timezone'
 
-import { granularityToHours, type Incident } from '@acx-ui/analytics/utils'
-import { GridRow, GridCol }                  from '@acx-ui/components'
-import { Features, useIsSplitOn }            from '@acx-ui/feature-toggle'
+import { calculateGranularity,
+  granularityToHours, type Incident } from '@acx-ui/analytics/utils'
+import { GridRow, GridCol }       from '@acx-ui/components'
+import { Features, useIsSplitOn } from '@acx-ui/feature-toggle'
 
 import { FixedAutoSizer }                 from '../../DescriptionSection/styledComponents'
 import { ImpactedSwitchDDoSTable }        from '../Charts/ImpactedSwitchDDoS'
@@ -29,16 +30,24 @@ export const SwitchTcpSynDDoS = (incident: Incident) => {
     TimeSeriesChartTypes.SwitchImpactedPortsCount
   ]
 
-  const buffer = {
-    front: { value: 0, unit: 'seconds' as unitOfTime.Base },
-    back: { value: 0, unit: 'seconds' as unitOfTime.Base }
-  }
-
   const granularities: typeof granularityToHours = [
     { granularity: 'PT30M', hours: 24 * 3 }, // 30 mins for 3 days and above
     { granularity: 'PT15M', hours: 24 * 1 }, // 15 mins for 1 day and above
     { granularity: 'PT3M', hours: 0 } // 3 mins for less than 1 day
   ]
+
+  const start = incident.impactedStart || incident.startTime
+  const end = incident.impactedEnd || incident.endTime
+
+  const granularity = calculateGranularity(start, end, 'PT3M', granularities)
+
+  const binMins = granularity === 'PT15M' ? 15 : granularity === 'PT30M' ? 30 : 3
+  const noOfBinsForBuffer = 3
+
+  const buffer = {
+    front: { value: binMins * noOfBinsForBuffer, unit: 'minutes' as unitOfTime.Base },
+    back: { value: binMins * noOfBinsForBuffer, unit: 'minutes' as unitOfTime.Base }
+  }
 
   const isEnabled = [
     useIsSplitOn(Features.INCIDENTS_SWITCH_DDOS_TOGGLE),
