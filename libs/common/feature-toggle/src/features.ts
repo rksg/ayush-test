aadbd3edc944d644cb3ce94197e4a48bf1c44a14
Joe Kao <joe.kao@commscope.com>
2025-01-02 13:42:58 +0800

    ACX-75133: Implement the Dynamic VLAN support with the PSK and Mac Auth network

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -253,7 +253,8 @@ export enum Features {
   AP_USB_PORT_SUPPORT_TOGGLE = 'wifi-usb-port-support-toggle',
   ACX_UI_NON_PROXY_ACCOUNTING_DPSK_TOGGLE = 'acx-ui-non-proxy-accounting-dpsk-toggle',
   AP_FW_MGMT_EARLY_ACCESS_TOGGLE = 'ap-fw-mgmt-early-access-toggle',
-  ACX_UI_DATE_RANGE_LIMIT = 'acx-ui-date-range-selection-limit-30-days-toggle'
+  ACX_UI_DATE_RANGE_LIMIT = 'acx-ui-date-range-selection-limit-30-days-toggle',
+  NETWORK_PSK_MACAUTH_DYNAMIC_VLAN_TOGGLE = 'wifi-psk-dynamic-vlan-toggle'
 }
 
 export enum TierFeatures { // for Tier (ex: Beta) feature flag
