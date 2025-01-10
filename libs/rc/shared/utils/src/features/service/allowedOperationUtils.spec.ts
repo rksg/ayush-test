0e74be15078ac6746833dfe0f1b882875f669a64
Jacky Chiu <jacky.chiu@commscope.com>
2025-01-10 12:03:06 +0800

    ACX-74350: Fix unit test

--- libs/rc/shared/utils/src/features/service/allowedOperationUtils.spec.ts
+++ libs/rc/shared/utils/src/features/service/allowedOperationUtils.spec.ts
@@ -26,32 +26,32 @@ describe('allowedOperationUtils', () => {
   describe('applyTemplateIfNeeded', () => {
     it('should modify the operation when isTemplate is true', () => {
       const operation = ['http:/path']
-      expect(applyTemplateIfNeeded(operation, true)).toBe('http:/templates/path')
+      expect(applyTemplateIfNeeded(operation, true)).toEqual(['http:/templates/path'])
     })
 
     it('should not modify the operation when isTemplate is false', () => {
       const operation = ['http:/path']
-      expect(applyTemplateIfNeeded(operation, false)).toBe('http:/path')
+      expect(applyTemplateIfNeeded(operation, false)).toEqual(['http:/path'])
     })
   })
 
   describe('getServiceAllowedOperation', () => {
     it('should retrieve the correct service operation', () => {
       expect(getServiceAllowedOperation(ServiceType.WIFI_CALLING, ServiceOperation.CREATE))
-        .toBe('POST:/wifiCallingServiceProfiles')
+        .toEqual(['POST:/wifiCallingServiceProfiles'])
 
       expect(getServiceAllowedOperation(ServiceType.WIFI_CALLING, ServiceOperation.CREATE, true))
-        .toBe('POST:/templates/wifiCallingServiceProfiles')
+        .toEqual(['POST:/templates/wifiCallingServiceProfiles'])
     })
   })
 
   describe('getPolicyAllowedOperation', () => {
     it('should retrieve the correct policy operation', () => {
       expect(getPolicyAllowedOperation(PolicyType.AAA, PolicyOperation.CREATE))
-        .toBe('POST:/radiusServerProfiles')
+        .toEqual(['POST:/radiusServerProfiles'])
 
       expect(getPolicyAllowedOperation(PolicyType.AAA, PolicyOperation.CREATE, true))
-        .toBe('POST:/templates/radiusServerProfiles')
+        .toEqual(['POST:/templates/radiusServerProfiles'])
     })
   })
 
@@ -60,7 +60,7 @@ describe('allowedOperationUtils', () => {
     const { result } = renderHook(() => {
       return useTemplateAwareServiceAllowedOperation(ServiceType.DHCP, ServiceOperation.CREATE)
     })
-    expect(result.current).toBe('POST:/templates/dhcpConfigServiceProfiles')
+    expect(result.current).toEqual(['POST:/templates/dhcpConfigServiceProfiles'])
   })
 
   it('useTemplateAwarePolicyAllowedOperation', () => {
@@ -68,6 +68,6 @@ describe('allowedOperationUtils', () => {
     const { result } = renderHook(() => {
       return useTemplateAwarePolicyAllowedOperation(PolicyType.AAA, PolicyOperation.CREATE)
     })
-    expect(result.current).toBe('POST:/templates/radiusServerProfiles')
+    expect(result.current).toEqual(['POST:/templates/radiusServerProfiles'])
   })
 })
