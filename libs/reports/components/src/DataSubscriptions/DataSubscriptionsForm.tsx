1347d22be9ab8c437bfefccb71ab99f3e0b59ed7
cbokil <chinmay.bokil@commscope.com>
2025-01-13 10:46:32 +0530

    MLSA-8934: add steps form

--- libs/reports/components/src/DataSubscriptions/DataSubscriptionsForm.tsx
+++ libs/reports/components/src/DataSubscriptions/DataSubscriptionsForm.tsx
@@ -2,32 +2,55 @@ import React from 'react'
 
 import { useIntl } from 'react-intl'
 
-import { GridRow, GridCol, PageHeader } from '@acx-ui/components'
-
 import { generateBreadcrumb } from './utils'
+import { GridRow, GridCol, StepsForm, PageHeader } from '@acx-ui/components'
+import { useNavigate } from '@acx-ui/react-router-dom'
 
 
 type DataSubscriptionsFormProps = {
-  isRAI?:boolean
   editMode?: boolean
+  isRAI?: boolean
 }
 
+const CloudStorage = () => <div>Cloud Storage</div>
+const Settings = () => <div>Settings</div>
+const Summary = () => <div>Summary</div>
 const DataSubscriptionsForm: React.FC<DataSubscriptionsFormProps> = ({ isRAI, editMode=false }) => {
   const { $t } = useIntl()
-  return (
-    <>
-      <PageHeader
+  const navigate = useNavigate()
+  
+  return (<>
+    <PageHeader
         title={editMode? $t({ defaultMessage: 'Edit Subscription' }) :
           $t({ defaultMessage: 'New Subscription' })}
         breadcrumb={generateBreadcrumb({ isRAI })}
       />
-      <GridRow>
-        <GridCol col={{ span: 24 }} style={{ minHeight: '180px' }}>
-
-        </GridCol>
-      </GridRow>
-    </>
-  )
+    <GridRow>
+      <GridCol col={{ span: 24 }} style={{ minHeight: '180px' }}>
+      <StepsForm
+      onCancel={() => { navigate(-1) }}
+      onFinish={async (values) => { console.log(values) }}
+      buttonLabel={{
+        submit: $t({ defaultMessage: 'Apply' })
+      }}
+      initialValues={{}}
+    >
+      <StepsForm.StepForm
+        title={$t({ defaultMessage: 'Introduction' })}
+        children={<CloudStorage />}
+      />
+      <StepsForm.StepForm
+        title={$t({ defaultMessage: 'Intent Priority' })}
+        children={<Settings />}
+      />
+      <StepsForm.StepForm
+        title={$t({ defaultMessage: 'Summary' })}
+        children={<Summary />}
+      />
+    </StepsForm>
+      </GridCol>
+    </GridRow>
+  </>)
 }
 
 export default DataSubscriptionsForm
