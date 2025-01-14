af24d91cb2061446b2669b5f868b581e446ef1f0
Hao, PoHan <PoHan.Hao@commscope.com>
2025-01-14 14:30:12 +0800

    MLRD-1551: reused useAnySplitsOn for performance

--- libs/analytics/components/src/ConfigChange/PagedTable/index.tsx
+++ libs/analytics/components/src/ConfigChange/PagedTable/index.tsx
@@ -13,12 +13,12 @@ import {
   getConfigChangeEntityTypeMapping,
   Filter
 }                                    from '@acx-ui/components'
-import { ConfigChangePaginationParams }           from '@acx-ui/components'
-import { get }                                    from '@acx-ui/config'
-import { Features, useAnySplitsOn, useIsSplitOn } from '@acx-ui/feature-toggle'
-import { DateFormatEnum, formatter }              from '@acx-ui/formatter'
-import { TenantLink }                             from '@acx-ui/react-router-dom'
-import { noDataDisplay }                          from '@acx-ui/utils'
+import { ConfigChangePaginationParams } from '@acx-ui/components'
+import { get }                          from '@acx-ui/config'
+import { Features, useAnySplitsOn }     from '@acx-ui/feature-toggle'
+import { DateFormatEnum, formatter }    from '@acx-ui/formatter'
+import { TenantLink }                   from '@acx-ui/react-router-dom'
+import { noDataDisplay }                from '@acx-ui/utils'
 
 import { ConfigChangeContext }                                       from '../context'
 import { usePagedConfigChangeQuery, PagedConfigChange, SORTER_ABBR } from '../services'
@@ -135,10 +135,10 @@ export const useColumns = () => {
 }
 
 export function PagedTable () {
-  const showIntentAI = [
-    useIsSplitOn(Features.INTENT_AI_CONFIG_CHANGE_TOGGLE),
-    useIsSplitOn(Features.RUCKUS_AI_INTENT_AI_CONFIG_CHANGE_TOGGLE)
-  ].some(Boolean)
+  const showIntentAI = useAnySplitsOn([
+    Features.INTENT_AI_CONFIG_CHANGE_TOGGLE,
+    Features.RUCKUS_AI_INTENT_AI_CONFIG_CHANGE_TOGGLE
+  ]) ?? false
 
   const { pathFilters } = useAnalyticsFilter()
   const {
