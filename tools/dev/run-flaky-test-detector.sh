24fe8f94645ac0571d5789d93dfed7056fe065ff
huihui.fang <huihui.fang@commscope.com>
2025-01-22 11:53:21 +0800

    MLSA-8829: feat: remove pre-push, add comments to run-flaky-test-detector script

--- tools/dev/run-flaky-test-detector.sh
+++ tools/dev/run-flaky-test-detector.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-new_test_files_on_branch=$(git diff --name-only master...HEAD  | grep -E ".*\\.spec\\.(js|ts|tsx|jsx)$")
+# Get all test files that have been added / updated on branch
+updated_test_files_on_branch=$(git diff --name-only master...HEAD  | grep -E ".*\\.spec\\.(js|ts|tsx|jsx)$")
 
-if [ -z "$new_test_files_on_branch" ]; then
+if [ -z "$updated_test_files_on_branch" ]; then
   echo "No new test files to test."
   exit 0
 fi
 
-for file in $new_test_files_on_branch; do
-  echo  "Running test $file"
+for file in $updated_test_files_on_branch; do
+  echo "Running test $file"
+  # Run flaky test detector on file, 10 times
   npx flaky-test-detector --run-tests "node 'node_modules/.bin/jest' $file -c './jest.config.ts'" --test-output-file=./flaky-test-detector-results.xml --test-output-format=junit --repeat=10
 done
