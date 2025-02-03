d94cd3f84b68674dc360c5d0fba5e28d163e332a
JSYU <jeffrey.syu@commscope.com>
2025-02-02 18:02:29 -0800

    ACX-75290: apply the suggestion and update the opsApi based on the wiki

--- libs/rc/shared/utils/src/urls/firmwareUrls.ts
+++ libs/rc/shared/utils/src/urls/firmwareUrls.ts
@@ -250,7 +250,7 @@ export const FirmwareUrlsInfo: { [key: string]: ApiInfo } = {
   patchVenueApModelFirmwares: {
     method: 'put',
     url: '/apModelFirmwares/batchOperations/:batchId/venues/:venueId',
-    opsApi: 'PUT:/apModelFirmwares/batchOperations/:batchId/venues/:venueId',
+    opsApi: 'PUT:/apModelFirmwares/batchOperations/{id}/venues/{id}',
     newApi: true,
     defaultHeaders: {
       'Accept': 'application/vnd.ruckus.v1+json',
@@ -268,7 +268,7 @@ export const FirmwareUrlsInfo: { [key: string]: ApiInfo } = {
   updateVenueSchedulesPerApModel: {
     method: 'put',
     url: '/apModelFirmwares/batchOperations/:batchId/venues/:venueId',
-    opsApi: 'PUT:/apModelFirmwares/batchOperations/:batchId/venues/:venueId',
+    opsApi: 'PUT:/apModelFirmwares/batchOperations/{id}/venues/{id}',
     newApi: true,
     defaultHeaders: {
       'Accept': 'application/vnd.ruckus.v1+json',
@@ -278,7 +278,7 @@ export const FirmwareUrlsInfo: { [key: string]: ApiInfo } = {
   skipVenueSchedulesPerApModel: {
     method: 'delete',
     url: '/apModelFirmwares/batchOperations/:batchId/venues/:venueId',
-    opsApi: 'DELETE:/apModelFirmwares/batchOperations/:batchId/venues/:venueId',
+    opsApi: 'DELETE:/apModelFirmwares/batchOperations/{id}/venues/{id}',
     newApi: true,
     defaultHeaders: {
       Accept: 'application/vnd.ruckus.v1+json'
