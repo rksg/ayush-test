0d9456e7ead45d78c209be04ac4a51bf346f83e9
huihui.fang <huihui.fang@commscope.com>
2025-02-04 10:36:36 +0800

    MLSA-8829: fix: run flaky test detector on all updated tests, 7 runs

--- tools/dev/run-flaky-test-detector.sh
+++ tools/dev/run-flaky-test-detector.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # Get all test files that have been added / updated on branch
-updated_test_files_on_branch=$(git diff --name-only master...HEAD  | grep -E ".*\\.spec\\.(js|ts|tsx|jsx)$")
+updated_test_files_on_branch=$(git diff --name-only master...HEAD  | grep -E ".*\\.spec\\.(js|ts|tsx|jsx)$" | tr '\n' ' ')
 
 if [ -z "$updated_test_files_on_branch" ]; then
   echo "No new test files to test."
   exit 0
 fi
 
-for file in $updated_test_files_on_branch; do
-  echo "Running test $file"
-  # Run flaky test detector on file, 10 times
-  npx flaky-test-detector --run-tests "node 'node_modules/.bin/jest' $file -c './jest.config.ts'" --test-output-file=./flaky-test-detector-results.xml --test-output-format=junit --repeat=10
-done
+# Run flaky test detector on file, 7 times
+npx flaky-test-detector --run-tests "node 'node_modules/.bin/jest' $updated_test_files_on_branch -c './jest.config.ts'" --test-output-file=./flaky-test-detector-results.xml --test-output-format=junit --repeat=7
