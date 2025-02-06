8403d32d309616b1f4963be5d1dd0a06cc9add70
Rose <rose.menacherry@commscope.com>
2025-02-06 15:36:18 +0530

    ACX-77572 adding identity count column in unit list behing ff multiple-identity-units

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -280,7 +280,8 @@ export enum Features {
   ENTITLEMENT_SIS_INTEGRATION_TOGGLE='entitlement-sis-integration-toggle',
   ENTITLEMENT_HYBRID_CLOUD_SECURITY_TOGGLE='entitlement-hybrid-cloud-security-toggle',
   ACX_UI_SHOW_ADMIN_TYPE_COLUMN_TOGGLE='acx-ui-show-admin-type-column-toggle',
-  ACX_UI_VENUE_CHANNEL_SELECTION_MANUAL = 'acx-ui-venue-channel-selection-manual-toggle'
+  ACX_UI_VENUE_CHANNEL_SELECTION_MANUAL = 'acx-ui-venue-channel-selection-manual-toggle',
+  MULTIPLE_IDENTITY_UNITS = 'multiple-identity-units'
 }
 
 export enum TierFeatures { // for Tier (ex: Beta) feature flag
