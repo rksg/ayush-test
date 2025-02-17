baa948107e56bb6acfdc0d90b2a56db70a47c60d
sb1212 <Suraj.Bande@commscope.com>
2025-02-17 14:32:21 +0530

    ACX-70580: addressline will be shown on venue table, venue name validation to support its length upto 63 chars, upload pdf file for floorplan and other ARC changes

--- libs/rc/shared/components/src/FloorPlan/FloorPlanForm/FloorPlanUpload.tsx
+++ libs/rc/shared/components/src/FloorPlan/FloorPlanForm/FloorPlanUpload.tsx
@@ -1,6 +1,10 @@
 import { useContext, useEffect, useState } from 'react'
 
-import { Upload, Space } from 'antd'
+import { Upload, Space }                    from 'antd'
+import { RcFile }                           from 'antd/lib/upload'
+import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist'
+// @ts-ignore
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry'
 
 import { showToast }          from '@acx-ui/components'
 import { PlusCircleOutlined } from '@acx-ui/icons'
@@ -9,6 +13,8 @@ import { getIntl }            from '@acx-ui/utils'
 
 import { ModalContext } from '../FloorPlanModal'
 
+
+GlobalWorkerOptions.workerSrc = pdfjsWorker
 
 export default function FloorplanUpload ({ validateFile, imageFile } : {
   validateFile: Function, imageFile?: string }) {
@@ -46,29 +52,80 @@ export default function FloorplanUpload ({ validateFile, imageFile } : {
       'image/jpg',
       'image/gif',
       'image/svg',
-      'image/svg+xml']
+      'image/svg+xml',
+      'application/pdf'
+    ]
     const validImage = acceptedImageTypes.includes(file.type)
     if (!validImage) {
-      const content = $t({ defaultMessage: 'Invalid Image type!' })
+      const content = $t({ defaultMessage: 'Invalid file type!' })
       openToastAndResetFile({ ...fileValidation, isValidfileType: false }, content)
       return
     }
-    const isLt20M = file.size / 1024 / 1024 < 20
-    if (!isLt20M) {
-      const content = $t({ defaultMessage: 'Image must smaller than 20MB!' })
-      openToastAndResetFile({ ...fileValidation, isValidFileSize: false }, content)
-      return
-    }
 
-    setFileValidation({ ...fileValidation, file: file })
-    validateFile({ ...fileValidation, file: file })
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => {
-      setTempUrl(URL.createObjectURL(file))
-      setImageUrl(reader.result as string)
+    if (file.type === 'application/pdf') {
+      const uid = (file as RcFile).uid
+      convertPdfToImage(file, uid).then(uploadedFile => {
+        beforeUpload(uploadedFile)
+      })
+        .catch(err => {
+          const content = err.name === 'PasswordException'
+            ? $t({ defaultMessage: 'Password protected files are not allowed!' })
+            : err?.message ? err.message : $t({ defaultMessage: 'Unsupported file!' })
+          openToastAndResetFile({ ...fileValidation, isValidFileSize: false }, content)
+          return
+        })
+    } else {
+      const isLt20M = file.size / 1024 / 1024 < 20
+      if (!isLt20M) {
+        const content = $t({ defaultMessage: 'File must smaller than 20MB!' })
+        openToastAndResetFile({ ...fileValidation, isValidFileSize: false }, content)
+        return
+      }
+
+      setFileValidation({ ...fileValidation, file: file })
+      validateFile({ ...fileValidation, file: file })
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        setTempUrl(URL.createObjectURL(file))
+        setImageUrl(reader.result as string)
+      }
     }
     return false
+  }
+
+  const convertPdfToImage = async (pdfFile: File, uid: string): Promise<RcFile> => {
+    const pdf = await getDocument(URL.createObjectURL(pdfFile)).promise
+    const page = await pdf.getPage(1) // Get the first page
+
+    const scale = 1.5
+    const viewport = page.getViewport({ scale })
+
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')!
+    canvas.height = viewport.height
+    canvas.width = viewport.width
+
+    const renderContext = {
+      canvasContext: context,
+      viewport
+    }
+
+    await page.render(renderContext).promise
+
+    const blob: Blob = await new Promise((resolve) => {
+      canvas.toBlob((blob) => {
+        resolve(blob as Blob)
+      }, 'image/jpeg')
+    })
+
+    // Convert Blob to RcFile
+    const rcFile = await new File([blob], `${pdfFile.name.split('.')[0]}.jpg`, {
+      type: 'image/jpeg'
+    }) as RcFile
+
+    rcFile.uid = uid
+    return rcFile
   }
 
   const openToastAndResetFile = function (validationAttributes: FileValidation, content: string) {
@@ -92,7 +149,7 @@ export default function FloorplanUpload ({ validateFile, imageFile } : {
         showUploadList={false}
         action={tempUrl}
         beforeUpload={beforeUpload}
-        accept='image/*'
+        accept='image/*, application/pdf'
         style={{
           height: '180px'
         }}
