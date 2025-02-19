373fc3ceac55feb950200feb11050bb8edae3b6e
TLIN4 <Tsaihuei.Lin@commscope.com>
2025-02-19 14:31:40 +0800

    ACX-73796: hide the pendo guide temporarily

--- libs/common/utils/src/useTrackLoadTime.tsx
+++ libs/common/utils/src/useTrackLoadTime.tsx
@@ -501,9 +501,9 @@ export const LoadTimeProvider = ({ children }: {
 
       if (pendo) {
         pendo.track?.(PENDO_TRACK_EVENT_NAME, trackEventData)
-        if (loadTimeStatus === LoadTimeStatus.UNACCEPTABLE) {
-          pendo.showGuideById?.('zdnqb9J5L32vRHw9CbvBQSsGhyw')
-        }
+        // if (loadTimeStatus === LoadTimeStatus.UNACCEPTABLE) {
+        //   pendo.showGuideById?.('zdnqb9J5L32vRHw9CbvBQSsGhyw')
+        // }
       }
     }
   }, [loadTimes])
