69ca5edf26f44a3788ee528d5e93caeb84d0f176
Chen, Sander <Sander.Chen@commscope.com>
2025-02-27 02:34:50 +0800

    ACX-78355: Add FF

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -298,7 +298,8 @@ export enum Features {
   SWITCH_ERROR_DISABLE_STATUS = 'switch-indicate-error-disable-status-toggle',
   SWITCH_ERROR_DISABLE_RECOVERY_TOGGLE = 'switch-error-disable-recovery-toggle',
   WIFI_CAPTIVE_PORTAL_OWE_TRANSITION = 'wifi-captive-portal-owe-transition',
-  LOGIN_SSO_SAML_TECHPARTNER = 'login-sso-saml-techpartner'
+  LOGIN_SSO_SAML_TECHPARTNER = 'login-sso-saml-techpartner',
+  WIFI_IDENTITY_AND_IDENTITY_GROUP_MANAGEMENT_TOGGLE = 'wifi-identity-and-identity-group-management-toggle'
 }
 
 export enum TierFeatures { // for Tier (ex: Beta) feature flag
