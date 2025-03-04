e18a0901df0c5f16431df660c5324476a836ed6b
Guo, Yizhu <Yizhu.Guo@commscope.com>
2025-03-04 18:52:30 +0800

    ACX-78101: Remove "AI Canvas" menu

--- apps/main/src/pages/Layout/menuConfig.tsx
+++ apps/main/src/pages/Layout/menuConfig.tsx
@@ -62,8 +62,6 @@ export function useMenuConfig () {
     useIsSplitOn(Features.SWITCH_HEALTH_TOGGLE)
   ].some(Boolean)
   const isIntentAIEnabled = useIsSplitOn(Features.INTENT_AI_TOGGLE)
-  const isInCanvasPlmList = useIsTierAllowed(Features.CANVAS)
-  const isCanvasEnabled = useIsSplitOn(Features.CANVAS) || isInCanvasPlmList
   const isMspAppMonitoringEnabled = useIsSplitOn(Features.MSP_APP_MONITORING)
   const isDataSubscriptionsEnabled = useIsSplitOn(Features.ACX_UI_DATA_SUBSCRIPTIONS_TOGGLE)
   const isAdmin = hasRoles([RolesEnum.PRIME_ADMIN, RolesEnum.ADMINISTRATOR])
@@ -356,13 +354,6 @@ export function useMenuConfig () {
         { uri: '/reports', label: $t({ defaultMessage: 'Reports' }) }
       ]
     },
-    ...(isCanvasEnabled ? [ {
-      label: $t({ defaultMessage: 'AI Canvas' }),
-      uri: '/canvas',
-      inactiveIcon: BulbOutlined,
-      activeIcon: BulbSolid
-    }] : [])
-    ,
     {
       label: $t({ defaultMessage: 'Administration' }),
       inactiveIcon: AdminOutlined,
