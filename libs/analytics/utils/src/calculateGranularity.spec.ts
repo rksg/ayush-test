599fae62a21ffbe68eac58fc27ecaab45d061c9e
wilson.loh <wilson.loh@commscope.com>
2025-03-11 11:13:17 +0800

    MLSA-9059: update unit test

--- libs/analytics/utils/src/calculateGranularity.spec.ts
+++ libs/analytics/utils/src/calculateGranularity.spec.ts
@@ -30,27 +30,11 @@ it('should return correct granularity based on interval and min granularity', ()
     {
       input: { start: '2022-01-01T00:00:00+08:00', end: '2022-01-01T00:10:00+08:00' },
       output: 'PT15M'
-    },
-    {
-      input: {
-        start: '2022-01-01T00:00:00+08:00',
-        end: '2022-01-01T00:10:00+08:00',
-        minGranularity: 'PT15M'
-      },
-      output: 'PT15M'
-    },
-    {
-      input: {
-        start: '2022-01-01T00:00:00+08:00',
-        end: '2022-01-01T00:10:00+08:00',
-        minGranularity: 'PT30M'
-      },
-      output: 'PT30M'
     }
   ]
   data.forEach(({ input, output }) => {
     expect(
-      calculateGranularity(input.start, input.end, input.minGranularity)
+      calculateGranularity(input.start, input.end)
     ).toStrictEqual(output)
   })
   jest.useRealTimers()
@@ -68,5 +52,17 @@ describe('Rollup', () => {
       moment().subtract(60, 'days').format(),
       moment().format()
     )).toStrictEqual('PT24H')
+  })
+})
+
+describe('Min Granularity Precedence', () => {
+  it('Should return PT1H when minGranularity is greater than calculated granularity', () => {
+    jest.mocked(get).mockReturnValue('32')
+    jest.spyOn(require('.'), 'overlapsRollup').mockReturnValue(true)
+
+    const start = '2022-01-01T00:00:00+08:00'
+    const end = '2022-01-01T00:10:00+08:00'
+
+    expect(calculateGranularity(start, end)).toStrictEqual('PT1H')
   })
 })
