77e09d20ae1fc064cf36940bf5fe9612c1f3552b
Asraf Ali <asraf.najmudeen@commscope.com>
2025-03-11 14:58:29 +0530

    RSA-7281 | chore: added app visibility feature in AP details overview page

--- libs/analytics/components/src/TopApplicationsByTraffic/index.spec.tsx
+++ libs/analytics/components/src/TopApplicationsByTraffic/index.spec.tsx
@@ -1,13 +1,48 @@
 /* eslint-disable testing-library/no-node-access */
-import { dataApiURL, Provider, store }                 from '@acx-ui/store'
-import { fireEvent, render, screen, mockGraphqlQuery } from '@acx-ui/test-utils'
-import type { AnalyticsFilter }                        from '@acx-ui/utils'
-import { DateRange }                                   from '@acx-ui/utils'
+import { rest } from 'msw'
+
+import { useIsSplitOn }                                            from '@acx-ui/feature-toggle'
+import { AdministrationUrlsInfo }                                  from '@acx-ui/rc/utils'
+import { dataApiURL, Provider, store }                             from '@acx-ui/store'
+import { fireEvent, render, screen, mockGraphqlQuery, mockServer } from '@acx-ui/test-utils'
+import type { AnalyticsFilter }                                    from '@acx-ui/utils'
+import { DateRange }                                               from '@acx-ui/utils'
 
 import { topApplicationByTrafficFixture } from './__tests__/fixtures'
 import { api }                            from './services'
 
 import { TopApplicationsByTraffic } from './index'
+
+jest.mock('@acx-ui/utils', () => ({
+  ...jest.requireActual('@acx-ui/utils'),
+  getJwtTokenPayload: () => ({ tenantId: 'tenantId' })
+}))
+
+const settingsEnabled = {
+  privacyFeatures: [
+    {
+      featureName: 'APP_VISIBILITY',
+      isEnabled: true
+    },
+    {
+      featureName: 'ARC',
+      isEnabled: true
+    }
+  ]
+}
+
+const settingsDisabled = {
+  privacyFeatures: [
+    {
+      featureName: 'APP_VISIBILITY',
+      isEnabled: false
+    },
+    {
+      featureName: 'ARC',
+      isEnabled: false
+    }
+  ]
+}
 
 type ColumnElements = Array<HTMLElement|SVGSVGElement>
 
@@ -38,8 +73,17 @@ describe('TopApplicationsByTrafficWidget', () => {
     filter: {}
   }
 
-  beforeEach(() =>
+  beforeEach(() => {
+    jest.mocked(useIsSplitOn).mockReturnValue(false)
+    mockServer.use(
+      rest.get(AdministrationUrlsInfo.getPrivacySettings.url,
+        (_req, res, ctx) => res(ctx.json(settingsEnabled))),
+      rest.patch(AdministrationUrlsInfo.updatePrivacySettings.url,
+        (_req, res, ctx) => res(ctx.json(settingsEnabled)))
+    )
     store.dispatch(api.util.resetApiState())
+  }
+
   )
 
   it('should render loader', () => {
@@ -83,6 +127,66 @@ describe('TopApplicationsByTrafficWidget', () => {
     fireEvent.click(screen.getByText('Download'))
     const downloadRows = extractRows(asFragment())
     expect(downloadRows).toMatchSnapshot('downloadRows')
+  })
+
+  it('should render chart when APP_VISIBILITY is true', async () => {
+    jest.mocked(useIsSplitOn).mockReturnValue(true)
+    mockGraphqlQuery(dataApiURL, 'TopApplicationsByTrafficWidget', {
+      data: { network: { hierarchyNode: topApplicationByTrafficFixture } }
+    })
+    render( <Provider> <TopApplicationsByTraffic filters={filters}/></Provider>)
+    expect(screen.getByRole('img', { name: 'loader' })).toBeVisible()
+    expect(screen.queryByText('No data to display')).not.toBeInTheDocument()
+  })
+  it('should render for empty data when APP_VISIBILITY is false', async () => {
+    jest.mocked(useIsSplitOn).mockReturnValue(true)
+    mockServer.use(
+      rest.get(AdministrationUrlsInfo.getPrivacySettings.url,
+        (_req, res, ctx) => res(ctx.json(settingsDisabled))),
+      rest.patch(AdministrationUrlsInfo.updatePrivacySettings.url,
+        (_req, res, ctx) => res(ctx.json(settingsDisabled)))
+    )
+    mockGraphqlQuery(dataApiURL, 'TopApplicationsByTrafficWidget', {
+      data: { network: { hierarchyNode: {
+        topNAppByTotalTraffic: []
+      } } }
+    })
+    const { asFragment } = render( <Provider>
+      <TopApplicationsByTraffic filters={filters}/>
+    </Provider>)
+    await screen.findByText('No data to display')
+    expect(asFragment()).toMatchSnapshot('NoData when APP_VISIBILITY is false')
+  })
+  it('should render for empty data when app privacy api is failed', async () => {
+    jest.mocked(useIsSplitOn).mockReturnValue(true)
+    mockServer.use(
+      rest.get(AdministrationUrlsInfo.getPrivacySettings.url,
+        (_req, res, ctx) => res(ctx.status(500), ctx.json(null))),
+      rest.patch(AdministrationUrlsInfo.updatePrivacySettings.url,
+        (_req, res, ctx) => res(ctx.status(500), ctx.json(null)))
+    )
+    mockGraphqlQuery(dataApiURL, 'TopApplicationsByTrafficWidget', {
+      data: { network: { hierarchyNode: {
+        topNAppByTotalTraffic: []
+      } } }
+    })
+    const { asFragment } = render( <Provider>
+      <TopApplicationsByTraffic filters={filters}/>
+    </Provider>)
+    await screen.findByText('No data to display')
+    expect(asFragment()).toMatchSnapshot('NoData when privacy api is failed')
+  })
+  it('should render chart when IS_MLISA_RA is true', async () => {
+    jest.mocked(useIsSplitOn).mockReturnValue(true)
+    const originalEnv = process.env
+    process.env.NX_IS_MLISA_SA = 'true'
+    mockGraphqlQuery(dataApiURL, 'TopApplicationsByTrafficWidget', {
+      data: { network: { hierarchyNode: topApplicationByTrafficFixture } }
+    })
+    render( <Provider> <TopApplicationsByTraffic filters={filters}/></Provider>)
+    expect(screen.getByRole('img', { name: 'loader' })).toBeVisible()
+    expect(screen.queryByText('No data to display')).not.toBeInTheDocument()
+    process.env = originalEnv
   })
 
 })
