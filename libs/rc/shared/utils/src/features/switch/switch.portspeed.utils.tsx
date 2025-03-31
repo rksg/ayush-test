d2f623f8fca8c3dc314ee8f371f5fce28260fb37
Yu-Yen Lee <yuan.lee@commscope.com>
2025-03-31 17:25:04 +0800

    ACX-80391: modify port speed

--- libs/rc/shared/utils/src/features/switch/switch.portspeed.utils.tsx
+++ libs/rc/shared/utils/src/features/switch/switch.portspeed.utils.tsx
@@ -266,7 +266,7 @@ const ICX_MODELS_PORTSPEED: Record<string, MODELS_PORTSPEED | MODELS_PORTSPEED[]
       'TEN_G_FULL', 'TEN_G_FULL_MASTER', 'TEN_G_FULL_SLAVE']
   }, {
     start: '1/2/1', end: '1/2/2',
-    capacity: ['NONE', 'FORTY_G_FULL', 'ONE_HUNDRED_G_FULL']
+    capacity: ['NONE', 'FORTY_G_FULL'] // TBD: ONE_HUNDRED_G_FULL
   }, {
     start: '1/3/1', end: '1/3/4',
     capacity: ['NONE', 'ONE_G_FULL', 'TEN_G_FULL', 'TWENTY_FIVE_G_FULL']
