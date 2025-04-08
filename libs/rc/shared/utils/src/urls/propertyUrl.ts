d70543f757fc2bf4e6c79159c4c73e3df5248b3b
Rose <rose.menacherry@commscope.com>
2025-04-08 15:38:34 +0530

    ACX-78202 adding rbac scopes in unit details updated opsapi paths

--- libs/rc/shared/utils/src/urls/propertyUrl.ts
+++ libs/rc/shared/utils/src/urls/propertyUrl.ts
@@ -179,7 +179,7 @@ export const PropertyUrlsInfo: { [key in PropertyUrlType]: ApiInfo } = {
     method: 'PUT',
     newApi: true,
     url: '/venues/:venueId/units/:unitId/identities/:identityId',
-    opsApi: 'PUT:/venues/{venueId}/units/{unitId}/identities/{identityId}',
+    opsApi: 'PUT:/venues/{id}/units/{id}/identities/{id}',
     defaultHeaders: {
       'Accept': 'application/vnd.ruckus.v1+json',
       'Content-Type': 'application/vnd.ruckus.v1+json'
@@ -188,7 +188,7 @@ export const PropertyUrlsInfo: { [key in PropertyUrlType]: ApiInfo } = {
   removeUnitLinkedIdenity: {
     method: 'delete',
     url: '/venues/:venueId/units/:unitId/identities/:identityId',
-    opsApi: 'DELETE:/venues/{venueId}/units/{unitId}/identities/{identityId}',
+    opsApi: 'DELETE:/venues/{id}/units/{id}/identities/{id}',
     newApi: true,
     defaultHeaders: {
       Accept: 'application/vnd.ruckus.v1+json'
