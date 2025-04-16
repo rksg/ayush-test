a533c816ae348475b0ffbf2ddaa6f48904d50c95
Chen, Sander <Sander.Chen@commscope.com>
2025-04-16 16:55:13 +0800

    ACX-82301: Add FF

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -325,7 +325,8 @@ export enum Features {
   DURGA_TENANT_CONVERSION_REC_TO_MSP_REC = 'durga-tenant-conversion-rec-to-msp-rec',
   SWITCH_SUPPORT_MAC_ACL_TOGGLE = 'switch-support-mac-acl-toggle',
   WIFI_WIRED_CLIENT_VISIBILITY_TOGGLE = 'wifi-wired-client-visibility-for-lanport-toggle',
-  PASSPHRASE_ENFORCEMENT = 'property-dpsk-passphrase-enforcement'
+  PASSPHRASE_ENFORCEMENT = 'property-dpsk-passphrase-enforcement',
+  WIFI_OPEN_NETWORK_INTEGRATE_IDENTITY_GROUP_TOGGLE = 'wifi-open-network-integrate-identity-group-toggle'
 }
 
 export enum TierFeatures { // for Tier (ex: Beta) feature flag
