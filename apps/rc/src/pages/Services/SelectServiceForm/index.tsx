f882a0c0cbca35eae578914f993d9ea1633cb672
Cheng, Amy <Amy.Cheng@commscope.com>
2025-04-18 13:59:09 +0800

    ACX-79664: revert test code

--- apps/rc/src/pages/Services/SelectServiceForm/index.tsx
+++ apps/rc/src/pages/Services/SelectServiceForm/index.tsx
@@ -139,7 +139,7 @@ export default function SelectServiceForm () {
         ]}
       />
       <StepsFormLegacy
-        onCancel={() => true && navigate(myServicesPath)}
+        onCancel={() => navigate(myServicesPath)}
         buttonLabel={{ submit: $t({ defaultMessage: 'Next' }) }}
       >
         <StepsFormLegacy.StepForm
