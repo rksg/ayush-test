1b6df3f8c9b0ec279886d870c505af32718ce6a2
YLEE1 <yuan.lee@commscope.com>
2025-04-22 19:25:28 +0800

    ACX-80113: update supported fw version of ICX8100 10G to 10010g_cd1

--- apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmwareV1002/VenueFirmwareList/SwitchUpgradeWizard/SwitchNote/index.tsx
+++ apps/main/src/pages/Administration/FWVersionMgmt/SwitchFirmwareV1002/VenueFirmwareList/SwitchUpgradeWizard/SwitchNote/index.tsx
@@ -35,7 +35,7 @@ export function SwitchNote (props: NoteProps) {
     [NotesEnum.NOTE8100_1]:
     $t({
       // eslint-disable-next-line max-len
-      defaultMessage: 'The following switches will not be upgraded because the ICX8100-X models only support FI version 10.0.10g and later or 10.0.20c and later. You can still upgrade them by selecting the switches individually.'
+      defaultMessage: 'The following switches will not be upgraded because the ICX8100-X models only support FI version 10.0.10g_cd1 and later or 10.0.20c and later. You can still upgrade them by selecting the switches individually.'
     }),
     [NotesEnum.NOTE8200_1]:
       $t({
