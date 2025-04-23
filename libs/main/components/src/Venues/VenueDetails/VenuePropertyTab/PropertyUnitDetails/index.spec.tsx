7962dc5c5f3d4a8255ecf74a312e0f7f2bfd4623
karen.yeh <karen.yeh@commscope.com>
2025-04-23 19:05:41 +0800

    ACX-81104: Fix unit tests

--- libs/main/components/src/Venues/VenueDetails/VenuePropertyTab/PropertyUnitDetails/index.spec.tsx
+++ libs/main/components/src/Venues/VenueDetails/VenuePropertyTab/PropertyUnitDetails/index.spec.tsx
@@ -1,10 +1,16 @@
 import userEvent from '@testing-library/user-event'
 import { rest }  from 'msw'
 
-import { PersonaUrls, PropertyConfigStatus, ConnectionMetering, PropertyUrlsInfo, PropertyUnitStatus } from '@acx-ui/rc/utils'
-import { BrowserRouter as Router }                                                                     from '@acx-ui/react-router-dom'
-import { Provider }                                                                                    from '@acx-ui/store'
-import { fireEvent, mockServer, render, screen,  waitFor, within }                                     from '@acx-ui/test-utils'
+import { PersonaUrls,
+  PropertyConfigStatus,
+  ConnectionMetering,
+  PropertyUrlsInfo,
+  PropertyUnitStatus,
+  WifiUrlsInfo
+} from '@acx-ui/rc/utils'
+import { BrowserRouter as Router }                                 from '@acx-ui/react-router-dom'
+import { Provider }                                                from '@acx-ui/store'
+import { fireEvent, mockServer, render, screen,  waitFor, within } from '@acx-ui/test-utils'
 
 
 import { mockPropertyUnitList } from '../../../__tests__/fixtures'
@@ -136,6 +142,9 @@ describe('Property Unit Details', () => {
         PropertyUrlsInfo.getUnitById.url,
         (_, res, ctx) => res(ctx.json(unitData))
       ),
+      rest.get(
+        WifiUrlsInfo.getVenueApCapabilities.url,
+        (_, res, ctx) => res(ctx.json({}))),
       rest.post(
         PropertyUrlsInfo.getPropertyUnitList.url,
         (req, res, ctx) => {
