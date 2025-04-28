90982a637d0d3beb7cf845905d54481ab8b16ccd
NCHANG2 <neil.chang2@commscope.com>
2025-04-28 16:58:58 +0800

    ACX-84146:Venue and network tunneling forwarding destination options should filter by edge fw and validate by core port

--- libs/rc/shared/components/src/NetworkTunnelActionModal/EdgeSdLanSelectOptionL2greContent.spec.tsx
+++ libs/rc/shared/components/src/NetworkTunnelActionModal/EdgeSdLanSelectOptionL2greContent.spec.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react'
-import { Form }           from 'antd'
-import { MemoryRouter }   from 'react-router-dom'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent                   from '@testing-library/user-event'
+import { Form }                    from 'antd'
+import { MemoryRouter }            from 'react-router-dom'
 
 import { EdgeMvSdLanViewData, NetworkTypeEnum } from '@acx-ui/rc/utils'
 
@@ -11,13 +12,42 @@ jest.mock('@acx-ui/edge/components', () => ({
   useGetAvailableTunnelProfile: jest.fn(() => ({
     isDataLoading: false,
     availableTunnelProfiles: [
-      { id: 'tunnel1', name: 'Tunnel 1', tunnelType: 'VXLAN_GPE' },
-      { id: 'tunnel2', name: 'Tunnel 2', tunnelType: 'VXLAN_GPE' }
+      { id: 'tunnel1', name: 'Tunnel 1', tunnelType: 'VXLAN_GPE',
+        destinationEdgeClusterId: 'edgeClusterId1' },
+      { id: 'tunnel2', name: 'Tunnel 2', tunnelType: 'VXLAN_GPE' ,
+        destinationEdgeClusterId: 'edgeClusterId2' },
+      { id: 'tunnel3', name: 'Tunnel 3', tunnelType: 'L2GRE' ,
+        destinationIpAddress: '10.206.11.11' }
     ]
   })),
   transToOptions: jest.fn((profiles) =>
     profiles.map((p: { name: string; id: string }) => ({ label: p.name, value: p.id }))
   )
+}))
+
+jest.mock('@acx-ui/rc/services', () => ({
+  ...jest.requireActual('@acx-ui/rc/services'),
+  useGetEdgeFeatureSetsQuery: jest.fn(() => ({
+    requiredFw: '2.4.0',
+    isFeatureSetsLoading: false
+  })),
+  useGetEdgeListQuery: jest.fn(() => ({
+    nodesData: [{
+      serialNumber: 'edgeId1',
+      firmwareVersion: '2.4.0'
+    }],
+    isFwVerLoading: false
+  })),
+  useGetEdgeClusterListQuery: jest.fn(() => ({
+    associatedEdgeClusters: [{
+      clusterId: 'edgeClusterId1',
+      hasCorePort: true
+    },{
+      clusterId: 'edgeClusterId2',
+      hasCorePort: false
+    }],
+    isEdgeClustersLoading: false
+  }))
 }))
 
 jest.mock('@acx-ui/rc/utils', () => ({
@@ -45,6 +75,7 @@ describe('EdgeSdLanSelectOptionL2greContent', () => {
     venueSdLan: {
       id: 'sdlan1',
       name: 'Test Sdlan',
+      edgeClusterId: 'edgeClusterId1',
       tunnelProfileId: 'profile1',
       tunnelProfileName: 'Test Profile',
       tunneledWlans: []
@@ -81,7 +112,7 @@ describe('EdgeSdLanSelectOptionL2greContent', () => {
     expect(screen.getByText('See more information')).toBeInTheDocument()
   })
 
-  it('should filter out VXLAN-GPE options for non-CAPTIVEPORTAL networks', () => {
+  it('should filter out VXLAN-GPE options for non-CAPTIVEPORTAL networks', async () => {
     renderComponent({
       ...defaultProps,
       networkType: NetworkTypeEnum.DPSK
@@ -89,8 +120,67 @@ describe('EdgeSdLanSelectOptionL2greContent', () => {
 
     const select = screen.getByRole('combobox')
     expect(select).toBeInTheDocument()
-
+    await userEvent.click(select)
     // Should only see GRE option and Core Port
+    expect(screen.getByText('Tunnel 3')).toBeInTheDocument()
     expect(screen.queryByText('Tunnel 2')).not.toBeInTheDocument()
   })
+
+  it('should has VXLAN-GPE options for CAPTIVEPORTAL networks', async () => {
+    renderComponent({
+      ...defaultProps,
+      networkType: NetworkTypeEnum.CAPTIVEPORTAL
+    })
+
+    const select = screen.getByRole('combobox')
+    expect(select).toBeInTheDocument()
+    await userEvent.click(select)
+    expect(screen.getByText('Tunnel 1')).toBeInTheDocument()
+    expect(screen.getByText('Tunnel 2')).toBeInTheDocument()
+  })
+
+  it('should filter out L2GRE options when firmware version is lower than required', async () => {
+    const originalMock =jest.requireMock('@acx-ui/rc/services').useGetEdgeListQuery
+    jest.requireMock('@acx-ui/rc/services').useGetEdgeListQuery = jest.fn(() => ({
+      nodesData: [{
+        serialNumber: 'edgeId1',
+        firmwareVersion: '2.3.0'
+      }],
+      isFwVerLoading: false
+    }))
+
+    renderComponent({
+      ...defaultProps,
+      networkType: NetworkTypeEnum.CAPTIVEPORTAL
+    })
+
+    const select = screen.getByRole('combobox')
+    expect(select).toBeInTheDocument()
+    await userEvent.click(select)
+
+    expect(screen.getByText('Tunnel 1')).toBeInTheDocument()
+    expect(screen.getByText('Tunnel 2')).toBeInTheDocument()
+    expect(screen.queryByText('Tunnel 3')).not.toBeInTheDocument()
+    jest.requireMock('@acx-ui/rc/services').useGetEdgeListQuery = originalMock
+  })
+
+  it('should display error message when selecting tunnel option without Core Port', async () => {
+    renderComponent({
+      ...defaultProps,
+      networkType: NetworkTypeEnum.CAPTIVEPORTAL
+    })
+
+    const select = screen.getByRole('combobox')
+    expect(select).toBeInTheDocument()
+    await userEvent.click(select)
+
+    const tunnel2Option = screen.getByText('Tunnel 2')
+    expect(tunnel2Option).toBeInTheDocument()
+    await userEvent.click(tunnel2Option)
+
+    await waitFor(() => {
+      expect(screen.getByText(/To use the SD-LAN service/i)).toBeInTheDocument()
+    })
+  })
+
 })
--- libs/rc/shared/components/src/NetworkTunnelActionModal/EdgeSdLanSelectOptionL2greContent.tsx
+++ libs/rc/shared/components/src/NetworkTunnelActionModal/EdgeSdLanSelectOptionL2greContent.tsx
@@ -4,9 +4,12 @@ import { Form, Row, Select, Typography } from 'antd'
 import { DefaultOptionType }             from 'antd/es/cascader'
 import { useIntl }                       from 'react-intl'
 
-import { transToOptions, useGetAvailableTunnelProfile } from '@acx-ui/edge/components'
+import { transToOptions, useGetAvailableTunnelProfile }                                from '@acx-ui/edge/components'
+import { InformationSolid }                                                            from '@acx-ui/icons'
+import { useGetEdgeClusterListQuery, useGetEdgeFeatureSetsQuery, useGetEdgeListQuery } from '@acx-ui/rc/services'
 import {
   EdgeMvSdLanViewData,
+  IncompatibilityFeatures,
   NetworkTypeEnum,
   PolicyOperation,
   PolicyType,
@@ -19,9 +22,11 @@ import {
   getServiceRoutePath,
   useHelpPageLink
 } from '@acx-ui/rc/utils'
-import { TenantLink } from '@acx-ui/react-router-dom'
+import { TenantLink }      from '@acx-ui/react-router-dom'
+import { compareVersions } from '@acx-ui/utils'
 
-
+import { messageMappings } from './messageMappings'
+import * as UI             from './styledComponents'
 
 interface EdgeSdLanContentProps {
   venueSdLan: EdgeMvSdLanViewData | undefined
@@ -66,15 +71,59 @@ export const EdgeSdLanSelectOptionL2greContent = (props: EdgeSdLanContentProps) 
     ? <TenantLink to={linkToTunnelProfileDetail}>{venueSdLan?.tunnelProfileName}</TenantLink>
     : ''
 
+  const { requiredFw, isFeatureSetsLoading } = useGetEdgeFeatureSetsQuery({
+    payload: {
+      filters: {
+        featureNames: [IncompatibilityFeatures.L2OGRE]
+      }
+    } }, {
+    selectFromResult: ({ data, isLoading }) => {
+      return {
+        requiredFw: data?.featureSets
+          ?.find(item => item.featureName === IncompatibilityFeatures.L2OGRE)?.requiredFw,
+        isFeatureSetsLoading: isLoading
+      }
+    }
+  })
 
-  // eslint-disable-next-line max-len
+  const { nodesData, isFwVerLoading } = useGetEdgeListQuery({
+    payload: {
+      fields: [
+        'serialNumber',
+        'firmwareVersion'
+      ],
+      filters: { clusterId: [venueSdLan?.edgeClusterId] }
+    } }, {
+    selectFromResult: ({ data, isLoading }) => ({
+      nodesData: data?.data ?? [],
+      isFwVerLoading: isLoading
+    })
+  })
+
   const activatedForwardingTunnelProfileIds = venueSdLan?.tunneledWlans
     ?.map(item => item.forwardingTunnelProfileId)
     ?.filter((id): id is string => typeof id === 'string') || []
 
-  const { isDataLoading,
+  const { isDataLoading: isTunnelProfilesLoading,
     // eslint-disable-next-line max-len
     availableTunnelProfiles } = useGetAvailableTunnelProfile({ serviceIds: venueSdLan?.id ? [venueSdLan.id] : [undefined] })
+
+  const { associatedEdgeClusters, isEdgeClustersLoading } = useGetEdgeClusterListQuery({
+    payload: {
+      fields: ['clusterId', 'hasCorePort'],
+      filters: {
+        clusterId: availableTunnelProfiles?.map(profile =>
+          profile.destinationEdgeClusterId).filter(Boolean)
+      },
+      pageSize: 10000
+    }
+  }, {
+    skip: !availableTunnelProfiles?.length,
+    selectFromResult: ({ data, isLoading }) => ({
+      associatedEdgeClusters: data?.data,
+      isEdgeClustersLoading: isLoading
+    })
+  })
 
   const tunnelProfileOptions = useMemo(() => [
     {
@@ -85,19 +134,27 @@ export const EdgeSdLanSelectOptionL2greContent = (props: EdgeSdLanContentProps) 
       (availableTunnelProfiles || []).filter(item => item.id !== venueSdLan?.tunnelProfileId),
       activatedForwardingTunnelProfileIds
     )
-  ], [availableTunnelProfiles, isDataLoading])
+  ], [availableTunnelProfiles, isTunnelProfilesLoading])
 
   const getFilteredTunnelProfileOptions = (
     tunnelProfileOptions: DefaultOptionType[],
     availableTunnelProfiles: TunnelProfileViewData[]
   ) => {
     const isCaptivePortal = networkType === NetworkTypeEnum.CAPTIVEPORTAL
+    const edgesData = [...nodesData]?.sort((n1, n2) =>
+      compareVersions(n1.firmwareVersion, n2.firmwareVersion))
+    const minNodeVersion = edgesData?.[0]?.firmwareVersion
+    const isLower = !!minNodeVersion && compareVersions(minNodeVersion, requiredFw) < 0
     return tunnelProfileOptions
       .map(item => {
         const profile = availableTunnelProfiles.find(profile => profile.id === item.value)
 
         // Skip VXLAN-GPE options for non-CAPTIVEPORTAL networks
         if (!isCaptivePortal && profile?.tunnelType === TunnelTypeEnum.VXLAN_GPE) {
+          return null
+        }
+
+        if(isLower && profile?.tunnelType === TunnelTypeEnum.L2GRE) {
           return null
         }
 
@@ -118,6 +175,30 @@ export const EdgeSdLanSelectOptionL2greContent = (props: EdgeSdLanContentProps) 
   const onChangeTunnel = (value:string) => {
     form.setFieldValue(['sdLan', 'forwardingTunnelType'],
       availableTunnelProfiles?.find(item => item.id === value)?.tunnelType ?? '')
+  }
+
+  const checkCorePortConfigured = (tunnelProfileId: string) => {
+    const targetTunnelProfile = availableTunnelProfiles.find((tunnelProfile) =>
+      tunnelProfile.id === tunnelProfileId)
+    // eslint-disable-next-line max-len
+    if(TunnelTypeEnum.VXLAN_GPE !== targetTunnelProfile?.tunnelType) {
+      return Promise.resolve()
+    }
+    const associatedEdgeCluster = associatedEdgeClusters?.find((cluster) =>
+      cluster.clusterId === targetTunnelProfile?.destinationEdgeClusterId)
+
+    if (associatedEdgeCluster?.hasCorePort) {
+      return Promise.resolve()
+    } else {
+      return Promise.reject(<UI.ClusterSelectorHelper>
+        <InformationSolid />
+        {$t(messageMappings.setting_cluster_helper, {
+          infoLink: <a href={helpUrl} target='_blank' rel='noreferrer'>
+            {$t({ defaultMessage: 'See more information' })}
+          </a>
+        })}
+      </UI.ClusterSelectorHelper>)
+    }
   }
 
   return <Row><Form.Item noStyle >
@@ -145,11 +226,16 @@ export const EdgeSdLanSelectOptionL2greContent = (props: EdgeSdLanContentProps) 
                 name={['sdLan', 'forwardingTunnelProfileId']}
                 label={$t({ defaultMessage: 'Forwarding Destination' })}
                 initialValue={''}
+                rules={[
+                  { validator: (_, value) => checkCorePortConfigured(value) }
+                ]}
                 children={<Select
                   style={{ width: '220px' }}
                   // eslint-disable-next-line max-len
                   options={getFilteredTunnelProfileOptions(tunnelProfileOptions, availableTunnelProfiles)}
                   onChange={onChangeTunnel}
+                  disabled={isTunnelProfilesLoading && isEdgeClustersLoading
+                    && isFwVerLoading && isFeatureSetsLoading}
                 />}
               />
             </Row>
