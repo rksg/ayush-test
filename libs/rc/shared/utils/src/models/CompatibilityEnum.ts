05a60eefa9c4fc77536b69c80001f63884265f9f
Sting.Huang <sting.huang@commscope.com>
2025-04-28 13:19:40 +0800

    ACX-80447: Add new EdgeCompatibilityFeatureEnum for dual-wan

--- libs/rc/shared/utils/src/models/CompatibilityEnum.ts
+++ libs/rc/shared/utils/src/models/CompatibilityEnum.ts
@@ -27,7 +27,8 @@ export enum IncompatibilityFeatures {
   PIN_DS = 'PIN_DS',
   PIN_AS = 'PIN_AS',
   EDGE_MDNS_PROXY = 'MDNS_PROXY', // edge mDNS
-  ARP_TERMINATION = 'ARP_TERMINATION'
+  ARP_TERMINATION = 'ARP_TERMINATION',
+  DUAL_WAN = 'Dual WAN'
 }
 
 export enum IncompatibilityFeatureGroups {
@@ -42,5 +43,6 @@ export enum EdgeCompatibilityFeatureEnum {
   HQOS = 'HQoS',
   DHCP = 'DHCP',
   MDNS_PROXY = 'MDNS_PROXY',
-  ARP_TERMINATION = 'ARP_TERMINATION'
+  ARP_TERMINATION = 'ARP_TERMINATION',
+  DUAL_WAN = 'Dual WAN'
 }
