730d44746dc2574747480763eebe107f494b4ab2
Chiu, George <george.chiu@commscope.com>
2025-05-05 12:33:49 +0800

    ACX-85474: fix test

--- apps/rc/src/pages/Services/Portal/PortalTable/index_preview.spec.tsx
+++ apps/rc/src/pages/Services/Portal/PortalTable/index_preview.spec.tsx
@@ -92,11 +92,11 @@ describe('PortalTable', () => {
       }
     )
 
-    const targetPortal = mockedPortalList.content[0]
+    const targetPortal = mockedPortalList.content[1]
     expect(await screen.findByRole('button', { name: /Add Guest Portal/i })).toBeVisible()
-    // eslint-disable-next-line max-len
-    expect(await screen.findByRole('row', { name: new RegExp(targetPortal.serviceName) })).toBeVisible()
-    await userEvent.click(await screen.findByLabelText('78f92fbf80334e8b83cddd3210db4920'))
+    const row = await screen.findByRole('row', { name: new RegExp(targetPortal.serviceName) })
+    expect(row).toBeVisible()
+    await userEvent.click(row)
     expect(await screen.findByText('1 selected')).toBeVisible()
   })
 })
