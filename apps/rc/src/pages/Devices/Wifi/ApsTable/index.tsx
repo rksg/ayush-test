351c46ed4cc682bcddaedcd1ed9b27b7f7aec924
jl1203 <julie.lin@commscope.com>
2025-05-07 16:47:06 +0800

    ACX-57977 Fix lint error

--- apps/rc/src/pages/Devices/Wifi/ApsTable/index.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApsTable/index.tsx
@@ -14,11 +14,11 @@ import {
   useApListQuery,
   useVenuesListQuery
 } from '@acx-ui/rc/services'
-import { COUNT_ALL_REQ_CONTENT, usePollingTableQuery, WifiRbacUrlsInfo }  from '@acx-ui/rc/utils'
-import { TenantLink, useParams }                                          from '@acx-ui/react-router-dom'
-import { WifiScopes }                                                     from '@acx-ui/types'
-import { hasPermission }                                                  from '@acx-ui/user'
-import { getOpsApi }                                                      from '@acx-ui/utils'
+import { COUNT_ALL_REQ_CONTENT, usePollingTableQuery, WifiRbacUrlsInfo } from '@acx-ui/rc/utils'
+import { TenantLink, useParams }                                         from '@acx-ui/react-router-dom'
+import { WifiScopes }                                                    from '@acx-ui/types'
+import { hasPermission }                                                 from '@acx-ui/user'
+import { getOpsApi }                                                     from '@acx-ui/utils'
 
 const apsCountQueryPayload = {
   fields: ['serialNumber', 'name'],
