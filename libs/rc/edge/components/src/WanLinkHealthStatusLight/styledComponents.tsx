d05eaeeca68500d73e06be12cf40bfbca04a1f86
Sting.Huang <sting.huang@commscope.com>
2025-05-07 15:39:01 +0800

    ACX-85213: Update StyledWanLinkTargetWrapper css

--- libs/rc/edge/components/src/WanLinkHealthStatusLight/styledComponents.tsx
+++ libs/rc/edge/components/src/WanLinkHealthStatusLight/styledComponents.tsx
@@ -3,7 +3,11 @@ import styled    from 'styled-components'
 
 export const StyledWanLinkTargetWrapper = styled(Space)`
   color: var(--acx-primary-white);
-  & .ant-badge-status-text {
-    color: var(--acx-primary-white);
+  & .ant-badge.ant-badge-status {
+    display: flex;
+    align-items: center;
+    & .ant-badge-status-text {
+      color: var(--acx-primary-white);
+    }
   }
 `
