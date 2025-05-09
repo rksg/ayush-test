21101fd2163fe95d740ded9b243a3983394867cf
Cecil Wei <cecil.wei@commscope.com>
2025-05-09 15:23:22 +0800

    ACX-83787: Fixed the validation rule for passphrase length

--- libs/rc/shared/components/src/services/DpskPassphraseManagement/AddDpskPassphrasesForm.tsx
+++ libs/rc/shared/components/src/services/DpskPassphraseManagement/AddDpskPassphrasesForm.tsx
@@ -253,7 +253,8 @@ export default function AddDpskPassphrasesForm (props: AddDpskPassphrasesFormPro
                   return Promise.resolve()
                 }
                 if (isPassphraseEnforcement) {
-                  if (value.length < dpskPoolData?.passphraseLength) {
+                  if (dpskPoolData?.passphraseLength &&
+                    value.length < dpskPoolData?.passphraseLength) {
                     throw new Error(
                       // eslint-disable-next-line max-len
                       $t({ defaultMessage: 'Passphrase must be at least {length} characters' }, { length: dpskPoolData?.passphraseLength })
