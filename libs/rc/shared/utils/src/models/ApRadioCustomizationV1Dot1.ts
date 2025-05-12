0df2265ccb0a913e30fb9398d0da8103a75126d5
lc1158 <Lish.Chen@commscope.com>
2025-05-12 11:18:03 +0800

    ACX-80330: Add ap radio settings and band management 1Dot1 api

---
+++ libs/rc/shared/utils/src/models/ApRadioCustomizationV1Dot1.ts
@@ -0,0 +1,39 @@
+import { ApRadioParams24GV1Dot1 } from './ApRadioParams24GV1Dot1'
+import { ApRadioParams50GV1Dot1 } from './ApRadioParams50GV1Dot1'
+import { ApRadioParams6GV1Dot1 }  from './ApRadioParams6GV1Dot1'
+import { ApRadioParamsDual5G }    from './ApRadioParamsDual5G'
+
+export class ApRadioCustomizationV1Dot1 {
+  apRadioParams24G: ApRadioParams24GV1Dot1
+
+  apRadioParams50G?: ApRadioParams50GV1Dot1
+
+  apRadioParamsDual5G?: ApRadioParamsDual5G
+
+  apRadioParams6G?: ApRadioParams6GV1Dot1
+
+  // True if 6 GHz is enabled
+  enable6G?: boolean
+
+  // True if 2.4 GHz is enabled
+  enable24G?: boolean
+
+  // True if 5 GHz is enabled
+  enable50G?: boolean
+
+  constructor () {
+    this.apRadioParams24G = new ApRadioParams24GV1Dot1()
+
+    this.apRadioParams50G = new ApRadioParams50GV1Dot1()
+
+    this.apRadioParamsDual5G = new ApRadioParamsDual5G()
+
+    this.apRadioParams6G = new ApRadioParams6GV1Dot1()
+
+    this.enable6G = false
+
+    this.enable24G = false
+
+    this.enable50G = false
+  }
+}
--- libs/rc/shared/utils/src/urls/wifiConfigUrls.ts
+++ libs/rc/shared/utils/src/urls/wifiConfigUrls.ts
@@ -36,6 +36,24 @@ export const WifiConfigUrlsInfo: { [key: string]: ApiInfo } = {
     url: '/venues/:venueId/aps/:serialNumber/bandModeSettings',
     newApi: true
   },
+  getApBandModeSettingsV1Dot1: {
+    method: 'get',
+    url: '/venues/:venueId/aps/:serialNumber/bandModeSettings',
+    newApi: true,
+    defaultHeaders: {
+      'Accept': 'application/vnd.ruckus.v1+json',
+      'Content-Type': 'application/vnd.ruckus.v1+json'
+    }
+  },
+  updateApBandModeSettingsV1Dot1: {
+    method: 'put',
+    url: '/venues/:venueId/aps/:serialNumber/bandModeSettings',
+    newApi: true,
+    defaultHeaders: {
+      'Accept': 'application/vnd.ruckus.v1+json',
+      'Content-Type': 'application/vnd.ruckus.v1+json'
+    }
+  },
 
   getVenueRadioCustomization: {
     method: 'get',
@@ -82,6 +100,26 @@ export const WifiConfigUrlsInfo: { [key: string]: ApiInfo } = {
     url: '/venues/:venueId/aps/:serialNumber/radioSettings',
     opsApi: 'PUT:/venues/{id}/aps/{id}/radioSettings',
     newApi: true
+  },
+  getApRadioCustomizationV1Dot1: {
+    method: 'get',
+    url: '/venues/:venueId/aps/:serialNumber/radioSettings',
+    opsApi: 'GET:/venues/{id}/aps/{id}/radioSettings',
+    newApi: true,
+    defaultHeaders: {
+      'Accept': 'application/vnd.ruckus.v1+json',
+      'Content-Type': 'application/vnd.ruckus.v1+json'
+    }
+  },
+  updateApRadioCustomizationV1Dot1: {
+    method: 'put',
+    url: '/venues/:venueId/aps/:serialNumber/radioSettings',
+    opsApi: 'PUT:/venues/{id}/aps/{id}/radioSettings',
+    newApi: true,
+    defaultHeaders: {
+      'Accept': 'application/vnd.ruckus.v1+json',
+      'Content-Type': 'application/vnd.ruckus.v1+json'
+    }
   },
 
   // ===== Load Balancing =====
