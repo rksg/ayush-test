01395b5c69e105a122ac5c532b37d29ea5407ed6
Chiu, George <george.chiu@commscope.com>
2025-05-13 15:17:16 +0800

    ACX-83700: Update for Portal Profile Consolidation

--- apps/rc/src/pages/Services/PortalProfile/create.tsx
+++ apps/rc/src/pages/Services/PortalProfile/create.tsx
@@ -6,13 +6,13 @@ import { useIsSplitOn, Features } from '@acx-ui/feature-toggle'
 import { useIsEdgeFeatureReady }  from '@acx-ui/rc/components'
 import {
   getServiceAllowedOperation,
-  getServiceListRoutePath,
   getSelectServiceRoutePath,
   ServiceOperation,
   ServiceType,
   PortalProfileTabsEnum,
   getServiceRoutePath,
-  LocationExtended
+  LocationExtended,
+  useServicesBreadcrumb
 } from '@acx-ui/rc/utils'
 import { useLocation, useNavigate, useTenantLink } from '@acx-ui/react-router-dom'
 import { hasAllowedOperations }                    from '@acx-ui/user'
@@ -54,10 +54,11 @@ export default function CreatePortalProfile () {
       <PageHeader
         title={$t({ defaultMessage: 'Add Portal' })}
         breadcrumb={[
-          { text: $t({ defaultMessage: 'Network Control' }) },
+          ...useServicesBreadcrumb(),
           {
-            text: $t({ defaultMessage: 'My Services' }),
-            link: getServiceListRoutePath(true)
+            text: $t({ defaultMessage: 'Portal Profiles' }),
+            link: getServiceRoutePath({
+              type: ServiceType.PORTAL_PROFILE, oper: ServiceOperation.LIST })
           }
         ]}
       />
