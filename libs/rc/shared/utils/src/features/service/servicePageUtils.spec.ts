cca3db9260c524219be1db5fccc5fa7fdbc46ea8
Jacky Chiu <jacky.chiu@commscope.com>
2025-05-15 15:35:22 +0800

    ACX-83700: Fix unit tests

--- libs/rc/shared/utils/src/features/service/servicePageUtils.spec.ts
+++ libs/rc/shared/utils/src/features/service/servicePageUtils.spec.ts
@@ -1,9 +1,9 @@
 import { TierFeatures, useIsTierAllowed } from '@acx-ui/feature-toggle'
 import { renderHook }                     from '@acx-ui/test-utils'
 
-import { PolicyType, PolicyOperation } from '../../types'
+import { ServiceOperation, ServiceType } from '../../constants'
 
-import { getPolicyListRoutePath, getPolicyRoutePath, usePolicyListBreadcrumb, usePolicyPageHeaderTitle } from '.'
+import { getServiceListRoutePath, getServiceRoutePath, useServiceListBreadcrumb, useServicePageHeaderTitle } from '.'
 
 const mockedUseConfigTemplate = jest.fn()
 jest.mock('../../configTemplate', () => ({
@@ -25,7 +25,7 @@ jest.mock('../unifiedServices', () => ({
   generateUnifiedServicesBreadcrumb: (from: any) => mockedGenerateUnifiedServicesBreadcrumb(from)
 }))
 
-describe('policyPageUtils', () => {
+describe('servicePageUtils', () => {
   beforeEach(() => {
     mockedUseConfigTemplate.mockReturnValue({ isTemplate: false })
     jest.mocked(useIsTierAllowed).mockReturnValue(false)
@@ -36,45 +36,45 @@ describe('policyPageUtils', () => {
     mockedGenerateUnifiedServicesBreadcrumb.mockClear()
   })
 
-  it('should generate Policy PageHeader Title correctly', () => {
+  it('should generate Service PageHeader Title correctly', () => {
     mockedUseConfigTemplate.mockReturnValue({ isTemplate: true })
-    const { result } = renderHook(() => usePolicyPageHeaderTitle(false, PolicyType.AAA))
-    expect(result.current).toBe('Add RADIUS Server Template')
+    const { result } = renderHook(() => useServicePageHeaderTitle(false, ServiceType.DPSK))
+    expect(result.current).toBe('Add DPSK Template')
 
     mockedUseConfigTemplate.mockReturnValue({ isTemplate: false })
     // eslint-disable-next-line max-len
-    const { result: nonTemplateResult } = renderHook(() => usePolicyPageHeaderTitle(false, PolicyType.AAA))
-    expect(nonTemplateResult.current).toBe('Add RADIUS Server ')
+    const { result: nonTemplateResult } = renderHook(() => useServicePageHeaderTitle(false, ServiceType.DPSK))
+    expect(nonTemplateResult.current).toBe('Add DPSK ')
   })
 
-  it('usePolicyListBreadcrumb when isTemplate is true', () => {
+  it('useServiceListBreadcrumb when isTemplate is true', () => {
     mockedUseConfigTemplate.mockReturnValue({ isTemplate: true })
-    const { result } = renderHook(() => usePolicyListBreadcrumb(PolicyType.AAA))
+    const { result } = renderHook(() => useServiceListBreadcrumb(ServiceType.DPSK))
 
     expect(result.current).toEqual([])
   })
 
-  it('usePolicyListBreadcrumb when isTemplate is false', () => {
-    const targetPolicyType = PolicyType.AAA
-    const { result } = renderHook(() => usePolicyListBreadcrumb(targetPolicyType))
+  it('useServiceListBreadcrumb when isTemplate is false', () => {
+    const targetServiceType = ServiceType.DPSK
+    const { result } = renderHook(() => useServiceListBreadcrumb(targetServiceType))
 
     expect(result.current).toEqual([
       { text: 'Network Control' },
       {
-        text: 'Policies & Profiles',
-        link: getPolicyListRoutePath(true)
+        text: 'My Services',
+        link: getServiceListRoutePath(true)
       },
-      { text: 'RADIUS Server',
-        link: getPolicyRoutePath({ type: targetPolicyType, oper: PolicyOperation.LIST })
+      { text: 'DPSK',
+        link: getServiceRoutePath({ type: targetServiceType, oper: ServiceOperation.LIST })
       }
     ])
   })
 
-  it('usePolicyListBreadcrumb when isNewServiceCatalogEnabled is true', () => {
+  it('useServiceListBreadcrumb when isNewServiceCatalogEnabled is true', () => {
     // eslint-disable-next-line max-len
     jest.mocked(useIsTierAllowed).mockImplementation(ff => ff === TierFeatures.SERVICE_CATALOG_UPDATED)
 
-    renderHook(() => usePolicyListBreadcrumb(PolicyType.AAA))
+    renderHook(() => useServiceListBreadcrumb(ServiceType.DPSK))
 
     // eslint-disable-next-line max-len
     expect(mockedGenerateUnifiedServicesBreadcrumb).toHaveBeenCalledWith(mockedLocationFrom)
