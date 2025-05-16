7f65b6634e0596cf5fef16ad2286ca04a7d847b2
JSYU <jeffrey.syu@commscope.com>
2025-05-16 04:18:59 -0700

    ACX-82834: update the test case

--- libs/rc/shared/components/src/ApGroupEdit/styledComponents.ts
+++ libs/rc/shared/components/src/ApGroupEdit/styledComponents.ts
@@ -1,12 +1,14 @@
 import styled from 'styled-components/macro'
 
-export const FieldLabel = styled.div<{ width: string }>`
-  font-size: var(--acx-body-4-font-size);
-  display: grid;
-  line-height: 32px;
-  grid-template-columns: ${props => props.width} 1fr;
-  align-items: baseline;
-`
+// TODO: For individual band mode enabled
+// export const FieldLabel = styled.div<{ width: string }>`
+//   font-size: var(--acx-body-4-font-size);
+//   display: grid;
+//   line-height: 32px;
+//   grid-template-columns: ${props => props.width} 1fr;
+//   align-items: baseline;
+// `
+
 export const DisabledDiv = styled.div`
   width: 800px;
   height: 400px;
