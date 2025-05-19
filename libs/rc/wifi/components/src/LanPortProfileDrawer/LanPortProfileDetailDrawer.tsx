fc8f863b3a8dac63703d14ab42f529005e03090f
Sharon Chen <sharon.chen2@commscope.com>
2025-05-20 00:23:25 +0800

    ACX-86481: Add lan port detail drawer

---
+++ libs/rc/wifi/components/src/LanPortProfileDrawer/LanPortProfileDetailDrawer.tsx
@@ -0,0 +1,232 @@
+/* eslint-disable max-len */
+
+import { useMemo } from 'react'
+
+import { Divider, Form } from 'antd'
+import { useIntl }       from 'react-intl'
+import { useParams }     from 'react-router-dom'
+
+import { Drawer }                          from '@acx-ui/components'
+import { Features, useIsSplitOn }          from '@acx-ui/feature-toggle'
+import {
+  useGetAAAPolicyViewModelListQuery,
+  useGetEthernetPortProfileByIdQuery,
+  useGetEthernetPortProfileTemplateQuery
+} from '@acx-ui/rc/services'
+import {
+  AAAViewModalType,
+  ClientIsolationSaveData,
+  EthernetPortAuthType,
+  EthernetPortProfileViewData,
+  PolicyOperation,
+  PolicyType,
+  SoftGreViewData,
+  getEthernetPortAuthTypeString,
+  getEthernetPortCredentialTypeString,
+  getEthernetPortTypeString,
+  getPolicyDetailsLink,
+  transformDisplayOnOff,
+  useConfigTemplateQueryFnSwitcher } from '@acx-ui/rc/utils'
+import { TenantLink } from '@acx-ui/react-router-dom'
+
+interface LanPortProfileDetailsDrawerProps {
+  title: string
+  visible: boolean
+  setVisible: (visible: boolean) => void
+  wiredPortVisible: boolean
+  ethernetPortProfileData?: EthernetPortProfileViewData
+  softGreProfileData?: SoftGreViewData
+  clientIsolationData?: ClientIsolationSaveData
+}
+
+
+
+const LanPortProfileDetailsDrawer = (props: LanPortProfileDetailsDrawerProps) => {
+  const { $t } = useIntl()
+  const params = useParams()
+  const { title, visible, setVisible, wiredPortVisible, ethernetPortProfileData } = props
+  const enableServicePolicyRbac = useIsSplitOn(Features.RBAC_SERVICE_POLICY_TOGGLE)
+  const enableClientVisibility = useIsSplitOn(Features.WIFI_WIRED_CLIENT_VISIBILITY_TOGGLE)
+
+  const onClose = () => {
+    setVisible(false)
+  }
+
+  // eslint-disable-next-line max-len
+  const { radiusNameMap = [] } = useGetAAAPolicyViewModelListQuery({
+    params: { tenantId: params.tenantId },
+    payload: {
+      fields: ['name', 'id'],
+      sortField: 'name',
+      sortOrder: 'ASC',
+      page: 1,
+      pageSize: 10000
+    },
+    enableRbac: enableServicePolicyRbac
+  }, {
+    selectFromResult: ({ data }: { data?: { data: AAAViewModalType[] } }) => ({
+      radiusNameMap: data?.data?.map(radius => ({ key: radius.id!, value: radius.name }))
+    })
+  })
+
+  const { data: ethernetData } = useConfigTemplateQueryFnSwitcher({
+    useQueryFn: useGetEthernetPortProfileByIdQuery,
+    useTemplateQueryFn: useGetEthernetPortProfileTemplateQuery,
+    enableRbac: true,
+    extraParams: { id: ethernetPortProfileData?.id },
+    skip: !ethernetPortProfileData?.id
+  })
+
+  const ethernetDataForDisplay = useMemo(() => ({
+    ...ethernetData,
+    ...ethernetPortProfileData
+  }), [ethernetData, ethernetPortProfileData])
+
+  const content = (
+    <Form
+      labelCol={{ span: 9 }}
+      labelAlign='left'
+    >
+      <Form.Item
+        label={$t({ defaultMessage: 'Name' })}
+        children={ethernetDataForDisplay?.name}
+      />
+      <Form.Item
+        label={$t({ defaultMessage: 'Port type' })}
+        children={getEthernetPortTypeString(ethernetDataForDisplay?.type)
+        }
+      />
+      <Form.Item
+        label={$t({ defaultMessage: 'VLAN Untag ID' })}
+        children={ethernetDataForDisplay?.untagId}
+      />
+      <Form.Item
+        label={$t({ defaultMessage: 'VLAN Member' })}
+        children={ethernetDataForDisplay?.vlanMembers}
+      />
+
+      <Form.Item
+        label={$t({ defaultMessage: '802.1X' })}
+        children={
+          transformDisplayOnOff(!(ethernetDataForDisplay?.authType === EthernetPortAuthType.DISABLED ||
+           ethernetDataForDisplay?.authType === EthernetPortAuthType.OPEN))
+        }
+      />
+      {!wiredPortVisible && enableClientVisibility &&
+        <Form.Item
+          label={$t({ defaultMessage: 'Client Visibility' })}
+          children={
+            transformDisplayOnOff(ethernetDataForDisplay?.authType === EthernetPortAuthType.OPEN ||
+            ethernetDataForDisplay?.authType === EthernetPortAuthType.MAC_BASED ||
+            ethernetDataForDisplay?.authType === EthernetPortAuthType.PORT_BASED)
+          }
+        />
+      }
+      {!(ethernetDataForDisplay?.authType === EthernetPortAuthType.DISABLED) &&
+      <>
+        <Divider/>
+
+        <Form.Item
+          label={$t({ defaultMessage: '802.1X Role' })}
+          children={getEthernetPortAuthTypeString(ethernetDataForDisplay?.authType)}
+        />
+
+        {ethernetDataForDisplay?.authType === EthernetPortAuthType.SUPPLICANT &&
+        <Form.Item
+          label={$t({ defaultMessage: 'Credential Type' })}
+          children={getEthernetPortCredentialTypeString(ethernetDataForDisplay?.supplicantAuthenticationOptions?.type)}
+        />
+        }
+        {!(ethernetDataForDisplay?.authType === EthernetPortAuthType.SUPPLICANT) &&
+        <>
+          <Form.Item
+            label={$t({ defaultMessage: 'Authentication Service' })}
+            children={
+              (!ethernetDataForDisplay?.authRadiusId)
+                ? ''
+                : (
+                  <TenantLink to={getPolicyDetailsLink({
+                    type: PolicyType.AAA,
+                    oper: PolicyOperation.DETAIL,
+                    policyId: ethernetDataForDisplay.authRadiusId })}>
+                    {radiusNameMap.find(radius => radius.key === ethernetDataForDisplay?.authRadiusId)?.value || ''}
+                  </TenantLink>)
+            }
+          />
+
+          <Form.Item
+            label={$t({ defaultMessage: 'Proxy Service(Auth)' })}
+            children={transformDisplayOnOff(!!ethernetDataForDisplay?.enableAuthProxy)}
+          />
+
+          <Form.Item
+            label={$t({ defaultMessage: 'Accounting Service' })}
+            children={
+              (!ethernetDataForDisplay?.accountingRadiusId)
+                ? '-'
+                : (
+                  <TenantLink to={getPolicyDetailsLink({
+                    type: PolicyType.AAA,
+                    oper: PolicyOperation.DETAIL,
+                    policyId: ethernetDataForDisplay.accountingRadiusId })}>
+                    {radiusNameMap.find(radius => radius.key === ethernetDataForDisplay?.accountingRadiusId)?.value || ''}
+                  </TenantLink>)
+            }
+          />
+          <Form.Item
+            label={$t({ defaultMessage: 'Proxy Service(Accounting)' })}
+            children={transformDisplayOnOff(!!ethernetDataForDisplay?.enableAuthProxy)}
+          />
+
+          {ethernetDataForDisplay?.authType === EthernetPortAuthType.MAC_BASED &&
+          <>
+            <Form.Item
+              label={$t({ defaultMessage: 'MAC Auth Bypass' })}
+              children={transformDisplayOnOff(!!ethernetDataForDisplay?.bypassMacAddressAuthentication)}
+            />
+
+            {ethernetDataForDisplay?.authType === EthernetPortAuthType.MAC_BASED &&
+              <>
+                <Form.Item
+                  label={$t({ defaultMessage: 'Dynamic VLAN' })}
+                  children={transformDisplayOnOff(!!ethernetDataForDisplay?.dynamicVlanEnabled)}
+                />
+                { ethernetDataForDisplay?.dynamicVlanEnabled &&
+                  <Form.Item
+                    label={$t({ defaultMessage: 'Guest VLAN' })}
+                    children={ethernetDataForDisplay?.unauthenticatedGuestVlan}
+                  />
+                }
+              </>
+            }
+          </>
+          }
+        </>
+        }
+      </>
+      }
+      {wiredPortVisible && enableClientVisibility &&
+      <>
+        <Divider/>
+
+        <Form.Item
+          label={$t({ defaultMessage: 'SoftGRE Tunnel' })}
+          children={getEthernetPortAuthTypeString(ethernetDataForDisplay?.authType)}
+        />
+      </>
+      }
+    </Form>
+  )
+
+  return (
+    <Drawer
+      title={title}
+      visible={visible}
+      onClose={onClose}
+      children={content}
+      width={'480px'}
+    />
+  )
+}
+
+export default LanPortProfileDetailsDrawer
