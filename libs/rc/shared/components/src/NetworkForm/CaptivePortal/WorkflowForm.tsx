6ea4ef05970a7397bc4b867ef2676e8b4bc5d143
Chen, Sander <Sander.Chen@commscope.com>
2025-05-22 20:11:56 +0800

    ACX-66928: Add Accounting Service functionality to WorkflowForm
    Integrated UI components for enabling and configuring Accounting Service within the Captive Portal WorkflowForm. This includes options for toggling the service, adding AAAInstance server details, and configuring a proxy service with tooltips for additional guidance.

--- libs/rc/shared/components/src/NetworkForm/CaptivePortal/WorkflowForm.tsx
+++ libs/rc/shared/components/src/NetworkForm/CaptivePortal/WorkflowForm.tsx
@@ -1,14 +1,29 @@
 import React, { useContext } from 'react'
 
-import { Form }    from 'antd'
-import { useIntl } from 'react-intl'
+import { Form, Space, Switch } from 'antd'
+import { useIntl }             from 'react-intl'
 
-import { GridCol, GridRow, Select, StepsFormLegacy }                                 from '@acx-ui/components'
-import { GuestNetworkTypeEnum, NetworkSaveData, NetworkTypeEnum, useConfigTemplate } from '@acx-ui/rc/utils'
+import {
+  GridCol,
+  GridRow,
+  Select,
+  StepsFormLegacy,
+  Subtitle,
+  Tooltip
+} from '@acx-ui/components'
+import {
+  GuestNetworkTypeEnum,
+  NetworkSaveData,
+  NetworkTypeEnum,
+  useConfigTemplate,
+  WifiNetworkMessages
+} from '@acx-ui/rc/utils'
 
+import { AAAInstance }             from '../AAAInstance'
 import { NetworkDiagram }          from '../NetworkDiagram/NetworkDiagram'
 import NetworkFormContext          from '../NetworkFormContext'
 import { NetworkMoreSettingsForm } from '../NetworkMoreSettings/NetworkMoreSettingsForm'
+import * as UI                     from '../styledComponents'
 
 import {
   BypassCaptiveNetworkAssistantCheckbox
@@ -16,16 +31,24 @@ import {
 import { WalledGardenTextArea }  from './SharedComponent/WalledGarden/WalledGardenTextArea'
 import { WlanSecurityFormItems } from './SharedComponent/WlanSecurity/WlanSecuritySettings'
 
-export function WorkflowForm () {
 
-  const { data, editMode, isRuckusAiMode, cloneMode } =
+const { useWatch } = Form
+
+export function WorkflowForm () {
+  const labelWidth = '250px'
+  const { data, setData, editMode, isRuckusAiMode, cloneMode } =
     useContext(NetworkFormContext)
-  const { isTemplate } = useConfigTemplate()
-  const { $t } = useIntl()
   const form = Form.useFormInstance()
 
-  // SANTODO: implement editing logic
+  const onProxyChange = (value: boolean, fieldName: string) => {
+    setData && setData({ ...data, [fieldName]: value })
+  }
+  const enableAccountingService = useWatch('enableAccountingService', form)
 
+  const { isTemplate } = useConfigTemplate()
+  const { $t } = useIntl()
+
+  // SANTODO: implement editing logic
   return (
     <>
       <GridRow>
@@ -51,9 +74,47 @@ export function WorkflowForm () {
           <WlanSecurityFormItems />
           <BypassCaptiveNetworkAssistantCheckbox />
           <WalledGardenTextArea
-            enableDefaultWalledGarden={false}
+            enableDefaultWalledGarden={true}
           />
-          {/*SANTODO: Add Accounting Service*/}
+          <div>
+            <UI.FieldLabel width={labelWidth}>
+              <Subtitle level={3}>{$t({ defaultMessage: 'Accounting Service' })}</Subtitle>
+              <Form.Item
+                name='enableAccountingService'
+                valuePropName='checked'
+                style={{ marginTop: '-5px', marginBottom: '0' }}
+                initialValue={false}
+                children={<Switch
+                  onChange={(value) => onProxyChange(value,'enableAccountingService')}
+                />}
+              />
+            </UI.FieldLabel>
+            {enableAccountingService && <>
+              <AAAInstance serverLabel={$t({ defaultMessage: 'Accounting Server' })}
+                type='accountingRadius'
+                networkType={data?.type}
+              />
+              <UI.FieldLabel width={labelWidth}>
+                <Space align='start'>
+                  { $t({ defaultMessage: 'Proxy Service' }) }
+                  <Tooltip.Question
+                    placement='bottom'
+                    title={$t(WifiNetworkMessages.ENABLE_PROXY_IN_CAPTIVE_PORTAL_TOOLTIP)}
+                    iconStyle={{ height: '16px', width: '16px', marginBottom: '-3px' }}
+                  />
+                </Space>
+                <Form.Item
+                  name='enableAccountingProxy'
+                  valuePropName='checked'
+                  initialValue={false}
+                  children={<Switch
+                    data-testid='enable-accounting-proxy'
+                    onChange={(value)=>onProxyChange(value,'enableAccountingProxy')}
+                  />}
+                />
+              </UI.FieldLabel>
+            </>}
+          </div>
         </GridCol>
         {/*SANTODO: Images need update, no psk and owe.*/}
         <GridCol col={{ span: 14 }}>
@@ -61,6 +122,7 @@ export function WorkflowForm () {
             type={NetworkTypeEnum.CAPTIVEPORTAL}
             networkPortalType={GuestNetworkTypeEnum.Workflow}
             wlanSecurity={data?.wlan?.wlanSecurity}
+            forceHideAAAButton={true}
           />
         </GridCol>
       </GridRow>
