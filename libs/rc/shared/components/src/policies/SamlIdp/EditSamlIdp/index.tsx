0238448b2e075d2710cf76d92aa041c0cc69420b
mchou <morris.chou@commscope.com>
2025-05-26 16:12:43 +0800

    ACX-86046: remove unused comment

--- libs/rc/shared/components/src/policies/SamlIdp/EditSamlIdp/index.tsx
+++ libs/rc/shared/components/src/policies/SamlIdp/EditSamlIdp/index.tsx
@@ -93,35 +93,6 @@ export const EditSamlIdp = () => {
     if (sourceData.metadataUrl) {
       form.setFieldValue('metadataContent', sourceData.metadataUrl)
     }
-
-    // if (sourceData.attributeMappings) {
-    //   form.setFieldValue('identityName',
-    //     sourceData.attributeMappings
-    //       .find(
-    //         mapping => mapping.name === IdentityAttributeMappingNameType.DISPLAY_NAME
-    //       )?.mappedByName
-    //   )
-    //   form.setFieldValue('identityEmail',
-    //     sourceData.attributeMappings
-    //       .find(
-    //         mapping => mapping.name === IdentityAttributeMappingNameType.EMAIL
-    //       )?.mappedByName
-    //   )
-    //   form.setFieldValue('identityPhone',
-    //     sourceData.attributeMappings
-    //       .find(
-    //         mapping => mapping.name === IdentityAttributeMappingNameType.PHONE_NUMBER
-    //       )?.mappedByName
-    //   )
-
-    //   // remove above three mappings from attributeMappings
-    //   form.setFieldValue('attributeMappings', sourceData.attributeMappings.filter(
-    //     mapping => !excludedAttributeTypes.includes(
-    //       mapping.name as IdentityAttributeMappingNameType
-    //     )
-    //   ))
-    // }
-
   }, [samlIdpProfileData])
 
   return (
