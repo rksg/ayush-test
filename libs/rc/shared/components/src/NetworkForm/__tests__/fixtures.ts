34764b851e3bffcf374ab01f416490cbecf2f326
mchou <morris.chou@commscope.com>
2025-05-29 17:41:29 +0800

    ACX-86292: Support single component for accounting service Replace the radiusSetting use new component

--- libs/rc/shared/components/src/NetworkForm/__tests__/fixtures.ts
+++ libs/rc/shared/components/src/NetworkForm/__tests__/fixtures.ts
@@ -2140,6 +2140,58 @@ export const mockAAAPolicyListResponse = {
   ]
 }
 
+export const mockedAccountingServiceName1 = '__mocked_accounting_service_name1__'
+export const mockedAccountingServiceName2 = '__mocked_accounting_service_name2__'
+export const mockedAccountingRadsecName1 = '__mocked_account_radsec_service_name1__'
+
+export const mockedAccountingServiceId1 = '__mocked_accounting_service_Id1__'
+export const mockedAccountingServiceId2 = '__mocked_accounting_service_Id2__'
+export const mockedAccountingRadsecId1 = '__mocked_account_radsec_service_Id1__'
+
+export const mockedAccountingService1 = {
+  name: mockedAccountingServiceName1,
+  type: 'ACCOUNTING',
+  primary: '201.1.1.1:1812',
+  id: mockedAccountingServiceId1
+}
+
+export const mockedAccountingService2 = {
+  name: mockedAccountingServiceName2,
+  type: 'ACCOUNTING',
+  primary: '201.1.1.2:1812',
+  secondary: '201.2.1.2:1187',
+  id: mockedAccountingServiceId2
+}
+
+export const mockedAccountingRadsecService1 = {
+  id: mockedAccountingRadsecId1,
+  name: mockedAccountingRadsecName1,
+  primary: {
+    ip: '201.1.2.1',
+    port: 2083
+  },
+  radSecOptions: {
+    tlsEnabled: true,
+    cnSanIdentity: 'cnSan',
+    ocspUrl: 'aaa.com'
+  },
+  type: 'ACCOUNTING'
+}
+
+export const mockAAAPolicyAccountingListResponse = {
+  page: 1,
+  totalCount: 3,
+  data: [
+    mockedAccountingService1,
+    mockedAccountingService2,
+    mockedAccountingRadsecService1
+  ]
+}
+
+
+
+
+
 export const mockAAAPolicyTemplateListResponse = {
   page: 1,
   totalCount: 2,
