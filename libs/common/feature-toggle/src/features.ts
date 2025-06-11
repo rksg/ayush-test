0fedac83c5b4f47a830543d54c5b960e5532e46b
TLIN4 <Tsaihuei.Lin@commscope.com>
2025-06-11 10:44:44 +0800

    ACX-88770: Remove SWITCH_PROFILE_ONBOARD_ONLY feature flag and related code

--- libs/common/feature-toggle/src/features.ts
+++ libs/common/feature-toggle/src/features.ts
@@ -185,7 +185,6 @@ export enum Features {
   SWITCH_SUPPORT_ICX8100= 'acx-ui-switch-support-icx8100-toggle',
   SWITCH_CYCLE_POE = 'switch-cycle-poe-toggle',
   SWITCH_FIRMWARE_V1002_TOGGLE= 'removable-ff',
-  SWITCH_PROFILE_ONBOARD_ONLY= 'removable-ff',
   SWITCH_LEVEL_CLI_PROFILE = 'switch-consumer-switch-level-cli-profile-toggle',
   SWITCH_CABLE_TEST = 'switch-cable-testing-toggle',
   SWITCH_FLEXIBLE_AUTHENTICATION = 'switch-consumer-flexible-authentication-toggle',
