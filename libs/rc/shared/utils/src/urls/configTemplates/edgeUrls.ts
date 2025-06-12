83a291de69b171688b15ab58c4038405793ff3f5
Cheng, Amy <Amy.Cheng@commscope.com>
2025-06-12 14:16:27 +0800

    ACX-86121: remove deactivate network from edge template URLs

--- libs/rc/shared/utils/src/urls/configTemplates/edgeUrls.ts
+++ libs/rc/shared/utils/src/urls/configTemplates/edgeUrls.ts
@@ -1,17 +1,6 @@
-import { ApiInfo } from '@acx-ui/utils'
-
 export const EdgeConfigTemplateUrlsInfo: { [key: string]: ApiInfo } = {
   activateSdLanNetworkTemplate: {
     method: 'put',
-    url: '/edgeSdLanServices/:serviceId/venues/:venueId/wifiNetworks/:wifiNetworkId',
-    newApi: true,
-    defaultHeaders: {
-      'Content-Type': 'application/vnd.ruckus.v1.1+json',
-      'Accept': 'application/vnd.ruckus.v1.1+json'
-    }
-  },
-  deactivateSdLanNetworkTemplate: {
-    method: 'delete',
     url: '/edgeSdLanServices/:serviceId/venues/:venueId/wifiNetworks/:wifiNetworkId',
     newApi: true,
     defaultHeaders: {
