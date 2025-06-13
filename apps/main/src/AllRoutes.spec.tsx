6940af0f728f4d3316442a0531cacc5bdf6232c4
Jacky Chiu <jacky.chiu@commscope.com>
2025-06-13 12:16:14 +0800

    ACX-88591: Refactor unit test based on review feedback

--- apps/main/src/AllRoutes.spec.tsx
+++ apps/main/src/AllRoutes.spec.tsx
@@ -40,7 +40,7 @@ jest.mock('@acx-ui/main/components', () => ({
   FetchBot: () => <div data-testid='fetch-bot' />
 }))
 jest.mock('@acx-ui/rc/components', () => ({
-  ...jest.requireActual('@acx-ui/rc/components'),
+  withTemplateFeatureGuard: () => () => <div data-testid='with-template-feature-guard' />,
   CloudMessageBanner: () => <div data-testid='cloud-message-banner' />,
   useUpdateGoogleMapRegion: () => { return { update: jest.fn() }},
   useIsEdgeReady: jest.fn().mockReturnValue(false),
