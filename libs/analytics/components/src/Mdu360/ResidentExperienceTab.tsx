3eddd46a4bc5faffba6c098fa00ed75ed3c05c96
HK1055 <hsienkit.khor@commscope.com>
2025-06-13 14:21:57 +0800

    MLSA-9945: Update Mdu360 structure

--- libs/analytics/components/src/Mdu360/ResidentExperienceTab.tsx
+++ libs/analytics/components/src/Mdu360/ResidentExperienceTab.tsx
@@ -2,20 +2,107 @@ import moment from 'moment-timezone'
 
 import { GridRow , GridCol } from '@acx-ui/components'
 
+import { TrafficBy } from './Widgets/TrafficBy'
+
 import type { Mdu360TabPros } from '.'
+
 
 const ResidentExperienceTab: React.FC<Mdu360TabPros> = ({ startDate, endDate }) => {
 
   return (
     <GridRow>
-      <GridCol col={{ span: 24 }} >
-        {/* Please add widgets and remove this content */}
-       startDate: {moment(startDate).format('YYYY-MM-DD HH:mm:ss')},
-       endDate: {moment(endDate).format('YYYY-MM-DD HH:mm:ss')}
+      <GridCol col={{ span: 12 }} style={{ height: '240px' }}>
+        <TrafficBy filters={filters}/>
       </GridCol>
     </GridRow>
-
   )
 }
 
 export default ResidentExperienceTab
+
+// import { useState } from 'react'
+
+// import Card        from 'antd/lib/card/Card'
+// import moment      from 'moment-timezone'
+// import { useIntl } from 'react-intl'
+// import AutoSizer   from 'react-virtualized-auto-sizer'
+
+// import { useAnalyticsFilter } from '@acx-ui/analytics/utils'
+// import {
+//   getDefaultEarliestStart,
+//   GridRow,
+//   GridCol,
+//   Loader,
+//   PageHeader,
+//   RangePicker,
+//   Tabs} from '@acx-ui/components'
+// import { VenueFilter }                           from '@acx-ui/main/components'
+// import { useNavigate, useTenantLink, useParams } from '@acx-ui/react-router-dom'
+// import { useDateFilter }                         from '@acx-ui/utils'
+
+// import Mdu360Tabs from './Mdu360Tabs'
+// import { TrafficBy } from './Widgets/TrafficBy'
+
+// const Mdu360: React.FC<{}> = () => {
+//   const { $t } = useIntl()
+//   const basePath = useTenantLink('/mdu360', 'v')
+//   const navigate = useNavigate()
+//   const { activeTab } = useParams()
+//   const [selectedVenues, setSelectedVenues] = useState<string[]>([])
+//   const { startDate, endDate, setDateFilter, range } = useDateFilter({
+//     showResetMsg: true, earliestStart: getDefaultEarliestStart() })
+//   const { filters } = useAnalyticsFilter()
+
+//   const onTabChange = (tab: string) =>
+//     navigate({
+//       ...basePath,
+//       pathname: `${basePath.pathname}/${tab}`
+//     })
+
+//   return (
+//     <Loader>
+//       <PageHeader
+//         title={$t({ defaultMessage: 'MDU 360' })}
+//         extra={[
+//           <VenueFilter
+//             // selectedVenues={selectedVenues}
+//             // setSelectedVenues={setSelectedVenues}
+//           />,
+//           <RangePicker
+//             selectedRange={{ startDate: moment(startDate), endDate: moment(endDate) }}
+//             onDateApply={setDateFilter as CallableFunction}
+//             showTimePicker
+//             selectionType={range}
+//             maxMonthRange={3} //TODO: check
+//           />
+//         ]}
+//       />
+//       <Tabs onChange={onTabChange} activeKey={activeTab}>
+//         <Tabs.TabPane
+//           tab={$t({ defaultMessage: 'Network Overview' })}
+//           key='networkOverview'
+//         />
+//         <Tabs.TabPane
+//           tab={$t({ defaultMessage: 'Resident Experience' })}
+//           key='residentExperience'
+//         >
+//           <GridRow>
+//             <GridCol col={{ span: 12 }} style={{ height: '179px' }}>
+//               <Card/>
+//             </GridCol>
+//             <GridCol col={{ span: 12 }} style={{ height: '179px' }}>
+//               <Card/>
+//             </GridCol>
+//             <GridCol col={{ span: 12 }} style={{ height: '240px' }}>
+//               <TrafficBy filters={filters}/>
+//               {/* <WifiClient filters={filters} /> */}
+//             </GridCol>
+//             <GridCol col={{ span: 12 }} style={{ height: '240px' }}>
+//               <Card/>
+//             </GridCol>
+//           </GridRow>
+//         </Tabs.TabPane>
+//       </Tabs>
+//     </Loader>
+//   )
+// }
---
+++ libs/analytics/components/src/Mdu360/Widgets/TrafficByVolume/services.ts
@@ -0,0 +1,64 @@
+import { gql } from 'graphql-request'
+
+import { getFilterPayload, calculateGranularity } from '@acx-ui/analytics/utils'
+import { dataApi }                                from '@acx-ui/store'
+import type { AnalyticsFilter }                   from '@acx-ui/utils'
+
+export type TrafficByVolumeData = {
+  time: string[]
+  userTraffic_all: number[]
+  userTraffic_6: number[]
+  userTraffic_5: number[]
+  userTraffic_24: number[]
+}
+
+interface Response <TimeSeriesData> {
+  network: {
+    hierarchyNode: {
+      timeSeries: TimeSeriesData
+    }
+  }
+}
+
+export const api = dataApi.injectEndpoints({
+  endpoints: (build) => ({
+    trafficByVolume: build.query<
+      TrafficByVolumeData,
+      AnalyticsFilter
+    >({
+      query: (payload) => ({
+        document: gql`
+          query TrafficByVolumeWidget(
+            $path: [HierarchyNodeInput]
+            $start: DateTime
+            $end: DateTime
+            $granularity: String
+            $filter: FilterInput
+          ) {
+            network(start: $start, end: $end, filter : $filter) {
+              hierarchyNode(path: $path) {
+                timeSeries(granularity: $granularity) {
+                  time
+                  userTraffic_all: userTraffic
+                  userTraffic_6: userTraffic(band: "6")
+                  userTraffic_5: userTraffic(band: "5")
+                  userTraffic_24: userTraffic(band: "2.4")
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          start: payload.startDate,
+          end: payload.endDate,
+          granularity: calculateGranularity(payload.startDate, payload.endDate),
+          ...getFilterPayload(payload)
+        }
+      }),
+      transformResponse: (response: Response<TrafficByVolumeData>) =>
+        response.network.hierarchyNode.timeSeries
+    })
+  })
+})
+
+export const { useTrafficByVolumeQuery } = api
