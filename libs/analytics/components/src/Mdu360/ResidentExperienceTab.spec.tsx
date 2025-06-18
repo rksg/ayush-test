7f5e18af52d6f0330b568b3dfca9f647237d3e40
cl1092 <Bess.Leng@commscope.com>
2025-06-18 16:07:45 +0800

    MLSA-10008: Updated UT

--- libs/analytics/components/src/Mdu360/ResidentExperienceTab.spec.tsx
+++ libs/analytics/components/src/Mdu360/ResidentExperienceTab.spec.tsx
@@ -21,19 +21,14 @@ jest.mock('./widgets/WifiGeneration', () => ({
   WifiGeneration: jest.fn(() => <div>Wi-Fi Generation</div>)
 }))
 
-
 describe('ResidentExperienceTab', () => {
-
   it('renders ResidentExperienceTab correct', async () => {
 
     render(<ResidentExperienceTab
       startDate='2023-02-01T00:00:00.000Z'
       endDate='2023-02-01T00:00:00.000Z' />, { wrapper: Provider })
 
-    // when data-api ready, uncomment the following lines
     expect(await screen.findByText('Wi-Fi Client')).toBeVisible()
     expect(await screen.findByText('Wi-Fi Generation')).toBeVisible()
-
   })
-
 })
