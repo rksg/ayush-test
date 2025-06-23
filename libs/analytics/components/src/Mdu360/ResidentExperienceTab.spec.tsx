f3cfd478a8b8c010acb74a8037c8c0f60bf85f08
HK1055 <hsienkit.khor@commscope.com>
2025-06-23 16:14:33 +0800

    MLSA-9945: Add test for ResidentExperienceTab

--- libs/analytics/components/src/Mdu360/ResidentExperienceTab.spec.tsx
+++ libs/analytics/components/src/Mdu360/ResidentExperienceTab.spec.tsx
@@ -1,26 +1,28 @@
-
-import { Provider } from '@acx-ui/store'
+import { Provider }       from '@acx-ui/store'
+import { render, screen } from '@acx-ui/test-utils'
 
 import ResidentExperienceTab from './ResidentExperienceTab'
 
-const mockNavigate = jest.fn()
-
-jest.mock('@acx-ui/react-router-dom', () => ({
-  ...jest.requireActual('@acx-ui/react-router-dom'),
-  useParams: jest.fn().mockReturnValue({ activeTab: 'networkOverview' }),
-  useNavigate: () => mockNavigate,
-  useTenantLink: jest.fn().mockReturnValue({ pathname: '/t1/v/mdu360' })
+jest.mock('./Widgets/TrafficByRadio/services', () => ({
+  useTrafficByRadioQuery: jest.fn().mockReturnValue({ isLoading: false })
+}))
+jest.mock('./Widgets/WifiClient/services', () => ({
+  useTopNWifiClientQuery: jest.fn().mockReturnValue({ isLoading: false })
 }))
 
 describe('ResidentExperienceTab', () => {
 
-  it('renders ResidentExperienceTab correct', () => {
-    // please remove MatchSnapshot test when the component is added widget
-    expect(<Provider>
+  it('renders ResidentExperienceTab widgets correctly', async () => {
+    render(
       <ResidentExperienceTab
         startDate='2023-02-01T00:00:00.000Z'
-        endDate='2023-02-01T00:00:00.000Z' />
-    </Provider>).toMatchSnapshot()
+        endDate='2023-02-02T00:00:00.000Z'
+      />,
+      { wrapper: Provider }
+    )
+
+    expect(await screen.findByText('Wi-Fi Client')).toBeVisible()
+    expect(await screen.findByText('Traffic By Radio')).toBeVisible()
   })
 
 })
