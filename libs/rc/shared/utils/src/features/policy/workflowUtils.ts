56e37cebd10d9dd50c59eb4a35842717f5fb7ef1
Daniel Fowler <daniel.fowler@commscope.com>
2025-06-25 14:37:52 -0600

    ACX-89621: Display workflow start step when start branch has no real steps

--- libs/rc/shared/utils/src/features/policy/workflowUtils.ts
+++ libs/rc/shared/utils/src/features/policy/workflowUtils.ts
@@ -44,9 +44,7 @@ export const useGetActionDefaultValueByType = (actionType: ActionType) => {
 }
 
 export const findAllFirstSteps = (steps: WorkflowStep[]): WorkflowStep[] | undefined => {
-  return steps.filter(step =>
-    step.priorStepId === undefined && !step.splitOptionId
-  )
+  return steps.filter(step => step.priorStepId === undefined && !step.splitOptionId)
 }
 
 export const toStepMap = (steps: WorkflowStep[])
@@ -198,7 +196,7 @@ export const composeNext = (
       attachCandidate: false
     },
 
-    hidden: type === StepType.End || (type === StepType.Start && stepMap.size !== 2),
+    hidden: type === StepType.End || (type === StepType.Start && nextStep?.type !== StepType.End),
     deletable: false
   })
 
