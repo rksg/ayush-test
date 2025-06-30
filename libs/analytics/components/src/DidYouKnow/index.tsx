1e697e8875b1625a934788314ca63738d05996c6
stan2 <shiawuen.tan@commscope.com>
2025-06-30 18:03:45 +0800

    Revert "ACX-90412: remove DYK for core tier"
    This reverts commit d7e869faecb7e7b6afe865038b29c259b598c384.

--- libs/analytics/components/src/DidYouKnow/index.tsx
+++ libs/analytics/components/src/DidYouKnow/index.tsx
@@ -6,6 +6,7 @@ import AutoSizer   from 'react-virtualized-auto-sizer'
 
 import { Loader }                           from '@acx-ui/components'
 import { Features, useIsSplitOn }           from '@acx-ui/feature-toggle'
+import { getUserProfile, isCoreTier }       from '@acx-ui/user'
 import type { DashboardFilter, PathFilter } from '@acx-ui/utils'
 import { useTrackLoadTime, widgetsMapping } from '@acx-ui/utils'
 
@@ -17,16 +18,34 @@ export { DidYouKnowWidget as DidYouKnow }
 type DidYouKnowWidgetProps = {
   filters: PathFilter | DashboardFilter
 }
+export const getCarouselFactsMap = (facts: string[]) => {
+  let map: Record<number, { facts: string[] }> = {}
+  for (let i = 0; i < facts.length; i += 2) {
+    map[i / 2 + 1] = { facts: facts.slice(i, i + 2) }
+  }
+  return map
+}
 
+const nonCoreTierFacts = [
+  'topIncidentsZones',
+  'topIncidentsApGroups',
+  'l3AuthFailure'
+]
 function DidYouKnowWidget ({ filters }: DidYouKnowWidgetProps) {
   const intl = useIntl()
   const [offset, setOffset] = useState(0)
   const [loaded, setLoaded] = useState<string[]>([])
   const [content, setContent] = useState<Record<string, string>>({})
+  const userProfileR1 = getUserProfile()
+  const { accountTier } = userProfileR1 ?
+    userProfileR1 : { accountTier: undefined }
 
+  const isCore = isCoreTier(accountTier)
   const isMonitoringPageEnabled = useIsSplitOn(Features.MONITORING_PAGE_LOAD_TIMES)
   const factListQuery = useAvailableFactsQuery(filters)
-  const factsList = factListQuery.data
+  const factsList = (factListQuery.data ?? []).filter(factArray =>
+    !isCore || _.isEmpty(_.intersection(nonCoreTierFacts, factArray))
+  )
   const factsQuery = useFactsQuery(factsList, loaded, offset, filters)
 
   useEffect(() => {
