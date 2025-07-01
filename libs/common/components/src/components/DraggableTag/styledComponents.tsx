5f732a34332f8ebfa443485ddfbdd1289f4e8000
TLIN4 <Tsaihuei.Lin@commscope.com>
2025-07-01 18:11:11 +0800

    ACX-90395: Implement draggable tag selector component

---
+++ libs/common/components/src/components/DraggableTag/styledComponents.tsx
@@ -0,0 +1,88 @@
+import { Tag } from 'antd'
+import styled  from 'styled-components/macro'
+
+export const TagsContainer = styled.div`
+  min-height: 100px;
+  padding: 8px;
+  border: 1px solid #ACAEB0;
+  border-radius: 4px;
+  overflow: hidden;
+  white-space: normal;
+  word-break: break-word;
+  transition: all 0.3s;
+
+  &.error {
+    border: 1px solid red;
+  }
+  &.active {
+    border: 1px solid var(--acx-primary-black);
+  }
+`
+
+export const TagWrapper = styled.div`
+  display: inline-block;
+  .ant-tag {
+    margin: 0 4px 8px 0;
+    padding: 0 8px;
+    cursor: move;
+  }
+  &:after {
+    content: ':';
+    display: inline-block;
+    margin-right: 4px;
+    width: 7px;
+    text-align: center;
+    color: var(--acx-primary-black);
+  }
+  &:last-child:after {
+    content: '';
+    width: 0px;
+  }
+  .ant-tag {
+    border-radius: 4px;
+    border-color: var(--acx-neutrals-20);
+    background: var(--acx-neutrals-20);
+    &.invalid {
+      border-color: #FFCBCD;
+      background: #FFCBCD;
+    }
+  }
+  .ant-tag-close-icon {
+    margin-left: 4px;
+    color: var(--acx-primary-black);
+  }
+`
+
+export const TagSelector = styled.div`
+  display: inline-block;
+  .ant-select-selector {
+    border: 0 !important;
+    padding: 0 !important;
+    box-shadow: none !important;
+  }
+  .ant-select-selection-search,
+  .ant-select-selection-search-input {
+    width: 80px;
+    height: 18px !important;
+    line-height: 18px;
+  }
+`
+
+export const Placeholder = styled.span`
+  font-size: 12px;
+  color: var(--acx-neutrals-60);
+  padding: 4px;
+  line-height: 16px;
+`
+
+export const AddTag = styled(Tag)`
+  display: inline-flex;
+  position: relative;
+  top: -1px;   
+  border: 0;
+  padding: 0 !important;
+  background: none;
+  vertical-align: middle;
+  align-items: center;
+  cursor: pointer;
+`
