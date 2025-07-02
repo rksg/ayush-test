096ccf469530f675120d8306cfcb73428edc4014
Guo, Yizhu <yizhu.guo@commscope.com>
2025-07-02 16:13:13 +0800

    ACX-90395: Fix rc-generic-features-components test

--- libs/rc/generic-features/components/src/usePreference/index.spec.tsx
+++ libs/rc/generic-features/components/src/usePreference/index.spec.tsx
@@ -23,8 +23,8 @@ jest.mock('@acx-ui/config', () => ({
   get: jest.fn().mockReturnValue('fake-google-maps-key')
 }))
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
+jest.mock('@acx-ui/react-router-dom', () => ({
+  ...jest.requireActual('@acx-ui/react-router-dom'),
   useNavigate: () => mockedUsedNavigate
 }))
 
