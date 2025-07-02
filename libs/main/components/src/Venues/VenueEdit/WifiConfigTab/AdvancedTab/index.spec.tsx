994eb2524ad9be337cc8e00c2761c0056ce7492d
Guo, Yizhu <yizhu.guo@commscope.com>
2025-07-02 12:32:40 +0800

    ACX-90262: Fix main-components test

--- libs/main/components/src/Venues/VenueEdit/WifiConfigTab/AdvancedTab/index.spec.tsx
+++ libs/main/components/src/Venues/VenueEdit/WifiConfigTab/AdvancedTab/index.spec.tsx
@@ -45,8 +45,8 @@ let editAdvancedContextData = {} as AdvanceSettingContext
 const setEditAdvancedContextData = jest.fn()
 
 const mockedUsedNavigate = jest.fn()
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
+jest.mock('@acx-ui/react-router-dom', () => ({
+  ...jest.requireActual('@acx-ui/react-router-dom'),
   useNavigate: () => mockedUsedNavigate
 }))
 
