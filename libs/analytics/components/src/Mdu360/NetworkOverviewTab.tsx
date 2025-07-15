db42f452a7522332bed0487b3eb2e10906894e68
HK1055 <hsienkit.khor@commscope.com>
2025-07-15 14:35:42 +0800

    MLSA-10152: Create NetworkOverview tab

---
+++ libs/analytics/components/src/Mdu360/NetworkOverviewTab.tsx
@@ -0,0 +1,12 @@
+import * as UI from './styledComponents'
+
+import type { Mdu360TabProps } from '.'
+
+const NetworkOverviewTab: React.FC<Mdu360TabProps> = ({ startDate, endDate }) => {
+  return (
+    <UI.Grid>
+    </UI.Grid>
+  )
+}
+
+export default NetworkOverviewTab
