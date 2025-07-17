2180f1811c8936953bb9e938eecea7494a485e63
HK1055 <hsienkit.khor@commscope.com>
2025-07-17 15:19:11 +0800

    MLSA-10152: Fix services test

--- libs/analytics/components/src/Mdu360/Widgets/IntentAISummary/services.spec.tsx
+++ libs/analytics/components/src/Mdu360/Widgets/IntentAISummary/services.spec.tsx
@@ -14,32 +14,30 @@ describe('IntentAISummary services', () => {
 
   it('should return correct data', async () => {
     const expectedResult = {
-      data: {
-        highlights: {
-          rrm: {
-            new: 0,
-            active: 0,
-            paused: 7,
-            verified: 3
-          },
-          probeflex: {
-            new: 0,
-            active: 0,
-            paused: 3,
-            verified: 6
-          },
-          ops: {
-            new: 0,
-            active: 0,
-            paused: 8,
-            verified: 14
-          },
-          ecoflex: {
-            new: 0,
-            active: 0,
-            paused: 1,
-            verified: 1
-          }
+      highlights: {
+        rrm: {
+          new: 0,
+          active: 0,
+          paused: 7,
+          verified: 3
+        },
+        probeflex: {
+          new: 0,
+          active: 0,
+          paused: 3,
+          verified: 6
+        },
+        ops: {
+          new: 0,
+          active: 0,
+          paused: 8,
+          verified: 14
+        },
+        ecoflex: {
+          new: 0,
+          active: 0,
+          paused: 1,
+          verified: 1
         }
       }
     }
@@ -50,7 +48,7 @@ describe('IntentAISummary services', () => {
       api.endpoints.intentAISummary.initiate(mockPayload)
     )
     expect(status).toBe('fulfilled')
-    expect(data).toStrictEqual(expectedResult.data.highlights)
+    expect(data).toStrictEqual(expectedResult.highlights)
     expect(error).toBe(undefined)
   })
 
