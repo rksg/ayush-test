b61f2b1c6054ba3d50b4516de800206b222a989e
yguo1 <Yizhu.Guo@commscope.com>
2025-07-22 18:46:20 +0800

    ACX-92252: Remove unused code

--- apps/main/src/pages/OnboardingAssistant/styledComponents.tsx
+++ apps/main/src/pages/OnboardingAssistant/styledComponents.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable max-len */
 import { StepsForm } from '@ant-design/pro-form'
 import {
-  Button as AntButton,
   Card,
   Modal,
   Steps
@@ -11,47 +10,6 @@ import styled from 'styled-components/macro'
 
 
 import RuckusAiBackground from './assets/RuckusAiBackground.svg'
-
-
-const Button = styled(AntButton).attrs({ type: 'primary' })`
-  &&& {
-    background-color: var(--acx-neutrals-10);
-    border: none;
-    &:hover, &:focus {
-      border-color: var(--acx-accents-orange-10);
-      background-color: var(--acx-accents-orange-10);
-    }
-    > svg {
-      width: 20px;
-      height: 100%;
-    }
-  }
-`
-
-export const ButtonSolid = styled(Button)`
-  > svg {
-    transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-  }
-  &:hover {
-    > svg {
-      stroke: var(--acx-accents-orange-55);
-    }
-  }
-`
-export const AiButton = styled.div`
-height: 36px;
-margin: -4px;
-cursor: pointer;
-svg {
-  opacity: 0.95;
-  width: 38px;
-  height: 38px;
-}
-&:hover {
-  > svg {
-    opacity: 1;
-}
-`
 
 export const GptModal = styled(Modal)<{ titleType: string, needBackground: boolean }>`
   .ant-modal-content {
