f39401b21f45420771b3ae704f3044df0fe87f66
Daniel Fowler <daniel.fowler@commscope.com>
2025-07-23 08:58:09 -0600

    ACX-92068: Attempt to prevent removal of sort in minification

--- libs/rc/shared/utils/src/features/policy/workflowUtils.ts
+++ libs/rc/shared/utils/src/features/policy/workflowUtils.ts
@@ -44,8 +44,10 @@ export const useGetActionDefaultValueByType = (actionType: ActionType) => {
 }
 
 export const findAllFirstSteps = (steps: WorkflowStep[]): WorkflowStep[] | undefined => {
-  return steps.filter(step => step.priorStepId === undefined && !step.splitOptionId)
-    .sort((a) => a.type === StepType.Start ? -1 : 0)
+  const startSteps = steps.filter(step => step.priorStepId === undefined && !step.splitOptionId)
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  const sortedStartSteps = startSteps.sort((a, b) => a.type === StepType.Start ? -1 : 1)
+  return sortedStartSteps
 }
 
 export const toStepMap = (steps: WorkflowStep[])
