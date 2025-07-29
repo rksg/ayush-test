65d28e34695995e301144bbeac4311e1c6ee9418
Jacky Chiu <jacky.chiu@commscope.com>
2025-07-29 15:38:00 +0800

    ACX-88591: Merge code from master

--- apps/msp/src/pages/ConfigTemplates/Templates/AppliedToListView/index.tsx
+++ apps/msp/src/pages/ConfigTemplates/Templates/AppliedToListView/index.tsx
@@ -1,20 +1,26 @@
 import { useIntl } from 'react-intl'
 
-import { DetailsItemList }         from '@acx-ui/main/components'
-import { useMspCustomerListQuery } from '@acx-ui/msp/services'
-import { ConfigTemplate }          from '@acx-ui/rc/utils'
+import { DetailsItemList, useConfigTemplateListContext } from '@acx-ui/main/components'
+import { useMspCustomerListQuery }                       from '@acx-ui/msp/services'
+import { ConfigTemplate }                                from '@acx-ui/rc/utils'
 
 import { useEcFilters } from '../../utils'
 
 export function AppliedToTenantList ({ selectedTemplate }: { selectedTemplate: ConfigTemplate }) {
   const { $t } = useIntl()
   const { appliedOnTenants } = selectedTemplate
+  const { setAppliedToViewVisible } = useConfigTemplateListContext()
+  const maxItemsToShow = 25
 
   const mspEcTenantsPayload = {
     filters: {
       ...useEcFilters(),
       id: appliedOnTenants
     },
+    page: 1,
+    pageSize: maxItemsToShow,
+    sortField: 'name',
+    sortOrder: 'ASC',
     fields: ['id', 'name']
   }
 
@@ -27,5 +33,7 @@ export function AppliedToTenantList ({ selectedTemplate }: { selectedTemplate: C
     title={$t({ defaultMessage: 'Applied to' })}
     items={data?.data.map(mspEcTenant => mspEcTenant.name) || []}
     isLoading={isLoading}
+    showMore={(data?.totalCount ?? 0) > maxItemsToShow}
+    showMoreCallback={() => setAppliedToViewVisible(true)}
   />
 }
