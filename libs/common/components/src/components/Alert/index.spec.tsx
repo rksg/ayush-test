5d8033f374d774c520242e7cc3a491c6e32d96e5
sp1179 <Sammanita.Pattnayak@commscope.com>
2025-07-29 16:43:11 +0530

    RSA-7828: Updated design to card from alert

--- libs/common/components/src/components/Alert/index.spec.tsx
+++ libs/common/components/src/components/Alert/index.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@acx-ui/test-utils'
+import { render } from '@acx-ui/test-utils'
 
 import { Alert } from './index'
 
@@ -9,20 +9,9 @@ describe('Alert', () => {
       <>
         <Alert message='Informational Notes' type='info' showIcon closable />
         <p />
-        <Alert message='Primary/Blue Information' type='primary' showIcon closable />
-        <p />
         <Alert message='Error' type='error' showIcon closable />
       </>
     )
     expect(asFragment()).toMatchSnapshot()
-  })
-
-  it('should render primary type with correct class', () => {
-    render(
-      <Alert message='Primary Alert' type='primary' showIcon />
-    )
-    const alert = screen.getByRole('alert')
-    expect(alert).toHaveClass('ant-alert-primary')
-    expect(alert).toHaveClass('ant-alert-info') // Should map to info type for Antd
   })
 })
