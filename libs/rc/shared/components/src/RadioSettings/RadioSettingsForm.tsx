9ffc6580ad5007ca15f3d4c241c71b3245fa71a8
rl1085 <ryan.lin@commscope.com>
2025-08-07 17:58:49 +0800

    ACX-71465:  Update aggressiveTxTooltip message to reflect context-specific calibration instructions

--- libs/rc/shared/components/src/RadioSettings/RadioSettingsForm.tsx
+++ libs/rc/shared/components/src/RadioSettings/RadioSettingsForm.tsx
@@ -95,11 +95,9 @@ export function RadioSettingsForm (props:{
   const [band320DrawerVisible, setBand320DrawerVisible] = useState(false)
 
   const afcTooltip = $t({ defaultMessage: 'For outdoor APs, AFC will be enabled automatically.' })
-  const aggressiveTxTooltip = $t({
-    defaultMessage: context === 'ap'
-      ? 'Adjust the value based on the calibration TX power on this device'
-      : 'Adjust the value based on the calibration TX power of each access point'
-  })
+  const aggressiveTxTooltip = context === 'ap' ?
+    $t({ defaultMessage: 'Adjust the value based on the calibration TX power on this device' }) :
+    $t({ defaultMessage: 'Adjust the value based on the calibration TX power of each access point' })
   const channelSelectionOpts = (!isVenueChannelSelectionManualEnabled && (context === 'venue' || context ==='apGroup')) ?
     channelSelectionMethodsOptions :
     (radioType === ApRadioTypeEnum.Radio6G) ?
