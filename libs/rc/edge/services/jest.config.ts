264232b23dc69c6806542d0d9daf4b4db753e081
Cheng, Amy <Amy.Cheng@commscope.com>
2025-08-12 12:38:29 +0800

    ACX-94108: remove coverage config from edge-services module

--- libs/rc/edge/services/jest.config.ts
+++ libs/rc/edge/services/jest.config.ts
@@ -5,13 +5,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'babel-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/rc/edge/services',
-  coverageThreshold: {
-    global: {
-      statements: 90,
-      branches: 90,
-      functions: 90,
-      lines: 90
-    }
-  }
+  coverageDirectory: '../../../../coverage/libs/rc/edge/services'
 }
