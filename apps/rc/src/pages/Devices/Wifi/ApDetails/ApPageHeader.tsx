41caf25f81baf62c6bcd882add6b10103955c703
Sharon Chen <sharon.chen2@commscope.com>
2025-08-13 13:05:05 +0800

    ACX-94495: Consist AP CLI actions between ap table and ap overview

--- apps/rc/src/pages/Devices/Wifi/ApDetails/ApPageHeader.tsx
+++ apps/rc/src/pages/Devices/Wifi/ApDetails/ApPageHeader.tsx
@@ -170,7 +170,10 @@ function ApPageHeader () {
     return (
       (currentApOperational && isHasPermission) ||
           (item.key === 'delete' && isHasPermission) ||
-          (item.key === 'downloadLog' && status === ApDeviceStatusEnum.CONFIGURATION_UPDATE_FAILED)
+          (item.key === 'downloadLog' && status === ApDeviceStatusEnum.CONFIGURATION_UPDATE_FAILED) ||
+          (item.key === 'cliSession' && [
+            ApDeviceStatusEnum.CONFIGURATION_UPDATE_FAILED,
+            ApDeviceStatusEnum.FIRMWARE_UPDATE_FAILED].includes(status))
     )
   }).map(item => {
     const { rbacOpsIds, ...others } = item
