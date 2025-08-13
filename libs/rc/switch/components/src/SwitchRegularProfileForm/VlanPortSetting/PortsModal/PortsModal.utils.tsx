7f7f5c978dee9b88455954be1e99925cfd7f9b6f
yw1046 <Yc.Wong@commscope.com>
2025-08-13 16:54:13 +0800

    ACX-94411: Refactor model check for 4X25G support in getSlots utility function

--- libs/rc/switch/components/src/SwitchRegularProfileForm/VlanPortSetting/PortsModal/PortsModal.utils.tsx
+++ libs/rc/switch/components/src/SwitchRegularProfileForm/VlanPortSetting/PortsModal/PortsModal.utils.tsx
@@ -61,11 +61,10 @@ export const getModelModules = (family: string, model: string): string[][] => {
 
 export const getSlots = (family: string, model: string,
   isSupport8100Module3_4x25G?: boolean): DefaultOptionType[][] => {
-  const notSupportModel = ['24F', '48F', '24ZP', '48ZP']
   const modelModules = getModelModules(family, model).map(item => {
     return item.filter(subItem => {
       if (!isSupport8100Module3_4x25G && family === 'ICX7550'
-        && notSupportModel.includes(model) && subItem === '4X25G') {
+        && ['24F', '48F', '24ZP', '48ZP'].includes(model) && subItem === '4X25G') {
         return false
       }
       return true
