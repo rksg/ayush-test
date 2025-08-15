4eca3b8d2c5c38bf809d3d45288f3d4fb1e986c0
yw1046 <Yc.Wong@commscope.com>
2025-08-15 14:56:22 +0800

    ACX-90264: Revert

--- libs/rc/switch/components/src/SwitchVeTable/index.tsx
+++ libs/rc/switch/components/src/SwitchVeTable/index.tsx
@@ -277,8 +277,6 @@ export function SwitchVeTable (props: {
   const isActionHidden = (data?: VeViewModel[]) => {
     return !isVenueLevel && switchDetail?.model?.startsWith('ICX8100')
       && (data?.length || 0) > 0
-      && (!isSupport8100StaticRouteMaxVe ||
-        !isFirmwareVersionAbove10010h(switchDetail?.firmware))
   }
 
   return <Loader states={[tableQuery]}>
