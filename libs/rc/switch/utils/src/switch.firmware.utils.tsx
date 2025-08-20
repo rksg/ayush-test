2723ea311d8696595417da17a428d7f831b16605
yw1046 <Yc.Wong@commscope.com>
2025-08-20 12:51:27 +0800

    ACX-95151: Simplify stack member limit logic for ICX8100-X, returning a fixed value of 4

--- libs/rc/switch/utils/src/switch.firmware.utils.tsx
+++ libs/rc/switch/utils/src/switch.firmware.utils.tsx
@@ -107,12 +107,7 @@ export const getStackUnitsMinLimitationV1002 = (
     }
     return (checkVersionAtLeast09010h(currentFirmware) ? 4 : 2)
   } else if (switchModel?.match(/ICX8100-.*-[xX]/)) {
-    if(currentFirmware.includes('10020')){
-      if (checkVersionAtLeast10020c(currentFirmware)) {
-        return 4
-      }
-    }
-    return (checkVersionAtLeast10010h(currentFirmware) ? 4 : 0)
+    return 4
   } else { // 7550, 7650, 7850
     if (checkVersionAtLeast10010c(currentFirmware)) {
       // For the switch's own firmware, this field contains the value '10010'.
