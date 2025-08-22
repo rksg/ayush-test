b1de29675b3323f7db5ebd1c3ea6f19dfa3227fd
shivend singh <shivend.singh@commscope.com>
2025-08-22 14:41:22 +0530

    ACX-87888: FF is disabling the QR code fix

--- apps/rc/src/pages/Policies/Workflow/WorkflowDetail/index.tsx
+++ apps/rc/src/pages/Policies/Workflow/WorkflowDetail/index.tsx
@@ -167,7 +167,7 @@ export default function WorkflowDetails () {
       visible: workflowValidationEnhancementFFToggle,
       content: <PublishReadinessProgress variant='text'
         reasons={data?.statusReasons as StatusReason[]} />,
-      colSpan: 4
+      colSpan: 3
     },
     {
       title: $t({ defaultMessage: 'Workflow URL Acccess' }),
@@ -184,7 +184,7 @@ export default function WorkflowDetails () {
             <QuestionMarkCircleOutlinedIcon/>
           </Tooltip></div>
         : $t({ defaultMessage: 'Public' }),
-      colSpan: 3
+      colSpan: 4
     },
     {
       title: $t({ defaultMessage: 'URL Actions' }),
